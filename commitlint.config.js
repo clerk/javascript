85d299740ea6e90645c56a5ee611b0508ef0fd1f
Peter Perlepes <p.perlepes@gmail.com>
2022-01-12 08:32:14 +0200

    feat(repo): Add husky hooks with commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,17 @@
+const {
+  utils: { getPackages },
+} = require('@commitlint/config-lerna-scopes');
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [2, 'always', ['sentence-case']],
+    'body-max-line-length': [1, 'always', '150'],
+    'scope-empty': [2, 'never'],
+    'scope-enum': async (ctx) => [
+      2,
+      'always',
+      [...(await getPackages(ctx)), 'repo'],
+    ],
+  },
+};
