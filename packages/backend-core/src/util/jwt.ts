4ebfcc9bc1852230e57671b02d3f57b9b6ef04c9
Peter Perlepes <p.perlepes@gmail.com>
2022-01-25 11:34:03 +0200

    fix(backend-core): Additional exp skew checks

--- packages/backend-core/src/util/jwt.ts
+++ packages/backend-core/src/util/jwt.ts
@@ -1,16 +1,20 @@
 import { JWTPayload } from './types';
+const DEFAULT_ADDITIONAL_CLOCK_SKEW = 0;
 
 export function isExpired(
-  payload: JWTPayload
+  payload: JWTPayload,
+  additionalClockSkew = DEFAULT_ADDITIONAL_CLOCK_SKEW
 ) {
   // verify exp+nbf claims
   const now = Date.now() / 1000;
 
-  if (payload.exp && payload.exp < now) {
+  if (payload.exp && now > payload.exp + additionalClockSkew) {
     throw new Error(`Expired token`);
   }
 
-  // TODO: Error handling logic based on nbf attribute and clock skew.
+  if (payload.nbf && now < payload.nbf - additionalClockSkew) {
+    throw new Error(`Token not active yet`);
+  }
 }
 
 export function checkClaims(claims: JWTPayload) {
