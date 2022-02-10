21b5a442954151cc084ada51040ab26d53bd8d62
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-02-10 03:35:36 +0200

    chore(shared): Import `mocked` from `jest-mocked`

--- packages/shared/utils/testUtils.ts
+++ packages/shared/utils/testUtils.ts
@@ -1,10 +1,10 @@
 import '@testing-library/jest-dom/extend-expect';
 
-import { render, RenderOptions } from '@testing-library/react';
+import {render, RenderOptions} from '@testing-library/react';
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import { noop } from '../utils';
+import {noop} from '../utils';
 
 // Wrap components with test providers
 const customRender = (ui: React.ReactElement, options?: RenderOptions) => {
@@ -30,7 +30,7 @@ const renderJSONAfterFirstAct = (c: React.ReactElement) => {
 export * from '@testing-library/react';
 export { act as actHook, renderHook } from '@testing-library/react-hooks';
 export { default as userEvent } from '@testing-library/user-event';
-export { mocked } from 'ts-jest/utils';
+export { mocked } from 'jest-mock';
 
 // Override render method
 export { customRender as render, renderJSON, renderJSONAfterFirstAct, noop };
