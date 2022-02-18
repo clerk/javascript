5c1e23db40b7a49b7cec5a1d8206daad160e6361
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-02-18 14:07:24 +0200

    feat(clerk-remix): Remove load options from `getAuth`

--- packages/remix/src/ssr/getAuth.ts
+++ packages/remix/src/ssr/getAuth.ts
@@ -1,18 +1,15 @@
 import { noRequestPassedInGetAuth } from '../errors';
 import { getAuthData } from './getAuthData';
-import { GetAuthOptions, GetAuthReturn, LoaderFunctionArgs } from './types';
+import { GetAuthReturn, LoaderFunctionArgs } from './types';
 import { sanitizeAuthData } from './utils';
 
-export async function getAuth<Options extends GetAuthOptions>(
-  argsOrReq: Request | LoaderFunctionArgs,
-  options?: Options,
-): GetAuthReturn<Options> {
+export async function getAuth(argsOrReq: Request | LoaderFunctionArgs): GetAuthReturn {
   if (!argsOrReq) {
     throw new Error(noRequestPassedInGetAuth);
   }
 
   const request = 'request' in argsOrReq ? argsOrReq.request : argsOrReq;
-  const { authData } = await getAuthData(request, options || {});
+  const { authData } = await getAuthData(request);
   // @ts-expect-error This can only return null during interstitial,
   // but the public types should not know that
   return sanitizeAuthData(authData || {});
