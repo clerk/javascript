8b898a1aa503889921180850292fbfa3c8133ef5
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-02-18 03:01:25 +0200

    feat(nextjs): Move shared NextJS SSR types to types package

--- packages/nextjs/src/middleware/types.ts
+++ packages/nextjs/src/middleware/types.ts
@@ -1,5 +1,5 @@
 import type { Session, User } from '@clerk/clerk-sdk-node';
-import { GetSessionTokenOptions } from '@clerk/types';
+import { ServerSideAuth } from '@clerk/types';
 import { GetServerSidePropsContext } from 'next';
 
 // TODO: Remove when we're using TS >=4.5
@@ -10,9 +10,7 @@ export type WithServerSideAuthOptions = {
   loadSession?: boolean;
 };
 
-export type WithServerSideAuthCallback<Return, Options> = (
-  context: ContextWithAuth<Options>,
-) => Return;
+export type WithServerSideAuthCallback<Return, Options> = (context: ContextWithAuth<Options>) => Return;
 
 export type WithServerSideAuthResult<CallbackReturn> = (
   context: GetServerSidePropsContext,
@@ -26,16 +24,7 @@ export type AuthData = {
   getToken: (...args: any) => Promise<string | null>;
 };
 
-export type ContextWithAuth<Options extends WithServerSideAuthOptions = any> =
-  GetServerSidePropsContext & {
-    auth: ServerSideAuth;
-  } & (Options extends { loadSession: true }
-      ? { session: Session | null }
-      : {}) &
-    (Options extends { loadUser: true } ? { user: User | null } : {});
-
-export type ServerSideAuth = {
-  sessionId: string | null;
-  userId: string | null;
-  getToken: (options?: GetSessionTokenOptions) => Promise<string | null>;
-};
+export type ContextWithAuth<Options extends WithServerSideAuthOptions = any> = GetServerSidePropsContext & {
+  auth: ServerSideAuth;
+} & (Options extends { loadSession: true } ? { session: Session | null } : {}) &
+  (Options extends { loadUser: true } ? { user: User | null } : {});
