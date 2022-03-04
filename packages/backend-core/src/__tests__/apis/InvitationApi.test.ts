ae74b90a13db8a58ad6dd937fd290b9ecb8fccbc
Devin Riegle <riegledevin@gmail.com>
2022-03-04 16:16:57 -0500

    feat(backend-core): Add public_metadata to invitation create API
    Invitations can optionally carry metadata that will eventually end up in the created user once they sign up.
    The metadata must be a well-formed JSON object. In order to add metadata to an invitation, you can use the
    public_metadata property when the invitation is created:
    
    https://docs.clerk.dev/popular-guides/invitations#invitation-metadata

--- packages/backend-core/src/__tests__/apis/InvitationApi.test.ts
+++ packages/backend-core/src/__tests__/apis/InvitationApi.test.ts
@@ -76,6 +76,42 @@ test('createInvitation() accepts a redirectUrl', async () => {
   );
 });
 
+test('createInvitation() accepts publicMetadata', async () => {
+  const emailAddress = 'test@example.com';
+  const publicMetadata = {
+    hello: 'world',
+  };
+  const resJSON = {
+    object: 'invitation',
+    id: 'inv_randomid',
+    email_address: emailAddress,
+    public_metadata: publicMetadata,
+    created_at: 1611948436,
+    updated_at: 1611948436,
+  };
+
+  nock('https://api.clerk.dev')
+    .post('/v1/invitations', {
+      email_address: emailAddress,
+      public_metadata: publicMetadata,
+    })
+    .reply(200, resJSON);
+
+  const invitation = await TestBackendAPIClient.invitations.createInvitation({
+    emailAddress,
+    publicMetadata,
+  });
+  expect(invitation).toEqual(
+    new Invitation({
+      id: resJSON.id,
+      emailAddress,
+      publicMetadata,
+      createdAt: resJSON.created_at,
+      updatedAt: resJSON.updated_at,
+    })
+  );
+});
+
 test('revokeInvitation() revokes an invitation', async () => {
   const id = 'inv_randomid';
   const resJSON = {
