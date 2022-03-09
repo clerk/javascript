e593e55e59b69c19d6e8d821efac2d325d19e090
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-03-09 02:12:49 +0200

    feat(clerk-react): Add isLoaded to `useSessionList` hook

--- packages/react/src/hooks/useSignIn.ts
+++ packages/react/src/hooks/useSignIn.ts
@@ -3,7 +3,7 @@ import { SignInResource } from '@clerk/types';
 import { useClientContext } from '../contexts/ClientContext';
 
 type UseSignInReturn =
-  | { isLoaded: false; signIn: null }
+  | { isLoaded: false; signIn: undefined }
   | { isLoaded: true; signIn: SignInResource };
 
 type UseSignIn = () => UseSignInReturn;
@@ -12,7 +12,7 @@ export const useSignIn: UseSignIn = () => {
   const client = useClientContext();
 
   if (!client) {
-    return { isLoaded: false, signIn: null };
+    return { isLoaded: false, signIn: undefined };
   }
 
   return { isLoaded: true, signIn: client.signIn };
--- packages/react/src/hooks/useSignUp.ts
+++ packages/react/src/hooks/useSignUp.ts
@@ -3,7 +3,7 @@ import { SignUpResource } from '@clerk/types';
 import { useClientContext } from '../contexts/ClientContext';
 
 type UseSignUpReturn =
-  | { isLoaded: false; signUp: null }
+  | { isLoaded: false; signUp: undefined }
   | { isLoaded: true; signUp: SignUpResource };
 
 type UseSignUp = () => UseSignUpReturn;
@@ -12,7 +12,7 @@ export const useSignUp: UseSignUp = () => {
   const client = useClientContext();
 
   if (!client) {
-    return { isLoaded: false, signUp: null };
+    return { isLoaded: false, signUp: undefined };
   }
 
   return { isLoaded: true, signUp: client.signUp };
