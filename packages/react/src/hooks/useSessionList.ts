b85f98c86b44c5862ff5501278350a9858797c7b
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-03-14 02:17:05 +0200

    feat(clerk-react): Add setSession to useSignIn, useSignUp and useSessionList hooks

--- packages/react/src/hooks/useSessionList.ts
+++ packages/react/src/hooks/useSessionList.ts
@@ -1,19 +1,21 @@
-import { SessionResource } from '@clerk/types';
+import { SessionResource, SetSession } from '@clerk/types';
 
 import { useClientContext } from '../contexts/ClientContext';
+import { useIsomorphicClerkContext } from '../contexts/IsomorphicClerkContext';
 
 type UseSessionListReturn =
-  | { isLoaded: false; sessions: undefined }
-  | { isLoaded: true; sessions: SessionResource[] };
+  | { isLoaded: false; sessions: undefined; setSession: undefined }
+  | { isLoaded: true; sessions: SessionResource[]; setSession: SetSession };
 
 type UseSessionList = () => UseSessionListReturn;
 
 export const useSessionList: UseSessionList = () => {
+  const isomorphicClerk = useIsomorphicClerkContext();
   const client = useClientContext();
 
   if (!client) {
-    return { isLoaded: false, sessions: undefined };
+    return { isLoaded: false, sessions: undefined, setSession: undefined };
   }
 
-  return { isLoaded: true, sessions: client.sessions };
+  return { isLoaded: true, sessions: client.sessions, setSession: isomorphicClerk.setSession };
 };
--- packages/react/src/hooks/useSignIn.ts
+++ packages/react/src/hooks/useSignIn.ts
@@ -1,19 +1,21 @@
-import { SignInResource } from '@clerk/types';
+import { SetSession, SignInResource } from '@clerk/types';
 
 import { useClientContext } from '../contexts/ClientContext';
+import { useIsomorphicClerkContext } from '../contexts/IsomorphicClerkContext';
 
 type UseSignInReturn =
-  | { isLoaded: false; signIn: undefined }
-  | { isLoaded: true; signIn: SignInResource };
+  | { isLoaded: false; signIn: undefined; setSession: undefined }
+  | { isLoaded: true; signIn: SignInResource; setSession: SetSession };
 
 type UseSignIn = () => UseSignInReturn;
 
 export const useSignIn: UseSignIn = () => {
+  const isomorphicClerk = useIsomorphicClerkContext();
   const client = useClientContext();
 
   if (!client) {
-    return { isLoaded: false, signIn: undefined };
+    return { isLoaded: false, signIn: undefined, setSession: undefined };
   }
 
-  return { isLoaded: true, signIn: client.signIn };
+  return { isLoaded: true, signIn: client.signIn, setSession: isomorphicClerk.setSession };
 };
