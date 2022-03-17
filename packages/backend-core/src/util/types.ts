2144577c8fff668ba872073e89ed17e33460ef82
Peter Perlepes <p.perlepes@gmail.com>
2022-03-17 12:42:40 +0200

    feat(backend-core): Fix JWTPayload type with Clerk specifications

--- packages/backend-core/src/util/types.ts
+++ packages/backend-core/src/util/types.ts
@@ -2,37 +2,32 @@ export interface JWTPayload {
   /**
    * JWT Issuer - [RFC7519#section-4.1.1](https://tools.ietf.org/html/rfc7519#section-4.1.1).
    */
-  iss?: string;
+  iss: string;
 
   /**
    * JWT Subject - [RFC7519#section-4.1.2](https://tools.ietf.org/html/rfc7519#section-4.1.2).
    */
-  sub?: string;
+  sub: string;
 
   /**
-   * JWT Audience [RFC7519#section-4.1.3](https://tools.ietf.org/html/rfc7519#section-4.1.3).
+   * Session ID
    */
-  aud?: string | string[];
-
-  /**
-   * JWT ID - [RFC7519#section-4.1.7](https://tools.ietf.org/html/rfc7519#section-4.1.7).
-   */
-  jti?: string;
+  sid: string;
 
   /**
    * JWT Not Before - [RFC7519#section-4.1.5](https://tools.ietf.org/html/rfc7519#section-4.1.5).
    */
-  nbf?: number;
+  nbf: number;
 
   /**
    * JWT Expiration Time - [RFC7519#section-4.1.4](https://tools.ietf.org/html/rfc7519#section-4.1.4).
    */
-  exp?: number;
+  exp: number;
 
   /**
    * JWT Issued At - [RFC7519#section-4.1.6](https://tools.ietf.org/html/rfc7519#section-4.1.6).
    */
-  iat?: number;
+  iat: number;
 
   /**
    * JWT Authorized party - [RFC7800#section-3](https://tools.ietf.org/html/rfc7800#section-3).
