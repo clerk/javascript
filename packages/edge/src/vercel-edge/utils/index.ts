fb2cdeff2e6bb2a639936e5bb0cf0fd6a2bf8570
Peter Perlepes <p.perlepes@gmail.com>
2022-03-17 14:38:07 +0200

    fix(edge): Correct logic placement about enriching authData with user
    Remove the getAuthData method

--- packages/edge/src/vercel-edge/utils/index.ts
+++ packages/edge/src/vercel-edge/utils/index.ts
@@ -1,2 +1,1 @@
-export * from './getAuthData';
 export * from './injectAuthIntoRequest';
--- packages/edge/src/vercel-edge/utils/injectAuthIntoRequest.ts
+++ packages/edge/src/vercel-edge/utils/injectAuthIntoRequest.ts
@@ -6,11 +6,11 @@ export function injectAuthIntoRequest(
   req: NextRequest,
   authData: AuthData,
 ): RequestWithAuth {
-  const { user, session, userId, sessionId } = authData;
+  const { user, session, userId, sessionId, getToken } = authData;
   const auth = {
     userId,
     sessionId,
-    getToken: authData.getToken,
+    getToken,
   };
 
   /* Object.assign is used here as NextRequest properties also include Symbols */
