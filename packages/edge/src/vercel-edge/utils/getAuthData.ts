2039896853d2623f298fc33f40f7605789c62544
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-03-19 23:28:54 +0200

    feat(edge): Add getToken to edge `withEdgeMiddlewareAuth`

--- packages/edge/src/vercel-edge/utils/getAuthData.ts
+++ packages/edge/src/vercel-edge/utils/getAuthData.ts
@@ -1,42 +1,29 @@
-import { JWTPayload } from '@clerk/backend-core';
-import { ServerGetTokenOptions } from '@clerk/types';
+import { createGetToken, JWTPayload } from '@clerk/backend-core';
 import { NextRequest } from 'next/server';
 
 import { ClerkAPI } from '../ClerkAPI';
 import { AuthData, WithEdgeMiddlewareAuthOptions } from '../types';
 
+/**
+ * @internal
+ */
 export async function getAuthData(
   req: NextRequest,
-  {
-    sid,
-    sub,
-    loadSession,
-    loadUser,
-  }: WithEdgeMiddlewareAuthOptions & JWTPayload,
+  { sid, sub, loadSession, loadUser }: WithEdgeMiddlewareAuthOptions & JWTPayload,
 ): Promise<AuthData> {
-  const getToken = (options: ServerGetTokenOptions = {}) => {
-    if (options.template) {
-      throw new Error(
-        'Retrieving a JWT template during edge runtime will be supported soon.',
-      );
-    }
-    return Promise.resolve(req.cookies['__session']);
-  };
-
   const [user, session] = await Promise.all([
-    loadUser
-      ? ClerkAPI.users.getUser(sub as string)
-      : Promise.resolve(undefined),
-    loadSession
-      ? ClerkAPI.sessions.getSession(sid as string)
-      : Promise.resolve(undefined),
+    loadUser ? ClerkAPI.users.getUser(sub as string) : Promise.resolve(undefined),
+    loadSession ? ClerkAPI.sessions.getSession(sid as string) : Promise.resolve(undefined),
   ]);
 
-  return {
-    sessionId: sid as string,
-    userId: sub as string,
-    getToken,
-    user,
-    session,
-  };
+  const sessionId = sid;
+  const userId = sub;
+  const getToken = createGetToken({
+    sessionId,
+    cookieToken: req.cookies['__session'],
+    headerToken: req.headers.get('authorization')?.replace('Bearer ', ''),
+    fetcher: (...args) => ClerkAPI.sessions.getToken(...args),
+  });
+
+  return { sessionId, userId, getToken, user, session };
 }
