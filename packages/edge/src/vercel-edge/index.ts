aad22c2271c7bf9933e598f9eb70788d0b3e44b5
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-03-19 23:28:54 +0200

    feat(edge): Add getToken to edge `withEdgeMiddlewareAuth`

--- packages/edge/src/vercel-edge/index.ts
+++ packages/edge/src/vercel-edge/index.ts
@@ -1,4 +1,4 @@
-import { AuthStatus, Base } from '@clerk/backend-core';
+import { AuthStatus, Base, createSignedOutState } from '@clerk/backend-core';
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
 import { ClerkAPI } from './ClerkAPI';
@@ -26,12 +26,7 @@ const importKey = async (jwk: JsonWebKey, algorithm: Algorithm) => {
   return await crypto.subtle.importKey('jwk', jwk, algorithm, true, ['verify']);
 };
 
-const verifySignature = async (
-  algorithm: Algorithm,
-  key: CryptoKey,
-  signature: Uint8Array,
-  data: Uint8Array,
-) => {
+const verifySignature = async (algorithm: Algorithm, key: CryptoKey, signature: Uint8Array, data: Uint8Array) => {
   return await crypto.subtle.verify(algorithm, key, signature, data);
 };
 
@@ -56,15 +51,7 @@ const smsMessages = ClerkAPI.smsMessages;
 const users = ClerkAPI.users;
 
 // Export sub-api objects
-export {
-  allowlistIdentifiers,
-  clients,
-  emails,
-  invitations,
-  sessions,
-  smsMessages,
-  users,
-};
+export { allowlistIdentifiers, clients, emails, invitations, sessions, smsMessages, users };
 
 async function fetchInterstitial() {
   const response = await ClerkAPI.fetchInterstitial<Response>();
@@ -91,20 +78,19 @@ export function withEdgeMiddlewareAuth(
 ): any {
   return async function clerkAuth(req: NextRequest, event: NextFetchEvent) {
     /* Get authentication state */
-    const { status, interstitial, sessionClaims } =
-      await vercelEdgeBase.getAuthState({
-        cookieToken: req.cookies['__session'],
-        clientUat: req.cookies['__client_uat'],
-        headerToken: req.headers.get('authorization'),
-        origin: req.headers.get('origin'),
-        host: req.headers.get('host') as string,
-        userAgent: req.headers.get('user-agent'),
-        forwardedPort: req.headers.get('x-forwarded-port'),
-        forwardedHost: req.headers.get('x-forwarded-host'),
-        referrer: req.headers.get('referrer'),
-        authorizedParties: options.authorizedParties,
-        fetchInterstitial,
-      });
+    const { status, interstitial, sessionClaims } = await vercelEdgeBase.getAuthState({
+      cookieToken: req.cookies['__session'],
+      clientUat: req.cookies['__client_uat'],
+      headerToken: req.headers.get('authorization'),
+      origin: req.headers.get('origin'),
+      host: req.headers.get('host') as string,
+      userAgent: req.headers.get('user-agent'),
+      forwardedPort: req.headers.get('x-forwarded-port'),
+      forwardedHost: req.headers.get('x-forwarded-host'),
+      referrer: req.headers.get('referrer'),
+      authorizedParties: options.authorizedParties,
+      fetchInterstitial,
+    });
 
     if (status === AuthStatus.Interstitial) {
       return new NextResponse(interstitial, {
@@ -115,44 +101,26 @@ export function withEdgeMiddlewareAuth(
 
     const getToken = (options: ServerGetTokenOptions = {}) => {
       if (options.template) {
-        throw new Error(
-          'Retrieving a JWT template during edge runtime will be supported soon.',
-        );
+        throw new Error('Retrieving a JWT template during edge runtime will be supported soon.');
       }
       return Promise.resolve(req.cookies['__session']);
     };
 
     /* In both SignedIn and SignedOut states, we just add the attributes to the request object and passthrough. */
     if (status === AuthStatus.SignedOut) {
-      /* Predetermined signed out attributes */
-      const signedOutState = {
-        sessionId: null,
-        session: null,
-        userId: null,
-        user: null,
-        getToken,
-      };
-      return handler(injectAuthIntoRequest(req, signedOutState), event);
+      return handler(injectAuthIntoRequest(req, createSignedOutState()), event);
     }
 
+    const sessionId = sessionClaims!.sid;
+    const userId = sessionClaims!.sub;
+
     const [user, session] = await Promise.all([
-      options.loadUser
-        ? ClerkAPI.users.getUser(sessionClaims?.sub as string)
-        : Promise.resolve(undefined),
-      options.loadSession
-        ? ClerkAPI.sessions.getSession(sessionClaims?.sid as string)
-        : Promise.resolve(undefined),
+      options.loadUser ? ClerkAPI.users.getUser(userId) : Promise.resolve(undefined),
+      options.loadSession ? ClerkAPI.sessions.getSession(sessionId) : Promise.resolve(undefined),
     ]);
 
     /* Inject the auth state into the NextResponse object */
-    const authRequest = injectAuthIntoRequest(req, {
-      user,
-      session,
-      sessionId: sessionClaims?.sid as string,
-      userId: sessionClaims?.sub as string,
-      getToken,
-    });
-
+    const authRequest = injectAuthIntoRequest(req, { user, session, sessionId, userId, getToken });
     return handler(authRequest, event);
   };
 }
