601c8528aafd24f1ff88407d3f1390e56fa10afd
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-04-07 23:54:13 +0300

    feat(gatsby-plugin-clerk): Introduce basic structure

---
+++ packages/gatsby-plugin-clerk/index.js
@@ -0,0 +1,9 @@
+/**
+ * noop
+ *
+ * This is a Gatsby plugin, so a default export is not needed.
+ * Gatsby will import and use the config files automatically
+ *
+ * For more info, take a look at the official Gatsby docs:
+ * https://www.gatsbyjs.com/docs/plugins/
+ */
