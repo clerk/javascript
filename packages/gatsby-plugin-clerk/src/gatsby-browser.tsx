481dc4c2e54e0d978cc418f36615e6b6a2d5d77f
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-04-20 18:10:42 +0300

    feat(gatsby-plugin-clerk): Add `no-cache` header on Gatsby interstitial page

--- packages/gatsby-plugin-clerk/src/gatsby-browser.tsx
+++ packages/gatsby-plugin-clerk/src/gatsby-browser.tsx
@@ -6,8 +6,8 @@ import { ClerkProvider } from './GatsbyClerkProvider';
 export const wrapPageElement: GatsbyBrowser['wrapPageElement'] = ({ element, props }, options) => {
   let clerkSsrState: any;
   if ((props.serverData as any)?.clerkState) {
-    clerkSsrState = (props.serverData as any)?.clerkState;
-    delete (props.serverData as any)?.clerkState;
+    clerkSsrState = (props.serverData as any).clerkState;
+    delete (props.serverData as any).clerkState;
   }
 
   return (
