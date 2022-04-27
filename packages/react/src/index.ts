a89f836c76baeb48c3528a077f1bc84d65d8fcde
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-04-27 14:22:57 +0300

    fix(clerk-react): Define global in window if not defined
    Vite does not define `global` by default. One workaround is to use the `define` config prop:
    https://vitejs.dev/config/#define
    However, we decided to solve this on the SDK level as well to reduce setup steps.

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -4,3 +4,13 @@ export * from './hooks';
 export type { ClerkProp } from './types';
 export { isMagicLinkError, MagicLinkErrorCode } from './errors';
 export { useMagicLink } from './hooks/useMagicLink';
+
+/**
+ * Vite does not define `global` by default
+ * One workaround is to use the `define` config prop
+ * https://vitejs.dev/config/#define
+ * We are solving this in the SDK level to reduce setup steps.
+ */
+if (typeof global === 'undefined' && typeof window !== 'undefined' && !window.global) {
+  (window as any).global = window;
+}
