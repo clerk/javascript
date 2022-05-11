8291d75a7f3172d05f76c0f9aeb08aab98e7d81c
Haris Chaniotakis <chanioxaris@gmail.com>
2022-05-11 19:29:37 +0300

    feat(types): Introduce Web3 provider types and helpers

--- packages/types/src/web3.ts
+++ packages/types/src/web3.ts
@@ -1,3 +1,35 @@
+import { Web3Strategy } from './strategies';
+
+export interface Web3ProviderData {
+  provider: Web3Provider;
+  strategy: Web3Strategy;
+  name: string;
+}
+
 export type MetamaskWeb3Provider = 'metamask';
 
 export type Web3Provider = MetamaskWeb3Provider;
+
+export const WEB3_PROVIDERS: Web3ProviderData[] = [
+  {
+    provider: 'metamask',
+    strategy: 'web3_metamask_signature',
+    name: 'Metamask',
+  },
+];
+
+interface getWeb3ProviderDataProps {
+  provider?: Web3Provider;
+  strategy?: Web3Strategy;
+}
+
+export function getWeb3ProviderData({
+  provider,
+  strategy,
+}: getWeb3ProviderDataProps): Web3ProviderData | undefined | null {
+  if (provider) {
+    return WEB3_PROVIDERS.find(p => p.provider == provider);
+  }
+
+  return WEB3_PROVIDERS.find(p => p.strategy == strategy);
+}
