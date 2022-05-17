fcb3b3cfffc0670e00a0918132f52ba1c3a5038a
David Vargas <dvargas92495@gmail.com>
2022-05-17 16:37:18 -0500

    feat(remix): Pass options to remix getAuth

--- packages/remix/src/ssr/getAuth.ts
+++ packages/remix/src/ssr/getAuth.ts
@@ -2,17 +2,17 @@ import { json } from '@remix-run/server-runtime';
 
 import { getAuthInterstitialErrorRendered, noRequestPassedInGetAuth } from '../errors';
 import { getAuthData } from './getAuthData';
-import { GetAuthReturn, LoaderFunctionArgs } from './types';
+import { GetAuthReturn, LoaderFunctionArgs, RootAuthLoaderOptions } from './types';
 import { sanitizeAuthData } from './utils';
 
 const EMPTY_INTERSTITIAL_RESPONSE = { message: getAuthInterstitialErrorRendered };
 
-export async function getAuth(argsOrReq: Request | LoaderFunctionArgs): GetAuthReturn {
+export async function getAuth(argsOrReq: Request | LoaderFunctionArgs, options?: RootAuthLoaderOptions): GetAuthReturn {
   if (!argsOrReq) {
     throw new Error(noRequestPassedInGetAuth);
   }
   const request = 'request' in argsOrReq ? argsOrReq.request : argsOrReq;
-  const { authData, showInterstitial } = await getAuthData(request);
+  const { authData, showInterstitial } = await getAuthData(request, options);
 
   if (showInterstitial || !authData) {
     throw json(EMPTY_INTERSTITIAL_RESPONSE, { status: 401 });
