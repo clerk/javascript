33867481d236f81c3d4df55faad34c484769b65f
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-05-20 08:17:58 +0300

    chore(clerk-js): Add custom utils to css method

--- packages/clerk-js/src/v4/styledSystem/css.ts
+++ packages/clerk-js/src/v4/styledSystem/css.ts
@@ -1,29 +1,55 @@
-import type { Interpolation, SerializedStyles, Theme } from '@emotion/react';
-import { css as _css } from '@emotion/react';
+import type { Interpolation, Theme } from '@emotion/react';
 
-type ThemedCallback = (theme: Theme) => Interpolation<Theme>;
-interface ThemedCss {
-  (params: ThemedCallback): Interpolation<Theme>;
-  (params: Interpolation<Theme>): Interpolation<Theme>;
-  (params: TemplateStringsArray): SerializedStyles;
-}
+import type { StaticCssFn, ThemableCssFn } from './types';
 
-/*
-  This is our abstraction over emotion's css() util, intended to replace the need
-  for the emotion/styled package. The base css() util does not accept a theme prop
-  and in many cases, TS cannot infer the correct types if not used directly inside
-  JSX. See /primitives for usage examples
+/**
+ *  Add any custom helpers here. These will be expanded and applied on the element
+ *  during render.
  */
-// @ts-expect-error
-export const css: ThemedCss = (param): Interpolation<Theme> | SerializedStyles => {
+const utils = [
+  ['px', (value: any) => ({ paddingLeft: value, paddingRight: value })],
+  ['py', (value: any) => ({ paddingTop: value, paddingBottom: value })],
+  ['mx', (value: any) => ({ marginTop: value, marginBottom: value })],
+  ['my', (value: any) => ({ marginTop: value, marginBottom: value })],
+] as const;
+
+type UtilKeys = typeof utils[number][0];
+
+type CssUtils = {
+  [k in UtilKeys]?: string | number;
+};
+
+type WithUtils<T> = T & CssUtils;
+
+/**
+ * This is our abstraction over emotion's css() util, intended to replace the need
+ * for the emotion/styled package. The base css() util does not accept a theme prop
+ * and in many cases, TS cannot infer the correct types if not used directly inside
+ * JSX. See /primitives for usage examples.
+ */
+const css: ThemableCssFn<WithUtils<Interpolation<Theme>>> = (param): Interpolation<Theme> => {
   return (theme: Theme) => {
-    if (typeof param === 'function') {
-      return param(theme);
+    let result = typeof param === 'function' ? param(theme) : param;
+    if (!!result && typeof result === 'object') {
+      result = { ...result };
+      for (const [name, fn] of utils) {
+        if (name in result) {
+          result = { ...result, ...fn(result[name as keyof typeof result]) };
+        }
+      }
     }
-    if (typeof param === 'string') {
-      return param;
-    }
-    // @ts-expect-error
-    return _css(param);
+    return result;
   };
 };
+
+/**
+ * This function is only used for strict typing
+ * Can be useful when creating base styles and is
+ * useful for resets
+ */
+const staticCss: StaticCssFn<Interpolation<Theme>> = param => {
+  return param;
+};
+
+export { css, staticCss };
+export type { CssUtils };
