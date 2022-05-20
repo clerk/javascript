f5097d37e719ae170fb5e42bd061006e97c832c0
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-05-20 07:20:57 +0300

    chore(clerk-js): Introduce createInternalTheme helper

---
+++ packages/clerk-js/src/v4/styledSystem/createInternalTheme.ts
@@ -0,0 +1,18 @@
+type PrefixWith<K extends string, T> = `${K}${Extract<T, boolean | number | string>}`;
+
+type ThemeWithPrefix<Theme> = {
+  [scale in keyof Theme]: {
+    [token in keyof Theme[scale] as PrefixWith<'$', token>]: Theme[scale][token];
+  };
+};
+
+const createInternalTheme = <T extends Record<string, any>>(theme: T): ThemeWithPrefix<T> => {
+  return Object.fromEntries(
+    Object.entries(theme).map(([scale, values]) => [
+      scale,
+      Object.fromEntries(Object.entries(values).map(([name, value]) => [`$${name}`, value])),
+    ]),
+  ) as any;
+};
+
+export { createInternalTheme };
