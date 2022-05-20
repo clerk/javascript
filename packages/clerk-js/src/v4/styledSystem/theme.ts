bffb47bfad93cfadce45ff9c92832e438bc4dae2
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-05-20 07:21:31 +0300

    chore(clerk-js): Create internal base theme

---
+++ packages/clerk-js/src/v4/styledSystem/theme.ts
@@ -0,0 +1,25 @@
+import { colors } from './colors';
+import { createInternalTheme } from './createInternalTheme';
+import { shadows } from './shadows';
+import { radii, sizes, space } from './sizes';
+import { fonts, fontSizes, fontWeights, letterSpacings, lineHeights } from './typography';
+
+export const baseTheme = createInternalTheme({
+  colors,
+  fonts,
+  fontSizes,
+  fontWeights,
+  letterSpacings,
+  lineHeights,
+  radii,
+  sizes,
+  space,
+  shadows,
+} as const);
+
+type BaseTheme = typeof baseTheme;
+
+declare module '@emotion/react' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface Theme extends BaseTheme {}
+}
