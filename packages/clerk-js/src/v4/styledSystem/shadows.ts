672668bc5eef1c33b31c319b3d9c0cb6afe8cfb6
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-05-20 07:19:48 +0300

    chore(clerk-js): Introduce a sample theme based on the SystemUI specification
    Same specification used by Radix, Chakra and other popular UI libs: https://system-ui.com/

---
+++ packages/clerk-js/src/v4/styledSystem/shadows.ts
@@ -0,0 +1,3 @@
+export const shadows = {
+  boxShadow1: '0px 24px 48px rgba(0, 0, 0, 0.16)',
+};
---
+++ packages/clerk-js/src/v4/styledSystem/sizes.ts
@@ -0,0 +1,54 @@
+const space = {
+  px: '1px',
+  '0x5': '0.125rem',
+  '1': '0.25rem',
+  '1x5': '0.375rem',
+  '2': '0.5rem',
+  '2x5': '0.625rem',
+  3: '0.75rem',
+  '3x5': '0.875rem',
+  4: '1rem',
+  5: '1.25rem',
+  6: '1.5rem',
+  7: '1.75rem',
+  8: '2rem',
+  9: '2.25rem',
+  10: '2.5rem',
+  12: '3rem',
+  14: '3.5rem',
+  16: '4rem',
+  20: '5rem',
+  24: '6rem',
+  28: '7rem',
+  32: '8rem',
+  36: '9rem',
+  40: '10rem',
+  44: '11rem',
+  48: '12rem',
+  52: '13rem',
+  56: '14rem',
+  60: '15rem',
+  64: '16rem',
+  72: '18rem',
+  80: '20rem',
+  96: '24rem',
+};
+
+const sizes = {
+  ...space,
+  cardContainer: '25rem',
+};
+
+const radii = {
+  none: '0',
+  sm: '0.125rem',
+  base: '0.25rem',
+  md: '0.375rem',
+  lg: '0.5rem',
+  xl: '0.75rem',
+  '2xl': '1rem',
+  '3xl': '1.5rem',
+  full: '9999px',
+};
+
+export { sizes, space, radii };
