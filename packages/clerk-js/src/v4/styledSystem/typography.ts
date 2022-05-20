cb0a9d6cf9cf62ce169589f022c8f319fea47087
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-05-20 07:19:48 +0300

    chore(clerk-js): Introduce a sample theme based on the SystemUI specification
    Same specification used by Radix, Chakra and other popular UI libs: https://system-ui.com/

---
+++ packages/clerk-js/src/v4/styledSystem/typography.ts
@@ -0,0 +1,42 @@
+const fontWeights = {
+  hairline: 100,
+  thin: 200,
+  light: 300,
+  normal: 400,
+  medium: 500,
+  semibold: 600,
+  bold: 700,
+  extrabold: 800,
+  black: 900,
+};
+
+const lineHeights = {
+  normal: 'normal',
+  none: 1,
+  shorter: 1.25,
+  short: 1.375,
+  base: 1.5,
+  tall: 1.625,
+  taller: '2',
+};
+
+const letterSpacings = {
+  tighter: '-0.05em',
+  tight: '-0.025em',
+  normal: '0',
+  wide: '0.025em',
+  wider: '0.05em',
+  widest: '0.1em',
+};
+
+const fontSizes = {
+  xs: '0.75rem',
+  sm: '0.875rem',
+  md: '1rem',
+  lg: '1.125rem',
+  xl: '1.25rem',
+};
+
+const fonts = {};
+
+export { fontSizes, fontWeights, letterSpacings, lineHeights, fonts };
