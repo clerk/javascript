6532b2ccc6ce22a5cea87b9af34be4bc134ef138
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-05-25 13:28:39 +0300

    chore(repo): Introduce Link primitive

---
+++ packages/clerk-js/src/v4/primitives/Link.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import { Text } from '../primitives';
+import { createVariants, cssutils, PickSiblingProps, PrimitiveProps, StyleVariants } from '../styledSystem';
+
+const { applyVariants } = createVariants(theme => ({
+  base: {
+    boxSizing: 'border-box',
+    margin: 0,
+    ...cssutils.addFocusRing(theme),
+    '&:hover': { textDecoration: 'underline' },
+  },
+  variants: {
+    colorScheme: {
+      primary: {
+        color: theme.colors.$primary500,
+        '&:hover': { color: theme.colors.$primary400 },
+        '&:active': { color: theme.colors.$primary600 },
+      },
+      danger: {
+        color: theme.colors.$danger500,
+        '&:hover': { color: theme.colors.$danger400 },
+        '&:active': { color: theme.colors.$danger600 },
+      },
+    },
+  },
+  defaultVariants: {
+    colorScheme: 'primary',
+  },
+}));
+
+type SiblingProps = PickSiblingProps<typeof Text, 'size'>;
+type OwnProps = { isExternal?: boolean };
+export type LinkProps = PrimitiveProps<'a'> & OwnProps & SiblingProps & StyleVariants<typeof applyVariants>;
+
+export const Link = (props: LinkProps): JSX.Element => {
+  const { isExternal, children, size, ...rest } = props;
+  return (
+    <a
+      {...rest}
+      target={isExternal ? '_blank' : undefined}
+      rel={isExternal ? 'noopener' : undefined}
+      css={applyVariants(props)}
+    >
+      <Text variant='link' size={size}>
+        children
+      </Text>
+    </a>
+  );
+};
