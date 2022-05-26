d0e3094254d2a928d2f93bb83822ee994ca544ee
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-05-26 04:30:13 +0300

    chore(repo): Introduce v4 icons dir

--- packages/clerk-js/src/globals.d.ts
+++ packages/clerk-js/src/globals.d.ts
@@ -2,4 +2,10 @@ declare global {
   const __DEV__: boolean;
   const __CLERKJS_VERSION__: string;
 }
+
+declare module '*.svg' {
+  const value: React.FC<React.SVGAttributes<SVGElement>>;
+  export default value;
+}
+
 export {};
