56e24130155b314e2dd075a618bf5244a0caf799
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-05-26 05:14:12 +0300

    chore(repo): Introduce `customizables`

---
+++ packages/clerk-js/src/v4/customizables/index.ts
@@ -0,0 +1,13 @@
+import * as Primitives from '../primitives';
+import { makeCustomizable } from './makeCustomizable';
+
+export const Box = makeCustomizable(Primitives.Box);
+export const Flex = makeCustomizable(Primitives.Flex);
+export const Button = makeCustomizable(Primitives.Button);
+export const Card = makeCustomizable(Primitives.Card);
+export const Heading = makeCustomizable(Primitives.Heading);
+export const Input = makeCustomizable(Primitives.Input);
+export const Link = makeCustomizable(Primitives.Link);
+export const Text = makeCustomizable(Primitives.Text);
+
+export { ContainerMetadata, useContainerMetadata } from './useContainerMetadata';
