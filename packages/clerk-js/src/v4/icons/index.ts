c3b5fadcbd74aaedefd29f8e91657009f7cb8fa6
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-05-26 04:30:13 +0300

    chore(repo): Introduce v4 icons dir

---
+++ packages/clerk-js/src/v4/icons/index.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+/**
+ * TypeScript configuration (typings) is not correctly configured for all projects.
+ * Consequently, the files are correctly imported but the TS checker emits errors.
+ * The above no-check is safe, as webpack will not allow compilation if for example a file is not resolved.
+ */
+export { default as ArrowLeftIcon } from './arrow-left.svg';
+export { default as ArrowRightIcon } from './arrow-right.svg';
