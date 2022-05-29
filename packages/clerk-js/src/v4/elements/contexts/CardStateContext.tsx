74147c8b456055d10ea8b44e1f3ed07b79229b4b
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-05-30 01:44:13 +0300

    chore(clerk-js): Introduce CardStateContext

---
+++ packages/clerk-js/src/v4/elements/contexts/CardStateContext.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { makeContextAndHook } from '../utils';
+
+type Action = { type: 'block' | 'unblock' };
+
+type CardState = {
+  blocked: boolean;
+};
+
+type CardStateCtxValue = CardState & {
+  dispatch: React.Dispatch<Action>;
+};
+
+const [CardStateCtx, useCardState] = makeContextAndHook<CardStateCtxValue>('CardState');
+
+const reducer = (state: CardState = { blocked: false }, action: Action): CardState => {
+  switch (action.type) {
+    case 'block':
+      return { ...state, blocked: true };
+    case 'unblock':
+      return { ...state, blocked: false };
+    default:
+      return state;
+  }
+};
+
+const CardStateProvider = (props: React.PropsWithChildren<any>) => {
+  const [cardState, dispatch] = React.useReducer(reducer, { blocked: false });
+  const value = React.useMemo(() => ({ value: { ...cardState, dispatch } }), [cardState]);
+  return <CardStateCtx.Provider value={value}>{props.children}</CardStateCtx.Provider>;
+};
+
+export { useCardState, CardStateProvider };
+export type { CardState };
