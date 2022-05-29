5141829a4a0de094603ac68612e2bcb7c5840e3d
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-05-30 01:56:15 +0300

    chore(clerk-js): Propagate `appearance` prop from ClerkProvider to clerkjs

--- packages/react/src/isomorphicClerk.ts
+++ packages/react/src/isomorphicClerk.ts
@@ -247,6 +247,15 @@ export default class IsomorphicClerk {
     }
   }
 
+  __unstable__updateProps(props: any): any {
+    if (this.clerkjs) {
+      (this.clerkjs as any).__unstable__updateProps(props);
+    } else {
+      return undefined;
+    }
+  }
+
+
   /**
    * `setActive` can be used to set the active session and/or organization.
    * It will eventually replace `setSession`.
