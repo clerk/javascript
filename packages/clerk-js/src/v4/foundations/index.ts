8c90acaf40206d77fb284be4814b0e31c3f999ff
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-06-02 01:34:31 +0300

    chore(clerk-js): Refactor BaseTheme to InternalTheme and update types

--- packages/clerk-js/src/v4/foundations/index.ts
+++ packages/clerk-js/src/v4/foundations/index.ts
@@ -1,27 +1,12 @@
-import { colors } from './colors';
 import { createInternalTheme } from './createInternalTheme';
-import { opacity } from './opacity';
-import { shadows } from './shadows';
-import { radii, sizes, space } from './sizes';
-import { transitionDuration, transitionProperty, transitionTiming } from './transitions';
-import { fonts, fontSizes, fontStyles, fontWeights, letterSpacings, lineHeights } from './typography';
+import {
+  defaultFoundationsCopy,
+  defaultInternalThemeFoundations,
+  InternalTheme,
+  InternalThemeFoundations,
+} from './defaultFoundations';
 
-export const baseTheme = createInternalTheme({
-  colors,
-  fonts,
-  fontStyles,
-  fontSizes,
-  fontWeights,
-  letterSpacings,
-  lineHeights,
-  radii,
-  sizes,
-  space,
-  shadows,
-  transitionProperty,
-  transitionTiming,
-  transitionDuration,
-  opacity,
-});
+const defaultInternalTheme = createInternalTheme(defaultInternalThemeFoundations);
 
-export type BaseTheme = typeof baseTheme;
+export { defaultInternalThemeFoundations, defaultInternalTheme, createInternalTheme, defaultFoundationsCopy };
+export type { InternalTheme, InternalThemeFoundations };
