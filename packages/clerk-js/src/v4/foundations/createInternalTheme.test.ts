d793a28e2f28945818b79d0aff7e282558a53397
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-06-02 01:33:15 +0300

    chore(clerk-js): Simplify createInternalTheme

---
+++ packages/clerk-js/src/v4/foundations/createInternalTheme.test.ts
@@ -0,0 +1,37 @@
+import { createInternalTheme } from './createInternalTheme';
+
+describe('createInternalTheme', () => {
+  it('handles empty objects', () => {
+    const foundations = {};
+    const res = createInternalTheme(foundations as any);
+    expect(res).toEqual({});
+  });
+
+  it('handles empty objects', () => {
+    const foundations = {
+      colors: {
+        primary500: 'primary500',
+        primary50: 'primary50',
+      },
+      radii: {
+        1: '1',
+        2: '2',
+      },
+      sizes: {},
+      spaces: undefined,
+    };
+    const res = createInternalTheme(foundations as any);
+    expect(res).toEqual({
+      colors: {
+        $primary500: 'primary500',
+        $primary50: 'primary50',
+      },
+      radii: {
+        $1: '1',
+        $2: '2',
+      },
+      sizes: {},
+      spaces: {},
+    });
+  });
+});
