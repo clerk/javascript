dbf5a7e9b3179a751a5ffed0222629c1432862aa
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-06-02 01:34:31 +0300

    chore(clerk-js): Refactor BaseTheme to InternalTheme and update types

--- packages/clerk-js/src/v4/styledSystem/theme.ts
+++ packages/clerk-js/src/v4/styledSystem/theme.ts
@@ -1,6 +1,6 @@
-import type { BaseTheme } from '../foundations';
+import type { InternalTheme } from '../foundations';
 
 declare module '@emotion/react' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface Theme extends BaseTheme {}
+  export interface Theme extends InternalTheme {}
 }
