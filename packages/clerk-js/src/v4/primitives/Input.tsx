657342bf75855ae6f87acf800679abd1f45c5d7e
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-06-13 02:47:05 +0300

    chore(clerk-js): Introduce Form primitive

--- packages/clerk-js/src/v4/primitives/Input.tsx
+++ packages/clerk-js/src/v4/primitives/Input.tsx
@@ -1,53 +1,48 @@
 import React from 'react';
 
-import { createVariants, PrimitiveProps, StyleVariants } from '../styledSystem';
-import { useFormControlContext } from './hooks';
+import { common, createVariants, PrimitiveProps, StyleVariants } from '../styledSystem';
+import { useFormControl } from './hooks';
 import { useInput } from './hooks/useInput';
-
-// TODO: Connect with CSS variables
 
 const { applyVariants, filterProps } = createVariants((theme, props) => ({
   base: {
     margin: 0,
     outline: 0,
     padding: `${theme.sizes.$2x5} ${theme.sizes.$4}`,
-    fontStyle: 'normal',
-    fontWeight: theme.fontWeights.$normal,
-    border: '1px solid',
-    borderColor: props.hasError ? theme.colors.$danger500 : theme.colors.$blackAlpha300,
-    borderRadius: theme.radii.$md,
-    '&:focus-visible': {
-      borderColor: props.hasError ? theme.colors.$danger500 : theme.colors.$primary50,
-      boxShadow: 'none',
-    },
+    backgroundColor: theme.colors.$inputBackground500,
+    color: theme.colors.$inputText500,
+    ...common.textVariants(theme).input,
+    ...common.borderVariants(theme, props).normal,
+    ...(props.focusRing === false ? {} : common.focusRingInput(theme)),
+    ...common.disabled(theme),
   },
   variants: {},
 }));
 
-type InputProps = PrimitiveProps<'input'> &
-  StyleVariants<typeof applyVariants> & {
-    isDisabled?: boolean;
-    hasError?: boolean;
-  };
+type OwnProps = {
+  isDisabled?: boolean;
+  hasError?: boolean;
+  focusRing?: boolean;
+};
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>((props, ref) => {
-  const controlInputProps = useFormControlContext();
-  const propsWithoutVariants = filterProps({ ...props, hasError: props.hasError || controlInputProps.hasError });
+export type InputProps = PrimitiveProps<'input'> & StyleVariants<typeof applyVariants> & OwnProps;
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+  const formControlProps = useFormControl() || {};
+  const propsWithoutVariants = filterProps({ ...props, hasError: props.hasError || formControlProps.hasError });
   const { onChange } = useInput(propsWithoutVariants.onChange);
-  const { isDisabled, hasError, ...rest } = propsWithoutVariants;
+  const { isDisabled, hasError, focusRing, ...rest } = propsWithoutVariants;
   return (
     <input
       {...rest}
       ref={ref}
       onChange={onChange}
       disabled={isDisabled}
-      id={props.id || controlInputProps.id}
-      aria-invalid={hasError || controlInputProps.hasError}
-      aria-describedby={controlInputProps.errorMessageId}
-      aria-required={controlInputProps.isRequired}
+      id={props.id || formControlProps.id}
+      aria-invalid={hasError || formControlProps.hasError}
+      aria-describedby={formControlProps.errorMessageId}
+      aria-required={formControlProps.isRequired}
       css={applyVariants(propsWithoutVariants)}
     />
   );
 });
-
-export { Input };
