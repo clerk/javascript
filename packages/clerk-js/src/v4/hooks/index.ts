e876bdadd799b6db7d66eedd8e418e773dfb574d
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-06-13 02:46:44 +0300

    chore(clerk-js): Introduce useSearchInput

---
+++ packages/clerk-js/src/v4/hooks/index.ts
@@ -0,0 +1,6 @@
+export * from './useOAuthProviders';
+export * from './useLoadingStatus';
+export * from './useSafeLayoutEffect';
+export * from './usePopover';
+export * from './useLocalStorage';
+export * from './useSearchInput';
