0b61c75ea39ab6e725581f4c68eb3b6664b7d0f4
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-06-13 02:40:30 +0300

    chore(clerk-js): Add more foundations

--- packages/clerk-js/src/v4/foundations/shadows.ts
+++ packages/clerk-js/src/v4/foundations/shadows.ts
@@ -1,6 +1,7 @@
 export const shadows = Object.freeze({
   /* Dashboard/Shadow 2 */
-  cardDropShadow: '0px 24px 48px rgba(0, 0, 0, 0.16)',
+  cardDropShadow: '0px 24px 48px rgba(0, 0, 0, 0.36)',
   boxShadow1: '0px 24px 48px rgba(0, 0, 0, 0.16)',
-  focusRing: '0 0 0 3px rgba(66, 153, 225, 0.6)',
+  focusRing: '0 0 0 3px {{color}}',
+  focusRingInput: '0 0 0 1px {{color}}',
 } as const);
