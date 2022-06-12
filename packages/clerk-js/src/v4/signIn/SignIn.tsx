2ef994786187b361cae4f0bd1dc439949c733e04
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-06-13 02:56:18 +0300

    chore(clerk-js): Add SignIn routes

--- packages/clerk-js/src/v4/signIn/SignIn.tsx
+++ packages/clerk-js/src/v4/signIn/SignIn.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 
 import { ComponentContext, useCoreClerk, useSignInContext, withCoreSessionSwitchGuard } from '../../ui/contexts';
 import { Route, Switch, VIRTUAL_ROUTER_BASE_PATH } from '../../ui/router';
+import { SignInSSOCallback } from '../common/';
+import { SignInEmailLinkVerify } from './EmailLinkVerificationCard';
+import { SignInFactorOne } from './SignInFactorOne';
+import { SignInFactorTwo } from './SignInFactorTwo';
 import { SignInStart } from './SignInStart';
 
 function RedirectToSignIn() {
@@ -18,8 +22,32 @@ function SignInRoutes(): JSX.Element {
 
   return (
     <Switch>
+      <Route path='factor-one'>
+        <SignInFactorOne />
+      </Route>
+      <Route path='factor-two'>
+        <SignInFactorTwo />
+      </Route>
+      <Route path='sso-callback'>
+        <SignInSSOCallback
+          afterSignInUrl={signInContext.afterSignInUrl}
+          afterSignUpUrl={signInContext.afterSignUpUrl}
+          redirectUrl={signInContext.redirectUrl}
+          secondFactorUrl={'../factor-two'}
+        />
+      </Route>
+      <Route path='choose'>{/* TODO: Should be implemented with user button */}</Route>
+      <Route path='verify'>
+        <SignInEmailLinkVerify
+          redirectUrlComplete={signInContext.afterSignInUrl || signInContext.redirectUrl || undefined}
+          redirectUrl='../factor-two'
+        />
+      </Route>
       <Route index>
         <SignInStart />
+      </Route>
+      <Route>
+        <RedirectToSignIn />
       </Route>
     </Switch>
   );
