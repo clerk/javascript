2dc2bca3e04c3ff07d0458ad660bfe382e40022d
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-06-14 02:36:35 +0300

    chore(clerk-js): Add v4 SignUp routes

--- packages/clerk-js/src/v4/utils/useFormControl.ts
+++ packages/clerk-js/src/v4/utils/useFormControl.ts
@@ -15,6 +15,7 @@ type FieldStateProps<Id> = {
 
 export type FormControlState<Id = string> = FieldStateProps<Id> & {
   setError: (error: string | undefined) => void;
+  setValue: (val: string | undefined) => void;
   props: FieldStateProps<Id>;
 };
 
@@ -24,21 +25,21 @@ export const useFormControl = <Id extends string>(
   opts?: Options,
 ): FormControlState<Id> => {
   opts = opts || { type: 'text', label: '' };
-  const [value, setValue] = React.useState(initialState);
+  const [value, setValueInternal] = React.useState(initialState);
   const [errorText, setErrorText] = React.useState<string | undefined>(undefined);
 
-  const onChange: FormControlState['onChange'] = event => setValue(event.target.value || '');
-  const setError: FormControlState['setError'] = error => {
-    console.log('setError called', error);
-    setErrorText(error || undefined);
-  };
+  const onChange: FormControlState['onChange'] = event => setValueInternal(event.target.value || '');
+  const setValue: FormControlState['setValue'] = val => setValueInternal(val || '');
+  const setError: FormControlState['setError'] = error => setErrorText(error || undefined);
 
   const props = { id, name: id, value, errorText, onChange, ...opts };
 
-  return { props, ...props, setError };
+  return { props, ...props, setError, setValue };
 };
 
-export const buildRequest = (fieldStates: Array<FormControlState<string>>): Record<string, any> => {
+export type FormControlStateLike = Pick<FormControlState, 'id' | 'value'>;
+
+export const buildRequest = (fieldStates: Array<FormControlStateLike>): Record<string, any> => {
   const request: { [x: string]: any } = {};
   fieldStates.forEach(x => {
     request[x.id] = x.value;
