7403bb0cdba6b2705b0fd28af7443348583398dc
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-06-19 13:22:27 +0300

    chore(clerk-js): Completely replace cssUtils with styledSystem/common

--- packages/clerk-js/src/v4/styledSystem/index.ts
+++ packages/clerk-js/src/v4/styledSystem/index.ts
@@ -2,6 +2,5 @@ export * from './types';
 export * from './InternalThemeProvider';
 export * from './createVariants';
 export * from './createCssVariables';
-export * from './cssUtilities';
 export * from './common';
 export * from './animations';
