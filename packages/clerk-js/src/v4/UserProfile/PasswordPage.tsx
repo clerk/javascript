d2db9a53d554c677d2260b98d5193f99ed5ed76d
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-07-05 05:04:17 +0300

    chore(clerk-js): Introduce simple Wizard

--- packages/clerk-js/src/v4/UserProfile/PasswordPage.tsx
+++ packages/clerk-js/src/v4/UserProfile/PasswordPage.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 
 import { useCoreUser } from '../../ui/contexts';
 import { useNavigate } from '../../ui/hooks/useNavigate';
-import { Form, useCardState } from '../elements';
+import { useWizard, Wizard } from '../common';
+import { Form, useCardState, withCardStateProvider } from '../elements';
 import { handleError, useFormControl } from '../utils';
 import { FormButtons } from './FormButtons';
 import { ContentPage } from './Page';
 import { SuccessPage } from './SuccessPage';
 
-export const PasswordPage = () => {
+export const PasswordPage = withCardStateProvider(() => {
   const user = useCoreUser();
   const card = useCardState();
-  const [showSuccess, setShowSuccess] = React.useState(false);
+  const wizard = useWizard();
   const { navigate } = useNavigate();
   const passwordField = useFormControl('password', '', { type: 'password', label: 'New password' });
   const confirmField = useFormControl('confirmPassword', '', { type: 'password', label: 'Confirm password' });
@@ -31,45 +32,43 @@ export const PasswordPage = () => {
   const updatePassword = async () => {
     try {
       await user.update({ password: passwordField.value });
-      setShowSuccess(true);
+      wizard.nextStep();
     } catch (e) {
       handleError(e, [passwordField, confirmField], card.setError);
     }
   };
 
-  if (showSuccess) {
-    return (
+  return (
+    <Wizard {...wizard.props}>
+      <ContentPage.Root headerTitle='Set password'>
+        <Form.Root
+          onSubmit={updatePassword}
+          onBlur={validateForm}
+        >
+          <Form.ControlRow>
+            <Form.Control
+              {...passwordField.props}
+              minLength={6}
+              required
+              autoFocus
+            />
+          </Form.ControlRow>
+          <Form.ControlRow>
+            <Form.Control {...confirmField.props} />
+          </Form.ControlRow>
+          <ContentPage.Toolbar>
+            <FormButtons
+              isDisabled={!canSubmit}
+              onCancel={() => navigate('../')}
+            />
+          </ContentPage.Toolbar>
+        </Form.Root>
+      </ContentPage.Root>
+
       <SuccessPage
         title={'Set password'}
         text={'Your password has been set.'}
       />
-    );
-  }
-
-  return (
-    <ContentPage.Root headerTitle='Set password'>
-      <Form.Root
-        onSubmit={updatePassword}
-        onBlur={validateForm}
-      >
-        <Form.ControlRow>
-          <Form.Control
-            {...passwordField.props}
-            minLength={6}
-            required
-            autoFocus
-          />
-        </Form.ControlRow>
-        <Form.ControlRow>
-          <Form.Control {...confirmField.props} />
-        </Form.ControlRow>
-        <ContentPage.Toolbar>
-          <FormButtons
-            isDisabled={!canSubmit}
-            onCancel={() => navigate('../')}
-          />
-        </ContentPage.Toolbar>
-      </Form.Root>
-    </ContentPage.Root>
+    </Wizard>
   );
-};
+});
--- packages/clerk-js/src/v4/UserProfile/UsernamePage.tsx
+++ packages/clerk-js/src/v4/UserProfile/UsernamePage.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 
 import { useCoreUser } from '../../ui/contexts';
 import { useNavigate } from '../../ui/hooks/useNavigate';
-import { Form, useCardState } from '../elements';
+import { useWizard, Wizard } from '../common';
+import { Form, useCardState, withCardStateProvider } from '../elements';
 import { handleError, useFormControl } from '../utils';
 import { FormButtons } from './FormButtons';
 import { ContentPage } from './Page';
 import { SuccessPage } from './SuccessPage';
 
-export const UsernamePage = () => {
+export const UsernamePage = withCardStateProvider(() => {
   const user = useCoreUser();
   const card = useCardState();
-  const [showSuccess, setShowSuccess] = React.useState(false);
+  const wizard = useWizard();
   const { navigate } = useNavigate();
   const usernameField = useFormControl('username', user.username || '', { type: 'text', label: 'Username' });
 
@@ -20,38 +21,35 @@ export const UsernamePage = () => {
   const updatePassword = async () => {
     try {
       await user.update({ username: usernameField.value });
-      setShowSuccess(true);
+      wizard.nextStep();
     } catch (e) {
       handleError(e, [usernameField], card.setError);
     }
   };
 
-  if (showSuccess) {
-    return (
+  return (
+    <Wizard {...wizard.props}>
+      <ContentPage.Root headerTitle='Update username'>
+        <Form.Root onSubmit={updatePassword}>
+          <Form.ControlRow>
+            <Form.Control
+              {...usernameField.props}
+              required
+              autoFocus
+            />
+          </Form.ControlRow>
+          <ContentPage.Toolbar>
+            <FormButtons
+              isDisabled={!canSubmit}
+              onCancel={() => navigate('../')}
+            />
+          </ContentPage.Toolbar>
+        </Form.Root>
+      </ContentPage.Root>
       <SuccessPage
         title={'Update username'}
         text={'Your username has been updated.'}
       />
-    );
-  }
-
-  return (
-    <ContentPage.Root headerTitle='Update username'>
-      <Form.Root onSubmit={updatePassword}>
-        <Form.ControlRow>
-          <Form.Control
-            {...usernameField.props}
-            required
-            autoFocus
-          />
-        </Form.ControlRow>
-        <ContentPage.Toolbar>
-          <FormButtons
-            isDisabled={!canSubmit}
-            onCancel={() => navigate('../')}
-          />
-        </ContentPage.Toolbar>
-      </Form.Root>
-    </ContentPage.Root>
+    </Wizard>
   );
-};
+});
