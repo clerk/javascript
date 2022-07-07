87521085e96bd050c50bf67daef8a90c2b4ccf78
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-07-07 05:32:39 +0300

    chore(clerk-js): Introduce Web3Page and Web3Section.tsx

---
+++ packages/clerk-js/src/v4/UserProfile/Web3Section.tsx
@@ -0,0 +1,64 @@
+import { Web3WalletResource } from '@clerk/types';
+
+import { useCoreUser } from '../../ui/contexts';
+import { useNavigate } from '../../ui/hooks/useNavigate';
+import { Col, Flex, Image } from '../customizables';
+import { AccordionItem } from '../elements';
+import { useEnabledThirdPartyProviders } from '../hooks';
+import { LinkButtonWithDescription } from './LinkButtonWithDescription';
+import { ProfileSection } from './Section';
+import { AddBlockButton } from './UserProfileBlockButtons';
+
+export const Web3Section = () => {
+  const user = useCoreUser();
+  const { navigate } = useNavigate();
+
+  return (
+    <ProfileSection
+      title='Web3 wallets'
+      id='web3Wallets'
+    >
+      {user.web3Wallets.map(wallet => (
+        <Web3WalletAccordion
+          key={wallet.id}
+          wallet={wallet}
+        />
+      ))}
+      <AddBlockButton onClick={() => navigate(`web3-wallet`)}>Add web3 wallet</AddBlockButton>
+    </ProfileSection>
+  );
+};
+
+const Web3WalletAccordion = ({ wallet }: { wallet: Web3WalletResource }) => {
+  const { navigate } = useNavigate();
+  const { strategyToDisplayData } = useEnabledThirdPartyProviders();
+  const strategy = wallet.verification.strategy as keyof typeof strategyToDisplayData;
+
+  return (
+    <AccordionItem
+      title={
+        <Flex
+          align='center'
+          gap={4}
+        >
+          <Image
+            src={strategyToDisplayData[strategy].iconUrl}
+            alt={strategyToDisplayData[strategy].name}
+            sx={theme => ({ width: theme.sizes.$4 })}
+          />
+          {strategyToDisplayData[strategy].name} ({wallet.web3Wallet})
+        </Flex>
+      }
+    >
+      <Col gap={4}>
+        <LinkButtonWithDescription
+          title='Remove'
+          subtitle='Remove this web3 wallet from your account'
+          actionLabel='Remove connected account'
+          colorScheme='danger'
+          onClick={() => navigate(`web3-wallet/${wallet.id}/remove`)}
+        />
+      </Col>
+    </AccordionItem>
+  );
+};
