c54ceeedee7cf5e2ec07a08d686c30f237897bf6
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-07-08 13:25:55 +0300

    chore(clerk-js): Introduce remove resource pages

--- packages/clerk-js/src/v4/UserProfile/RemoveResourcePage.tsx
+++ packages/clerk-js/src/v4/UserProfile/RemoveResourcePage.tsx
@@ -49,6 +49,67 @@ export const RemovePhonePage = () => {
   );
 };
 
+export const RemoveConnectedAccountPage = () => {
+  const user = useCoreUser();
+  const { id } = useRouter().params;
+  const ref = React.useRef(user.externalAccounts.find(e => e.id === id));
+
+  if (!ref.current) {
+    return null;
+  }
+
+  return (
+    <RemoveResourcePage
+      title='Remove connected account'
+      messageLine1={`${ref.current.accountIdentifier()} will be removed from this account.`}
+      messageLine2={`You will no longer be able to sign in using this connected account.`}
+      successMessage={`${ref.current.accountIdentifier()} has been removed from your account.`}
+      deleteResource={() => Promise.resolve(ref.current?.destroy())}
+    />
+  );
+};
+
+export const RemoveWeb3WalletPage = () => {
+  const user = useCoreUser();
+  const { id } = useRouter().params;
+  const ref = React.useRef(user.web3Wallets.find(e => e.id === id));
+
+  if (!ref.current) {
+    return null;
+  }
+
+  return (
+    <RemoveResourcePage
+      title='Remove web3 wallet'
+      messageLine1={`${ref.current.web3Wallet} will be removed from this account.`}
+      messageLine2={`You will no longer be able to sign in using this web3 wallet.`}
+      successMessage={`${ref.current.web3Wallet} has been removed from your account.`}
+      deleteResource={() => Promise.resolve(ref.current?.destroy())}
+    />
+  );
+};
+
+export const RemoveMfaPage = () => {
+  const user = useCoreUser();
+  const { id } = useRouter().params;
+  // TODO: This logic will need to change when we add more 2fa methods
+  const ref = React.useRef(user.phoneNumbers.find(e => e.id === id));
+
+  if (!ref.current) {
+    return null;
+  }
+
+  return (
+    <RemoveResourcePage
+      title='Remove multi-factor authentication'
+      messageLine1={`${ref.current.phoneNumber} will be no longer receive verification codes when signing in.`}
+      messageLine2={`Your account may not be as secure. Are you sure you want to continue?`}
+      successMessage={`SMS code multi-factor authentication has been removed for ${ref.current.phoneNumber}.`}
+      deleteResource={() => Promise.resolve(ref.current?.setReservedForSecondFactor({ reserved: false }))}
+    />
+  );
+};
+
 type RemovePageProps = {
   title: string;
   messageLine1: string;
@@ -57,7 +118,7 @@ type RemovePageProps = {
   deleteResource: () => Promise<any>;
 };
 
-export const RemoveResourcePage = (props: RemovePageProps) => {
+const RemoveResourcePage = (props: RemovePageProps) => {
   const { title, messageLine1, messageLine2, successMessage, deleteResource } = props;
   const wizard = useWizard();
   return (
