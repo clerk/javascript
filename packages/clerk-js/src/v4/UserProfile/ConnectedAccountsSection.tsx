c7441b0e4c440e977384f9adcfe9a080c0c6780a
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-07-10 22:00:45 +0300

    chore(clerk-js): Add retry button for failed connections in ConnectedAccountsSection

--- packages/clerk-js/src/v4/UserProfile/ConnectedAccountsSection.tsx
+++ packages/clerk-js/src/v4/UserProfile/ConnectedAccountsSection.tsx
@@ -1,10 +1,12 @@
+import { OAuthStrategy } from '@clerk/types';
 import { ExternalAccountResource } from '@clerk/types/src';
 
 import { useCoreUser } from '../../ui/contexts';
 import { useNavigate } from '../../ui/hooks/useNavigate';
-import { Col, Image } from '../customizables';
-import { AccordionItem, UserPreview } from '../elements';
+import { Badge, Col, Flex, Image } from '../customizables';
+import { AccordionItem, useCardState, UserPreview } from '../elements';
 import { useEnabledThirdPartyProviders } from '../hooks';
+import { handleError } from '../utils';
 import { LinkButtonWithDescription } from './LinkButtonWithDescription';
 import { ProfileSection } from './Section';
 import { AddBlockButton } from './UserProfileBlockButtons';
@@ -12,13 +14,17 @@ import { AddBlockButton } from './UserProfileBlockButtons';
 export const ConnectedAccountsSection = () => {
   const user = useCoreUser();
   const { navigate } = useNavigate();
+  const accounts = [
+    ...user.verifiedExternalAccounts,
+    ...user.unverifiedExternalAccounts.filter(a => a.verification?.error),
+  ];
 
   return (
     <ProfileSection
       title='Connected accounts'
       id='connectedAccounts'
     >
-      {user.verifiedExternalAccounts.map(account => (
+      {accounts.map(account => (
         <ConnectedAccountAccordion
           key={account.id}
           account={account}
@@ -30,9 +36,12 @@ export const ConnectedAccountsSection = () => {
 };
 
 const ConnectedAccountAccordion = ({ account }: { account: ExternalAccountResource }) => {
+  const card = useCardState();
   const user = useCoreUser();
   const { navigate } = useNavigate();
   const { providerToDisplayData } = useEnabledThirdPartyProviders();
+  const error = account.verification?.error?.longMessage;
+  const label = account.username || account.emailAddress;
 
   return (
     <AccordionItem
@@ -43,21 +52,47 @@ const ConnectedAccountAccordion = ({ account }: { account: ExternalAccountResour
           sx={theme => ({ width: theme.sizes.$4 })}
         />
       }
-      title={`${providerToDisplayData[account.provider].name} (${account.username || account.emailAddress})`}
+      title={
+        <Flex
+          gap={2}
+          center
+        >
+          {`${providerToDisplayData[account.provider].name} ${label ? `(${label})` : ''}`}
+          {error && <Badge colorScheme='danger'>Requires action</Badge>}
+        </Flex>
+      }
     >
       <Col gap={4}>
-        <UserPreview
-          user={user}
-          size='lg'
-          profileImageUrl={account.avatarUrl || null}
-          icon={
-            <Image
-              alt={providerToDisplayData[account.provider].name}
-              src={providerToDisplayData[account.provider].iconUrl}
-              sx={theme => ({ width: theme.sizes.$4 })}
-            />
-          }
-        />
+        {!error && (
+          <UserPreview
+            user={user}
+            size='lg'
+            profileImageUrl={account.avatarUrl || null}
+            icon={
+              <Image
+                alt={providerToDisplayData[account.provider].name}
+                src={providerToDisplayData[account.provider].iconUrl}
+                sx={theme => ({ width: theme.sizes.$4 })}
+              />
+            }
+          />
+        )}
+        {error && (
+          <LinkButtonWithDescription
+            title='Retry failed connection'
+            subtitle={error}
+            actionLabel='Try again'
+            onClick={() => {
+              return user
+                .createExternalAccount({
+                  strategy: account.verification!.strategy as OAuthStrategy,
+                  redirect_url: window.location.href,
+                })
+                .then(res => navigate(res.verification!.externalVerificationRedirectURL))
+                .catch(err => handleError(err, [], card.setError));
+            }}
+          />
+        )}
         <LinkButtonWithDescription
           title='Remove'
           subtitle='Remove this connected account from your account'
