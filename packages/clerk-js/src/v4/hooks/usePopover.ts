d0e8d0332c390540d19c5fba3a0809419fa8074c
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-07-11 04:53:22 +0300

    chore(clerk-js): More responsive design improvements

--- packages/clerk-js/src/v4/hooks/usePopover.ts
+++ packages/clerk-js/src/v4/hooks/usePopover.ts
@@ -17,25 +17,27 @@ type UsePopoverProps = {
 };
 
 export const usePopover = (props: UsePopoverProps = {}) => {
-  const [open, setOpen] = React.useState(props.defaultOpen || false);
+  const [isOpen, setIsOpen] = React.useState(props.defaultOpen || false);
   const { reference, floating, strategy, x, y, context } = useFloating({
-    open,
-    onOpenChange: setOpen,
+    open: isOpen,
+    onOpenChange: setIsOpen,
     whileElementsMounted: props.autoUpdate === false ? undefined : autoUpdate,
     placement: props.placement || 'bottom-start',
     middleware: [offset(props.offset || 6), flip(), shift()],
   });
   useDismiss(context);
 
-  const toggle = React.useCallback(() => {
-    setOpen(o => !o);
-  }, [setOpen]);
+  const toggle = React.useCallback(() => setIsOpen(o => !o), [setIsOpen]);
+  const open = React.useCallback(() => setIsOpen(true), [setIsOpen]);
+  const close = React.useCallback(() => setIsOpen(false), [setIsOpen]);
 
   return {
     reference,
     floating,
     toggle,
-    isOpen: open,
+    open,
+    close,
+    isOpen,
     styles: { position: strategy, top: y ?? 0, left: x ?? 0 },
     context,
   };
