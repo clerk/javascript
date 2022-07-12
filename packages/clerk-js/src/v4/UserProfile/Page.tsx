5d6308ddd83da120812abe00330025c97ccaa052
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-07-12 11:08:11 +0300

    chore(clerk-js): Remove Page.Toolbar

--- packages/clerk-js/src/v4/UserProfile/Page.tsx
+++ packages/clerk-js/src/v4/UserProfile/Page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Col, descriptors, Flex } from '../customizables';
+import { Col, descriptors } from '../customizables';
 import { CardAlert, Header, useCardState } from '../elements';
 import { PropsOfComponent } from '../styledSystem';
 import { Breadcrumbs } from './Breadcrumbs';
@@ -20,7 +20,9 @@ const Page = (props: PageProps) => {
       gap={8}
       {...rest}
       sx={t => ({
-        height: t.sizes.$176,
+        // This needs to be less than the modal's max height
+        minHeight: t.sizes.$120,
+        // paddingBottom: '80px',
         // maxHeight: `min(${t.sizes.$176}, calc(100vh - ${t.sizes.$20}))`,
       })}
     >
@@ -35,26 +37,6 @@ const Page = (props: PageProps) => {
   );
 };
 
-const Toolbar = (props: React.PropsWithChildren<{}>) => {
-  return (
-    <Flex
-      direction='rowReverse'
-      justify='start'
-      gap={4}
-      sx={theme => ({
-        position: 'absolute',
-        bottom: 0,
-        right: 0,
-        left: 0,
-        padding: `${theme.space.$5} ${theme.space.$8}`,
-        borderTop: `${theme.borders.$normal} ${theme.colors.$blackAlpha300}`,
-      })}
-      {...props}
-    />
-  );
-};
-
 export const ContentPage = {
   Root: Page,
-  Toolbar: Toolbar,
 };
