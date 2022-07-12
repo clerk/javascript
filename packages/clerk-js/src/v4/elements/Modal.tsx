51a3d44cd5b1e8190ac4db0d5094e93948cbcc48
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-07-12 11:12:00 +0300

    chore(clerk-js): Improve modal overflow support for mobile devices

--- packages/clerk-js/src/v4/elements/Modal.tsx
+++ packages/clerk-js/src/v4/elements/Modal.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 
 import { descriptors, Flex } from '../customizables';
 import { usePopover, useSafeLayoutEffect, useScrollLock } from '../hooks';
-import { animations, common, ThemableCssProp } from '../styledSystem';
+import { animations, mqu, ThemableCssProp } from '../styledSystem';
 import { Portal } from '../UserButton/Portal';
 
 type ModalProps = React.PropsWithChildren<{
   handleOpen?: () => void;
   handleClose?: () => void;
   contentSx?: ThemableCssProp;
+  containerSx?: ThemableCssProp;
 }>;
 
 export const Modal = (props: ModalProps) => {
-  const { handleClose, handleOpen, contentSx } = props;
+  const { handleClose, handleOpen, contentSx, containerSx } = props;
   const { floating, isOpen } = usePopover({ defaultOpen: true, autoUpdate: false });
   const { disableScroll, enableScroll } = useScrollLock(document.body);
 
@@ -38,17 +39,23 @@ export const Modal = (props: ModalProps) => {
       <Flex
         aria-hidden
         elementDescriptor={descriptors.modalBackdrop}
-        sx={theme => ({
-          animation: `${animations.fadeIn} 150ms ${theme.transitionTiming.$common}`,
-          zIndex: theme.zIndices.$modal,
-          backgroundColor: theme.colors.$modalBackdrop,
-          ...common.centeredFlex(),
-          width: '100vw',
-          height: '100vh',
-          position: 'fixed',
-          left: 0,
-          top: 0,
-        })}
+        sx={[
+          t => ({
+            animation: `${animations.fadeIn} 150ms ${t.transitionTiming.$common}`,
+            zIndex: t.zIndices.$modal,
+            backgroundColor: t.colors.$modalBackdrop,
+            // ...common.centeredFlex(),
+            alignItems: 'flex-start',
+            justifyContent: 'center',
+            overflow: 'auto',
+            width: '100vw',
+            height: ['100vh', '-webkit-fill-available'],
+            position: 'fixed',
+            left: 0,
+            top: 0,
+          }),
+          containerSx,
+        ]}
       >
         <Flex
           elementDescriptor={descriptors.modalContent}
@@ -56,8 +63,12 @@ export const Modal = (props: ModalProps) => {
           aria-modal='true'
           role='dialog'
           sx={[
-            theme => ({
-              animation: `${animations.modalSlideAndFade} 180ms ${theme.transitionTiming.$easeOut}`,
+            t => ({
+              animation: `${animations.modalSlideAndFade} 180ms ${t.transitionTiming.$easeOut}`,
+              margin: `${t.space.$16} 0`,
+              [mqu.sm]: {
+                margin: `${t.space.$10} 0`,
+              },
             }),
             contentSx,
           ]}
