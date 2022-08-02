5e7511f69ef7ee2464f5c58b9cee833bb9f34914
Sokratis Vidros <sokratis.vidros@gmail.com>
2022-08-02 15:36:41 +0300

    chore(clerk-sdk-node): Add TS to NextJS example for better testing

--- packages/sdk-node/examples/next/pages/api/with-auth.js
+++
@@ -1,9 +0,0 @@
-import { withAuth } from '@clerk/clerk-sdk-node';
-
-function handler(req, res) {
-  console.log('Session optional');
-  res.statusCode = 200;
-  res.json(req.auth);
-}
-
-export default withAuth(handler);
