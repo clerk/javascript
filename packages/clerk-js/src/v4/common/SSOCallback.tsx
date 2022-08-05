d3eb05f51b8a8b14ea0fba7c0e3178b85726f492
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-08-05 21:38:17 +0300

    fix(clerk-js): Temporarily ignore handleRedirectCallback error
    This error is caused when the host app is using React18
    and strictMode is enabled. This useEffects runs twice because
    the clerk-react ui components mounts, unmounts and mounts again
    so the clerk-js component loses its state because of the custom
    unmount callback we're using.
    This needs to be solved by tweaking the logic in uiComponents.tsx
    or by making handleRedirectCallback idempotent

--- packages/clerk-js/src/v4/common/SSOCallback.tsx
+++ packages/clerk-js/src/v4/common/SSOCallback.tsx
@@ -11,8 +11,14 @@ export const SSOCallback = (props: HandleOAuthCallbackParams) => {
   const { navigate } = useNavigate();
 
   React.useEffect(() => {
-    handleRedirectCallback({ ...props }, navigate).catch(e => {
-      console.log(e);
+    handleRedirectCallback({ ...props }, navigate).catch(() => {
+      // This error is caused when the host app is using React18
+      // and strictMode is enabled. This useEffects runs twice because
+      // the clerk-react ui components mounts, unmounts and mounts again
+      // so the clerk-js component loses its state because of the custom
+      // unmount callback we're using.
+      // This needs to be solved by tweaking the logic in uiComponents.tsx
+      // or by making handleRedirectCallback idempotent
     });
   }, []);
 
