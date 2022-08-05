b5b55a8d7aa48753fa49915796c0bb78be9330ee
Sokratis Vidros <sokratis.vidros@gmail.com>
2022-08-05 15:17:00 +0300

    chore(clerk-sdk-node): Enhance the express.js example to ease testing

--- packages/sdk-node/examples/express/src/router-level-middelware.ts
+++ packages/sdk-node/examples/express/src/router-level-middelware.ts
@@ -1,34 +1,50 @@
+/* eslint-disable @typescript-eslint/no-namespace, @typescript-eslint/no-empty-interface */
+
+import dotenv from 'dotenv';
+dotenv.config();
+
 import {
   ClerkExpressRequireAuth,
   ClerkExpressWithAuth,
   RequireAuthProp,
+  StrictAuthProp,
   WithAuthProp,
 } from '@clerk/clerk-sdk-node';
-import dotenv from 'dotenv';
 import express, { Application, Request, Response } from 'express';
-dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const app: Application = express();
 
-// Root path uses lax middleware
+declare global {
+  namespace Express {
+    interface Request extends StrictAuthProp {}
+  }
+}
+
+// with-auth path uses lax middleware
 app.get(
-  '/',
+  '/with-auth',
   ClerkExpressWithAuth(),
   (req: WithAuthProp<Request>, res: Response) => {
     res.json(req.auth);
   }
 );
 
-// /require-session path uses strict middleware
+// /require-auth path uses strict middleware
 app.get(
-  '/require-session',
+  '/require-auth',
   ClerkExpressRequireAuth(),
   (req: RequireAuthProp<Request>, res) => {
     res.json(req.auth);
   }
 );
+
+// @ts-ignore
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(401).send('Unauthenticated!');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
