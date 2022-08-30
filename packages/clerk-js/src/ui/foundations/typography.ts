0cb4f795a36fff25625d77ca6d47ee24f3744a6b
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-08-30 21:08:03 +0300

    fix(clerk-js): Slightly increase the base font size for smaller elements

--- packages/clerk-js/src/ui/foundations/typography.ts
+++ packages/clerk-js/src/ui/foundations/typography.ts
@@ -24,8 +24,8 @@ const letterSpacings = Object.freeze({
 } as const);
 
 const fontSizes = Object.freeze({
-  '2xs': '0.625rem',
-  xs: '0.75rem',
+  '2xs': '0.6875rem',
+  xs: '0.8125rem',
   sm: '0.875rem',
   md: '1rem',
   lg: '1.125rem',
