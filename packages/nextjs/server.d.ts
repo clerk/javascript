7ccc8a9e85fd29c12a8465c9dabf1752c95d03db
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-09-27 02:25:05 +0300

    fix(nextjs): Correctly export /api via /next dir

--- packages/nextjs/server.d.ts
+++ packages/nextjs/server.d.ts
@@ -1,6 +1,7 @@
-export { AuthData, NextMiddlewareHandlerWithAuth } from './dist/server/types';
+export { AuthData } from './dist/server/types';
 
 export { withClerkMiddleware } from './dist/server/utils/withClerkMiddleware';
 
 // Export e.g. edge types since api is the same
 export { getAuthEdge as getAuth } from './dist/server/getAuthEdge';
+export * from './dist/api';
