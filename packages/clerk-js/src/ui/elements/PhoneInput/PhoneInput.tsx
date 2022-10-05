9619bfe6da4fb876f2ba6279f2c08d6a8c2d263c
George Desipris <george.desipris.56@gmail.com>
2022-10-05 20:05:02 +0300

    feat(clerk-js): Introduce Select element and use it in PhoneInput

--- packages/clerk-js/src/ui/elements/PhoneInput/PhoneInput.tsx
+++ packages/clerk-js/src/ui/elements/PhoneInput/PhoneInput.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
-import { Button, Flex, Icon, Input, Text } from '../../customizables';
-import { usePopover, useSearchInput } from '../../hooks';
-import { Caret } from '../../icons';
+import { Flex, Input, Text } from '../../customizables';
+import { Select, SelectButton, SelectOptionList } from '../../elements';
 import { PropsOfComponent } from '../../styledSystem';
 import { getFlagEmojiFromCountryIso } from '../../utils';
 import { CountryEntry, CountryIso, IsoToCountryMap } from './countryCodeData';
-import { DropdownBox, DropdownItemContainer, DropdownSearchbar } from './Dropdown';
 import { useFormattedPhoneNumber } from './useFormattedPhoneNumber';
 
-const createDropdownOption = (country: CountryEntry) => {
+const createSelectOption = (country: CountryEntry) => {
   return {
     searchTerm: `${country.iso} ${country.name} ${country.code}`,
     country,
@@ -17,30 +15,17 @@ const createDropdownOption = (country: CountryEntry) => {
   };
 };
 
-const countryOptions = [...IsoToCountryMap.values()].map(createDropdownOption);
+const countryOptions = [...IsoToCountryMap.values()].map(createSelectOption);
 
 type PhoneInputProps = PropsOfComponent<typeof Input>;
 
 export const PhoneInput = (props: PhoneInputProps) => {
   const { onChange: onChangeProp, value, ...rest } = props;
   const phoneInputRef = React.useRef<HTMLInputElement>(null);
-  const selectedItemRef = React.useRef<HTMLDivElement>(null);
-  const [selectedIndex, setSelectedIndex] = React.useState(0);
-  const { floating, reference, styles, toggle: togglePopover, isOpen } = usePopover({ autoUpdate: false });
-  const { selectedIso, setPhoneNumber, cleanPhoneNumber, formattedPhoneNumber, setSelectedIso } =
-    useFormattedPhoneNumber({ defaultPhone: value as string });
-  const { filteredItems, searchInputProps } = useSearchInput({
-    items: countryOptions,
-    comparator: (term, item) => item.searchTerm.toLowerCase().includes(term.toLowerCase()),
-  });
-
-  const scrollToItemOnSelectedIndexChange = () => {
-    if (!isOpen) {
-      setSelectedIndex(0);
-      return;
-    }
-    selectedItemRef.current?.scrollIntoView({ block: 'nearest' });
-  };
+  const { setPhoneNumber, cleanPhoneNumber, formattedPhoneNumber, selectedIso, setSelectedIso } =
+    useFormattedPhoneNumber({
+      defaultPhone: value as string,
+    });
 
   const callOnChangeProp = () => {
     // Quick and dirty way to match this component's public API
@@ -49,40 +34,11 @@ export const PhoneInput = (props: PhoneInputProps) => {
     onChangeProp?.({ target: { value: cleanPhoneNumber } } as any);
   };
 
+  const selectedCountryOption = React.useMemo(() => {
+    return countryOptions.find(o => o.country.iso === selectedIso) || countryOptions[0];
+  }, [selectedIso]);
+
   React.useEffect(callOnChangeProp, [cleanPhoneNumber]);
-  React.useEffect(scrollToItemOnSelectedIndexChange, [selectedIndex, isOpen]);
-
-  const selectIso = React.useCallback(
-    (iso: CountryIso) => {
-      setSelectedIso(iso);
-      togglePopover();
-      phoneInputRef.current?.focus();
-    },
-    [togglePopover, setSelectedIso],
-  );
-
-  const onKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
-    if (e.key === 'ArrowUp') {
-      e.preventDefault();
-      if (isOpen) {
-        return setSelectedIndex((i = 0) => Math.max(i - 1, 0));
-      }
-      return togglePopover();
-    }
-
-    if (e.key === 'ArrowDown') {
-      e.preventDefault();
-      if (isOpen) {
-        return setSelectedIndex((i = 0) => Math.min(i + 1, filteredItems.length - 1));
-      }
-      return togglePopover();
-    }
-
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      return selectIso(filteredItems[selectedIndex].country.iso);
-    }
-  };
 
   return (
     <Flex
@@ -90,130 +46,77 @@ export const PhoneInput = (props: PhoneInputProps) => {
       justify='center'
       sx={theme => ({ position: 'relative', borderRadius: theme.radii.$md })}
     >
-      <DropdownTrigger
-        selectedIso={selectedIso}
-        ref={reference}
-        isOpen={isOpen}
-        onClick={togglePopover}
-        isDisabled={rest.isDisabled}
-      />
+      <Select
+        value={selectedCountryOption}
+        options={countryOptions}
+        optionBuilder={(option, _index, isSelected) => (
+          <CountryCodeListItem
+            sx={theme => ({
+              ...(isSelected && { backgroundColor: theme.colors.$blackAlpha200 }),
+              '&:hover': {
+                backgroundColor: theme.colors.$blackAlpha200,
+              },
+            })}
+            country={option.country}
+          />
+        )}
+        onChange={option => {
+          setSelectedIso(option.country.iso);
+          phoneInputRef.current?.focus();
+        }}
+        noResultsMessage='No countries found'
+        placeholder='Search country or code'
+        comparator={(term, option) => option.searchTerm.toLowerCase().includes(term.toLowerCase())}
+      >
+        <Flex
+          sx={{
+            position: 'absolute',
+            height: `calc(100% - 2px)`,
+            marginLeft: '1px',
+          }}
+        >
+          <SelectButton>
+            <Flag iso={selectedIso} />
+          </SelectButton>
+        </Flex>
+        <SelectOptionList />
+      </Select>
       <Input
         value={formattedPhoneNumber}
         onChange={el => setPhoneNumber(el.target.value)}
         maxLength={25}
         type='tel'
         sx={theme => ({ paddingLeft: theme.space.$20 })}
-        {...rest}
         ref={phoneInputRef}
+        {...rest}
       />
-      <DropdownBox
-        isOpen={isOpen}
-        ref={floating}
-        style={{ ...styles, left: styles.left - 1 }}
-        onKeyDown={onKeyDown}
-      >
-        <DropdownSearchbar
-          placeholder='Search country or code'
-          {...searchInputProps}
-        />
-        <DropdownItemContainer>
-          {filteredItems.map((item, i) => (
-            <CountryCodeListItem
-              key={i}
-              country={item.country}
-              selectIso={selectIso}
-              isSelected={i === selectedIndex}
-              innerRef={i === selectedIndex ? selectedItemRef : undefined}
-            />
-          ))}
-          {filteredItems.length === 0 && <NoResults />}
-        </DropdownItemContainer>
-      </DropdownBox>
     </Flex>
   );
 };
 
-const DropdownTrigger = React.forwardRef<
-  HTMLButtonElement,
-  PropsOfComponent<typeof Button> & { selectedIso: CountryIso; isOpen: boolean }
->((props, ref) => {
-  return (
-    <Button
-      ref={ref}
-      onClick={props.onClick}
-      colorScheme='neutral'
-      variant='ghost'
-      textVariant='smallMedium'
-      isDisabled={props.isDisabled}
-      sx={theme => ({
-        paddingLeft: theme.space.$3x5,
-        paddingRight: theme.space.$3x5,
-        backgroundColor: theme.colors.$blackAlpha50,
-        position: 'absolute',
-        height: 'calc(100% - 2px)',
-        marginLeft: '1px',
-        width: theme.space.$16,
-        borderRadius: theme.radii.$md,
-        borderBottomRightRadius: '0',
-        borderTopRightRadius: '0',
-      })}
-    >
-      <Flag iso={props.selectedIso} />
-      <Icon
-        icon={Caret}
-        sx={theme => ({
-          width: theme.sizes.$3x5,
-          marginLeft: theme.space.$1,
-          transitionProperty: theme.transitionProperty.$common,
-          transitionDuration: theme.transitionDuration.$controls,
-          transform: `rotate(${props.isOpen ? '180' : '0'}deg)`,
-        })}
-      />
-    </Button>
-  );
-});
-
-type CountryCodeListItem = React.PropsWithChildren<{
+type CountryCodeListItem = PropsOfComponent<typeof Flex> & {
   country: CountryEntry;
-  selectIso: (iso: CountryIso) => void;
-  isSelected: boolean;
-  innerRef: any;
-}>;
-
-const NoResults = () => {
-  return (
-    <Text
-      as='div'
-      variant='smallRegular'
-      sx={theme => ({ width: '100%', padding: `${theme.space.$2} 0 0 ${theme.space.$4}` })}
-    >
-      No countries found
-    </Text>
-  );
 };
 
 const CountryCodeListItem = React.memo((props: CountryCodeListItem) => {
-  const { country, selectIso, isSelected, innerRef, ...rest } = props;
+  const { country, sx, ...rest } = props;
   return (
     <Flex
-      ref={innerRef}
-      onClick={() => selectIso(country.iso)}
       center
-      sx={theme => ({
-        userSelect: 'none',
-        gap: theme.space.$2,
-        padding: `${theme.space.$0x5} ${theme.space.$4}`,
-        ...(isSelected && { backgroundColor: theme.colors.$blackAlpha200 }),
-        '&:hover': {
-          backgroundColor: theme.colors.$blackAlpha200,
-        },
-      })}
+      sx={[
+        theme => ({
+          width: '100%',
+          gap: theme.space.$2,
+          padding: `${theme.space.$0x5} ${theme.space.$4}`,
+        }),
+        sx,
+      ]}
+      {...rest}
     >
       <Flag iso={country.iso} />
       <Text
         as='div'
         variant='smallRegular'
-        {...rest}
         sx={{ width: '100%' }}
       >
         {country.name}
---
+++ packages/clerk-js/src/ui/elements/Select.tsx
@@ -0,0 +1,342 @@
+import React, { isValidElement, PropsWithChildren, useState } from 'react';
+
+import { usePopover, useSearchInput } from '../../ui/hooks';
+import { Button, Flex, Icon, Text } from '../customizables';
+import { Caret, MagnifyingGlass } from '../icons';
+import { animations, PropsOfComponent } from '../styledSystem';
+import { colors, createContextAndHook } from '../utils';
+import { InputWithIcon } from './InputWithIcon';
+
+type UsePopoverReturn = ReturnType<typeof usePopover>;
+type UseSearchInputReturn = ReturnType<typeof useSearchInput>;
+
+type OptionBuilder<O> = (option: O, index?: number, isSelected?: boolean) => JSX.Element;
+
+type SelectProps<O> = {
+  options: O[];
+  value: O;
+  onChange: (option: O) => void;
+  placeholder?: string;
+  comparator?: (term: string, option: O) => boolean;
+  noResultsMessage?: string;
+  optionBuilder?: OptionBuilder<O>;
+};
+
+type SelectState<O = any> = Pick<SelectProps<O>, 'placeholder' | 'comparator' | 'noResultsMessage'> & {
+  popoverCtx: UsePopoverReturn;
+  searchInputCtx: UseSearchInputReturn;
+  optionBuilder: OptionBuilder<O>;
+  selectedOption: O;
+  select: (option: O) => void;
+  selectedItemRef: React.RefObject<HTMLDivElement>;
+  onTriggerClick: () => void;
+};
+
+const [SelectStateCtx, useSelectState] = createContextAndHook<SelectState>('SelectState');
+
+const defaultOptionBuilder = <O,>(option: O, _index?: number, isSelected?: boolean) => {
+  if (typeof option !== 'string' && typeof option !== 'number') {
+    throw new Error('Provide an optionBuilder function');
+  }
+
+  return (
+    <Flex
+      sx={theme => ({
+        width: '100%',
+        paddingLeft: theme.space.$2,
+        paddingRight: theme.space.$2,
+        ...(isSelected && { backgroundColor: theme.colors.$blackAlpha200 }),
+        '&:hover': {
+          backgroundColor: theme.colors.$blackAlpha200,
+        },
+      })}
+    >
+      <Text>{option}</Text>
+    </Flex>
+  );
+};
+
+export const Select = <O,>(props: PropsWithChildren<SelectProps<O>>) => {
+  const {
+    value,
+    options,
+    onChange,
+    optionBuilder = defaultOptionBuilder,
+    noResultsMessage,
+    comparator,
+    placeholder,
+    ...rest
+  } = props;
+  const popoverCtx = usePopover({ autoUpdate: false });
+  const togglePopover = popoverCtx.toggle;
+  const selectedItemRef = React.useRef<HTMLDivElement>(null);
+  const searchInputCtx = useSearchInput({
+    items: options,
+    comparator: comparator || (() => true),
+  });
+
+  const select = React.useCallback(
+    option => {
+      onChange?.(option);
+      togglePopover();
+    },
+    [togglePopover, onChange],
+  );
+
+  return (
+    <SelectStateCtx.Provider
+      value={{
+        value: {
+          popoverCtx,
+          searchInputCtx,
+          selectedOption: value,
+          noResultsMessage,
+          selectedItemRef,
+          optionBuilder,
+          placeholder,
+          comparator,
+          select,
+          onTriggerClick: togglePopover,
+        },
+      }}
+      {...rest}
+    />
+  );
+};
+
+type SelectOptionBuilderProps<O = unknown> = {
+  option: O;
+  index: number;
+  optionBuilder: OptionBuilder<O>;
+  handleSelect: (option: O) => void;
+  isSelected: boolean;
+};
+
+const _SelectOptionBuilder = (props: SelectOptionBuilderProps, ref?: React.ForwardedRef<HTMLDivElement>) => {
+  const { option, optionBuilder, index, handleSelect, isSelected } = props;
+  return (
+    <Flex
+      ref={ref}
+      sx={{
+        userSelect: 'none',
+      }}
+      onClick={() => {
+        handleSelect(option);
+      }}
+    >
+      {optionBuilder(option, index, isSelected)}
+    </Flex>
+  );
+};
+
+const SelectOptionBuilder = React.memo(React.forwardRef(_SelectOptionBuilder));
+
+const SelectSearchbar = (props: PropsOfComponent<typeof InputWithIcon>) => {
+  const { sx, ...rest } = props;
+  React.useEffect(() => {
+    // @ts-expect-error
+    return () => props.onChange({ target: { value: '' } });
+  }, []);
+
+  return (
+    <Flex sx={theme => ({ borderBottom: theme.borders.$normal, borderColor: theme.colors.$blackAlpha200 })}>
+      <InputWithIcon
+        focusRing={false}
+        autoFocus
+        leftIcon={
+          <Icon
+            colorScheme='neutral'
+            icon={MagnifyingGlass}
+          />
+        }
+        sx={[{ border: 'none', borderRadius: '0' }, sx]}
+        {...rest}
+      />
+    </Flex>
+  );
+};
+
+export const SelectNoResults = (props: PropsOfComponent<typeof Text>) => {
+  const { sx, ...rest } = props;
+  return (
+    <Text
+      as='div'
+      variant='smallRegular'
+      sx={[theme => ({ width: '100%', padding: `${theme.space.$2} 0 0 ${theme.space.$4}` }), sx]}
+      {...rest}
+    />
+  );
+};
+
+type SelectOptionListProps = PropsOfComponent<typeof Flex>;
+
+export const SelectOptionList = (props: SelectOptionListProps) => {
+  const { sx, ...rest } = props;
+  const {
+    popoverCtx,
+    searchInputCtx,
+    optionBuilder,
+    placeholder,
+    comparator,
+    selectedItemRef,
+    noResultsMessage,
+    select,
+    onTriggerClick,
+  } = useSelectState();
+  const { filteredItems: options, searchInputProps } = searchInputCtx;
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const { isOpen, floating, styles } = popoverCtx;
+  const containerRef = React.useRef<HTMLDivElement>(null);
+
+  const scrollToItemOnSelectedIndexChange = () => {
+    if (!isOpen) {
+      setSelectedIndex(0);
+      return;
+    }
+    selectedItemRef.current?.scrollIntoView({ block: 'nearest' });
+  };
+
+  React.useEffect(scrollToItemOnSelectedIndexChange, [selectedIndex, isOpen]);
+  React.useEffect(() => {
+    if (!comparator) {
+      containerRef?.current?.focus();
+    }
+  }, [isOpen]);
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === 'ArrowUp') {
+      e.preventDefault();
+      if (isOpen) {
+        return setSelectedIndex((i = 0) => Math.max(i - 1, 0));
+      }
+      return onTriggerClick();
+    }
+
+    if (e.key === 'ArrowDown') {
+      e.preventDefault();
+      if (isOpen) {
+        return setSelectedIndex((i = 0) => Math.min(i + 1, options.length - 1));
+      }
+      return onTriggerClick();
+    }
+
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      return select(options[selectedIndex]);
+    }
+  };
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <Flex
+      ref={floating}
+      onKeyDown={onKeyDown}
+      direction='col'
+      justify='start'
+      sx={[
+        theme => ({
+          backgroundColor: colors.makeSolid(theme.colors.$colorBackground),
+          border: theme.borders.$normal,
+          borderRadius: theme.radii.$lg,
+          borderColor: theme.colors.$blackAlpha200,
+          overflow: 'hidden',
+          width: '100%',
+          animation: `${animations.dropdownSlideInScaleAndFade} ${theme.transitionDuration.$slower} ${theme.transitionTiming.$slowBezier}`,
+          transformOrigin: 'top center',
+          boxShadow: theme.shadows.$cardDropShadow,
+          zIndex: theme.zIndices.$dropdown,
+        }),
+        sx,
+      ]}
+      style={{ ...styles, left: styles.left - 1 }}
+      {...rest}
+    >
+      {comparator && (
+        <SelectSearchbar
+          placeholder={placeholder}
+          {...searchInputProps}
+        />
+      )}
+      <Flex
+        ref={containerRef}
+        direction='col'
+        tabIndex={comparator ? undefined : 0}
+        sx={[
+          theme => ({
+            outline: 'none',
+            overflowY: 'scroll',
+            maxHeight: '18vh',
+            paddingBottom: theme.space.$2,
+          }),
+          sx,
+        ]}
+        {...rest}
+      >
+        {options.map((option, index) => {
+          const isSelected = index === selectedIndex;
+          return (
+            <SelectOptionBuilder
+              key={index}
+              index={index}
+              ref={isSelected ? selectedItemRef : undefined}
+              option={option}
+              optionBuilder={optionBuilder}
+              isSelected={isSelected}
+              handleSelect={select}
+            />
+          );
+        })}
+        {noResultsMessage && options.length === 0 && <SelectNoResults>{noResultsMessage}</SelectNoResults>}
+      </Flex>
+    </Flex>
+  );
+};
+
+export const SelectButton = (props: PropsOfComponent<typeof Button>) => {
+  const { sx, children, ...rest } = props;
+  const { popoverCtx, onTriggerClick, optionBuilder, selectedOption } = useSelectState();
+  const { isOpen, reference } = popoverCtx;
+
+  let show: React.ReactNode = children;
+  if (!isValidElement(children)) {
+    show = optionBuilder(selectedOption);
+  }
+
+  return (
+    <Button
+      ref={reference}
+      colorScheme='neutral'
+      variant='ghost'
+      textVariant='smallMedium'
+      onClick={onTriggerClick}
+      sx={[
+        theme => ({
+          paddingLeft: theme.space.$3x5,
+          paddingRight: theme.space.$3x5,
+          backgroundColor: theme.colors.$blackAlpha50,
+          borderRadius: theme.radii.$md,
+          borderBottomRightRadius: '0',
+          borderTopRightRadius: '0',
+          '> *': { pointerEvents: 'none' },
+        }),
+        sx,
+      ]}
+      {...rest}
+    >
+      {show}
+      <Icon
+        icon={Caret}
+        sx={theme => ({
+          width: theme.sizes.$3x5,
+          marginLeft: theme.space.$1,
+          transitionProperty: theme.transitionProperty.$common,
+          transitionDuration: theme.transitionDuration.$controls,
+          transform: `rotate(${isOpen ? '180' : '0'}deg)`,
+        })}
+      />
+    </Button>
+  );
+};
