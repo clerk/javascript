11689b6b75e8b92daddd7baabbf1e6dda96ff24d
George Desipris <73396808+desiprisg@users.noreply.github.com>
2022-10-06 13:05:29 +0300

    feat(clerk-js): Make the ImpersonationFab draggable
    The ImpersonationFab can now be dragged by the red circle with the eye inside. It will reposition itself if dragged outside the viewport!

--- packages/clerk-js/src/ui/ImpersonationFab/ImpersonationFab.tsx
+++ packages/clerk-js/src/ui/ImpersonationFab/ImpersonationFab.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { PointerEventHandler, useEffect, useRef } from 'react';
 
-import { mqu } from '../../ui/styledSystem';
+import { mqu, PropsOfComponent } from '../../ui/styledSystem';
 import { getFullName, getIdentifier } from '../../ui/utils';
 import { useCoreClerk, useCoreSession } from '../contexts';
 import {
@@ -18,24 +18,29 @@ import {
 import { Portal } from '../elements/Portal';
 import { Eye } from '../icons';
 
-type EyeCircleProps = {
+type EyeCircleProps = PropsOfComponent<typeof Col> & {
   width: string;
   height: string;
 };
 
-const EyeCircle = ({ width, height }: EyeCircleProps) => {
+const EyeCircle = ({ width, height, ...props }: EyeCircleProps) => {
+  const { sx, ...rest } = props;
   return (
     <Col
       id='cl-impersonationEye'
       elementDescriptor={descriptors.impersonationFabIconContainer}
       center
-      sx={t => ({
-        width,
-        height,
-        transition: `transform ${t.transitionDuration.$slowest} ease`,
-        backgroundColor: t.colors.$danger500,
-        borderRadius: t.radii.$circle,
-      })}
+      sx={[
+        t => ({
+          width,
+          height,
+          transition: `transform ${t.transitionDuration.$slowest} ease`,
+          backgroundColor: t.colors.$danger500,
+          borderRadius: t.radii.$circle,
+        }),
+        sx,
+      ]}
+      {...rest}
     >
       <Icon
         elementDescriptor={descriptors.impersonationFabIcon}
@@ -91,14 +96,70 @@ export const ImpersonationFab = () => {
   const session = useCoreSession();
   const { t } = useLocalizations();
   const { parsedInternalTheme } = useAppearance();
+  const containerRef = useRef<HTMLDivElement>(null);
   const actor = session?.actor;
   const isImpersonating = !!actor;
 
   //essentials for calcs
-  const top = '109px';
-  const right = '23px';
   const eyeWidth = parsedInternalTheme.sizes.$16;
   const eyeHeight = eyeWidth;
+  const topProperty = '--cl-impersonation-fab-top';
+  const rightProperty = '--cl-impersonation-fab-right';
+  const defaultTop = 109;
+  const defaultRight = 23;
+
+  const handleResize = () => {
+    const current = containerRef.current;
+    if (!current) {
+      return;
+    }
+
+    const outsideViewport =
+      current.offsetLeft < 0 ||
+      current.offsetLeft > window.innerWidth ||
+      current.offsetTop < 0 ||
+      current.offsetTop > window.innerHeight;
+
+    if (outsideViewport) {
+      document.documentElement.style.setProperty(rightProperty, `${defaultRight}px`);
+      document.documentElement.style.setProperty(topProperty, `${defaultTop}px`);
+    }
+  };
+
+  const onPointerDown: PointerEventHandler = () => {
+    window.addEventListener('pointermove', onPointerMove);
+    window.addEventListener(
+      'pointerup',
+      () => {
+        window.removeEventListener('pointermove', onPointerMove);
+        handleResize();
+      },
+      { once: true },
+    );
+  };
+
+  const onPointerMove = React.useCallback((e: PointerEvent) => {
+    e.stopPropagation();
+    e.preventDefault();
+    const current = containerRef.current;
+    if (!current) {
+      return;
+    }
+    const rightOffestBasedOnViewportAndContent = `${
+      window.innerWidth - current.offsetLeft - current.offsetWidth - e.movementX
+    }px`;
+    document.documentElement.style.setProperty(rightProperty, rightOffestBasedOnViewportAndContent);
+    document.documentElement.style.setProperty(topProperty, `${current.offsetTop + e.movementY}px`);
+  }, []);
+
+  const repositionFabOnResize = () => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  };
+
+  useEffect(repositionFabOnResize, []);
 
   if (!isImpersonating || !session.user) {
     return null;
@@ -112,22 +173,23 @@ export const ImpersonationFab = () => {
   return (
     <Portal>
       <Flex
+        ref={containerRef}
         elementDescriptor={descriptors.impersonationFab}
         align='center'
         sx={t => ({
           position: 'fixed',
           overflow: 'hidden',
-          top,
-          right,
+          top: `var(${topProperty}, ${defaultTop}px)`,
+          right: `var(${rightProperty}, ${defaultRight}px)`,
           zIndex: t.zIndices.$fab,
           boxShadow: t.shadows.$fabShadow,
           borderRadius: t.radii.$halfHeight, //to match the circular eye perfectly
           backgroundColor: t.colors.$white,
           fontFamily: t.fonts.$main,
           ':hover #cl-impersonationText': {
-            maxWidth: `min(calc(50vw - ${eyeWidth} - 2 * ${right}), ${titleLength}ch)`,
+            maxWidth: `min(calc(50vw - ${eyeWidth} - 2 * ${defaultRight}px), ${titleLength}ch)`,
             [mqu.md]: {
-              maxWidth: `min(calc(100vw - ${eyeWidth} - 2 * ${right}), ${titleLength}ch)`,
+              maxWidth: `min(calc(100vw - ${eyeWidth} - 2 * ${defaultRight}px), ${titleLength}ch)`,
             },
           },
           ':hover #cl-impersonationEye': {
@@ -136,6 +198,12 @@ export const ImpersonationFab = () => {
         })}
       >
         <EyeCircle
+          onPointerDown={onPointerDown}
+          sx={{
+            ':hover': {
+              cursor: 'grab',
+            },
+          }}
           width={eyeWidth}
           height={eyeHeight}
         />
