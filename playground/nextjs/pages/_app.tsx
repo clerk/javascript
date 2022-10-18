d0ba5f60ec35e85e6a60af0fc05b1baacbf1ffd4
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-10-18 22:23:59 +0300

    chore(repo): Add simple theme switcher to nextjs playground

--- playground/nextjs/pages/_app.tsx
+++ playground/nextjs/pages/_app.tsx
@@ -2,22 +2,32 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
 import { ClerkProvider, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+import { dark, neobrutalism, shadesOfPurple } from '@clerk/themes';
 import Link from 'next/link';
+import { useState } from 'react';
+
+const themes = { default: undefined, dark, neobrutalism, shadesOfPurple };
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [selectedTheme, setSelectedTheme] = useState<keyof typeof themes>('default');
   return (
     <ClerkProvider
+      appearance={{ baseTheme: themes[selectedTheme] }}
       frontendApi={'clerk.touching.camel-78.dev.lclclerk.com'}
       clerkJSUrl={'https://js.lclclerk.com/npm/clerk.browser.js'}
       {...pageProps}
     >
-      <AppBar />
+      <AppBar
+        onChangeTheme={e => {
+          setSelectedTheme(e.target.value as any);
+        }}
+      />
       <Component {...pageProps} />
     </ClerkProvider>
   );
 }
 
-const AppBar = () => {
+const AppBar = (props: { onChangeTheme: React.ChangeEventHandler<HTMLSelectElement> }) => {
   return (
     <div
       style={{
@@ -34,6 +44,16 @@ const AppBar = () => {
           <h2>Nextjs Playground</h2>
         </a>
       </Link>
+      <select
+        name='theme'
+        id='theme'
+        onChange={props.onChangeTheme}
+      >
+        <option value='default'>default</option>
+        <option value='dark'>dark</option>
+        <option value='neobrutalism'>neobrutalism</option>
+        <option value='shadesOfPurple'>shadesOfPurple</option>
+      </select>
       <UserButton />
       <SignedOut>
         <SignInButton mode={'modal'} />
