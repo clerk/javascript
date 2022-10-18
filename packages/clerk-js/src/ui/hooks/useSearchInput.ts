056efb07706f0e4dd8fddbd3990fe9bce2e04085
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-10-18 22:27:58 +0300

    feat(clerk-js): Make useSearchInput faster and improve its api

--- packages/clerk-js/src/ui/hooks/useSearchInput.ts
+++ packages/clerk-js/src/ui/hooks/useSearchInput.ts
@@ -4,17 +4,30 @@ type Unarray<T> = T extends Array<infer U> ? U : T;
 
 type UseSearchInputProps<Items> = {
   items: Items;
-  comparator: (term: string, item: Unarray<Items>) => boolean;
+  comparator: (term: string, item: Unarray<Items>, itemTerm?: string) => boolean;
+  searchTermForItem?: (item: Unarray<Items>) => string;
 };
 
-export const useSearchInput = <Items extends Array<any>>(props: UseSearchInputProps<Items>) => {
-  const { items, comparator } = props;
+type UseSearchInputReturn<Items> = { filteredItems: Items; searchInputProps: any };
+
+export const useSearchInput = <Items extends Array<any>>(
+  props: UseSearchInputProps<Items>,
+): UseSearchInputReturn<Items> => {
+  const { items, comparator, searchTermForItem } = props;
   const [searchTerm, setSearchTerm] = React.useState('');
   const onChange: ChangeEventHandler<HTMLInputElement> = e => setSearchTerm(e.target.value || '');
 
+  const searchTermMap = React.useMemo(() => {
+    type TermMap = Map<Unarray<Items>, string | undefined>;
+    return items.reduce((acc, item) => {
+      (acc as TermMap).set(item, searchTermForItem?.(item));
+      return acc;
+    }, new Map() as TermMap) as TermMap;
+  }, [items]);
+
   const filteredItems = React.useMemo(
-    () => items.filter(i => comparator(searchTerm, i)),
-    [items, comparator, searchTerm],
+    () => (searchTerm ? items.filter(i => comparator(searchTerm, i, searchTermMap.get(i))) : items),
+    [items, searchTerm],
   ) as Items;
 
   return { searchInputProps: { onChange, value: searchTerm }, filteredItems };
