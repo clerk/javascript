c3f5098dfda64671eb058646a578520e86f72612
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-10-21 03:23:32 +0300

    chore(clerk-react,shared): Extract and move contexts from clerk-react to shared

--- packages/react/src/contexts/OrganizationContext.tsx
+++ packages/react/src/contexts/OrganizationContext.tsx
@@ -1,11 +1,1 @@
-import { createContextAndHook } from '@clerk/shared';
-import { OrganizationInvitationResource, OrganizationMembershipResource, OrganizationResource } from '@clerk/types';
-
-/**
- * @internal
- */
-export const [OrganizationContext, useOrganizationContext] = createContextAndHook<{
-  organization: OrganizationResource | null | undefined;
-  lastOrganizationInvitation: OrganizationInvitationResource | null | undefined;
-  lastOrganizationMember: OrganizationMembershipResource | null | undefined;
-}>('OrganizationContext');
+export { OrganizationContext, useOrganizationContext } from '@clerk/shared';
---
+++ packages/shared/src/hooks/useOrganizationList.tsx
@@ -0,0 +1,45 @@
+import {
+  CreateOrganizationParams,
+  OrganizationMembershipResource,
+  OrganizationResource,
+  SetActive,
+} from '@clerk/types';
+
+import { useClerkInstanceContext, useUserContext } from './contexts';
+
+type OrganizationList = ReturnType<typeof createOrganizationList>;
+
+type UseOrganizationListReturn =
+  | { isLoaded: false; organizationList: undefined; createOrganization: undefined; setActive: undefined }
+  | {
+      isLoaded: true;
+      organizationList: OrganizationList;
+      createOrganization: (params: CreateOrganizationParams) => Promise<OrganizationResource>;
+      setActive: SetActive;
+    };
+
+type UseOrganizationList = () => UseOrganizationListReturn;
+
+export const useOrganizationList: UseOrganizationList = () => {
+  const clerk = useClerkInstanceContext();
+  const user = useUserContext();
+
+  // TODO: Properly check for SSR user values
+  if (!clerk.loaded || !user) {
+    return { isLoaded: false, organizationList: undefined, createOrganization: undefined, setActive: undefined };
+  }
+
+  return {
+    isLoaded: true,
+    organizationList: createOrganizationList(user.organizationMemberships),
+    setActive: clerk.setActive,
+    createOrganization: clerk.createOrganization,
+  };
+};
+
+function createOrganizationList(organizationMemberships: OrganizationMembershipResource[]) {
+  return organizationMemberships.map(organizationMembership => ({
+    membership: organizationMembership,
+    organization: organizationMembership.organization,
+  }));
+}
