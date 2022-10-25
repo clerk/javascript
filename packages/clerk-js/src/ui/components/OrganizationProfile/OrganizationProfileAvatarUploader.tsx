75745e84e1853c16fad1964794c1a99f88c73254
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-10-26 01:34:51 +0300

    chore(clerk-js): Make AvatarUploader reusable

---
+++ packages/clerk-js/src/ui/components/OrganizationProfile/OrganizationProfileAvatarUploader.tsx
@@ -0,0 +1,23 @@
+import { OrganizationResource } from '@clerk/types';
+
+import { AvatarUploader, AvatarUploaderProps, OrganizationAvatar } from '../../elements';
+import { localizationKeys } from '../../localization';
+
+export const OrganizationProfileAvatarUploader = (
+  props: Omit<AvatarUploaderProps, 'avatarPreview' | 'title'> & { organization: Partial<OrganizationResource> },
+) => {
+  const { organization, ...rest } = props;
+  return (
+    <AvatarUploader
+      {...rest}
+      title={localizationKeys('userProfile.profilePage.imageFormTitle')}
+      avatarPreview={
+        <OrganizationAvatar
+          size={theme => theme.sizes.$11}
+          optimize
+          {...organization}
+        />
+      }
+    />
+  );
+};
