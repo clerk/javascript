11bd66ea0151cdbb1b149364b19b81b5e7a7d49b
colinclerk <colin@clerk.dev>
2022-10-28 14:14:08 -0700

    Fix SSR

--- packages/nextjs/src/app-beta/ClerkProvider.tsx
+++ packages/nextjs/src/app-beta/ClerkProvider.tsx
@@ -4,6 +4,6 @@ import { initialState } from './auth';
 import { ClerkProvider as ClerkProviderClient } from './client/ClerkProvider';
 
 export function ClerkProvider({ children }: { children: React.ReactNode }) {
-  const state = initialState();
+  const state = initialState()?.__clerk_ssr_state || { sessionId: null, orgId: null, userId: null };
   return <ClerkProviderClient initialState={state}>{children}</ClerkProviderClient>;
 }
