8e441f69f48f9c6f07e182f77681c57c9ffcd5e3
Sokratis Vidros <sokratis.vidros@gmail.com>
2022-11-11 15:58:55 +0200

    feat(clerk-expo): Support Publishable Keys

--- packages/expo/src/ClerkProvider.tsx
+++ packages/expo/src/ClerkProvider.tsx
@@ -1,26 +1,35 @@
 import './polyfills';
 
-import { ClerkProvider as ClerkReactProvider, ClerkProviderProps as ClerkReactProviderProps } from '@clerk/clerk-react';
+import {
+  __internal__setErrorThrowerOptions,
+  ClerkProvider as ClerkReactProvider,
+  ClerkProviderProps as ClerkReactProviderProps,
+} from '@clerk/clerk-react';
 import React from 'react';
 
 import type { TokenCache } from './cache';
 import { isReactNative } from './runtime';
 import { buildClerk } from './singleton';
 
+__internal__setErrorThrowerOptions({
+  packageName: '@clerk/expo',
+});
+
 export type ClerkProviderProps = ClerkReactProviderProps & {
   children: React.ReactNode;
   tokenCache?: TokenCache;
-  hotload?: boolean;
 };
 
 export function ClerkProvider(props: ClerkProviderProps): JSX.Element {
-  const { children, tokenCache, ...rest } = props;
-  const frontendApi = props.frontendApi || process.env.CLERK_FRONTEND_API || '';
+  const { children, tokenCache, frontendApi, publishableKey, ...rest } = props;
+  const key =
+    publishableKey || process.env.CLERK_PUBLISHABLE_KEY || frontendApi || process.env.CLERK_FRONTEND_API || '';
 
   return (
+    //@ts-expect-error
     <ClerkReactProvider
       {...rest}
-      Clerk={buildClerk({ frontendApi, tokenCache })}
+      Clerk={buildClerk({ key, tokenCache })}
       standardBrowser={!isReactNative()}
     >
       {children}
