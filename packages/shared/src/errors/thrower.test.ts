e4f8be385c5485bc812bb556fc829f630d05b04b
Sokratis Vidros <sokratis.vidros@gmail.com>
2022-11-11 14:10:21 +0200

    feat(shared): Introduce a customizable error thrower
    The error thrower will be used by ClerkJS and Clerk-React to report package specific errors for improved DX.

---
+++ packages/shared/src/errors/thrower.test.ts
@@ -0,0 +1,33 @@
+import { buildErrorThrower, ErrorThrowerOptions } from './thrower';
+
+describe.concurrent('ErrorThrower', () => {
+  const errorThrower = buildErrorThrower({ packageName: '@clerk/test-package' });
+
+  it('throws the correct error message and interpolates pkg and known parameters', () => {
+    expect(() => errorThrower.throwInvalidPublishableKeyError({ key: 'whatever' })).toThrow(
+      '@clerk/test-package: The publishableKey passed to Clerk is invalid. You can get your Publishable key at https://dashboard.clerk.dev/last-active?path=api-keys. (key=whatever)',
+    );
+  });
+
+  it('throws the correct error message and interpolates pkg if no parameters are provided', () => {
+    expect(() => errorThrower.throwMissingFrontendApiPublishableKeyError()).toThrow(
+      '@clerk/test-package: Missing frontendApi or publishableKey. You can get your key at https://dashboard.clerk.dev/last-active?path=api-keys.',
+    );
+  });
+
+  it('throws a custom error message and interpolates pkg and known parameters', () => {
+    expect(() =>
+      errorThrower
+        .setPackageName({
+          packageName: '@clerk/another-test-package',
+        })
+        .setMessages({
+          customMessages: {
+            InvalidPublishableKeyErrorMessage:
+              'This is a custom error message for key={{key}} and an unknown {{replacement}}',
+          },
+        } as ErrorThrowerOptions)
+        .throwInvalidPublishableKeyError({ key: 'whatever' }),
+    ).toThrow('@clerk/another-test-package: This is a custom error message for key=whatever and an unknown ');
+  });
+});
---
+++ packages/shared/src/errors/thrower.ts
@@ -0,0 +1,75 @@
+const DefaultMessages = Object.freeze({
+  InvalidFrontendApiErrorMessage: `The frontendApi passed to Clerk is invalid. You can get your Frontend API key at https://dashboard.clerk.dev/last-active?path=api-keys. (key={{key}})`,
+  InvalidPublishableKeyErrorMessage: `The publishableKey passed to Clerk is invalid. You can get your Publishable key at https://dashboard.clerk.dev/last-active?path=api-keys. (key={{key}})`,
+  MissingFrontendApiPublishableKeyErrorMessage: `Missing frontendApi or publishableKey. You can get your key at https://dashboard.clerk.dev/last-active?path=api-keys.`,
+});
+
+type MessageKeys = keyof typeof DefaultMessages;
+
+type Messages = Record<MessageKeys, string>;
+
+type CustomMessages = Partial<Messages>;
+
+export type ErrorThrowerOptions = {
+  packageName: string;
+  customMessages?: CustomMessages;
+};
+
+export interface ErrorThrower {
+  setPackageName(options: ErrorThrowerOptions): ErrorThrower;
+  setMessages(options: ErrorThrowerOptions): ErrorThrower;
+  throwInvalidPublishableKeyError(params: { key?: string }): never;
+  throwInvalidFrontendApiError(params: { key?: string }): never;
+  throwMissingFrontendApiPublishableKeyError(): never;
+}
+
+export function buildErrorThrower({ packageName, customMessages }: ErrorThrowerOptions): ErrorThrower {
+  let pkg = packageName;
+
+  const messages = {
+    ...DefaultMessages,
+    ...customMessages,
+  };
+
+  function buildMessage(rawMessage: string, replacements?: Record<string, string | number>) {
+    if (!replacements) {
+      return `${pkg}: ${rawMessage}`;
+    }
+
+    let msg = rawMessage;
+    const matches = rawMessage.matchAll(/{{([a-zA-Z0-9-_]+)}}/g);
+
+    for (const match of matches) {
+      const replacement = (replacements[match[1]] || '').toString();
+      msg = msg.replace(`{{${match[1]}}}`, replacement);
+    }
+
+    return `${pkg}: ${msg}`;
+  }
+
+  return {
+    setPackageName({ packageName }: ErrorThrowerOptions): ErrorThrower {
+      if (typeof packageName === 'string') {
+        pkg = packageName;
+      }
+      return this;
+    },
+
+    setMessages({ customMessages }: ErrorThrowerOptions): ErrorThrower {
+      Object.assign(messages, customMessages || {});
+      return this;
+    },
+
+    throwInvalidPublishableKeyError(params: { key?: string }): never {
+      throw new Error(buildMessage(messages.InvalidPublishableKeyErrorMessage, params));
+    },
+
+    throwInvalidFrontendApiError(params: { key?: string }): never {
+      throw new Error(buildMessage(messages.InvalidFrontendApiErrorMessage, params));
+    },
+
+    throwMissingFrontendApiPublishableKeyError(): never {
+      throw new Error(buildMessage(messages.MissingFrontendApiPublishableKeyErrorMessage));
+    },
+  };
+}
