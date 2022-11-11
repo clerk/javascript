3417f32533ec3bcb89d1a2d9b7d0432c757074cb
Sokratis Vidros <sokratis.vidros@gmail.com>
2022-11-11 23:38:21 +0200

    chore(backend): Define root package exports

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,9 +1,2 @@
-import { createBackendApiClient } from './api';
-
-export const apiClient = createBackendApiClient();
-
 export * from './api';
 export * from './tokens';
-
-// TODO: Revise the following
-export { createGetToken, signedOutGetToken, createSignedOutState } from './util/createGetToken';
