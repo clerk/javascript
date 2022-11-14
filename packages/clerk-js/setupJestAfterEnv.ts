962731fd7dcbace117d1ee379c693361fa7a9fc5
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-11-14 02:56:15 +0200

    chore(clerk-js): Introduce new testUtils

---
+++ packages/clerk-js/setupJestAfterEnv.ts
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom';
+// import { afterEach } from '@jest/globals';
+// import { cleanup } from '@testing-library/react';
+//
+// /**
+//  * RTL cannot automatically run cleanup after every test because
+//  * we're not injecting jest helpers globally.
+//  * This file is picked up by jest.config.ts and mounts cleanup as RTL would
+//  * https://github.com/testing-library/testing-library-docs/pull/645
+//  */
+// afterEach(cleanup);
