55f38655d96726d061677dfc76b024b9364903b0
Sokratis Vidros <sokratis.vidros@gmail.com>
2022-11-15 11:28:23 +0200

    feat(nextjs): Support Publishable Keys
    Also add the new error thrower for package specific errors and better UX.

--- packages/nextjs/src/client/index.tsx
+++ packages/nextjs/src/client/index.tsx
@@ -1,33 +1,31 @@
-import { ClerkProvider as ReactClerkProvider } from '@clerk/clerk-react';
+import { __internal__setErrorThrowerOptions, ClerkProvider as ReactClerkProvider } from '@clerk/clerk-react';
 import { IsomorphicClerkOptions } from '@clerk/clerk-react/dist/types';
 import { useRouter } from 'next/router';
 import React from 'react';
 
-export * from '@clerk/clerk-react';
+__internal__setErrorThrowerOptions({
+  packageName: '@clerk/nextjs',
+});
 
-const NO_FRONTEND_API_ERR =
-  'The NEXT_PUBLIC_CLERK_FRONTEND_API environment variable must be set to use the ClerkProvider component.';
+export * from '@clerk/clerk-react';
 
 type NextClerkProviderProps = {
   children: React.ReactNode;
-  frontendApi?: string;
 } & IsomorphicClerkOptions;
 
 export function ClerkProvider({ children, ...rest }: NextClerkProviderProps): JSX.Element {
   // @ts-expect-error
   // Allow for overrides without making the type public
-  const { frontendApi, __clerk_ssr_state, authServerSideProps, clerkJSUrl, ...restProps } = rest;
+  const { authServerSideProps, frontendApi, publishableKey, __clerk_ssr_state, clerkJSUrl, ...restProps } = rest;
   const { push } = useRouter();
-
-  if (frontendApi == undefined && !process.env.NEXT_PUBLIC_CLERK_FRONTEND_API) {
-    throw Error(NO_FRONTEND_API_ERR);
-  }
 
   ReactClerkProvider.displayName = 'ReactClerkProvider';
 
   return (
+    // @ts-expect-error
     <ReactClerkProvider
-      frontendApi={frontendApi || process.env.NEXT_PUBLIC_CLERK_FRONTEND_API}
+      frontendApi={frontendApi || process.env.NEXT_PUBLIC_CLERK_FRONTEND_API || ''}
+      publishableKey={publishableKey || process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || ''}
       clerkJSUrl={clerkJSUrl || process.env.NEXT_PUBLIC_CLERK_JS}
       navigate={to => push(to)}
       // withServerSideAuth automatically injects __clerk_ssr_state
