06e6eb90f87ef784185f249cf6938e9ce1877bdc
Sokratis Vidros <sokratis.vidros@gmail.com>
2022-11-17 21:35:55 +0200

    chore(repo): Add remix playground apps

---
+++ playground/remix-cf-worker/app/routes/index.tsx
@@ -0,0 +1,62 @@
+import type { LoaderFunction } from '@remix-run/cloudflare';
+import { getAuth } from '@clerk/remix/experimental/ssr.server';
+import { useUser, SignedIn, ClerkLoaded } from '@clerk/remix/experimental';
+
+export const loader: LoaderFunction = async args => {
+  const authState = await getAuth(args);
+  console.log('AuthState', authState);
+  return authState;
+};
+
+export default function Index() {
+  const user = useUser();
+  console.log('User:', user);
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+      <h1>Welcome to Remix</h1>
+      <ul>
+        <li>
+          <a
+            target='_blank'
+            href='https://remix.run/tutorials/blog'
+            rel='noreferrer'
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target='_blank'
+            href='https://remix.run/tutorials/jokes'
+            rel='noreferrer'
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target='_blank'
+            href='https://remix.run/docs'
+            rel='noreferrer'
+          >
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+      <div>
+        <ul>
+          <li>
+            Clerkjs loaded:
+            <ClerkLoaded>
+              <h2>yes</h2>
+            </ClerkLoaded>
+          </li>
+          <li>
+            SignedIn:
+            <SignedIn>signed in data</SignedIn>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
---
+++ playground/remix-node/app/routes/index.tsx
@@ -0,0 +1,60 @@
+import type { LoaderFunction } from '@remix-run/node';
+import { getAuth } from '@clerk/remix/experimental/ssr.server';
+import { useUser, ClerkLoaded, SignedIn } from '@clerk/remix/experimental';
+
+export const loader: LoaderFunction = async args => {
+  return getAuth(args);
+};
+
+export default function Index() {
+  const user = useUser();
+  console.log('User:', user);
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+      <h1>Welcome to Remix</h1>
+      <ul>
+        <li>
+          <a
+            target='_blank'
+            href='https://remix.run/tutorials/blog'
+            rel='noreferrer'
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target='_blank'
+            href='https://remix.run/tutorials/jokes'
+            rel='noreferrer'
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target='_blank'
+            href='https://remix.run/docs'
+            rel='noreferrer'
+          >
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+      <div>
+        <ul>
+          <li>
+            Clerkjs loaded:
+            <ClerkLoaded>
+              <h2>yes</h2>
+            </ClerkLoaded>
+          </li>
+          <li>
+            SignedIn:
+            <SignedIn>Signed In!</SignedIn>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
