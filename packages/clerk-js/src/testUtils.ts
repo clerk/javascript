4f6fd9b84463b74d76641e009d369563b45094ed
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-11-17 14:59:59 +0200

    chore(clerk-js): Rename `user` to `userEvent` in testUtils

--- packages/clerk-js/src/testUtils.ts
+++ packages/clerk-js/src/testUtils.ts
@@ -1,10 +1,10 @@
 import { render as _render, RenderOptions } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import UserEvent from '@testing-library/user-event';
 import React from 'react';
 
 const render = (ui: React.ReactElement, options?: RenderOptions) => {
-  const user = userEvent.setup();
-  return { ..._render(ui, options), user };
+  const userEvent = UserEvent.setup();
+  return { ..._render(ui, { ...options }), userEvent };
 };
 
 export * from './ui/utils/test/createFixture';
