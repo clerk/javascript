fd682c4ae9e1e39d0f17768120b975765cd08010
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-11-21 23:55:48 +0200

    chore(clerk-react): Update jest config

--- packages/react/jest.config.js
+++ packages/react/jest.config.js
@@ -1,38 +1,16 @@
 module.exports = {
-  // The root of your source code, typically /src
-  // `<rootDir>` is a token Jest substitutes
+  displayName: 'clerk-js',
+  injectGlobals: true,
+
   roots: ['<rootDir>/src'],
   testEnvironment: 'jsdom',
-
-  // Global settings
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json',
-    },
-  },
-
   setupFilesAfterEnv: ['<rootDir>../../setupJest.afterEnv.ts'],
 
-  // Coverage
-  collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}'],
-
-  // Jest transformations -- this adds support for TypeScript
-  // using ts-jest
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.m?tsx?$': ['ts-jest', { tsconfig: 'tsconfig.test.json', diagnostics: false }],
   },
 
-  // Test spec file resolution pattern
-  // Matches parent folder `__tests__` and filename
-  // should contain `test` or `spec`.
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-
-  // Module file extensions for importings
-  transformIgnorePatterns: ['^.+\\.module\\.(css|sass|scss)$'],
-
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-  },
-
+  testRegex: ['/ui/.*/__tests__/.*.test.[jt]sx?$', '/(core|utils)/.*.test.[jt]sx?$'],
   testPathIgnorePatterns: ['/node_modules/'],
 };
