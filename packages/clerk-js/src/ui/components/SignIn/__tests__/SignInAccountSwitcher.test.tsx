e94c85c2f834d8893c8116fbac59323eb2f48129
anagstef <anagstef@gmail.com>
2022-11-23 02:39:44 +0200

    test(clerk-js): Add tests for SignInAccountSwitcher

--- packages/clerk-js/src/ui/components/SignIn/__tests__/SignInAccountSwitcher.test.tsx
+++ packages/clerk-js/src/ui/components/SignIn/__tests__/SignInAccountSwitcher.test.tsx
@@ -8,14 +8,67 @@ const { createFixtures } = bindCreateFixtures('SignIn');
 
 describe('SignInAccountSwitcher', () => {
   it('renders the component', async () => {
-    const { wrapper } = await createFixtures(f => {
-      f.withEmailAddress();
-    });
+    const { wrapper } = await createFixtures();
     render(<SignInAccountSwitcher />, { wrapper });
   });
 
-  it.todo('renders a list of buttons with all signed in accounts');
-  it.todo('sets an active session when user clicks an already logged in account from the list');
-  it.todo('navigates to SignInStart component if user clicks on "Add account" button');
-  it.todo('signs out of all accounts and clears the list when user clicks on "Sign out of all accounts"');
+  it('renders a list of buttons with all signed in accounts', async () => {
+    const { wrapper } = await createFixtures(f => {
+      f.withActiveSessions([
+        { first_name: 'Nick', last_name: 'Kouk', identifier: 'test1@clerk.dev' },
+        { first_name: 'Mike', last_name: 'Lamar', identifier: 'test2@clerk.dev' },
+        { first_name: 'Graciela', last_name: 'Brennan', identifier: 'test3@clerk.dev' },
+      ]);
+    });
+    const { getByText } = render(<SignInAccountSwitcher />, { wrapper });
+    expect(getByText('Nick Kouk')).toBeDefined();
+    expect(getByText('Mike Lamar')).toBeDefined();
+    expect(getByText('Graciela Brennan')).toBeDefined();
+  });
+
+  it('sets an active session when user clicks an already logged in account from the list', async () => {
+    const { wrapper, fixtures } = await createFixtures(f => {
+      f.withActiveSessions([
+        { first_name: 'Nick', last_name: 'Kouk', identifier: 'test1@clerk.dev' },
+        { first_name: 'Mike', last_name: 'Lamar', identifier: 'test2@clerk.dev' },
+        { first_name: 'Graciela', last_name: 'Brennan', identifier: 'test3@clerk.dev' },
+      ]);
+    });
+    fixtures.clerk.setActive.mockReturnValueOnce(Promise.resolve());
+    const { userEvent, getByText } = render(<SignInAccountSwitcher />, { wrapper });
+    await userEvent.click(getByText('Nick Kouk'));
+    expect(fixtures.clerk.setActive).toHaveBeenCalled();
+  });
+
+  // this one uses the windowNavigate method. we need to mock it correctly
+  it.skip('navigates to SignInStart component if user clicks on "Add account" button', async () => {
+    const { wrapper, fixtures } = await createFixtures(f => {
+      f.withActiveSessions([
+        { first_name: 'Nick', last_name: 'Kouk', identifier: 'test1@clerk.dev' },
+        { first_name: 'Mike', last_name: 'Lamar', identifier: 'test2@clerk.dev' },
+        { first_name: 'Graciela', last_name: 'Brennan', identifier: 'test3@clerk.dev' },
+      ]);
+    });
+    const { userEvent, getByText } = render(<SignInAccountSwitcher />, { wrapper });
+    await userEvent.click(getByText('Add account'));
+    expect(fixtures.router.navigate).toHaveBeenCalled();
+  });
+
+  it.skip('signs out of all accounts when user clicks on "Sign out of all accounts"', async () => {
+    const { wrapper } = await createFixtures(f => {
+      f.withActiveSessions([
+        { first_name: 'Nick', last_name: 'Kouk', identifier: 'test1@clerk.dev' },
+        { first_name: 'Mike', last_name: 'Lamar', identifier: 'test2@clerk.dev' },
+        { first_name: 'Graciela', last_name: 'Brennan', identifier: 'test3@clerk.dev' },
+      ]);
+    });
+    const { userEvent, getByText } = render(<SignInAccountSwitcher />, { wrapper });
+    expect(getByText('Nick Kouk')).toBeDefined();
+    expect(getByText('Mike Lamar')).toBeDefined();
+    expect(getByText('Graciela Brennan')).toBeDefined();
+    await userEvent.click(getByText('Sign out of all accounts'));
+    expect(getByText('Nick Kouk')).not.toBeDefined();
+    expect(getByText('Mike Lamar')).not.toBeDefined();
+    expect(getByText('Graciela Brennan')).not.toBeDefined();
+  });
 });
