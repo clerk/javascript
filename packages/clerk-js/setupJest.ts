2492f5b19a76c98837b37042fe3a37285e435f7b
George Desipris <george.desipris.56@gmail.com>
2022-11-24 10:55:42 +0200

    test(clerk-js): Tests for RootPage in UserProfile

--- packages/clerk-js/setupJest.ts
+++ packages/clerk-js/setupJest.ts
@@ -21,3 +21,25 @@ Object.defineProperty(window, 'matchMedia', {
     dispatchEvent: jest.fn(),
   })),
 });
+
+//@ts-expect-error
+global.IntersectionObserver = class IntersectionObserver {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
+
+  disconnect() {
+    return null;
+  }
+
+  observe() {
+    return null;
+  }
+
+  takeRecords() {
+    return null;
+  }
+
+  unobserve() {
+    return null;
+  }
+};
