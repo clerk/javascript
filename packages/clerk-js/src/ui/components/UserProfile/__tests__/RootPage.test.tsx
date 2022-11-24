a09c2e0685baa895e5aa46aef117cbc1c8232d65
George Desipris <george.desipris.56@gmail.com>
2022-11-24 15:06:04 +0200

    test(clerk-js): Add overrides for external accounts in fixture helpers

--- packages/clerk-js/src/ui/components/UserProfile/__tests__/RootPage.test.tsx
+++ packages/clerk-js/src/ui/components/UserProfile/__tests__/RootPage.test.tsx
@@ -1,7 +1,8 @@
+import { SessionWithActivitiesResource } from '@clerk/types';
 import { describe, it } from '@jest/globals';
 import React from 'react';
 
-import { bindCreateFixtures, render, screen } from '../../../../testUtils';
+import { bindCreateFixtures, render, screen, waitFor } from '../../../../testUtils';
 import { RootPage } from '../RootPage';
 
 const { createFixtures } = bindCreateFixtures('UserProfile');
@@ -14,6 +15,7 @@ describe('RootPage', () => {
     fixtures.clerk.user!.getSessions.mockReturnValue(Promise.resolve([]));
 
     render(<RootPage />, { wrapper });
+    await waitFor(() => expect(fixtures.clerk.user?.getSessions).toHaveBeenCalled());
   });
 
   describe('Sections', () => {
@@ -24,6 +26,7 @@ describe('RootPage', () => {
       fixtures.clerk.user!.getSessions.mockReturnValue(Promise.resolve([]));
 
       render(<RootPage />, { wrapper });
+      await waitFor(() => expect(fixtures.clerk.user?.getSessions).toHaveBeenCalled());
       screen.getAllByText(/Account/i);
       screen.getAllByText(/Security/i);
     });
@@ -35,6 +38,7 @@ describe('RootPage', () => {
       fixtures.clerk.user!.getSessions.mockReturnValue(Promise.resolve([]));
 
       render(<RootPage />, { wrapper });
+      await waitFor(() => expect(fixtures.clerk.user?.getSessions).toHaveBeenCalled());
       screen.getByText(/Profile/i);
       const button = screen.getByText('George Clerk');
       expect(button.closest('button')).not.toBeNull();
@@ -47,6 +51,7 @@ describe('RootPage', () => {
       fixtures.clerk.user!.getSessions.mockReturnValue(Promise.resolve([]));
 
       render(<RootPage />, { wrapper });
+      await waitFor(() => expect(fixtures.clerk.user?.getSessions).toHaveBeenCalled());
       screen.getByText(/Profile/i);
       const button = screen.getByText('George Clerk');
       expect(button.closest('button')).not.toBeNull();
@@ -65,6 +70,7 @@ describe('RootPage', () => {
       fixtures.clerk.user!.getSessions.mockReturnValue(Promise.resolve([]));
 
       render(<RootPage />, { wrapper });
+      await waitFor(() => expect(fixtures.clerk.user?.getSessions).toHaveBeenCalled());
       screen.getByText(/Email addresses/i);
       const emailButtons: HTMLElement[] = [];
       emails.forEach(email => {
@@ -88,6 +94,7 @@ describe('RootPage', () => {
       fixtures.clerk.user!.getSessions.mockReturnValue(Promise.resolve([]));
 
       render(<RootPage />, { wrapper });
+      await waitFor(() => expect(fixtures.clerk.user?.getSessions).toHaveBeenCalled());
       screen.getByText(/Phone numbers/i);
       const numberButtons: HTMLElement[] = [];
       numbers.forEach(number => {
@@ -96,6 +103,63 @@ describe('RootPage', () => {
       numberButtons.forEach(numberButton => {
         expect(numberButton.closest('button')).not.toBeNull();
       });
+    });
+
+    it('shows the connected accounts of the user and has appropriate buttons', async () => {
+      const { wrapper, fixtures } = await createFixtures(f => {
+        f.withSocialProvider({ provider: 'google' });
+        f.withUser({
+          external_accounts: ['google'],
+          first_name: 'George',
+          last_name: 'Clerk',
+        });
+      });
+      fixtures.clerk.user!.getSessions.mockReturnValue(Promise.resolve([]));
+
+      render(<RootPage />, { wrapper });
+      await waitFor(() => expect(fixtures.clerk.user?.getSessions).toHaveBeenCalled());
+      screen.getByText(/Connected Accounts/i);
+      const externalAccountButton = screen.getByText(/google/i);
+      expect(externalAccountButton.closest('button')).not.toBeNull();
+    });
+
+    it('shows the active devices of the user and has appropriate buttons', async () => {
+      const { wrapper, fixtures } = await createFixtures(f => {
+        f.withSocialProvider({ provider: 'google' });
+        f.withUser({
+          external_accounts: ['google'],
+          first_name: 'George',
+          last_name: 'Clerk',
+        });
+      });
+      fixtures.clerk.user!.getSessions.mockReturnValue(
+        Promise.resolve([
+          {
+            pathRoot: '/me/sessions',
+            id: 'sess_2HyQfBh8wRJUbpvCtPNllWdsHFK',
+            status: 'active',
+            expireAt: '2022-12-01T01:55:44.636Z',
+            abandonAt: '2022-12-24T01:55:44.636Z',
+            lastActiveAt: '2022-11-24T12:11:49.328Z',
+            latestActivity: {
+              id: 'sess_activity_2HyQwElm529O5NDL1KNpJAGWVJZ',
+              deviceType: 'Macintosh',
+              browserName: 'Chrome',
+              browserVersion: '107.0.0.0',
+              country: 'Greece',
+              city: 'Athens',
+              isMobile: false,
+            },
+            actor: null,
+          } as any as SessionWithActivitiesResource,
+        ]),
+      );
+
+      render(<RootPage />, { wrapper });
+      await waitFor(() => expect(fixtures.clerk.user?.getSessions).toHaveBeenCalled());
+      screen.getByText(/Active Devices/i);
+      const externalAccountButton = await screen.findByText(/Macintosh/i);
+      expect(externalAccountButton.closest('button')).not.toBeNull();
     });
   });
 });
