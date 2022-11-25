250f846ab7ca5f25ea36feeb775f616cf079e61f
George Desipris <george.desipris.56@gmail.com>
2022-11-25 06:25:34 +0200

    test(clerk-js): Tests for PhonePage

---
+++ packages/clerk-js/src/ui/components/UserProfile/__tests__/PhonePage.test.tsx
@@ -0,0 +1,80 @@
+import { describe, it } from '@jest/globals';
+import React from 'react';
+
+import { bindCreateFixtures, render, screen } from '../../../../testUtils';
+import { PhonePage } from '../PhonePage';
+
+const { createFixtures } = bindCreateFixtures('UserProfile');
+
+const defaultFixtures = f => {
+  f.withPhoneNumber();
+  f.withUser({ email_addresses: ['test@clerk.dev'] });
+};
+
+describe('PhonePage', () => {
+  it('renders the component', async () => {
+    const { wrapper } = await createFixtures(f => {
+      defaultFixtures(f);
+    });
+
+    render(<PhonePage />, { wrapper });
+  });
+
+  it('shows the title', async () => {
+    const { wrapper } = await createFixtures(f => {
+      defaultFixtures(f);
+    });
+
+    render(<PhonePage />, { wrapper });
+
+    screen.getByRole('heading', { name: /add phone number/i });
+  });
+
+  describe('Inputs', () => {
+    it('shows the input field for the new phone number', async () => {
+      const { wrapper } = await createFixtures(f => {
+        defaultFixtures(f);
+      });
+
+      render(<PhonePage />, { wrapper });
+
+      screen.getByLabelText(/phone number/i);
+    });
+  });
+
+  describe('Form buttons', () => {
+    it('navigates to the root page upon pressing cancel', async () => {
+      const { wrapper, fixtures } = await createFixtures(f => {
+        defaultFixtures(f);
+      });
+
+      const { userEvent } = render(<PhonePage />, { wrapper });
+
+      await userEvent.click(screen.getByRole('button', { name: /cancel/i }));
+      expect(fixtures.router.navigate).toHaveBeenCalledWith('/');
+    });
+
+    it('continue button is disabled by default', async () => {
+      const { wrapper } = await createFixtures(f => {
+        defaultFixtures(f);
+      });
+      render(<PhonePage />, { wrapper });
+
+      expect(screen.getByText(/continue/i, { exact: false }).closest('button')).toHaveAttribute('disabled');
+    });
+
+    it('calls the appropriate function if continue is pressed', async () => {
+      const { wrapper, fixtures } = await createFixtures(f => {
+        defaultFixtures(f);
+      });
+      fixtures.clerk.user!.createPhoneNumber.mockReturnValueOnce(Promise.resolve({} as any));
+      const { userEvent } = render(<PhonePage />, { wrapper });
+
+      await userEvent.type(screen.getByLabelText(/phone number/i), '6911111111');
+      await userEvent.click(screen.getByText(/continue/i));
+      expect(fixtures.clerk.user?.createPhoneNumber).toHaveBeenCalledWith({ phoneNumber: '+16911111111' }); //default is +1
+    });
+  });
+
+  it.todo('Test for verification of added phone number');
+});
