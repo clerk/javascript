6b2eccd1baeb2f1716b57fdbc1b657f952e56cb0
anagstef <anagstef@gmail.com>
2022-12-01 17:53:38 +0200

    test(clerk-js): Add tests for new date related modifiers

--- packages/clerk-js/src/ui/localization/__tests__/applyTokensToString.test.ts
+++ packages/clerk-js/src/ui/localization/__tests__/applyTokensToString.test.ts
@@ -33,15 +33,25 @@ describe('applyTokensToString', function () {
   });
 
   describe('Date related tokens and modifiers', () => {
+    beforeEach(() => {
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      jest.spyOn(console, 'warn').mockImplementation(() => {});
+    });
+
     const tokens = {
       date: new Date('2021-12-31T22:00:00.000Z'),
       dateString: '2021-12-31T22:00:00.000Z',
       dateNumber: 1640988000000,
+      errorString: 'test_error_case',
     };
 
     const cases = [
       ["Last {{ date | weekday('en-US','long') }} at {{ date | timeString('en-US') }}", 'Last Friday at 10:00 PM'],
-      ["Last {{ date | weekday('fr-FR','long') }} at {{ date | timeString('fr-FR') }}", 'Last vendredi at 10:00 PM'],
+      ["Last {{ date | weekday('fr-FR','long') }} at {{ date | timeString('fr-FR') }}", 'Last vendredi at 22:00'],
+      [
+        "Last {{ date | weekday('fr-FR','long') | titleize }} at {{ date | timeString('fr-FR') }}",
+        'Last Vendredi at 22:00',
+      ],
       [
         "Προηγούμενη {{ date | weekday('el-GR','long') }} στις {{ date | timeString('el-GR') }}",
         'Προηγούμενη Παρασκευή στις 10:00 μ.μ.',
@@ -63,6 +73,61 @@ describe('applyTokensToString', function () {
 
     it.each(cases)('.applyTokensToString(%s, tokens) => %s', (input, expected) => {
       expect(applyTokensToString(input, tokens as any)).toEqual(expected);
+    });
+
+    describe('Modifiers', () => {
+      describe('weekday', () => {
+        const cases = [
+          ['{{ date | weekday }}', 'Friday'],
+          ['{{ dateString | weekday }}', 'Friday'],
+          ['{{ dateNumber | weekday }}', 'Friday'],
+          ['{{ date | weekday("en-US") }}', 'Friday'],
+          ['{{ date | weekday("fr-FR") }}', 'vendredi'],
+          ['{{ date | weekday("fr-FR") | titleize }}', 'Vendredi'],
+          ['{{ date | weekday("en-US", "long") }}', 'Friday'],
+          ['{{ date | weekday("en-US", "short") }}', 'Fri'],
+          ['{{ date | weekday("en-US", "narrow") }}', 'F'],
+          ['{{ date | weekday("fr-FR", "narrow") }}', 'V'],
+          ['{{ errorString | weekday("en-US") }}', ''],
+        ];
+
+        it.each(cases)('.applyTokensToString(%s, tokens) => %s', (input, expected) => {
+          expect(applyTokensToString(input, tokens as any)).toEqual(expected);
+        });
+      });
+
+      describe('timeString', () => {
+        const cases = [
+          ['{{ date | timeString }}', '10:00 PM'],
+          ['{{ dateString | timeString }}', '10:00 PM'],
+          ['{{ dateNumber | timeString }}', '10:00 PM'],
+          ['{{ date | timeString("en-US") }}', '10:00 PM'],
+          ['{{ date | timeString("el-GR") }}', '10:00 μ.μ.'],
+          ['{{ date | timeString("el-GR") | titleize }}', '10:00 μ.μ.'],
+          ['{{ date | timeString("fr-FR") }}', '22:00'],
+          ['{{ errorString | timeString("en-US") }}', ''],
+        ];
+
+        it.each(cases)('.applyTokensToString(%s, tokens) => %s', (input, expected) => {
+          expect(applyTokensToString(input, tokens as any)).toEqual(expected);
+        });
+      });
+
+      describe('numeric', () => {
+        const cases = [
+          ['{{ date | numeric }}', '12/31/2021'],
+          ['{{ dateString | numeric }}', '12/31/2021'],
+          ['{{ dateNumber | numeric }}', '12/31/2021'],
+          ['{{ date | numeric("en-US") }}', '12/31/2021'],
+          ['{{ date | numeric("fr-FR") }}', '31/12/2021'],
+          ['{{ date | numeric("fr-FR") | titleize }}', '31/12/2021'],
+          ['{{ errorString | numeric("en-US") }}', ''],
+        ];
+
+        it.each(cases)('.applyTokensToString(%s, tokens) => %s', (input, expected) => {
+          expect(applyTokensToString(input, tokens as any)).toEqual(expected);
+        });
+      });
     });
   });
 });
