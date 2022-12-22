e1d1e3df5f86a6fa5aa7476796cf7649c6ae3395
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-12-22 14:35:31 +0200

    fix(backend,remix): Replace default with a named Clerk export

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -8,12 +8,9 @@ export * from './tokens/verify';
 
 export type ClerkOptions = CreateBackendApiOptions & Pick<CreateAuthenticateRequestOptions, 'jwtKey'>;
 
-export default function Clerk(options: ClerkOptions) {
+export function Clerk(options: ClerkOptions) {
   const apiClient = createBackendApiClient(options);
   const requestState = createAuthenticateRequest({ ...options, apiClient });
 
-  return {
-    ...apiClient,
-    ...requestState,
-  };
+  return { ...apiClient, ...requestState };
 }
