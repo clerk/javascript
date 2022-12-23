99888d6860a7dbacb22911d1005fddf8e274c1ce
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-12-23 04:39:35 +0200

    fix(nextjs): Fix default Clerk import

--- packages/nextjs/src/server/clerk.ts
+++ packages/nextjs/src/server/clerk.ts
@@ -1,4 +1,4 @@
-import Clerk from '@clerk/backend';
+import { Clerk } from '@clerk/backend';
 
 export const API_URL = process.env.CLERK_API_URL || 'https://api.clerk.dev';
 export const API_VERSION = process.env.CLERK_API_VERSION || 'v1';
