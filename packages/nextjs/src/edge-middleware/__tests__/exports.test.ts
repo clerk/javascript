8b77b30f530de404a0f995c7b32f221ed6b1682e
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-12-23 04:17:41 +0200

    chore(nextjs): Add snapshop testing for public exports

---
+++ packages/nextjs/src/edge-middleware/__tests__/exports.test.ts
@@ -0,0 +1,7 @@
+import * as publicExports from '../index';
+
+describe('/edge-middleware public exports', () => {
+  it('should not include a breaking change', () => {
+    expect(publicExports).toMatchSnapshot();
+  });
+});
---
+++ packages/nextjs/src/server/__tests__/exports.test.ts
@@ -0,0 +1,7 @@
+// import * as publicExports from '../index';
+
+describe('/server public exports', () => {
+  it.skip('should not include a breaking change', () => {
+    // expect(publicExports).toMatchSnapshot();
+  });
+});
