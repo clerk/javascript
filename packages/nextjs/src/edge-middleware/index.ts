c11fd91097afb2f58802e70c347c26c57e2fde96
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-01-02 00:39:04 +0200

    fix(nextjs): Replace clerk-sdk-node with backend in /edge-middleware

--- packages/nextjs/src/edge-middleware/index.ts
+++ packages/nextjs/src/edge-middleware/index.ts
@@ -1,1 +1,8 @@
-export * from '@clerk/edge/vercel-edge';
+import { requireAuth, withAuth } from '../api';
+import { clerkClient } from '../server';
+
+export * from '../api';
+export { clerkClient };
+export const clerkApi = clerkClient;
+export const requireEdgeMiddlewareAuth = requireAuth;
+export const withEdgeMiddlewareAuth = withAuth;
