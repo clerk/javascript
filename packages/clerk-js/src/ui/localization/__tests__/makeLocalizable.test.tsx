ae3d4a13608a50a5d7b6f0f4fba9af1296b4ff9c
George Desipris <george.desipris.56@gmail.com>
2023-01-04 17:50:01 +0200

    test(clerk-js): Tests for makeLocalizable

--- packages/clerk-js/src/ui/localization/__tests__/makeLocalizable.test.tsx
+++ packages/clerk-js/src/ui/localization/__tests__/makeLocalizable.test.tsx
@@ -46,4 +46,49 @@ describe('Test localizable components', () => {
       screen.getByText(localizedValue);
     },
   );
+
+  it.each(localizableElements)('$name renders the children if no localization key is provided', async ({ el: El }) => {
+    const { wrapper } = await createFixtures();
+
+    render(<El>test</El>, { wrapper });
+
+    screen.getByText('test');
+  });
+
+  it.each(localizableElements)(
+    '$name only renders the localization value if both children and key are provided',
+    async ({ el: El }) => {
+      const { wrapper } = await createFixtures();
+
+      render(<El localizationKey={localizationKeys('backButton')}>test</El>, { wrapper });
+
+      const { result } = renderHook(() => useLocalizations(), { wrapper });
+
+      const localizedValue = result.current.t(localizationKeys('backButton'));
+
+      screen.getByText(localizedValue);
+    },
+  );
+
+  it.each(localizableElements)('$name renders the global token if provided with one', async ({ el: El }) => {
+    const { wrapper } = await createFixtures();
+
+    render(<El localizationKey={localizationKeys('socialButtonsBlockButton', { provider: 'test_provider' })} />, {
+      wrapper,
+    });
+
+    screen.getByText(`Continue with Test_provider`); // this key makes use of titleize
+  });
+
+  it.each(localizableElements)('$name renders the global date token if provided with one', async ({ el: El }) => {
+    const { wrapper } = await createFixtures();
+
+    const date = new Date('11/12/1999');
+
+    render(<El localizationKey={localizationKeys('dates.numeric', { date })} />, {
+      wrapper,
+    });
+
+    screen.getByText('11/12/1999'); // this key makes use of numeric('en-US')
+  });
 });
