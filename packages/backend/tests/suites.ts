14a680f1695ad9ad46271da0424ee12a2608c325
Dimitris <dimitris.klouvas@gmail.com>
2023-01-09 14:21:52 +0200

    fix(backend): Update tokens/jwt old tests

--- packages/backend/tests/suites.ts
+++ packages/backend/tests/suites.ts
@@ -5,10 +5,11 @@ import requestTest from './dist/tokens/request.test.js';
 import keysTest from './dist/tokens/keys.test.js';
 import pathTest from './dist/util/path.test.js';
 import verifyTest from './dist/tokens/verify.test.js';
+import jwtTest from './dist/tokens/jwt.test.js';
 
 import utilRequestTest from './dist/util/request.test.js';
 
 // Add them to the suite array
-const suites = [apiTest, requestTest, utilRequestTest, keysTest, verifyTest, pathTest];
+const suites = [apiTest, requestTest, utilRequestTest, keysTest, verifyTest, pathTest, jwtTest];
 
 export default suites;
