2553c23d10c61e71e1c48740f9ab442e7be2f398
Dimitris <dimitris.klouvas@gmail.com>
2023-01-09 11:14:36 +0200

    fix(backend): Update tokens/key old tests

--- packages/backend/src/tokens/fixtures.ts
+++ packages/backend/src/tokens/fixtures.ts
@@ -31,3 +31,25 @@ export const mockRsaJwk = {
 export const mockJwks = {
   keys: [mockRsaJwk],
 };
+
+export const mockPEMKey =
+  'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8Z1oLQbaYkakUSIYRvjmOoeXMDFFjynGP2+gVy0mQJHYgVhgo34RsQgZoz7rSNm/EOL+l/mHTqQAhwaf9Ef8X5vsPX8vP3RNRRm3XYpbIGbOcANJaHihJZwnzG9zIGYF8ki+m55zftO7pkOoXDtIqCt+5nIUQjGJK5axFELrnWaz2qcR03A7rYKQc3F1gut2Ru1xfmiJVUlQe0tLevQO/FzfYpWu7+691q+ZRUGxWvGc0ays4ACa7JXElCIKXRv/yb3Vc1iry77HRAQ28J7Fqpj5Cb+sxfFI+Vhf1GB1bNeOLPR10nkSMJ74HB0heHi/SsM83JiGekv0CpZPCC8jcQIDAQAB';
+
+export const mockPEMJwk = {
+  kid: 'local',
+  kty: 'RSA',
+  alg: 'RS256',
+  n: '8Z1oLQbaYkakUSIYRvjmOoeXMDFFjynGP2-gVy0mQJHYgVhgo34RsQgZoz7rSNm_EOL-l_mHTqQAhwaf9Ef8X5vsPX8vP3RNRRm3XYpbIGbOcANJaHihJZwnzG9zIGYF8ki-m55zftO7pkOoXDtIqCt-5nIUQjGJK5axFELrnWaz2qcR03A7rYKQc3F1gut2Ru1xfmiJVUlQe0tLevQO_FzfYpWu7-691q-ZRUGxWvGc0ays4ACa7JXElCIKXRv_yb3Vc1iry77HRAQ28J7Fqpj5Cb-sxfFI-Vhf1GB1bNeOLPR10nkSMJ74HB0heHi_SsM83JiGekv0CpZPCC8jcQ',
+  e: 'AQAB',
+};
+
+export const mockPEMJwtKey =
+  '-----BEGIN PUBLIC KEY-----\n' +
+  'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8Z1oLQbaYkakUSIYRvjm\n' +
+  'OoeXMDFFjynGP2+gVy0mQJHYgVhgo34RsQgZoz7rSNm/EOL+l/mHTqQAhwaf9Ef8\n' +
+  'X5vsPX8vP3RNRRm3XYpbIGbOcANJaHihJZwnzG9zIGYF8ki+m55zftO7pkOoXDtI\n' +
+  'qCt+5nIUQjGJK5axFELrnWaz2qcR03A7rYKQc3F1gut2Ru1xfmiJVUlQe0tLevQO\n' +
+  '/FzfYpWu7+691q+ZRUGxWvGc0ays4ACa7JXElCIKXRv/yb3Vc1iry77HRAQ28J7F\n' +
+  'qpj5Cb+sxfFI+Vhf1GB1bNeOLPR10nkSMJ74HB0heHi/SsM83JiGekv0CpZPCC8j\n' +
+  'cQIDAQAB\n' +
+  '-----END PUBLIC KEY-----';
--- packages/backend/src/tokens/keys.test.ts
+++ packages/backend/src/tokens/keys.test.ts
@@ -1,30 +1,14 @@
 import type QUnit from 'qunit';
+import sinon from 'sinon';
 
+import runtime from '../runtime';
+import { jsonError, jsonOk } from '../util/mockFetch';
 import { TokenVerificationError, TokenVerificationErrorAction, TokenVerificationErrorReason } from './errors';
-import { loadClerkJWKFromLocal } from './keys';
+import { mockJwks, mockJwtPayload, mockRsaJwk, mockRsaJwkKid, mockPEMKey, mockPEMJwtKey, mockPEMJwk } from './fixtures';
+import { loadClerkJWKFromLocal, loadClerkJWKFromRemote } from './keys';
 
 export default (QUnit: QUnit) => {
   const { module, test } = QUnit;
-
-  const mockJWTKey =
-    'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8Z1oLQbaYkakUSIYRvjmOoeXMDFFjynGP2+gVy0mQJHYgVhgo34RsQgZoz7rSNm/EOL+l/mHTqQAhwaf9Ef8X5vsPX8vP3RNRRm3XYpbIGbOcANJaHihJZwnzG9zIGYF8ki+m55zftO7pkOoXDtIqCt+5nIUQjGJK5axFELrnWaz2qcR03A7rYKQc3F1gut2Ru1xfmiJVUlQe0tLevQO/FzfYpWu7+691q+ZRUGxWvGc0ays4ACa7JXElCIKXRv/yb3Vc1iry77HRAQ28J7Fqpj5Cb+sxfFI+Vhf1GB1bNeOLPR10nkSMJ74HB0heHi/SsM83JiGekv0CpZPCC8jcQIDAQAB';
-  const mockJWTKeyPEM =
-    '-----BEGIN PUBLIC KEY-----\n' +
-    'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8Z1oLQbaYkakUSIYRvjm\n' +
-    'OoeXMDFFjynGP2+gVy0mQJHYgVhgo34RsQgZoz7rSNm/EOL+l/mHTqQAhwaf9Ef8\n' +
-    'X5vsPX8vP3RNRRm3XYpbIGbOcANJaHihJZwnzG9zIGYF8ki+m55zftO7pkOoXDtI\n' +
-    'qCt+5nIUQjGJK5axFELrnWaz2qcR03A7rYKQc3F1gut2Ru1xfmiJVUlQe0tLevQO\n' +
-    '/FzfYpWu7+691q+ZRUGxWvGc0ays4ACa7JXElCIKXRv/yb3Vc1iry77HRAQ28J7F\n' +
-    'qpj5Cb+sxfFI+Vhf1GB1bNeOLPR10nkSMJ74HB0heHi/SsM83JiGekv0CpZPCC8j\n' +
-    'cQIDAQAB\n' +
-    '-----END PUBLIC KEY-----';
-  const expectedJWK = {
-    kid: 'local',
-    kty: 'RSA',
-    alg: 'RS256',
-    n: '8Z1oLQbaYkakUSIYRvjmOoeXMDFFjynGP2-gVy0mQJHYgVhgo34RsQgZoz7rSNm_EOL-l_mHTqQAhwaf9Ef8X5vsPX8vP3RNRRm3XYpbIGbOcANJaHihJZwnzG9zIGYF8ki-m55zftO7pkOoXDtIqCt-5nIUQjGJK5axFELrnWaz2qcR03A7rYKQc3F1gut2Ru1xfmiJVUlQe0tLevQO_FzfYpWu7-691q-ZRUGxWvGc0ays4ACa7JXElCIKXRv_yb3Vc1iry77HRAQ28J7Fqpj5Cb-sxfFI-Vhf1GB1bNeOLPR10nkSMJ74HB0heHi_SsM83JiGekv0CpZPCC8jcQ',
-    e: 'AQAB',
-  };
 
   module('tokens.loadClerkJWKFromLocal(localKey)', () => {
     test('throws an error if no key has been provided', assert => {
@@ -39,13 +23,139 @@ export default (QUnit: QUnit) => {
     });
 
     test('loads the local key', assert => {
-      const jwk = loadClerkJWKFromLocal(mockJWTKey);
-      assert.propContains(jwk, expectedJWK);
+      const jwk = loadClerkJWKFromLocal(mockPEMKey);
+      assert.propContains(jwk, mockPEMJwk);
     });
 
     test('loads the local key in PEM format', assert => {
-      const jwk = loadClerkJWKFromLocal(mockJWTKeyPEM);
-      assert.propContains(jwk, expectedJWK);
+      const jwk = loadClerkJWKFromLocal(mockPEMJwtKey);
+      assert.propContains(jwk, mockPEMJwk);
+    });
+  });
+
+  module('tokens.loadClerkJWKFromRemote(options)', hooks => {
+    let fakeClock;
+    let fakeFetch;
+    hooks.beforeEach(() => {
+      fakeClock = sinon.useFakeTimers(new Date(mockJwtPayload.iat * 1000).getTime());
+      fakeFetch = sinon.stub(runtime, 'fetch');
+    });
+    hooks.afterEach(() => {
+      fakeClock.restore();
+      fakeFetch.restore();
+      sinon.restore();
+    });
+
+    test('loads JWKS from Backend API when apiUrl and apiKey are provided', async assert => {
+      const jwk = await loadClerkJWKFromRemote({
+        apiUrl: 'https://api.clerk.test',
+        apiKey: 'deadbeef',
+        kid: mockRsaJwkKid,
+      });
+      assert.propEqual(jwk, mockRsaJwk);
+    });
+
+    test('loads JWKS from Frontend API when issuer is provided', async assert => {
+      const jwk = await loadClerkJWKFromRemote({
+        issuer: 'https://accounts.inspired.puma-74.lcl.dev',
+        kid: mockRsaJwkKid,
+      });
+      assert.propEqual(jwk, mockRsaJwk);
+    });
+
+    test('caches JWK by kid', async assert => {
+      fakeFetch.onCall(0).returns(jsonOk(mockJwks));
+      let jwk = await loadClerkJWKFromRemote({
+        apiUrl: 'https://api.clerk.test',
+        apiKey: 'deadbeef',
+        kid: mockRsaJwkKid,
+        skipJwksCache: true,
+      });
+      assert.propEqual(jwk, mockRsaJwk);
+      jwk = await loadClerkJWKFromRemote({
+        apiUrl: 'https://api.clerk.test',
+        apiKey: 'deadbeef',
+        kid: mockRsaJwkKid,
+      });
+      assert.propEqual(jwk, mockRsaJwk);
+    });
+
+    test('retries five times with exponential back-off policy to fetch JWKS before it fails', async assert => {
+      // TODO: remove sinon.restore & fakeFetch.restore
+      // currently it seems that the fetch stub is stuck and does not resolve with retries
+      fakeFetch.restore();
+      sinon.restore();
+      fakeFetch = sinon.stub(runtime, 'fetch');
+
+      fakeFetch.onCall(0).returns(jsonError('something awful happened', 503));
+      fakeFetch.onCall(1).returns(jsonError('server error'));
+      fakeFetch.onCall(2).returns(jsonError('server error'));
+      fakeFetch.onCall(3).returns(jsonError('server error'));
+      fakeFetch.onCall(4).returns(jsonError('Connection to the origin web server failed', 542));
+
+      try {
+        await loadClerkJWKFromRemote({
+          apiUrl: 'https://api.clerk.test',
+          apiKey: 'deadbeef',
+          kid: 'ins_whatever',
+          skipJwksCache: true,
+        });
+        assert.false(true);
+      } catch (err) {
+        if (err instanceof Error) {
+          assert.propEqual(err, {
+            reason: 'jwk-remote-failed-to-load',
+            action: 'Contact support@clerk.dev',
+          });
+        } else {
+          // This should never be reached. If it does, the suite should fail
+          assert.false(true);
+        }
+      }
+      assert.equal(fakeFetch.callCount, 5);
+    });
+
+    test('throws an error when JWKS can not be fetched from Backend or Frontend API', async assert => {
+      try {
+        await loadClerkJWKFromRemote({
+          apiKey: 'deadbeef',
+          kid: 'ins_whatever',
+        });
+        assert.false(true);
+      } catch (err) {
+        if (err instanceof Error) {
+          assert.propEqual(err, {
+            reason: 'jwk-remote-failed-to-load',
+            action: 'Contact support@clerk.dev',
+          });
+        } else {
+          // This should never be reached. If it does, the suite should fail
+          assert.false(true);
+        }
+      }
+    });
+
+    test('throws an error when no JWK matches the provided kid', async assert => {
+      fakeFetch.onCall(0).returns(jsonOk(mockJwks));
+
+      try {
+        await loadClerkJWKFromRemote({
+          apiUrl: 'https://api.clerk.test',
+          apiKey: 'deadbeef',
+          kid: 'ins_whatever',
+        });
+        assert.false(true);
+      } catch (err) {
+        if (err instanceof Error) {
+          assert.propEqual(err, {
+            reason: 'jwk-remote-missing',
+            action: 'Contact support@clerk.dev',
+          });
+        } else {
+          // This should never be reached. If it does, the suite should fail
+          assert.false(true);
+        }
+      }
     });
   });
 };
