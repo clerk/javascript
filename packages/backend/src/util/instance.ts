f428a631439fd80cd9319518705324b2b9e21b26
Agis Anastasopoulos <agis@clerk.dev>
2023-01-13 16:28:14 +0200

    fix(backend,clerk-react): Staging detection for new hosts
    The new FAPI/Accounts hosts when publishable key is used, end in
    accountsstage.dev. Therefore we fix the interstitial to properly detect
    that we're on staging and load the appropriate version, instead of using
    `latest`.
    
    Resolves AUTH-127

--- packages/backend/src/util/instance.ts
+++ packages/backend/src/util/instance.ts
@@ -10,6 +10,7 @@ export function isStaging(frontendApi: string): boolean {
   return (
     frontendApi.endsWith('.lclstage.dev') ||
     frontendApi.endsWith('.stgstage.dev') ||
-    frontendApi.endsWith('.clerkstage.dev')
+    frontendApi.endsWith('.clerkstage.dev') ||
+    frontendApi.endsWith('.accountsstage.dev')
   );
 }
