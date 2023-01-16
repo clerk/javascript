b44ba8c59aef4d367be3fa7fac10bd00e078520f
anagstef <anagstef@gmail.com>
2023-01-16 13:15:54 +0200

    fix(nextjs): Populate Frontend Api Url when Publishable Key is used

--- packages/nextjs/src/server/clerk.ts
+++ packages/nextjs/src/server/clerk.ts
@@ -1,10 +1,12 @@
 import { Clerk } from '@clerk/backend';
+import { parsePublishableKey } from '@clerk/shared';
 
 export const API_URL = process.env.CLERK_API_URL || 'https://api.clerk.dev';
 export const API_VERSION = process.env.CLERK_API_VERSION || 'v1';
 export const API_KEY = process.env.CLERK_SECRET_KEY || process.env.CLERK_API_KEY || '';
-export const FRONTEND_API = process.env.NEXT_PUBLIC_CLERK_FRONTEND_API || '';
 export const PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || '';
+export const FRONTEND_API =
+  parsePublishableKey(PUBLISHABLE_KEY)?.frontendApi || process.env.NEXT_PUBLIC_CLERK_FRONTEND_API || '';
 
 const clerkClient = Clerk({
   apiKey: API_KEY,
