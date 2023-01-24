d9169ab4873e1745d7250628f5bf8c8f7da49421
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-01-24 03:23:55 +0200

    fix(backend,clerk-sdk-node,shared): Drop support for NodeJS 12

--- packages/backend/src/runtime/node/fetch.js
+++ packages/backend/src/runtime/node/fetch.js
@@ -1,14 +1,2 @@
-let fetch;
-try {
-  // Pre-node14 runtimes do not support the node: prefix
-  // this package uses internally, and they throw.
-  // This package is preferred as it does not polyfill the native fetch
-  // if it's supported by the runtime
-  // https://github.com/node-fetch/node-fetch/issues/1367
-  fetch = require('node-fetch-native');
-} catch (e) {
-  // Otherwise, we will fall back to this lib
-  fetch = require('node-fetch');
-}
-
+const fetch = require('node-fetch-native');
 module.exports = fetch;
