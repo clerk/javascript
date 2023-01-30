fed661bbb3351cb0d1d1f6145c4f2a2524ef04f7
anagstef <anagstef@gmail.com>
2023-01-30 21:21:24 +0200

    fix(clerk-js,shared): Add extra checks before throwing error on ClerkJS initialization
    This commit adds extra checks when catching an error before bubbling it up to the user. The error gets muted if ClerkJS is run by an internet robot/crawler, or WebDriver flag is on, or user agent is not online, based on the experimental Navigator property `connection`.

--- packages/shared/src/utils/browser.ts
+++ packages/shared/src/utils/browser.ts
@@ -1,3 +1,62 @@
 export function inBrowser(): boolean {
   return typeof window !== 'undefined';
 }
+
+export function detectUserAgentRobot(userAgent: string): boolean {
+  const robots = new RegExp(
+    (
+      [
+        /bot/,
+        /spider/,
+        /crawl/,
+        /APIs-Google/,
+        /AdsBot/,
+        /Googlebot/,
+        /mediapartners/,
+        /Google Favicon/,
+        /FeedFetcher/,
+        /Google-Read-Aloud/,
+        /DuplexWeb-Google/,
+        /googleweblight/,
+        /bing/,
+        /yandex/,
+        /baidu/,
+        /duckduck/,
+        /yahoo/,
+        /ecosia/,
+        /ia_archiver/,
+        /facebook/,
+        /instagram/,
+        /pinterest/,
+        /reddit/,
+        /slack/,
+        /twitter/,
+        /whatsapp/,
+        /youtube/,
+        /semrush/,
+      ] as RegExp[]
+    )
+      .map(r => r.source)
+      .join('|'),
+    'i',
+  );
+
+  return robots.test(userAgent);
+}
+
+export function isValidBrowserOnline(): boolean {
+  const navigator = window?.navigator;
+  if (!inBrowser() || !navigator) {
+    return false;
+  }
+
+  const isUserAgentRobot = detectUserAgentRobot(navigator?.userAgent);
+  const isWebDriver = navigator?.webdriver;
+
+  // Being extra safe with the experimental `connection` property, as it is not defined in all browsers
+  // https://developer.mozilla.org/en-US/docs/Web/API/Navigator/connection#browser_compatibility
+  // @ts-ignore
+  const isUserAgentOnline = navigator?.connection?.rtt !== 0 && navigator?.connection?.downlink !== 0;
+
+  return !isUserAgentRobot && !isWebDriver && isUserAgentOnline;
+}
