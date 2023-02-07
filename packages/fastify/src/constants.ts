4957a23f8ca34d88d2aa63a4551b88ff5b3dcee9
Dimitris <dimitris.klouvas@gmail.com>
2023-02-07 12:03:26 +0200

    fix(fastify): Add deprecation warning for API_KEY & FRONTEND_API

--- packages/fastify/src/constants.ts
+++ packages/fastify/src/constants.ts
@@ -2,8 +2,15 @@ import { constants } from '@clerk/backend';
 
 export const API_URL = process.env.CLERK_API_URL || 'https://api.clerk.dev';
 export const API_VERSION = process.env.CLERK_API_VERSION || 'v1';
+/**
+ * Backend API key
+ * @deprecated Use `CLERK_SECRET_KEY` instead.
+ */
 export const API_KEY = process.env.CLERK_API_KEY || '';
 export const SECRET_KEY = process.env.CLERK_SECRET_KEY || '';
+/**
+ * @deprecated Use `CLERK_PUBLISHABLE_KEY` instead.
+ */
 export const FRONTEND_API = process.env.CLERK_FRONTEND_API || '';
 export const PUBLISHABLE_KEY = process.env.CLERK_PUBLISHABLE_KEY || '';
 export const JWT_KEY = process.env.CLERK_JWT_KEY || '';
