697b05a57c605cb6688fccc2724fad44413a98aa
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-02-10 15:16:47 +0200

    fix(fastify): Remove polyfillServerResponseMethods
    There is no need to polyfill the fastify response object, as we no longer depend on clerk-sdk-go. All response-related operations are performed using the reply helper from fastify, on the fastify level.

--- packages/fastify/src/clerkPlugin.ts
+++ packages/fastify/src/clerkPlugin.ts
@@ -1,12 +1,10 @@
 import type { FastifyInstance, FastifyPluginCallback } from 'fastify';
 import fp from 'fastify-plugin';
 
-import { polyfillServerResponseMethods } from './polyfill';
 import type { ClerkFastifyOptions } from './types';
 import { withClerkMiddleware } from './withClerkMiddleware';
 
 const plugin: FastifyPluginCallback = (instance: FastifyInstance, opts: ClerkFastifyOptions, done) => {
-  polyfillServerResponseMethods(instance);
   instance.decorateRequest('auth', null);
   // run clerk as a middleware to all scoped routes
   instance.addHook('preHandler', withClerkMiddleware(opts));
