2aeb68dbfd38c319767409d8bafadf178edede25
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-02-14 13:20:31 +0200

    fix(types): Simplify InitialState type

--- packages/types/src/ssr.ts
+++ packages/types/src/ssr.ts
@@ -1,7 +1,10 @@
-import type { OrganizationJSON, SessionJSON, UserJSON } from './json';
 import type { ActJWTClaim, ClerkJWTClaims } from './jwt';
+import type { ActClaim, JwtPayload } from './jwtv2';
+import type { OrganizationResource } from './organization';
+import type { MembershipRole } from './organizationMembership';
 import type { SessionResource } from './session';
 import type { UserResource } from './user';
+import type { Serializable } from './utils';
 
 export type ServerGetTokenOptions = { template?: string };
 export type ServerGetToken = (options?: ServerGetTokenOptions) => Promise<string | null>;
@@ -17,39 +20,16 @@ export type ServerSideAuth = {
   claims: ClerkJWTClaims | null;
 };
 
-type SsrSessionState<SessionType> =
-  | {
-      sessionId: null;
-      session: null;
-    }
-  | {
-      sessionId: string;
-      session: SessionType | undefined;
-    };
-
-type SsrOrganizationState<OrganizationType> =
-  | { organization: null; organizationId: null }
-  | { organization: OrganizationType | undefined; organizationId: string };
-
-type SsrUserState<UserType> =
-  | {
-      userId: null;
-      user: null;
-    }
-  | {
-      userId: string;
-      user: UserType | undefined;
-    };
-
-export type SsrAuthData = SsrSessionState<SessionResource> & SsrUserState<UserResource>;
-export type ClerkSsrState = SsrSessionState<SessionJSON> & SsrUserState<UserJSON>;
-export type InitialState =
-  | {
-      user: undefined;
-      userId: undefined;
-      session: undefined;
-      sessionId: undefined;
-      organizationId: undefined;
-      organization: undefined;
-    }
-  | (SsrSessionState<SessionJSON> & SsrUserState<UserJSON> & SsrOrganizationState<OrganizationJSON>);
+export type InitialState = Serializable<{
+  sessionClaims: JwtPayload;
+  sessionId: string;
+  session: SessionResource | undefined;
+  actor: ActClaim | undefined;
+  userId: string;
+  user: UserResource | undefined;
+  orgId: string | undefined;
+  orgRole: MembershipRole | undefined;
+  orgSlug: string | undefined;
+  organization: OrganizationResource | undefined;
+  getToken: ServerGetToken;
+}>;
