737b0e91d863744ebd52ac8fba145663a9c8545b
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-02-14 13:21:36 +0200

    fix(clerk-react): Make deriveState the single source of truth for ClerkContextProvider

--- packages/react/src/contexts/ClerkContextProvider.tsx
+++ packages/react/src/contexts/ClerkContextProvider.tsx
@@ -1,17 +1,9 @@
-import type {
-  ActiveSessionResource,
-  ClientResource,
-  InitialState,
-  OrganizationInvitationResource,
-  OrganizationMembershipResource,
-  OrganizationResource,
-  Resources,
-  UserResource,
-} from '@clerk/types';
+import type { ClientResource, InitialState, Resources } from '@clerk/types';
 import React from 'react';
 
 import IsomorphicClerk from '../isomorphicClerk';
 import type { IsomorphicClerkOptions } from '../types';
+import { deriveState } from '../utils/deriveState';
 import { AuthContext } from './AuthContext';
 import { ClientContext } from './ClientContext';
 import { IsomorphicClerkContext } from './IsomorphicClerkContext';
@@ -25,7 +17,7 @@ type ClerkContextProvider = {
   children: React.ReactNode;
 };
 
-type ClerkContextProviderState = Resources;
+export type ClerkContextProviderState = Resources;
 
 export function ClerkContextProvider(props: ClerkContextProvider): JSX.Element | null {
   const { isomorphicClerkOptions, initialState, children } = props;
@@ -48,37 +40,34 @@ export function ClerkContextProvider(props: ClerkContextProvider): JSX.Element |
   const clerkCtx = React.useMemo(() => ({ value: clerk }), [clerkLoaded]);
   const clientCtx = React.useMemo(() => ({ value: state.client }), [state.client]);
 
-  const { sessionId, session, userId, user, organizationId, organization } = derivedState;
-  const actor = session?.actor || null;
-  const organizationMembership = organization
-    ? user?.organizationMemberships.find(m => m.organization.id === organizationId)
-    : organization;
-  const orgId = organizationId;
-  const orgRole = organizationMembership ? organizationMembership.role : organizationMembership;
-  const orgSlug = organizationMembership ? organizationMembership.organization.slug : organizationMembership;
+  const {
+    sessionId,
+    session,
+    userId,
+    user,
+    orgId,
+    actor,
+    lastOrganizationInvitation,
+    lastOrganizationMember,
+    organization,
+    orgRole,
+    orgSlug,
+  } = derivedState;
 
-  const authCtx = React.useMemo(
-    () => ({
-      value: { sessionId, userId, actor, orgId, orgRole, orgSlug },
-    }),
-    [sessionId, userId, actor, orgId, orgRole, orgSlug],
-  );
+  const authCtx = React.useMemo(() => {
+    const value = { sessionId, userId, actor, orgId, orgRole, orgSlug };
+    return { value };
+  }, [sessionId, userId, actor, orgId, orgRole, orgSlug]);
   const userCtx = React.useMemo(() => ({ value: user }), [userId, user]);
   const sessionCtx = React.useMemo(() => ({ value: session }), [sessionId, session]);
   const organizationCtx = React.useMemo(() => {
-    return {
-      value: {
-        organization: derivedState.organization,
-        lastOrganizationInvitation: derivedState.lastOrganizationInvitation,
-        lastOrganizationMember: derivedState.lastOrganizationMember,
-      },
+    const value = {
+      organization: organization,
+      lastOrganizationInvitation: lastOrganizationInvitation,
+      lastOrganizationMember: lastOrganizationMember,
     };
-  }, [
-    derivedState.organizationId,
-    derivedState.organization,
-    derivedState.lastOrganizationInvitation,
-    derivedState.lastOrganizationMember,
-  ]);
+    return { value };
+  }, [orgId, organization, lastOrganizationInvitation, lastOrganizationMember]);
 
   return (
     // @ts-expect-error
@@ -114,60 +103,3 @@ const useLoadedIsomorphicClerk = (options: IsomorphicClerkOptions) => {
 
   return { isomorphicClerk, loaded };
 };
-
-// This should be provided from isomorphicClerk
-// TODO: move inside isomorphicClerk
-function deriveState(
-  clerkLoaded: boolean,
-  state: ClerkContextProviderState,
-  initialState: InitialState | undefined,
-): {
-  userId: string | null | undefined;
-  sessionId: string | null | undefined;
-  organizationId: string | null | undefined;
-  session: ActiveSessionResource | null | undefined;
-  user: UserResource | null | undefined;
-  organization: OrganizationResource | null | undefined;
-  lastOrganizationInvitation: OrganizationInvitationResource | null | undefined;
-  lastOrganizationMember: OrganizationMembershipResource | null | undefined;
-} {
-  if (!clerkLoaded && initialState) {
-    const userId = initialState.userId;
-    // TODO: Instantiate an actual user resource
-    const user = initialState.user as any as UserResource;
-    const sessionId = initialState.sessionId;
-    // TODO: Instantiate an actual session resource
-    const session = initialState.session as any as ActiveSessionResource;
-
-    const organization = initialState.organization as any as OrganizationResource;
-    const organizationId = initialState.organizationId;
-    return {
-      sessionId,
-      session,
-      userId,
-      user,
-      organizationId,
-      organization,
-      lastOrganizationInvitation: null,
-      lastOrganizationMember: null,
-    };
-  }
-  const userId: string | null | undefined = state.user ? state.user.id : state.user;
-  const user = state.user;
-  const sessionId: string | null | undefined = state.session ? state.session.id : state.session;
-  const session = state.session;
-  const organizationId: string | null | undefined = state.organization ? state.organization.id : state.organization;
-  const organization = state.organization;
-  const lastOrganizationInvitation = state.lastOrganizationInvitation;
-  const lastOrganizationMember = state.lastOrganizationMember;
-  return {
-    sessionId,
-    session,
-    userId,
-    user,
-    organization,
-    organizationId,
-    lastOrganizationInvitation,
-    lastOrganizationMember,
-  };
-}
