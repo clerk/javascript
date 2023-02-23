20e9b6a584ef9b13eb98da440b27f4e7eac018e0
Dimitris <dimitris.klouvas@gmail.com>
2023-02-23 15:55:10 +0200

    feat(clerk-js): Introduce eventBus#off method to clear events
    Will be used in tests at the moment

--- packages/clerk-js/src/core/events.ts
+++ packages/clerk-js/src/core/events.ts
@@ -14,14 +14,38 @@ type EventPayload = {
 };
 
 class ClerkEventBus extends EventTarget {
+  #handlers = {} as Record<ClerkEvent, Array<any>>;
+
   on<E extends ClerkEvent, P extends EventPayload[E]>(eventName: E, callback: (evt: P) => void): void {
-    this.addEventListener(eventName, (e: Event) => {
+    const handler = (e: Event) => {
       callback((e as CustomEvent).detail);
-    });
+    };
+
+    if (!this.#handlers[eventName]) {
+      this.#handlers[eventName] = [];
+    }
+    this.#handlers[eventName].push({ callback, handler });
+
+    this.addEventListener(eventName, handler);
   }
 
   dispatch<E extends ClerkEvent, P extends EventPayload[E]>(eventName: E, data: P): void {
     this.dispatchEvent(new CustomEvent(eventName, { detail: data }));
+  }
+
+  off<E extends ClerkEvent, P extends EventPayload[E]>(eventName: E, callback?: (evt: P) => void): void {
+    if (!this.#handlers[eventName]) {
+      return;
+    }
+
+    if (callback) {
+      const { handler } = this.#handlers[eventName].find(({ callback }) => callback === callback) || {};
+      this.removeEventListener(eventName, handler);
+    } else {
+      this.#handlers[eventName].forEach(({ handler }) => {
+        this.removeEventListener(eventName, handler);
+      });
+    }
   }
 }
 
