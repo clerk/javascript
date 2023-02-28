d9cfe9280b231ad0915ffbf286c79c54ba4c3f4c
panteliselef <panteliselef@outlook.com>
2023-02-28 18:13:53 +0100

    fix(clerk-expo): Invalid dist path for clerk-js types

--- packages/expo/src/singleton.ts
+++ packages/expo/src/singleton.ts
@@ -1,4 +1,4 @@
-import type { FapiRequestInit, FapiResponse } from '@clerk/clerk-js/dist/types/src/core/fapiClient';
+import type { FapiRequestInit, FapiResponse } from '@clerk/clerk-js/dist/types/core/fapiClient';
 import Clerk from '@clerk/clerk-js/headless';
 import type { HeadlessBrowserClerk } from '@clerk/clerk-react';
 
