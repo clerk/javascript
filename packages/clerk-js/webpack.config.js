e2930cee22c9f3775f6f498f1f1391519a2fe957
panteliselef <panteliselef@outlook.com>
2023-03-02 14:02:11 +0100

    fix(clerk-js): Adding missing config to webpack.config.js

--- packages/clerk-js/webpack.config.js
+++ packages/clerk-js/webpack.config.js
@@ -93,9 +93,18 @@ const commonForProd = () => {
     },
     output: {
       path: path.resolve(__dirname, 'dist'),
+      chunkFilename: pathData =>
+        !(pathData.chunk.name || '').startsWith('shared')
+          ? `[name].[fullhash:6].${packageJSON.version}.js`
+          : `shared-[id].[fullhash:6].${packageJSON.version}.js`,
       filename: '[name].js',
       libraryTarget: 'umd',
       globalObject: 'globalThis',
+    },
+    optimization: {
+      splitChunks: {
+        name: (module, chunks) => (chunks.length > 0 ? `shared-${chunks.map(chunk => chunk.name).join('-')}` : ''),
+      },
     },
   };
 };
@@ -164,6 +173,15 @@ const devConfig = ({ mode, env }) => {
         crossOriginLoading: 'anonymous',
         filename: `${variant}.js`,
         libraryTarget: 'umd',
+        chunkFilename: `[name].[fullhash:6].${packageJSON.version}.js`,
+      },
+      optimization: {
+        splitChunks: {
+          name: (module, chunks) =>
+            chunks
+              .map(chunk => chunk.name?.replace('Organization', 'Org')?.replace('User', 'Us')?.slice(0, 4))
+              ?.join('-'),
+        },
       },
       devServer: {
         allowedHosts: ['all'],
