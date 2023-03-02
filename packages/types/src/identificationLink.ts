edde03f2f19ac3567dd55eea64bc721b2114ed1a
Stefanos Anagnostou <stefanos@clerk.dev>
2023-03-02 15:12:17 +0200

    chore(types): All resource interfaces now extend ClerkResource

--- packages/types/src/identificationLink.ts
+++ packages/types/src/identificationLink.ts
@@ -1,4 +1,6 @@
-export interface IdentificationLinkResource {
+import type { ClerkResource } from './resource';
+
+export interface IdentificationLinkResource extends ClerkResource {
   id: string;
   type: string;
 }
