f29d3ff589eb0c827a41464f6d084d6e364e2ce5
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-03-03 11:45:43 +0200

    fix(backend,shared): Read Buffer from globalThis to avoid NextJS build issues for edge runtimes

--- packages/shared/src/utils/isomorphicAtob.ts
+++ packages/shared/src/utils/isomorphicAtob.ts
@@ -1,8 +1,8 @@
 export const isomorphicAtob = (data: string) => {
   if (typeof atob !== 'undefined' && typeof atob === 'function') {
     return atob(data);
-  } else if (typeof Buffer !== 'undefined') {
-    return new Buffer(data, 'base64').toString();
+  } else if (typeof global !== 'undefined' && global.Buffer) {
+    return new global.Buffer(data, 'base64').toString();
   }
   return data;
 };
