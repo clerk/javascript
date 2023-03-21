a32e2c859f77657eddb7a0f458c184961d1b089d
panteliselef <panteliselef@outlook.com>
2023-03-21 18:32:57 +0100

    refactor(clerk-js,shared): Move `isDevOrStagingUrl` from clerk-js to shared
    A new feature will need this logic available for more than one package. It is time to make this available for all our packages and eliminate duplication

--- packages/shared/src/utils/keys.ts
+++ packages/shared/src/utils/keys.ts
@@ -53,3 +53,35 @@ export function isLegacyFrontendApiKey(key: string) {
 
   return key.startsWith('clerk.');
 }
+
+export function createDevOrStagingUrlCache() {
+  const DEV_OR_STAGING_SUFFIXES = [
+    '.lcl.dev',
+    '.stg.dev',
+    '.lclstage.dev',
+    '.stgstage.dev',
+    '.dev.lclclerk.com',
+    '.stg.lclclerk.com',
+    '.accounts.lclclerk.com',
+    'accountsstage.dev',
+    'accounts.dev',
+  ];
+
+  const devOrStagingUrlCache = new Map<string, boolean>();
+
+  return {
+    isDevOrStagingUrl: (url: string | URL): boolean => {
+      if (!url) {
+        return false;
+      }
+
+      const hostname = typeof url === 'string' ? url : url.hostname;
+      let res = devOrStagingUrlCache.get(hostname);
+      if (res === undefined) {
+        res = DEV_OR_STAGING_SUFFIXES.some(s => hostname.endsWith(s));
+        devOrStagingUrlCache.set(hostname, res);
+      }
+      return res;
+    },
+  };
+}
