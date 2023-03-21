clerk/javascript

commit b1ca4d1f446c9095c13db5df9faa249a59f3d6ff

feat(remix): Add support for custom responses and headers in rootAuthLoader
We used to accept only plain objects as valid return values from the Remix rootAuthLoader helper. This PR adds support for custom instances of the Response class and custom headers.

It's also important to note with this change, the API of the root loader for Remix apps using Clerk is now aligned to the api of any other loader, as both can return the same type of responses.

One limitation that remains is that if a custom non-redirect response is returned, its body must be a valid json object or a valid json string; otherwise we won't be able to inject the Clerk state.

packages/remix/src/ssr/rootAuthLoader.ts
@@ -1,15 +1,15 @@
 import { sanitizeAuthObject } from '@clerk/backend';
 
-import { invalidRootLoaderCallbackResponseReturn, invalidRootLoaderCallbackReturn } from '../errors';
+import { invalidRootLoaderCallbackReturn } from '../errors';
 import { authenticateRequest } from './authenticateRequest';
 import type { LoaderFunctionArgs, LoaderFunctionReturn, RootAuthLoaderCallback, RootAuthLoaderOptions } from './types';
 import {
   assertObject,
   injectAuthIntoRequest,
+  injectRequestStateIntoResponse,
   interstitialJsonResponse,
   isRedirect,
   isResponse,
-  returnLoaderResultJsonResponse,
 } from './utils';
 
 interface RootAuthLoader {
@@ -23,14 +23,14 @@ interface RootAuthLoader {
 
 export const rootAuthLoader: RootAuthLoader = async (
   args: LoaderFunctionArgs,
-  cbOrOptions: any,
+  handlerOrOptions: any,
   options?: any,
 ): Promise<LoaderFunctionReturn> => {
-  const callback = typeof cbOrOptions === 'function' ? cbOrOptions : undefined;
+  const handler = typeof handlerOrOptions === 'function' ? handlerOrOptions : undefined;
   const opts: RootAuthLoaderOptions = options
     ? options
-    : !!cbOrOptions && typeof cbOrOptions !== 'function'
-    ? cbOrOptions
+    : !!handlerOrOptions && typeof handlerOrOptions !== 'function'
+    ? handlerOrOptions
     : {};
 
   const requestState = await authenticateRequest(args, opts);
@@ -39,20 +39,28 @@ export const rootAuthLoader: RootAuthLoader = async (
     throw interstitialJsonResponse(requestState, { loader: 'root' });
   }
 
-  if (!callback) {
-    return returnLoaderResultJsonResponse({ requestState });
+  if (!handler) {
+    // if the user did not provide a handler, simply inject requestState into an empty response
+    return injectRequestStateIntoResponse(new Response(), requestState);
   }
 
-  const callbackResult = await callback(injectAuthIntoRequest(args, sanitizeAuthObject(requestState.toAuth())));
-  assertObject(callbackResult, invalidRootLoaderCallbackReturn);
+  const handlerResult = await handler(injectAuthIntoRequest(args, sanitizeAuthObject(requestState.toAuth())));
+  assertObject(handlerResult, invalidRootLoaderCallbackReturn);
 
-  // Pass through custom responses
-  if (isResponse(callbackResult)) {
-    if (isRedirect(callbackResult)) {
-      return callbackResult;
+  if (isResponse(handlerResult)) {
+    try {
+      // respect and pass-through any redirects without modifying them
+      if (isRedirect(handlerResult)) {
+        return handlerResult;
+      }
+      // clone and try to inject requestState into all json-like responses
+      // if this fails, the user probably didn't return a json object or a valid json string
+      return injectRequestStateIntoResponse(handlerResult, requestState);
+    } catch (e) {
+      throw new Error(invalidRootLoaderCallbackReturn);
     }
-    throw new Error(invalidRootLoaderCallbackResponseReturn);
   }
 
-  return returnLoaderResultJsonResponse({ requestState, callbackResult });
+  // if the user returned a plain object, simply inject requestState into an empty response
+  return injectRequestStateIntoResponse(new Response(), requestState);
 };

packages/remix/src/errors.ts
@@ -1,7 +1,7 @@
 const createErrorMessage = (msg: string) => {
   return `🔒 Clerk: ${msg.trim()}
 
-For more info, check out the docs: https://docs.clerk.dev,
+For more info, check out the docs: https://clerk.dev/docs,
 or come say hi in our discord server: https://rebrand.ly/clerk-discord
 
 `;
@@ -48,20 +48,23 @@ export const loader: LoaderFunction = async (args) => {
 };
 `);
 
-export const invalidRootLoaderCallbackResponseReturn = createErrorMessage(`
-You're returning an invalid 'Response' object from the 'rootAuthLoader' in root.tsx.
-You can return numbers, strings, objects, booleans, and redirect 'Response' objects (status codes in the range of 300 to 400)
-`);
-
 export const invalidRootLoaderCallbackReturn = createErrorMessage(`
-You're returning an invalid value from 'rootAuthLoader' in root.tsx.
-You can only return plain objects or redirect 'Response' objects (status codes in the range of 300 to 400).
-If you want to return a primitive value or an array, wrap the response with an object.
+You're returning an invalid response from the 'rootAuthLoader' called from the loader in root.tsx.
+You can only return plain objects, responses created using the Remix 'json()' and 'redirect()' helpers,
+custom redirect 'Response' instances (status codes in the range of 300 to 400),
+or custom json 'Response' instances (containing a body that is a valid json string).
+If you want to return a primitive value or an array, you can always wrap the response with an object.
+
 Example:
 
 export const loader: LoaderFunction = args => rootAuthLoader(args, ({ auth }) => {
     const { userId } = auth;
     const posts: Post[] = database.getPostsByUserId(userId);
+
+    return json({ data: posts })
+    // or
+    return new Response(JSON.stringify({ data: posts }), { headers: { 'Content-Type': 'application/json' } });
+    // or
     return { data: posts };
 })
 `);

packages/remix/src/ssr/utils.ts
@@ -1,5 +1,5 @@
 import type { AuthObject, RequestState } from '@clerk/backend';
-import { debugRequestState, loadInterstitialFromLocal } from '@clerk/backend';
+import { constants, debugRequestState, loadInterstitialFromLocal } from '@clerk/backend';
 import { LIB_VERSION } from '@clerk/clerk-react/dist/info';
 import { json } from '@remix-run/server-runtime';
 import cookie from 'cookie';
@@ -23,9 +23,6 @@ export function injectAuthIntoRequest(args: LoaderFunctionArgs, authObject: Auth
   return args as LoaderFunctionArgsWithAuth;
 }
 
-/**
- * @internal
- */
 export function isResponse(value: any): value is Response {
   return (
     value != null &&
@@ -36,32 +33,20 @@ export function isResponse(value: any): value is Response {
   );
 }
 
-/**
- * @internal
- */
 export function isRedirect(res: Response): boolean {
   return res.status >= 300 && res.status < 400;
 }
 
-/**
- * @internal
- */
 export const parseCookies = (req: Request) => {
   return cookie.parse(req.headers.get('cookie') || '');
 };
 
-/**
- * @internal
- */
 export function assertObject(val: any, error?: string): asserts val is Record<string, unknown> {
   if (!val || typeof val !== 'object' || Array.isArray(val)) {
     throw new Error(error || '');
   }
 }
 
-/**
- * @internal
- */
 export const interstitialJsonResponse = (requestState: RequestState, opts: { loader: 'root' | 'nested' }) => {
   return json(
     wrapWithClerkState({
@@ -77,20 +62,21 @@ export const interstitialJsonResponse = (requestState: RequestState, opts: { loa
   );
 };
 
-/**
- * @internal
- */
-export const returnLoaderResultJsonResponse = (opts: { requestState: RequestState; callbackResult?: any }) => {
-  const { reason, message, isSignedIn, isInterstitial, ...rest } = opts.requestState;
-  return json({
-    ...(opts.callbackResult || {}),
-    ...wrapWithClerkState({
-      __clerk_ssr_state: rest,
-      __frontendApi: opts.requestState.frontendApi,
-      __publishableKey: opts.requestState.publishableKey,
-      __clerk_debug: debugRequestState(opts.requestState),
-    }),
+export const injectRequestStateIntoResponse = async (response: Response, requestState: RequestState) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { reason, message, isSignedIn, isInterstitial, ...rest } = requestState;
+  const clone = response.clone();
+  const data = await clone.json();
+  const clerkState = wrapWithClerkState({
+    __clerk_ssr_state: rest,
+    __frontendApi: requestState.frontendApi,
+    __publishableKey: requestState.publishableKey,
+    __clerk_debug: debugRequestState(requestState),
   });
+  // set the correct content-type header in case the user returned a `Response` directly
+  // without setting the header, instead of using the `json()` helper
+  clone.headers.set(constants.Headers.ContentType, constants.ContentTypes.Json);
+  return json({ ...(data || {}), ...clerkState }, clone);
 };
 
 /**

commit 21ccf1350d7833c748df51fd5d96d827ffe67c66

fix(remix): Fix support for plain objects in handler result
issue: https://github.com/clerkinc/javascript/issues/960
introduced in: https://github.com/clerkinc/javascript/pull/750

packages/remix/src/ssr/rootAuthLoader.ts
@@ -67,6 +67,6 @@ export const rootAuthLoader: RootAuthLoader = async (
     }
   }
 
-  // if the user returned a plain object, simply inject requestState into an empty response
-  return injectRequestStateIntoResponse(new Response(JSON.stringify({})), requestState);
+  // if the user returned a plain object, create an empty response and inject requestState and handlerResult
+  return injectRequestStateIntoResponse(new Response(JSON.stringify(handlerResult)), requestState);
 };

playground/remix-node/app/root.tsx
@@ -1,5 +1,5 @@
 import type { LoaderFunction, MetaFunction, Headers } from '@remix-run/node';
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from '@remix-run/react';
 import { rootAuthLoader } from '@clerk/remix/ssr.server';
 import { getClerkDebugHeaders } from '@clerk/remix/ssr.server';
 import { ClerkApp, ClerkCatchBoundary } from '@clerk/remix';
@@ -10,9 +10,9 @@ export const loader: LoaderFunction = args => {
     ({ request }) => {
       const { user } = request;
 
-      console.log('User:', user);
+      console.log('root User:', user);
 
-      return { user };
+      return null;
     },
     { loadUser: true },
   );
@@ -42,6 +42,9 @@ export const meta: MetaFunction = () => ({
 export const CatchBoundary = ClerkCatchBoundary();
 
 function App() {
+  const loaderData = useLoaderData();
+  console.log('root: ', { loaderData });
+
   return (
     <html lang='en'>
       <head>

playground/remix-node/app/routes/index.tsx
@@ -8,7 +8,8 @@ export const loader: LoaderFunction = async args => {
 
 export default function Index() {
   const user = useUser();
-  console.log('User:', user);
+  console.log({ user });
+
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Welcome to Remix</h1>