2828ab63f6f331fb78a89345d0c58a209c98103b
Dimitris Klouvas <dimitris@clerk.dev>
2023-03-30 14:25:10 +0300

    fix(clerk-react,shared): Update inClientSide() util to check for document
    This change resolve issues with react Native where `window` is defined
    but the API is not the same with the Browser resulting in failures
    when trying to use window.location or any other property.
    
    issue: https://github.com/clerkinc/javascript/issues/998

--- packages/react/src/utils/index.ts
+++ packages/react/src/utils/index.ts
@@ -1,6 +1,5 @@
 export * from './childrenUtils';
 export * from './errorThrower';
-export * from './inClientSide';
 export * from './isConstructor';
 export * from './scriptLoader';
 export * from './useMaxAllowedInstancesGuard';
