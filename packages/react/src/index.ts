cac2f3a1e756f27deacbff15e0c217c9f16e4496
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-04-04 20:11:52 +0300

    Revert "chore(clerk-react): Drop vite stale code (#1001)"
    This reverts commit 9ee4fc14d77e237e5a69112a6dbe5355646176bc.

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -11,3 +11,13 @@ export type {
 } from './types';
 export { isMagicLinkError, MagicLinkErrorCode } from './errors';
 export { useMagicLink } from './hooks/useMagicLink';
+
+/**
+ * Vite does not define `global` by default
+ * One workaround is to use the `define` config prop
+ * https://vitejs.dev/config/#define
+ * We are solving this in the SDK level to reduce setup steps.
+ */
+if (typeof global === 'undefined' && typeof window !== 'undefined' && !window.global) {
+  (window as any).global = window;
+}
