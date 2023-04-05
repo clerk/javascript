aea656e590c8e46a6466d9ed4b4567510e0c74e0
George Desipris <george.desipris.56@gmail.com>
2023-04-05 14:58:33 +0300

    test(clerk-react): Add tests for multi domain types

--- packages/react/src/contexts/ClerkProvider.test.tsx
+++ packages/react/src/contexts/ClerkProvider.test.tsx
@@ -23,8 +23,52 @@ describe('ClerkProvider', () => {
     });
   });
 
-  describe.skip('Multi domain', () => {
+  describe('Multi domain', () => {
     const defaultProps = { publishableKey: 'test', children: '' };
+
+    it('proxyUrl (primary app)', () => {
+      expectTypeOf({ ...defaultProps, proxyUrl: 'test' }).toMatchTypeOf<ClerkProviderProps>();
+    });
+
+    it('proxyUrl + isSatellite (satellite app)', () => {
+      expectTypeOf({ ...defaultProps, proxyUrl: 'test', isSatellite: true }).toMatchTypeOf<ClerkProviderProps>();
+    });
+
+    it('domain + isSatellite (satellite app)', () => {
+      expectTypeOf({ ...defaultProps, domain: 'test', isSatellite: true }).toMatchTypeOf<ClerkProviderProps>();
+    });
+
+    it('only domain is not allowed', () => {
+      expectTypeOf({ ...defaultProps, domain: 'test' }).not.toMatchTypeOf<ClerkProviderProps>();
+    });
+
+    it('only isSatellite is not allowed', () => {
+      expectTypeOf({ ...defaultProps, isSatellite: true }).not.toMatchTypeOf<ClerkProviderProps>();
+    });
+
+    it('proxyUrl + domain is not allowed', () => {
+      expectTypeOf({ ...defaultProps, proxyUrl: 'test', domain: 'test' }).not.toMatchTypeOf<ClerkProviderProps>();
+    });
+
+    it('proxyUrl + domain + isSatellite is not allowed', () => {
+      expectTypeOf({
+        ...defaultProps,
+        proxyUrl: 'test',
+        domain: 'test',
+        isSatellite: true,
+      }).not.toMatchTypeOf<ClerkProviderProps>();
+    });
+  });
+
+  describe('clerkJSVariant', () => {
+    const defaultProps = { publishableKey: 'test', children: '' };
+
+    it('is either headless or empty', () => {
+      expectTypeOf({ ...defaultProps, clerkJSVariant: 'headless' as const }).toMatchTypeOf<ClerkProviderProps>();
+      expectTypeOf({ ...defaultProps, clerkJSVariant: '' as const }).toMatchTypeOf<ClerkProviderProps>();
+      expectTypeOf({ ...defaultProps, clerkJSVariant: undefined }).toMatchTypeOf<ClerkProviderProps>();
+      expectTypeOf({ ...defaultProps, clerkJSVariant: 'test' }).not.toMatchTypeOf<ClerkProviderProps>();
+    });
   });
 
   describe('children', () => {
