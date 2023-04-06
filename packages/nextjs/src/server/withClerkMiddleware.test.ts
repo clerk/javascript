dd872075dd5fb9a44ce3ff26f0b296835987626c
George Desipris <george.desipris.56@gmail.com>
2023-04-06 12:59:15 +0300

    test(nextjs): Type tests for withClerkMiddleware

---
+++ packages/nextjs/src/server/withClerkMiddleware.test.ts
@@ -0,0 +1,53 @@
+import { expectTypeOf } from 'expect-type';
+
+import type { WithAuthOptions } from './types';
+
+describe('withClerkMiddleware', () => {
+  describe('Type tests', () => {
+    describe('WithAuthOptions', () => {
+      it('can receive the appropriate keys', () => {
+        expectTypeOf({ publishableKey: '', secretKey: '' }).toMatchTypeOf<WithAuthOptions>();
+        expectTypeOf({ frontendApi: '', secretKey: '' }).toMatchTypeOf<WithAuthOptions>();
+        expectTypeOf({ publishableKey: '', apiKey: '' }).toMatchTypeOf<WithAuthOptions>();
+        expectTypeOf({ frontendApi: '', apiKey: '' }).toMatchTypeOf<WithAuthOptions>();
+      });
+
+      describe('Multi domain', () => {
+        const defaultProps = { publishableKey: '', secretKey: '' };
+
+        it('proxyUrl (primary app)', () => {
+          expectTypeOf({ ...defaultProps, proxyUrl: 'test' }).toMatchTypeOf<WithAuthOptions>();
+        });
+
+        it('proxyUrl + isSatellite (satellite app)', () => {
+          expectTypeOf({ ...defaultProps, proxyUrl: 'test', isSatellite: true }).toMatchTypeOf<WithAuthOptions>();
+        });
+
+        it('domain + isSatellite (satellite app)', () => {
+          expectTypeOf({ ...defaultProps, domain: 'test', isSatellite: true }).toMatchTypeOf<WithAuthOptions>();
+        });
+
+        it('only domain is not allowed', () => {
+          expectTypeOf({ ...defaultProps, domain: 'test' }).not.toMatchTypeOf<WithAuthOptions>();
+        });
+
+        it('only isSatellite is not allowed', () => {
+          expectTypeOf({ ...defaultProps, isSatellite: true }).not.toMatchTypeOf<WithAuthOptions>();
+        });
+
+        it('proxyUrl + domain is not allowed', () => {
+          expectTypeOf({ ...defaultProps, proxyUrl: 'test', domain: 'test' }).not.toMatchTypeOf<WithAuthOptions>();
+        });
+
+        it('proxyUrl + domain + isSatellite is not allowed', () => {
+          expectTypeOf({
+            ...defaultProps,
+            proxyUrl: 'test',
+            domain: 'test',
+            isSatellite: true,
+          }).not.toMatchTypeOf<WithAuthOptions>();
+        });
+      });
+    });
+  });
+});
