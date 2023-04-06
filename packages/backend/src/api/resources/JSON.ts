6e86468038c345f9203530306f6e1a0ebf320fff
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-04-06 16:10:44 +0300

    fix(backend,types,clerk-js): Introduce image_url to User-related types
    Also, deprecate profile_image_url

--- packages/backend/src/api/resources/JSON.ts
+++ packages/backend/src/api/resources/JSON.ts
@@ -162,7 +162,11 @@ export interface OrganizationMembershipPublicUserDataJSON {
   identifier: string;
   first_name: string | null;
   last_name: string | null;
+  /**
+   * @deprecated Use image_url instead
+   */
   profile_image_url: string;
+  image_url: string;
   user_id: string;
 }
 
@@ -252,7 +256,11 @@ export interface UserJSON extends ClerkResourceJSON {
   last_name: string;
   gender: string;
   birthday: string;
+  /**
+   * @deprecated Use image_url instead
+   */
   profile_image_url: string;
+  image_url: string;
   primary_email_address_id: string;
   primary_phone_number_id: string;
   primary_web3_wallet_id: string;
