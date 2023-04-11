ce3f871d79eb68b6d0944d3e8280185ec6ef3518
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-04-11 17:42:30 +0300

    Revert "fix(clerk-js): Update isDefaultProfileImage to work with new image service"
    This reverts commit d55cf469066bbb835e3a58515edda01c9428cc3f.

--- packages/clerk-js/src/utils/decodeBase16.ts
+++
@@ -1,7 +0,0 @@
-export const decodeBase16 = (input: string): string => {
-  let result = '';
-  for (let i = 0; i < input.length; i += 2) {
-    result += String.fromCharCode(parseInt(input.substring(i, i + 2), 16));
-  }
-  return result;
-};
