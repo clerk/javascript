0c37ec47cbe04d2ce33e0aa9cc3949faa48eade2
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-04-12 12:47:55 +0300

    chore(repo): Add zx and helper scripts

---
+++ scripts/snapshot.mjs
@@ -0,0 +1,19 @@
+#!/usr/bin/env zx
+
+import 'zx/globals';
+import { constants } from './common.mjs';
+
+const prefix = argv.name || argv._[0] || 'snap';
+
+await $`npx json -I -f ${constants.ChangesetConfigFile} -e "this.changelog = false"`;
+
+const res = await $`npx changeset version --snapshot ${prefix}`;
+const success = !res.stderr.includes('No unreleased changesets found');
+
+await $`git checkout HEAD -- ${constants.ChangesetConfigFile}`;
+
+if (success) {
+  echo('success=1');
+} else {
+  echo('success=0');
+}
