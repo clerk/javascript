ee53de76a082064183804984a24dcdf3532b4723
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-04-13 01:04:05 +0300

    chore(repo): Setup commitlint.config.js without lerna

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,11 @@
-const {
-  utils: { getPackages },
-} = require('@commitlint/config-lerna-scopes');
+const glob = require('glob');
+const fs = require('fs');
+
+const getPackageNames = () => {
+  const files = glob.sync('./packages/*/package.json');
+  const names = files.map(f => JSON.parse(fs.readFileSync(f, 'utf8')).name);
+  return names.map(n => n.split('/').pop());
+};
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
@@ -8,6 +13,6 @@ module.exports = {
     'subject-case': [2, 'always', ['sentence-case']],
     'body-max-line-length': [1, 'always', '150'],
     'scope-empty': [2, 'never'],
-    'scope-enum': async ctx => [2, 'always', [...(await getPackages(ctx)), 'repo', 'release']],
+    'scope-enum': async ctx => [2, 'always', [...getPackageNames(), 'repo', 'release']],
   },
 };
