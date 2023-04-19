2f35fe0a4c2a3beb3da93d0baa2ef8b17f1e17b4
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-04-19 10:10:11 +0300

    fix(clerk-js): Do not append __clerk_db_jwt if it already exists
    That way we will avoid appending it twice for OAuth provider flows were a redirectUrl is passed in but it already includes a db jwt.
    
    In most cases, the redirectUrl will be provided by the user so if a db jwt does not exist, we do need to append it.
    
    However, flows that append a redirectUrl before the mounting of the components (eg, using one of our redirect helpers like RedirectToSignIn) do no need the extra db jwt appended, as the components will already decorate the URL in ClerkUIComponents

--- packages/clerk-js/src/utils/devBrowser.ts
+++ packages/clerk-js/src/utils/devBrowser.ts
@@ -1,17 +1,23 @@
 const DEV_BROWSER_JWT_MARKER = '__clerk_db_jwt';
 const DEV_BROWSER_JWT_MARKER_REGEXP = /__clerk_db_jwt\[(.*)\]/;
 
+function extractDevBrowserJWT(url: string): string {
+  const matches = url.match(DEV_BROWSER_JWT_MARKER_REGEXP);
+  return matches ? matches[1] : '';
+}
+
 export function setDevBrowserJWTInURL(url: string, jwt: string): string {
+  const dbJwt = extractDevBrowserJWT(url);
+  if (dbJwt) {
+    url.replace(`${DEV_BROWSER_JWT_MARKER}[${dbJwt}]`, jwt);
+    return url;
+  }
   const hasHash = (url || '').includes('#');
   return `${url}${hasHash ? '' : '#'}${DEV_BROWSER_JWT_MARKER}[${(jwt || '').trim()}]`;
 }
 
 export function getDevBrowserJWTFromURL(url: string): string {
-  const matches = url.match(DEV_BROWSER_JWT_MARKER_REGEXP);
-  if (!matches) {
-    return '';
-  }
-
+  const jwt = extractDevBrowserJWT(url);
   let newUrl = url.replace(DEV_BROWSER_JWT_MARKER_REGEXP, '');
 
   if (newUrl.endsWith('#')) {
@@ -22,5 +28,5 @@ export function getDevBrowserJWTFromURL(url: string): string {
     globalThis.history.replaceState(null, '', newUrl);
   }
 
-  return matches[1];
+  return jwt;
 }
