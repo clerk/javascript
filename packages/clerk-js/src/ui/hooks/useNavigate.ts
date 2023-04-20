febb0d7a7d9d526cf22674bcc897ba7af04a48e2
Andy Kenward <4893048+andykenward@users.noreply.github.com>
2023-04-20 19:02:54 +0100

    chore(clerk-js): Comment a todo that should be fixed

--- packages/clerk-js/src/ui/hooks/useNavigate.ts
+++ packages/clerk-js/src/ui/hooks/useNavigate.ts
@@ -3,6 +3,7 @@ import React from 'react';
 import { useCoreClerk } from '../contexts';
 import { RouteContext } from '../router';
 
+// FIXME: When you remove this return type and use TypeScript inference, it will show errors throughout the codebase.
 export function useNavigate(): { navigate: any } {
   const Clerk = useCoreClerk();
   const router = React.useContext(RouteContext);
