e311baac6197c6b65fb76b92833b504c0effed6d
Dimitris Klouvas <dimitris@clerk.dev>
2023-04-24 16:07:39 +0300

    feat(nextjs): Introduce NextJS specific redirect for signIn/signUp urls

--- packages/nextjs/server.d.ts
+++ packages/nextjs/server.d.ts
@@ -1,3 +1,4 @@
 export { buildClerkProps, getAuth } from './dist/server/getAuth';
+export { redirectToSignIn, redirectToSignUp } from './dist/server';
 export { withClerkMiddleware } from './dist/server/withClerkMiddleware';
 export { createClerkClient, clerkClient } from './dist/api';
---
+++ packages/nextjs/src/server/redirect.ts
@@ -0,0 +1,14 @@
+import { redirect } from '@clerk/backend';
+import { NextResponse } from 'next/server';
+
+import { FRONTEND_API, PUBLISHABLE_KEY, SIGN_IN_URL, SIGN_UP_URL } from './clerk';
+
+const redirectAdapter = (url: string) => NextResponse.redirect(url);
+
+export const { redirectToSignIn, redirectToSignUp } = redirect({
+  redirectAdapter,
+  signInUrl: SIGN_IN_URL,
+  signUpUrl: SIGN_UP_URL,
+  publishableKey: PUBLISHABLE_KEY,
+  frontendApi: FRONTEND_API,
+});
