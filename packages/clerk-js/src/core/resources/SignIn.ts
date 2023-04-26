6da395fd785467aa934896942408bdb5f64aa887
Mark Pitsilos <mark.pitsilos@gmail.com>
2023-04-26 16:51:06 +0300

    feat(clerk-js,types): Support sign in with SAML strategy

--- packages/clerk-js/src/core/resources/SignIn.ts
+++ packages/clerk-js/src/core/resources/SignIn.ts
@@ -26,6 +26,7 @@ import type {
   Web3SignatureFactor,
 } from '@clerk/types';
 import type { AuthenticateWithWeb3Params } from '@clerk/types/src';
+import type { SamlConfig } from '@clerk/types/src';
 
 import { generateSignatureWithMetamask, getMetamaskIdentifier, windowNavigate } from '../../utils';
 import {
@@ -98,6 +99,12 @@ export class SignIn extends BaseResource implements SignInResource {
           config = { phoneNumberId: factor?.phoneNumberId } as ResetPasswordCodeFactorConfig;
         }
         break;
+      case 'saml':
+        config = {
+          redirectUrl: factor.redirectUrl,
+          actionCompleteRedirectUrl: factor.actionCompleteRedirectUrl,
+        } as SamlConfig;
+        break;
       default:
         clerkInvalidStrategy('SignIn.prepareFirstFactor', factor.strategy);
     }
@@ -166,12 +173,21 @@ export class SignIn extends BaseResource implements SignInResource {
 
   public authenticateWithRedirect = async (params: AuthenticateWithRedirectParams): Promise<void> => {
     const { strategy, redirectUrl, redirectUrlComplete, identifier } = params || {};
-    const { firstFactorVerification } = await this.create({
-      strategy,
-      identifier,
-      redirectUrl: SignIn.clerk.buildUrlWithAuth(redirectUrl),
-      actionCompleteRedirectUrl: redirectUrlComplete,
-    });
+
+    const { firstFactorVerification } =
+      strategy === 'saml' && this.id
+        ? await this.prepareFirstFactor({
+            strategy,
+            redirectUrl: SignIn.clerk.buildUrlWithAuth(redirectUrl),
+            actionCompleteRedirectUrl: redirectUrlComplete,
+          })
+        : await this.create({
+            strategy,
+            identifier,
+            redirectUrl: SignIn.clerk.buildUrlWithAuth(redirectUrl),
+            actionCompleteRedirectUrl: redirectUrlComplete,
+          });
+
     const { status, externalVerificationRedirectURL } = firstFactorVerification;
 
     if (status === 'unverified' && externalVerificationRedirectURL) {
