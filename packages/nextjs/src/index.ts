143ddf96372173ca41e9cf6297beb46ade799e8e
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-04-27 04:10:26 +0300

    chore(repo): Export client-side hooks

--- packages/nextjs/src/index.ts
+++ packages/nextjs/src/index.ts
@@ -2,28 +2,6 @@
  * These need to be explicitly listed. Do not use an * here.
  * If you do, app router will break.
  */
-/**
- * Conditionally export components that exhibit different behavior
- * when used in /app vs /pages.
- * We defined the runtime and the type values explicitly,
- * because TS will not recognize the subpath import unless the HOST
- * application sets moduleResolution to 'NodeNext'.
- */
-// @ts-ignore
-import * as ComponentsModule from '#components';
-/**
- * Conditionally export server-side helpers.
- * This allows to import server-side helpers from the top-level path.
- * We defined the runtime and the type values explicitly,
- * because TS will not recognize the subpath import unless the HOST
- * application sets moduleResolution to 'NodeNext'.
- */
-// @ts-ignore
-import * as ServerHelperModule from '#server-helpers';
-
-import type { ServerComponentsServerModuleTypes } from './components.server';
-import type { ServerHelpersServerModuleTypes } from './server-helpers.server';
-
 export {
   RedirectToUserProfile,
   RedirectToSignUp,
@@ -51,6 +29,48 @@ export {
   CreateOrganization,
   SignInButton,
 } from './client-boundary/uiComponents';
+
+/**
+ * These need to be explicitly listed. Do not use an * here.
+ * If you do, app router will break.
+ */
+export {
+  useUser,
+  useAuth,
+  useSession,
+  useClerk,
+  useSignIn,
+  useSignUp,
+  useSessionList,
+  useOrganization,
+  useOrganizationList,
+  useOrganizations,
+  useMagicLink,
+  MagicLinkErrorCode,
+  isMagicLinkError,
+} from './client-boundary/hooks';
+
+/**
+ * Conditionally export components that exhibit different behavior
+ * when used in /app vs /pages.
+ * We defined the runtime and the type values explicitly,
+ * because TS will not recognize the subpath import unless the HOST
+ * application sets moduleResolution to 'NodeNext'.
+ */
+// @ts-ignore
+import * as ComponentsModule from '#components';
+/**
+ * Conditionally export server-side helpers.
+ * This allows to import server-side helpers from the top-level path.
+ * We defined the runtime and the type values explicitly,
+ * because TS will not recognize the subpath import unless the HOST
+ * application sets moduleResolution to 'NodeNext'.
+ */
+// @ts-ignore
+import * as ServerHelperModule from '#server-helpers';
+
+import type { ServerComponentsServerModuleTypes } from './components.server';
+import type { ServerHelpersServerModuleTypes } from './server-helpers.server';
 
 export const ClerkProvider = ComponentsModule.ClerkProvider as ServerComponentsServerModuleTypes['ClerkProvider'];
 export const SignedIn = ComponentsModule.SignedIn as ServerComponentsServerModuleTypes['SignedIn'];
