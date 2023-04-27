66904289046056f0ee9541b6d091ae2e0ea87a73
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-04-27 03:56:14 +0300

    chore(nextjs): Move all app-router related client apis to /app-router/client

---
+++ packages/nextjs/src/app-router/client/ClerkProvider.tsx
@@ -0,0 +1,55 @@
+'use client';
+// !!! Note the import from react
+import type { ClerkProviderProps } from '@clerk/clerk-react';
+import { ClerkProvider as ReactClerkProvider } from '@clerk/clerk-react';
+import { usePathname, useRouter } from 'next/navigation';
+import React, { useCallback, useEffect } from 'react';
+
+declare global {
+  export interface Window {
+    __clerk_nav_await: Array<(value: void) => void>;
+    __clerk_nav: (to: string) => Promise<void>;
+  }
+}
+
+const useAwaitableNavigate = () => {
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  const { push, refresh } = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    window.__clerk_nav = (to: string) => {
+      return new Promise(res => {
+        window.__clerk_nav_await.push(res);
+        if (to === pathname) {
+          refresh();
+        } else {
+          push(to);
+        }
+      });
+    };
+  }, [pathname]);
+
+  useEffect(() => {
+    if (typeof window.__clerk_nav_await === 'undefined') {
+      window.__clerk_nav_await = [];
+    }
+    window.__clerk_nav_await.forEach(resolve => resolve());
+    window.__clerk_nav_await = [];
+  });
+
+  return useCallback((to: string) => {
+    return window.__clerk_nav(to);
+  }, []);
+};
+
+export const ClientClerkProvider = (props: ClerkProviderProps) => {
+  console.log('APP-ROUTER/CLIENT/PROVIDER');
+  const navigate = useAwaitableNavigate();
+  return (
+    <ReactClerkProvider
+      navigate={navigate}
+      {...props}
+    />
+  );
+};
