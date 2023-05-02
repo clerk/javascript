75a37d5f8bd4276ed35201216fc0d80682d1e0af
panteliselef <panteliselef@outlook.com>
2023-05-02 16:42:56 +0200

    chore(clerk-js): Move useSetSessionWithTimeout to hooks

--- packages/clerk-js/src/ui/components/SignIn/ResetPasswordSuccess.tsx
+++ packages/clerk-js/src/ui/components/SignIn/ResetPasswordSuccess.tsx
@@ -1,34 +1,9 @@
-import { useEffect } from 'react';
-
 import { withRedirectToHomeSingleSessionGuard } from '../../common';
-import { useCoreClerk, useSignInContext } from '../../contexts';
 import { Col, descriptors, localizationKeys, Text } from '../../customizables';
 import { Card, CardAlert, Header, useCardState, withCardStateProvider } from '../../elements';
+import { useSetSessionWithTimeout } from '../../hooks/useSetSessionWithTimeout';
 import { Flex, Spinner } from '../../primitives';
-import { useRouter } from '../../router';
 
-const useSetSessionWithTimeout = () => {
-  const { queryString } = useRouter();
-  const { setActive } = useCoreClerk();
-  const { navigateAfterSignIn } = useSignInContext();
-
-  useEffect(() => {
-    let timeoutId: ReturnType<typeof setTimeout>;
-    const queryParams = new URLSearchParams(queryString);
-    const createdSessionId = queryParams.get('createdSessionId');
-    if (createdSessionId) {
-      timeoutId = setTimeout(() => {
-        void setActive({ session: createdSessionId, beforeEmit: navigateAfterSignIn });
-      }, 2000);
-    }
-
-    return () => {
-      if (timeoutId) {
-        clearTimeout(timeoutId);
-      }
-    };
-  }, []);
-};
 export const _ResetPasswordSuccess = () => {
   const card = useCardState();
   useSetSessionWithTimeout();
