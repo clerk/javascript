d6de557406ba2752cd7e3185ee78b710d0cddf0d
George Desipris <73396808+desiprisg@users.noreply.github.com>
2023-05-08 14:06:36 +0300

    fix(clerk-js): Only close modals when pressing on their overlay (#1159)
    This fixes the issue with the 1Password button where the modal would close when that was pressed. It also fixes the issue where having two modals open and clicking the latest one would close the first one.

--- packages/clerk-js/src/ui/elements/Modal.tsx
+++ packages/clerk-js/src/ui/elements/Modal.tsx
@@ -1,5 +1,5 @@
 import { createContextAndHook, useSafeLayoutEffect } from '@clerk/shared';
-import React from 'react';
+import React, { useRef } from 'react';
 
 import { descriptors, Flex } from '../customizables';
 import { usePopover, useScrollLock } from '../hooks';
@@ -19,11 +19,13 @@ type ModalProps = React.PropsWithChildren<{
 
 export const Modal = withFloatingTree((props: ModalProps) => {
   const { handleClose, handleOpen, contentSx, containerSx } = props;
+  const { disableScroll, enableScroll } = useScrollLock(document.body);
+  const overlayRef = useRef<HTMLDivElement>(null);
   const { floating, isOpen, context, nodeId, toggle } = usePopover({
     defaultOpen: true,
     autoUpdate: false,
+    outsidePress: e => e.target === overlayRef.current,
   });
-  const { disableScroll, enableScroll } = useScrollLock(document.body);
 
   React.useEffect(() => {
     if (!isOpen) {
@@ -50,6 +52,7 @@ export const Modal = withFloatingTree((props: ModalProps) => {
       <ModalContext.Provider value={modalCtx}>
         <Flex
           aria-hidden
+          ref={overlayRef}
           elementDescriptor={descriptors.modalBackdrop}
           sx={[
             t => ({
