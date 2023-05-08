5d47d24b1b3998f95387b5c2dee58bb5803e34c7
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-05-08 22:59:51 +0300

    fix(nextjs): Change the default matchers to improve TRPC support
    We used to ignore filenames by matching whether a `.` existed in a route or note, however many RPC-related frameworks use the `.` notation for the RP names.
    
    In order to improve DX, we no longer ignore routes starting with /api or /trpc by default.

--- packages/nextjs/src/server/authMiddleware.ts
+++ packages/nextjs/src/server/authMiddleware.ts
@@ -28,15 +28,16 @@ type RouteMatcherWithNextTypedRoutes =
   | (string & {});
 
 /**
- * The default ideal matcher that excludes the _next directory (internals) and all static files.
+ * The default ideal matcher that excludes the _next directory (internals) and all static files,
+ * but it will match the root route (/) and any routes that start with /api or /trpc.
  */
-export const DEFAULT_CONFIG_MATCHER = ['/((?!.*\\..*|_next).*)', '/'];
+export const DEFAULT_CONFIG_MATCHER = ['/((?!_next|.+\\..+))', '/', '/(api|trpc)(.*)'];
 
 /**
  * Any routes matching this path will be ignored by the middleware.
  * This is the inverted version of DEFAULT_CONFIG_MATCHER.
  */
-export const DEFAULT_IGNORED_ROUTES = ['/(.*\\..*|_next.*)'];
+export const DEFAULT_IGNORED_ROUTES = ['/((?!api|trpc))(_next|.+\\..+)(.*)'];
 
 type RouteMatcherParam =
   | Array<RegExp | RouteMatcherWithNextTypedRoutes>
