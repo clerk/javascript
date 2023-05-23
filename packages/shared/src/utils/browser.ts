144239e50790f3dd1d514969ddbeb34c3aa77b85
George Desipris <73396808+desiprisg@users.noreply.github.com>
2023-05-23 12:27:41 +0300

    feat(clerk-js): Retry get requests less frequently if online (#1198)
    * feat(clerk-js): Retry get requests less frequently if online, infinitely if offline
    
    * fix(clerk-js): Retry failed get requests 10 times if offline
    
    * refactor(clerk-js): Create smaller utils for isValidBrowserOnline
    
    * fix(shared): Cleanup userAgentIsRobot
    
    ---------
    
    Co-authored-by: Nikos Douvlis <nikosdouvlis@gmail.com>

--- packages/shared/src/utils/browser.ts
+++ packages/shared/src/utils/browser.ts
@@ -2,62 +2,65 @@ export function inBrowser(): boolean {
   return typeof window !== 'undefined';
 }
 
-export function detectUserAgentRobot(userAgent: string): boolean {
-  const robots = new RegExp(
-    (
-      [
-        /bot/,
-        /spider/,
-        /crawl/,
-        /APIs-Google/,
-        /AdsBot/,
-        /Googlebot/,
-        /mediapartners/,
-        /Google Favicon/,
-        /FeedFetcher/,
-        /Google-Read-Aloud/,
-        /DuplexWeb-Google/,
-        /googleweblight/,
-        /bing/,
-        /yandex/,
-        /baidu/,
-        /duckduck/,
-        /yahoo/,
-        /ecosia/,
-        /ia_archiver/,
-        /facebook/,
-        /instagram/,
-        /pinterest/,
-        /reddit/,
-        /slack/,
-        /twitter/,
-        /whatsapp/,
-        /youtube/,
-        /semrush/,
-      ] as RegExp[]
-    )
-      .map(r => r.source)
-      .join('|'),
-    'i',
-  );
+const botAgents = [
+  'bot',
+  'spider',
+  'crawl',
+  'APIs-Google',
+  'AdsBot',
+  'Googlebot',
+  'mediapartners',
+  'Google Favicon',
+  'FeedFetcher',
+  'Google-Read-Aloud',
+  'DuplexWeb-Google',
+  'googleweblight',
+  'bing',
+  'yandex',
+  'baidu',
+  'duckduck',
+  'yahoo',
+  'ecosia',
+  'ia_archiver',
+  'facebook',
+  'instagram',
+  'pinterest',
+  'reddit',
+  'slack',
+  'twitter',
+  'whatsapp',
+  'youtube',
+  'semrush',
+];
+const botAgentRegex = new RegExp(botAgents.join('|'), 'i');
 
-  return robots.test(userAgent);
+export function userAgentIsRobot(userAgent: string): boolean {
+  return !userAgent ? false : botAgentRegex.test(userAgent);
 }
 
-export function isValidBrowserOnline(): boolean {
+export function isValidBrowser(): boolean {
+  const navigator = window?.navigator;
+  if (!inBrowser() || !navigator) {
+    return false;
+  }
+  return !userAgentIsRobot(navigator?.userAgent) && !navigator?.webdriver;
+}
+
+export function isBrowserOnline(): boolean {
   const navigator = window?.navigator;
   if (!inBrowser() || !navigator) {
     return false;
   }
 
-  const isUserAgentRobot = detectUserAgentRobot(navigator?.userAgent);
-  const isWebDriver = navigator?.webdriver;
   const isNavigatorOnline = navigator?.onLine;
 
   // Being extra safe with the experimental `connection` property, as it is not defined in all browsers
   // https://developer.mozilla.org/en-US/docs/Web/API/Navigator/connection#browser_compatibility
   // @ts-ignore
   const isExperimentalConnectionOnline = navigator?.connection?.rtt !== 0 && navigator?.connection?.downlink !== 0;
+  return isExperimentalConnectionOnline && isNavigatorOnline;
+}
 
-  return !isUserAgentRobot && !isWebDriver && isExperimentalConnectionOnline && isNavigatorOnline;
+export function isValidBrowserOnline(): boolean {
+  return isBrowserOnline() && isValidBrowser();
 }
