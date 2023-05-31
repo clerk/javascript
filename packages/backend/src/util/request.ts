4160367db31fc4723a6a267627053b227a2de932
Dimitris Klouvas <dimitris@clerk.dev>
2023-05-31 17:56:25 +0300

    fix(backend): Fix AWS forwarded headers with multiple values

--- packages/backend/src/util/request.ts
+++ packages/backend/src/util/request.ts
@@ -18,14 +18,19 @@ export function checkCrossOrigin({
   forwardedPort?: string | null;
   forwardedProto?: string | null;
 }) {
-  if (forwardedProto && forwardedProto !== originURL.protocol) {
+  const proto = getFirstValueFromHeaderValue(forwardedProto);
+  const port = getFirstValueFromHeaderValue(forwardedPort);
+
+  const originProtocol = getProtocolVerb(originURL.protocol);
+
+  if (proto && proto !== originProtocol) {
     return true;
   }
 
-  const protocol = forwardedProto || originURL.protocol;
+  const protocol = proto || originProtocol;
   /* The forwarded host prioritised over host to be checked against the referrer.  */
   const finalURL = convertHostHeaderValueToURL(forwardedHost || host, protocol);
-  finalURL.port = forwardedPort || finalURL.port;
+  finalURL.port = port || finalURL.port;
 
   if (getPort(finalURL) !== getPort(originURL)) {
     return true;
@@ -37,17 +42,17 @@ export function checkCrossOrigin({
   return false;
 }
 
-export function convertHostHeaderValueToURL(host: string, protocol = 'https:'): URL {
+export function convertHostHeaderValueToURL(host: string, protocol = 'https'): URL {
   /**
    * The protocol is added for the URL constructor to work properly.
    * We do not check for the protocol at any point later on.
    */
-  return new URL(`${protocol}//${host}`);
+  return new URL(`${protocol}://${host}`);
 }
 
 const PROTOCOL_TO_PORT_MAPPING: Record<string, string> = {
-  'http:': '80',
-  'https:': '443',
+  http: '80',
+  https: '443',
 } as const;
 
 function getPort(url: URL) {
@@ -56,4 +61,12 @@ function getPort(url: URL) {
 
 function getPortFromProtocol(protocol: string) {
   return PROTOCOL_TO_PORT_MAPPING[protocol];
+}
+
+function getFirstValueFromHeaderValue(value?: string | null) {
+  return value?.split(',')[0]?.trim() || '';
+}
+
+function getProtocolVerb(protocol: string) {
+  return protocol?.replace(/:$/, '') || '';
 }
