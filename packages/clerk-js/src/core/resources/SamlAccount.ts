9651658c2ab00108ffcb9679cd119488c41ec521
Mark Pitsilos <mark@clerk.dev>
2023-06-08 23:41:26 +0300

    fix(clerk-js,types): Disable fields for SAML only if there is an active connection

--- packages/clerk-js/src/core/resources/SamlAccount.ts
+++ packages/clerk-js/src/core/resources/SamlAccount.ts
@@ -9,6 +9,8 @@ import { Verification } from './Verification';
 export class SamlAccount extends BaseResource implements SamlAccountResource {
   id!: string;
   provider: SamlIdpSlug = 'saml_custom';
+  providerUserId: string | null = null;
+  active = false;
   emailAddress = '';
   firstName = '';
   lastName = '';
@@ -28,6 +30,8 @@ export class SamlAccount extends BaseResource implements SamlAccountResource {
 
     this.id = data.id;
     this.provider = data.provider;
+    this.providerUserId = data.provider_user_id;
+    this.active = data.active;
     this.emailAddress = data.email_address;
     this.firstName = data.first_name;
     this.lastName = data.last_name;
