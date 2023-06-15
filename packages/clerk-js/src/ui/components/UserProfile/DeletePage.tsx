c4c3206f0cc2ad7db2949557b12846ec12cbb708
Jeff Escalante <jescalan@users.noreply.github.com>
2023-06-15 13:17:39 -0400

    fix(clerk-js): Remove sign out and show signIn on delete page

--- packages/clerk-js/src/ui/components/UserProfile/DeletePage.tsx
+++ packages/clerk-js/src/ui/components/UserProfile/DeletePage.tsx
@@ -1,4 +1,4 @@
-import { useCoreClerk, useCoreSession, useEnvironment, useCoreUser } from '../../contexts';
+import { useEnvironment, useCoreUser } from '../../contexts';
 import { localizationKeys, Text } from '../../customizables';
 import { ContentPage, Form, FormButtons, useCardState, withCardStateProvider } from '../../elements';
 import { handleError } from '../../utils';
@@ -9,36 +9,29 @@ export const DeletePage = withCardStateProvider(() => {
   const card = useCardState();
   const environment = useEnvironment();
   const router = useRouter();
-  const clerk = useCoreClerk();
-  const session = useCoreSession();
   const user = useCoreUser();
 
   const deleteUser = async () => {
     try {
       await user.delete();
-      await clerk.signOut({ sessionId: session.id });
       router.navigate(environment.displayConfig.homeUrl);
-      // TODO: for routerless mode, explicitly open the "choose" route
-      clerk.openSignIn();
     } catch (e) {
       handleError(e, [], card.setError);
     }
   };
 
   return (
-    <>
-      <ContentPage
-        headerTitle={localizationKeys('userProfile.deletePage.title')}
-        Breadcrumbs={UserProfileBreadcrumbs}
-      >
-        <Form.Root onSubmit={deleteUser}>
-          <Text localizationKey={localizationKeys('userProfile.deletePage.description')} />
-          <FormButtons
-            submitLabel={localizationKeys('userProfile.deletePage.confirm')}
-            colorScheme='danger'
-          />
-        </Form.Root>
-      </ContentPage>
-    </>
+    <ContentPage
+      headerTitle={localizationKeys('userProfile.deletePage.title')}
+      Breadcrumbs={UserProfileBreadcrumbs}
+    >
+      <Form.Root onSubmit={deleteUser}>
+        <Text localizationKey={localizationKeys('userProfile.deletePage.description')} />
+        <FormButtons
+          submitLabel={localizationKeys('userProfile.deletePage.confirm')}
+          colorScheme='danger'
+        />
+      </Form.Root>
+    </ContentPage>
   );
 });
