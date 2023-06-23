5c4cd59c7ec347594d02b41e871ddfd71c207bb4
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-06-23 04:57:34 +0300

    fix(backend): Align build scripts

--- packages/backend/tsup.config.ts
+++ packages/backend/tsup.config.ts
@@ -1,22 +1,45 @@
+import type { Options } from 'tsup';
 import { defineConfig } from 'tsup';
 
+import { runAfterLast } from '../../scripts/utils';
+// @ts-ignore
 import { name, version } from './package.json';
 
 export default defineConfig(overrideOptions => {
-  const isProd = overrideOptions.env?.NODE_ENV === 'production';
+  const isWatch = !!overrideOptions.watch;
+  const shouldPublish = !!overrideOptions.env?.publish;
 
-  return {
+  // const onSuccess = (format: 'esm' | 'cjs') => {
+  //   const rsync = `rsync -r --include '*/' --include '*.js' --include '*.mjs' --include '*.cjs' --exclude='*' ./src/runtime ./dist/${format}/`;
+  //   // return `cp ./package.${format}.json ./dist/${format}/package.json && ${rsync}`;
+  //   return rsync;
+  // };
+
+  const common: Options = {
     entry: ['src/index.ts'],
-    onSuccess: 'tsc && npm run build:runtime',
-    minify: isProd,
+    onSuccess: `rsync -r --include '*/' --include '*.js' --include '*.mjs' --include '*.cjs' --exclude='*' ./src/runtime ./dist/`,
     sourcemap: true,
-    format: ['cjs', 'esm'],
     define: {
       PACKAGE_NAME: `"${name}"`,
       PACKAGE_VERSION: `"${version}"`,
-      __DEV__: `${!isProd}`,
+      __DEV__: `${isWatch}`,
     },
     external: ['#crypto', '#fetch'],
     legacyOutput: true,
+    bundle: true,
+    clean: true,
+    minify: false,
   };
+
+  const esm: Options = {
+    ...common,
+    format: 'esm',
+  };
+
+  const cjs: Options = {
+    ...common,
+    format: 'cjs',
+  };
+
+  return runAfterLast(['npm run build:declarations', shouldPublish && 'npm run publish:local'])(esm, cjs);
 });
