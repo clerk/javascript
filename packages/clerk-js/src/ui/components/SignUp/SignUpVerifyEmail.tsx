6cf5a2925b6ede57969f825f73f98b3748120fd8
George Desipris <george.desipris.56@gmail.com>
2023-06-23 11:15:18 +0300

    fix(clerk-js): Navigate to flow start when email/phone are not available in verify routes

--- packages/clerk-js/src/ui/components/SignUp/SignUpVerifyEmail.tsx
+++ packages/clerk-js/src/ui/components/SignUp/SignUpVerifyEmail.tsx
@@ -1,11 +1,23 @@
-import { useEnvironment } from '../../contexts';
+import { useEffect } from 'react';
+
+import { useCoreSignUp, useEnvironment } from '../../contexts';
 import { withCardStateProvider } from '../../elements';
+import { useRouter } from '../../router';
 import { SignUpEmailCodeCard } from './SignUpEmailCodeCard';
 import { SignUpEmailLinkCard } from './SignUpEmailLinkCard';
 
 export const SignUpVerifyEmail = withCardStateProvider(() => {
   const { userSettings } = useEnvironment();
+  const signUp = useCoreSignUp();
+  const router = useRouter();
   const { attributes } = userSettings;
+
+  //TODO: remove this once a global solution for route validation is ready
+  useEffect(() => {
+    if (!signUp.emailAddress) {
+      void router.navigate('../');
+    }
+  }, []);
 
   const emailLinkStrategyEnabled = attributes.email_address.verifications.includes('email_link');
   if (emailLinkStrategyEnabled) {
