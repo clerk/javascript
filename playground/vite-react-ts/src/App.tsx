164cb443e1dd2d0e7e000ac3e6e67a51da3c44e3
Stefanos Anagnostou <stefanos@clerk.dev>
2023-06-28 12:25:29 +0300

    fix(repo): Remove the env var check from React Vite playground

--- playground/vite-react-ts/src/App.tsx
+++ playground/vite-react-ts/src/App.tsx
@@ -1,13 +1,20 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import clerkLogo from '/clerk.svg'
-import './App.css'
-import { ClerkProvider, RedirectToSignIn, SignIn, SignUp, SignedIn, SignedOut, UserButton, UserProfile, useUser } from "@clerk/clerk-react";
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import clerkLogo from '/clerk.svg';
+import './App.css';
+import {
+  ClerkProvider,
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+  SignIn,
+  SignUp,
+  UserButton,
+  UserProfile,
+  useUser,
+} from '@clerk/clerk-react';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 
-if (!import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
-}
 const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
 
 function NavBar() {
@@ -15,19 +22,19 @@ function NavBar() {
     <nav>
       <ul>
         <li>
-          <a href="/">Home</a>
+          <a href='/'>Home</a>
         </li>
         <li>
-          <a href="/protected">Protected</a>
+          <a href='/protected'>Protected</a>
         </li>
         <li>
-          <a href="/sign-in">Sign In</a>
+          <a href='/sign-in'>Sign In</a>
         </li>
         <li>
-          <a href="/sign-up">Sign Up</a>
+          <a href='/sign-up'>Sign Up</a>
         </li>
         <li>
-          <a href="/user">User Profile</a>
+          <a href='/user'>User Profile</a>
         </li>
       </ul>
       <UserButton afterSignOutUrl='/' />
@@ -39,7 +46,7 @@ function PublicPage() {
   return (
     <>
       <h1>Home</h1>
-      <a href="/protected">Go to protected page</a>
+      <a href='/protected'>Go to protected page</a>
     </>
   );
 }
@@ -60,46 +67,71 @@ function ClerkProviderWithRoutes() {
   return (
     <ClerkProvider
       publishableKey={clerkPubKey}
-      navigate={(to) => navigate(to)}
+      navigate={to => navigate(to)}
     >
       <div>
-        <a href="#">
-          <img src={clerkLogo} className="clerk logo" alt="Clerk logo" />
+        <a href='#'>
+          <img
+            src={clerkLogo}
+            className='clerk logo'
+            alt='Clerk logo'
+          />
         </a>
-        <a href="#">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href='#'>
+          <img
+            src={reactLogo}
+            className='logo react'
+            alt='React logo'
+          />
         </a>
-        <a href="#">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href='#'>
+          <img
+            src={viteLogo}
+            className='logo'
+            alt='Vite logo'
+          />
         </a>
       </div>
       <h1>Clerk + React + Vite</h1>
       <NavBar />
       <Routes>
-        <Route path="/" element={<PublicPage />} />
         <Route
-          path="/sign-in/*"
-          element={<SignIn routing="path" path="/sign-in" />}
+          path='/'
+          element={<PublicPage />}
         />
         <Route
-          path="/sign-up/*"
-          element={<SignUp routing="path" path="/sign-up" />}
+          path='/sign-in/*'
+          element={
+            <SignIn
+              routing='path'
+              path='/sign-in'
+            />
+          }
         />
         <Route
-          path="/user/*"
+          path='/sign-up/*'
+          element={
+            <SignUp
+              routing='path'
+              path='/sign-up'
+            />
+          }
+        />
+        <Route
+          path='/user/*'
           element={<UserProfile />}
         />
         <Route
-          path="/protected"
+          path='/protected'
           element={
-          <>
-            <SignedIn>
-              <ProtectedPage />
-            </SignedIn>
-             <SignedOut>
-              <RedirectToSignIn />
-           </SignedOut>
-          </>
+            <>
+              <SignedIn>
+                <ProtectedPage />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
           }
         />
       </Routes>
@@ -115,4 +147,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
