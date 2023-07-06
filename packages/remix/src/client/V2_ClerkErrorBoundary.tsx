ff28972085adeccdd33a118032f1585e2e6d89b4
Stefanos Anagnostou <stefanos@clerk.dev>
2023-07-06 12:22:29 +0300

    chore(remix): Add a better description for `V2_ClerkErrorBoundary`

--- packages/remix/src/client/V2_ClerkErrorBoundary.tsx
+++ packages/remix/src/client/V2_ClerkErrorBoundary.tsx
@@ -3,8 +3,13 @@ import React from 'react';
 
 import { Interstitial } from './Interstitial';
 
-/*
- * V2_ClerkErrorBoundary needs the `v2_errorBoundary` future flag to be enabled.
+/**
+ * Support for Remix ErrorBoundary v2.
+ * You need to have the v2_errorBoundary flag enabled in your Remix config,
+ * as shown here: https://remix.run/docs/en/main/route/error-boundary-v2
+ *
+ * @experimental This API is experimental and might change. This API will become stable
+ * once Remix officially releases v2.
  */
 export function V2_ClerkErrorBoundary(RootErrorBoundary?: React.ComponentType) {
   return () => {
