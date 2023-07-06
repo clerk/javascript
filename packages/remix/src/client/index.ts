bdc474ad96e90c0b32d3f0400563d0e92ee005f1
Stefanos Anagnostou <stefanos@clerk.dev>
2023-07-06 12:45:15 +0300

    chore(remix): Export explicitly the V2_ClerkErrorBoundary from its file

--- packages/remix/src/client/index.ts
+++ packages/remix/src/client/index.ts
@@ -1,5 +1,5 @@
 export * from './RemixClerkProvider';
 export { ClerkApp } from './ClerkApp';
 export * from './ClerkCatchBoundary';
-export * from './V2_ClerkErrorBoundary';
+export { V2_ClerkErrorBoundary } from './V2_ClerkErrorBoundary';
 export { WithClerkState } from './types';
