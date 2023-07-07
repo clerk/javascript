366dcd6095a2f43ec6f5e4c92decafaf86d2af17
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-07-07 21:25:29 +0300

    feat(repo): Introduce page objects and test utils

---
+++ integration/testUtils/appPageObject.ts
@@ -0,0 +1,43 @@
+import type { Page } from '@playwright/test';
+
+import type { Application } from '../adapters/application';
+
+export const createAppPageObject = (testArgs: { page: Page }, app: Application) => {
+  const { page } = testArgs;
+  const appPage = Object.create(page) as Page;
+  const helpers = {
+    goToStart: async () => {
+      try {
+        await page.goto(app.serverUrl);
+      } catch (e) {
+        // do not fail the test if interstitial is returned (401)
+      }
+    },
+    goToRelative: async (path: string, opts: { searchParams?: URLSearchParams } = {}) => {
+      const url = new URL(path, app.serverUrl);
+      if (opts.searchParams) {
+        url.search = opts.searchParams.toString();
+      }
+      await page.goto(url.toString(), { timeout: 10000 });
+    },
+    goToSignIn: (searchParams: URLSearchParams) => {
+      return helpers.goToRelative('/sign-in', { searchParams });
+    },
+    goToSignUp: (searchParams: URLSearchParams) => {
+      return helpers.goToRelative('/sign-up', { searchParams });
+    },
+    goToUserProfile: () => {
+      return helpers.goToRelative('/user');
+    },
+    waitForClerkJsLoaded: async () => {
+      return page.waitForFunction(() => {
+        // @ts-ignore
+        return window.Clerk?.isReady();
+      });
+    },
+    waitForClerkComponentMounted: async () => {
+      return page.waitForSelector('.cl-rootBox', { state: 'attached' });
+    },
+  };
+  return Object.assign(appPage, helpers);
+};
---
+++ integration/testUtils/index.ts
@@ -0,0 +1,92 @@
+import { Clerk } from '@clerk/backend';
+import type { Browser, BrowserContext, Page } from '@playwright/test';
+
+import type { Application } from '../adapters/application';
+import { createAppPageObject } from './appPageObject';
+import { createEmailService } from './emailService';
+import type { EnchancedPage, TestArgs } from './signInPageObject';
+import { createSignInComponentPageObject } from './signInPageObject';
+import { createSignUpComponentPageObject } from './signUpPageObject';
+import { createUserService } from './usersService';
+
+const createClerkClient = (app: Application) => {
+  return Clerk({
+    secretKey: app.env.privateVariables.get('CLERK_SECRET_KEY'),
+    publishableKey: app.env.publicVariables.get('CLERK_PUBLISHABLE_KEY'),
+  });
+};
+
+const createExpectPageObject = ({ page }: TestArgs) => {
+  return {
+    toBeSignedOut: () => {
+      return page.waitForFunction(() => {
+        // @ts-ignore
+        return !window.Clerk?.user;
+      });
+    },
+    toBeSignedIn: async () => {
+      return page.waitForFunction(() => {
+        // @ts-ignore
+        return !!window.Clerk?.user;
+      });
+    },
+  };
+};
+
+type CreateAppPageObjectArgs = { page: Page; context: BrowserContext; browser: Browser };
+
+export const createTestUtils = <
+  Params extends { app: Application } & Partial<CreateAppPageObjectArgs>,
+  Services = typeof services,
+  PO = typeof pageObjects,
+  BH = typeof browserHelpers,
+  FullReturn = { services: Services; po: PO; tabs: BH; page: EnchancedPage },
+  OnlyAppReturn = { services: Services },
+>(
+  params: Params,
+): Params extends Partial<CreateAppPageObjectArgs> ? FullReturn : OnlyAppReturn => {
+  const { app, context, browser } = params || {};
+
+  const clerkClient = createClerkClient(app);
+  const services = {
+    email: createEmailService(),
+    users: createUserService(clerkClient),
+    clerk: clerkClient,
+  };
+
+  if (!params.page) {
+    return { services } as any;
+  }
+
+  const page = createAppPageObject({ page: params.page }, app);
+  const testArgs = { page, context, browser };
+
+  const pageObjects = {
+    signUp: createSignUpComponentPageObject(testArgs),
+    signIn: createSignInComponentPageObject(testArgs),
+    expect: createExpectPageObject(testArgs),
+  };
+
+  const browserHelpers = {
+    runInNewTab: async (
+      cb: (u: { services: Services; po: PO; page: EnchancedPage }, context: BrowserContext) => Promise<unknown>,
+    ) => {
+      const u = createTestUtils({ app, page: createAppPageObject({ page: await context.newPage() }, app) });
+      await cb(u as any, context);
+      return u;
+    },
+    runInNewBrowser: async (
+      cb: (u: { services: Services; po: PO; page: EnchancedPage }, context: BrowserContext) => Promise<unknown>,
+    ) => {
+      if (!browser) {
+        throw new Error('Browser is not defined. Did you forget to pass it to createPageObjects?');
+      }
+      const context = await browser.newContext();
+      const u = createTestUtils({ app, page: createAppPageObject({ page: await context.newPage() }, app) });
+      await cb(u as any, context);
+      return u;
+    },
+  };
+
+  return { page, services, po: pageObjects, tabs: browserHelpers } as any;
+};
