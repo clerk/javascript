49afc862c8f09069c9fdc64e7f39d5aa7cfcb409
panteliselef <panteliselef@outlook.com>
2023-07-15 09:58:50 +0300

    chore(clerk-react): Mark `setSession` as deprecated
    This function was already deprecated but was not marked as such when re-exported from the hooks

--- packages/react/src/hooks/useSessionList.ts
+++ packages/react/src/hooks/useSessionList.ts
@@ -1,11 +1,34 @@
-import type { SetActive, SessionResource, SetSession } from '@clerk/types';
+import type { SessionResource, SetActive, SetSession } from '@clerk/types';
 
 import { useClientContext } from '../contexts/ClientContext';
 import { useIsomorphicClerkContext } from '../contexts/IsomorphicClerkContext';
 
 type UseSessionListReturn =
-  | { isLoaded: false; sessions: undefined; setSession: undefined; setActive: undefined }
-  | { isLoaded: true; sessions: SessionResource[]; setSession: SetSession; setActive: SetActive };
+  | {
+      isLoaded: false;
+      sessions: undefined;
+      /**
+       * @deprecated This method is deprecated and will be removed in the future. Use {@link Clerk.setActive} instead
+       * Set the current session explicitly. Setting the session to `null` unsets the active session and signs out the user.
+       * @param session Passed session resource object, session id (string version) or null
+       * @param beforeEmit Callback run just before the active session is set to the passed object. Can be used to hook up for pre-navigation actions.
+       */
+      setSession: undefined;
+      setActive: undefined;
+    }
+  | {
+      isLoaded: true;
+      sessions: SessionResource[];
+
+      /**
+       * @deprecated This method is deprecated and will be removed in the future. Use {@link Clerk.setActive} instead
+       * Set the current session explicitly. Setting the session to `null` unsets the active session and signs out the user.
+       * @param session Passed session resource object, session id (string version) or null
+       * @param beforeEmit Callback run just before the active session is set to the passed object. Can be used to hook up for pre-navigation actions.
+       */
+      setSession: SetSession;
+      setActive: SetActive;
+    };
 
 type UseSessionList = () => UseSessionListReturn;
 
