618c5f417d311a2d3046e10916e7a843115e5459
Dimitris Klouvas <dimitris@clerk.dev>
2023-07-17 18:19:01 +0300

    feat(backend): Introduce buildRequestUrl utility
    The buildRequestUrl utility will be used to construct the
    request url using a request and optional path parameter
    (used to overrided the path of the request url).
    This utility will use the forwarded headers values and the
    request.url to generate and return the actual request URL

--- packages/backend/src/exports.test.ts
+++ packages/backend/src/exports.test.ts
@@ -32,6 +32,7 @@ export default (QUnit: QUnit) => {
         'Token',
         'User',
         'Verification',
+        'buildRequestUrl',
         'constants',
         'createAuthenticateRequest',
         'createIsomorphicRequest',
