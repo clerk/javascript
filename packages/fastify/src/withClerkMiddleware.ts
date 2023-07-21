3dc68f4355b9a7e4c48aa94b0d7b6cd0e507a84f
Dimitris Klouvas <dimitris@clerk.dev>
2023-07-21 13:02:32 +0300

    chore(fastify): Cleanup request url construction and fixed playground issues
    Playground app issues:
    - missing yalc to link local deps
    - request to /private hangs since the connection is not closed
    
    Dropped the calculation of request urls using the forwarded headers
    since fastify does that already and populates the request with
    our needed values.

--- packages/fastify/src/withClerkMiddleware.ts
+++ packages/fastify/src/withClerkMiddleware.ts
@@ -4,9 +4,6 @@ import type { FastifyReply, FastifyRequest } from 'fastify';
 import { clerkClient } from './clerkClient';
 import * as constants from './constants';
 import type { ClerkFastifyOptions } from './types';
-import { getSingleValueFromArrayHeader } from './utils';
-
-const DUMMY_URL_BASE = 'http://clerk-dummy';
 
 export const withClerkMiddleware = (options: ClerkFastifyOptions) => {
   return async (req: FastifyRequest, reply: FastifyReply) => {
@@ -24,20 +21,12 @@ export const withClerkMiddleware = (options: ClerkFastifyOptions) => {
           (acc, key) => Object.assign(acc, { [key]: req?.headers[key] }),
           {},
         );
+        // Making some manual tests it seems that FastifyRequest populates the req protocol / hostname
+        // based on the forwarded headers. With that in mind we can use those attributes instead
+        // of using the headers to calculate them by our own.
+        const reqUrl = new URL(req.url, `${req.protocol}://${req.hostname}`);
         const headers = new Headers(requestHeaders);
-        const forwardedHostHeader = getSingleValueFromArrayHeader(
-          headers?.get(constants.Headers.ForwardedHost) || undefined,
-        );
-        if (forwardedHostHeader) {
-          headers.set(constants.Headers.ForwardedHost, forwardedHostHeader);
-        }
-        const forwardedPortHeader = getSingleValueFromArrayHeader(
-          headers?.get(constants.Headers.ForwardedPort) || undefined,
-        );
-        if (forwardedPortHeader) {
-          headers.set(constants.Headers.ForwardedPort, forwardedPortHeader);
-        }
-        const reqUrl = isRelativeUrl(req.url) ? getAbsoluteUrlFromHeaders(req.url, headers) : req.url;
+
         return new Request(reqUrl, {
           method: req.method,
           headers,
@@ -71,49 +60,4 @@ export const withClerkMiddleware = (options: ClerkFastifyOptions) => {
     // @ts-ignore
     req.auth = requestState.toAuth();
   };
-};
-
-// TODO: Move the utils below to shared package
-// Creating a Request object requires a valid absolute URL
-// Fastify's req.url is relative, so we need to construct an absolute URL
-const getAbsoluteUrlFromHeaders = (url: string, headers: Headers): URL => {
-  const forwardedProto = headers.get(constants.Headers.ForwardedProto);
-  const forwardedPort = headers.get(constants.Headers.ForwardedPort);
-  const forwardedHost = headers.get(constants.Headers.ForwardedHost);
-
-  const fwdProto = getFirstValueFromHeaderValue(forwardedProto);
-  let fwdPort = getFirstValueFromHeaderValue(forwardedPort);
-
-  // If forwardedPort mismatch with forwardedProto determine forwardedPort
-  // from forwardedProto as fallback (if exists)
-  // This check fixes the Railway App issue
-  const fwdProtoHasMoreValuesThanFwdPorts =
-    (forwardedProto || '').split(',').length > (forwardedPort || '').split(',').length;
-  if (fwdProto && fwdProtoHasMoreValuesThanFwdPorts) {
-    fwdPort = getPortFromProtocol(fwdProto);
-  }
-
-  try {
-    return new URL(url, `${fwdProto}://${forwardedHost}${fwdPort ? ':' + fwdPort : ''}`);
-  } catch (e) {
-    return new URL(url, DUMMY_URL_BASE);
-  }
-};
-
-const PROTOCOL_TO_PORT_MAPPING: Record<string, string> = {
-  http: '80',
-  https: '443',
-} as const;
-
-function getPortFromProtocol(protocol: string) {
-  return PROTOCOL_TO_PORT_MAPPING[protocol];
-}
-
-function getFirstValueFromHeaderValue(value?: string | null) {
-  return value?.split(',')[0]?.trim() || '';
-}
-
-const isRelativeUrl = (url: string) => {
-  const u = new URL(url, DUMMY_URL_BASE);
-  return u.origin === DUMMY_URL_BASE;
 };
