d9ce9d9feb0af723b6752107ef0910ea4c6830d3
Dimitris Klouvas <dimitris@clerk.dev>
2023-07-31 14:32:04 +0300

    chore(backend): Add tests for BAPI interstitial request

---
+++ packages/backend/src/tokens/interstitial.test.ts
@@ -0,0 +1,39 @@
+import type QUnit from 'qunit';
+import sinon from 'sinon';
+
+import runtime from '../runtime';
+import { loadInterstitialFromBAPI } from './interstitial';
+
+export default (QUnit: QUnit) => {
+  const { module, test } = QUnit;
+
+  module('loadInterstitialFromBAPI', hooks => {
+    let fakeFetch;
+
+    hooks.afterEach(() => {
+      fakeFetch?.restore();
+    });
+
+    test('executes GET request to /v1/public/interstitial', async assert => {
+      fakeFetch = sinon.stub(runtime, 'fetch');
+      fakeFetch.onCall(0).returns({
+        text: () => '<html>Interstitial Page</html>',
+        ok: true,
+        status: 200,
+      });
+
+      const publishableKey = 'pk_test_Y2xlcmsuaW5jbHVkZWQua2F0eWRpZC05Mi5sY2wuZGV2JA';
+      const resp = await loadInterstitialFromBAPI({
+        apiUrl: 'https://api.clerk.test',
+        publishableKey,
+        frontendApi: 'accounts.included.katydid-92.lcl.dev',
+      });
+
+      const qs = `clerk_js_version=latest&publishable_key=${publishableKey}&sign_in_url=`;
+      const interstitialUrl = `https://api.clerk.test/v1/public/interstitial?${qs}`;
+
+      assert.equal(resp, '<html>Interstitial Page</html>');
+      assert.ok(fakeFetch.calledOnceWith(interstitialUrl, { method: 'GET' }));
+    });
+  });
+};
