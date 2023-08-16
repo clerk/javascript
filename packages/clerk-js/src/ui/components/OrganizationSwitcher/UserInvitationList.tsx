d066c239e3fab5abe8b73310b720b6f551354c20
panteliselef <panteliselef@outlook.com>
2023-08-16 12:10:31 +0300

    feat(clerk-js): OrganizationSwitcher display accepted and pending suggestions (#1605)
    * fix(clerk-js): UserSuggestionList include accepted and pending suggestions
    
    * feat(clerk-js): Support arrays in search params
    
    * feat(clerk-js): UserSuggestionList fetches pending and accepted
    
    * chore(clerk-js): Add changeset

--- packages/clerk-js/src/ui/components/OrganizationSwitcher/UserInvitationList.tsx
+++ packages/clerk-js/src/ui/components/OrganizationSwitcher/UserInvitationList.tsx
@@ -6,6 +6,7 @@ import { OrganizationPreview, useCardState, withCardStateProvider } from '../../
 import { useInView } from '../../hooks';
 import { common } from '../../styledSystem';
 import { handleError } from '../../utils';
+import { organizationListParams } from './utils';
 
 export const UserInvitationList = () => {
   const { ref } = useInView({
@@ -18,9 +19,7 @@ export const UserInvitationList = () => {
   });
 
   const { userInvitations } = useCoreOrganizationList({
-    userInvitations: {
-      infinite: true,
-    },
+    userInvitations: organizationListParams.userInvitations,
   });
 
   if ((userInvitations.count ?? 0) === 0) {
@@ -100,11 +99,8 @@ export const UserInvitationList = () => {
 const AcceptRejectInvitationButtons = (props: UserOrganizationInvitationResource) => {
   const card = useCardState();
   const { userInvitations } = useCoreOrganizationList({
-    userInvitations: {
-      infinite: true,
-    },
+    userInvitations: organizationListParams.userInvitations,
   });
-
   const mutateSwrState = () => {
     (userInvitations as any)?.unstable__mutate?.();
   };
@@ -124,7 +120,7 @@ const AcceptRejectInvitationButtons = (props: UserOrganizationInvitationResource
       size='sm'
       isLoading={card.isLoading}
       onClick={handleAccept}
-      localizationKey={localizationKeys('organizationSwitcher.invitationAccept')}
+      localizationKey={localizationKeys('organizationSwitcher.action__invitationAccept')}
     />
   );
 };
