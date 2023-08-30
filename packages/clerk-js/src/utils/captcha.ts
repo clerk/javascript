193ec04d0ee19950b566515446bf4832b1a51882
panteliselef <panteliselef@outlook.com>
2023-08-30 15:32:59 +0300

    chore(clerk-js): Add retrying to failed turnstile challenges

--- packages/clerk-js/src/utils/captcha.ts
+++ packages/clerk-js/src/utils/captcha.ts
@@ -11,36 +11,37 @@ interface RenderOptions {
    * Controls whether the widget should automatically retry to obtain a token if it did not succeed.
    * The default is auto, which will retry automatically. This can be set to never to disable retry upon failure.
    */
-  retry: 'auto' | 'never';
+  retry?: 'auto' | 'never';
   /**
    * When retry is set to auto, retry-interval controls the time between retry attempts in milliseconds.
    * Value must be a positive integer less than 900000.
    * @default 8000
    */
-  'retry-interval': number;
+  'retry-interval'?: number;
   /**
    * Automatically refreshes the token when it expires.
    * Can take auto, manual or never.
    * @default 'auto'
    */
-  'refresh-expired': 'auto' | 'manual' | 'never';
+  'refresh-expired'?: 'auto' | 'manual' | 'never';
   /**
    * A JavaScript callback invoked upon success of the challenge.
    * The callback is passed a token that can be validated.
    * @param token string
    */
-  callback: (token: string) => void;
+  callback?: (token: string) => void;
   /**
    * A JavaScript callback invoked when there is an error (e.g. network error or the challenge failed)
    * @param errorCode string
    */
-  'error-callback': (errorCode: string) => void;
+  'error-callback'?: (errorCode: string) => void;
 }
 
 interface Turnstile {
   execute: (container?: string | HTMLElement | null, params?: RenderOptions) => void;
   render: (container?: string | HTMLElement | null, params?: RenderOptions) => string;
   remove: (widgetId: string) => void;
+  reset: (widgetId: string) => void;
 }
 
 declare global {
@@ -73,19 +74,31 @@ export const getCaptchaToken = async (captchaOptions: { siteKey: string; scriptU
   document.body.appendChild(div);
 
   const captcha = await loadCaptcha(scriptUrl);
+  let retries = 0;
 
   const handleCaptchaTokenGeneration = (): Promise<[string, string]> => {
     return new Promise((resolve, reject) => {
       try {
         const id = captcha.render(`.${WIDGET_CLASSNAME}`, {
           sitekey,
-          retry: 'auto',
-          'retry-interval': 500,
+          retry: 'never',
+          // 'retry-interval': 500,
           'refresh-expired': 'auto',
           callback: function (token: string) {
             resolve([token, id]);
           },
           'error-callback': function (errorCode) {
+            /**
+             * By setting retry to 'never' the responsibility for implementing retrying is ours
+             * https://developers.cloudflare.com/turnstile/reference/client-side-errors/#retrying
+             */
+            if (retries < 2) {
+              setTimeout(() => {
+                captcha.reset(id);
+                retries++;
+              }, 500);
+              return;
+            }
             reject([errorCode, id]);
           },
         });
