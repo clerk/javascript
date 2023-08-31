23284991ea2af81707e033229c75dc0308c6bb2a
Haris Chaniotakis <chanioxaris@gmail.com>
2023-08-31 15:37:02 +0300

    feat(clerk-js): Add retries in turnstile + improve error logging  (#1658)
    * chore(clerk-js): Consume CAPTCHA errors during sign up
    
    There are a possibility the CAPTCHA service we are using (CF Turnstile)
    during sign up, won't be able to generate a token but rather return an
    error. In this commit we update our logic to consume this error and pass
    it, along with other parameters, in the sign-up create request
    
    * chore(clerk-js): Improve error handling when turnstile fails to load or a challenge
    + Update Turnstile types
    + documentation comments
    
    * chore(clerk-js): Add retrying to failed turnstile challenges
    
    * fix(clerk-js): CaptchaError was never added to parameters of signup.create
    
    * chore(clerk-js): Turnstile should only retry for specific errorCodes
    
    * chore(clerk-js): Turnstile upon rejection send all errorCodes in a comma separated string
    
    * test(clerk-js): Update test for error codes
    
    * chore(clerk-js): Inplace populate captchaToken and captchaError for params
    
    ---------
    
    Co-authored-by: panteliselef <panteliselef@outlook.com>

--- packages/clerk-js/src/utils/captcha.ts
+++ packages/clerk-js/src/utils/captcha.ts
@@ -1,55 +1,139 @@
 import { loadScript } from '@clerk/shared';
 
+import { clerkFailedToLoadThirdPartyScript } from '../core/errors';
+
 interface RenderOptions {
+  /**
+   * Every widget has a sitekey. This sitekey is associated with the corresponding widget configuration and is created upon the widget creation.
+   */
   sitekey: string;
-  retry: string;
-  callback: (token: string) => void;
-  'error-callback': (err: any) => void;
+  /**
+   * Controls whether the widget should automatically retry to obtain a token if it did not succeed.
+   * The default is auto, which will retry automatically. This can be set to never to disable retry upon failure.
+   */
+  retry?: 'auto' | 'never';
+  /**
+   * When retry is set to auto, retry-interval controls the time between retry attempts in milliseconds.
+   * Value must be a positive integer less than 900000.
+   * @default 8000
+   */
+  'retry-interval'?: number;
+  /**
+   * Automatically refreshes the token when it expires.
+   * Can take auto, manual or never.
+   * @default 'auto'
+   */
+  'refresh-expired'?: 'auto' | 'manual' | 'never';
+  /**
+   * A JavaScript callback invoked upon success of the challenge.
+   * The callback is passed a token that can be validated.
+   * @param token string
+   */
+  callback?: (token: string) => void;
+  /**
+   * A JavaScript callback invoked when there is an error (e.g. network error or the challenge failed)
+   * @param errorCode string
+   */
+  'error-callback'?: (errorCode: string) => void;
+}
+
+interface Turnstile {
+  execute: (container?: string | HTMLElement | null, params?: RenderOptions) => void;
+  render: (container?: string | HTMLElement | null, params?: RenderOptions) => string;
+  remove: (widgetId: string) => void;
+  reset: (widgetId: string) => void;
 }
 
 declare global {
   export interface Window {
-    turnstile: { execute: (container?: string | HTMLElement | null, params?: RenderOptions) => void };
+    turnstile: Turnstile;
   }
 }
 
 const WIDGET_CLASSNAME = 'clerk-captcha';
 
+export const shouldRetryTurnstileErrorCode = (errorCode: string) => {
+  const codesWithRetries = ['crashed', 'undefined_error', '102', '103', '104', '106', '110600', '300100', '600'];
+
+  return !!codesWithRetries.find(w => errorCode.startsWith(w));
+};
+
 export async function loadCaptcha(url: string) {
   if (!window.turnstile) {
-    await loadScript(url, { defer: true });
+    try {
+      await loadScript(url, { defer: true });
+    } catch (_) {
+      // Rethrow with specific message
+      clerkFailedToLoadThirdPartyScript('Cloudflare Turnstile');
+    }
   }
   return window.turnstile;
 }
+
 export const getCaptchaToken = async (captchaOptions: { siteKey: string; scriptUrl: string }) => {
   const { siteKey: sitekey, scriptUrl } = captchaOptions;
-  let captchaToken = '';
+  let captchaToken = '',
+    id = '';
 
   const div = document.createElement('div');
   div.classList.add(WIDGET_CLASSNAME);
   document.body.appendChild(div);
-  const captcha = await loadCaptcha(scriptUrl);
 
-  const handleCaptchaTokenGeneration = (): Promise<string> => {
+  const captcha = await loadCaptcha(scriptUrl);
+  let retries = 0;
+  const errorCodes: string[] = [];
+
+  const handleCaptchaTokenGeneration = (): Promise<[string, string]> => {
     return new Promise((resolve, reject) => {
-      return captcha.execute(`.${WIDGET_CLASSNAME}`, {
-        sitekey,
-        retry: 'never',
-        callback: function (token: string) {
-          resolve(token);
-        },
-        'error-callback': function (err) {
-          reject(err);
-        },
-      });
+      try {
+        const id = captcha.render(`.${WIDGET_CLASSNAME}`, {
+          sitekey,
+          retry: 'never',
+          'refresh-expired': 'auto',
+          callback: function (token: string) {
+            resolve([token, id]);
+          },
+          'error-callback': function (errorCode) {
+            errorCodes.push(errorCode);
+            /**
+             * By setting retry to 'never' the responsibility for implementing retrying is ours
+             * https://developers.cloudflare.com/turnstile/reference/client-side-errors/#retrying
+             */
+            if (retries < 2 && shouldRetryTurnstileErrorCode(errorCode)) {
+              setTimeout(() => {
+                captcha.reset(id);
+                retries++;
+              }, 250);
+              return;
+            }
+            reject([errorCodes.join(','), id]);
+          },
+        });
+      } catch (e) {
+        /**
+         * There is a case the turnstile may fail before the challenge has started.
+         * In such case the 'error-callback' does not fire.
+         * We should mark the promise as rejected.
+         */
+        reject([e, undefined]);
+      }
     });
   };
 
   try {
-    captchaToken = await handleCaptchaTokenGeneration();
-  } catch (e) {
-    console.warn(e);
+    [captchaToken, id] = await handleCaptchaTokenGeneration();
+    // After a successful challenge remove it
+    captcha.remove(id);
+  } catch ([e, id]) {
+    if (id) {
+      // After a failed challenge remove it
+      captcha.remove(id);
+    }
+    throw {
+      captchaError: e,
+    };
   } finally {
+    // After challenge has run remove node element attached
     document.body.removeChild(div);
   }
 
