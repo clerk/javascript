42ff7025ace7253b5c5b107e61e996a4ad3998b0
panteliselef <panteliselef@outlook.com>
2023-09-05 12:10:00 +0300

    test(clerk-js): Test helpers for populateCache of swr mutate

--- packages/clerk-js/src/ui/components/OrganizationSwitcher/utils.ts
+++ packages/clerk-js/src/ui/components/OrganizationSwitcher/utils.ts
@@ -15,44 +15,54 @@ export const organizationListParams = {
   },
 } satisfies Parameters<typeof useCoreOrganizationList>[0];
 
+export const populateCacheUpdateItem = <T extends { id: string }>(
+  updatedItem: T,
+  itemsInfinitePages: ClerkPaginatedResponse<T>[],
+) => {
+  return itemsInfinitePages.map(item => {
+    const newData = item.data.map(obj => {
+      if (obj.id === updatedItem.id) {
+        return {
+          ...updatedItem,
+        };
+      }
+
+      return obj;
+    });
+    return { ...item, data: newData };
+  });
+};
+
 export const updateCacheInPlace =
   (userSuggestions: any) =>
   <T extends { id: string }>(result: T): any => {
     userSuggestions?.unstable__mutate?.(result, {
-      populateCache: (updatedItem: T, itemsInfinitePages: ClerkPaginatedResponse<T>[]) => {
-        return itemsInfinitePages.map(item => {
-          const newData = item.data.map(obj => {
-            if (obj.id === updatedItem.id) {
-              return {
-                ...updatedItem,
-              };
-            }
-
-            return obj;
-          });
-          return { ...item, data: newData };
-        });
-      },
+      populateCache: populateCacheUpdateItem,
       // Since `accept` gives back the updated information,
       // we don't need to revalidate here.
       revalidate: false,
     });
   };
 
+export const populateCacheRemoveItem = <T extends { id: string }>(
+  updatedItem: T,
+  itemsInfinitePages: ClerkPaginatedResponse<T>[],
+) => {
+  const prevTotalCount = itemsInfinitePages[itemsInfinitePages.length - 1].total_count;
+
+  return itemsInfinitePages.map(item => {
+    const newData = item.data.filter(obj => {
+      return obj.id !== updatedItem.id;
+    });
+    return { ...item, data: newData, total_count: prevTotalCount - 1 };
+  });
+};
+
 export const removeItemFromPaginatedCache =
   (userInvitations: any) =>
   <T extends { id: string }>(result: T): any => {
     userInvitations?.unstable__mutate?.(result, {
-      populateCache: (updatedItem: T, itemsInfinitePages: ClerkPaginatedResponse<T>[]) => {
-        const prevTotalCount = itemsInfinitePages[itemsInfinitePages.length - 1].total_count;
-
-        return itemsInfinitePages.map(item => {
-          const newData = item.data.filter(obj => {
-            return obj.id !== updatedItem.id;
-          });
-          return { ...item, data: newData, total_count: prevTotalCount - 1 };
-        });
-      },
+      populateCache: populateCacheRemoveItem,
       // Since `accept` gives back the updated information,
       // we don't need to revalidate here.
       revalidate: false,
