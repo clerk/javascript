3e8e7cdedbf561f35c12180f28d31869d2d7fcc4
Bryce Kalow <bryce@clerk.dev>
2023-09-13 14:14:07 -0500

    fix(remix): Ensure interstitial is triggered on route transitions

--- packages/remix/src/client/V2_ClerkErrorBoundary.tsx
+++ packages/remix/src/client/V2_ClerkErrorBoundary.tsx
@@ -18,6 +18,15 @@ export function V2_ClerkErrorBoundary(RootErrorBoundary?: React.ComponentType) {
     if (isRouteErrorResponse(error)) {
       const { __clerk_ssr_interstitial_html } = error?.data?.clerkState?.__internal_clerk_state || {};
       if (__clerk_ssr_interstitial_html) {
+        /**
+         * In the (unlikely) case we trigger an interstitial during a client-side transition, we need to reload the page so the interstitial can properly trigger. Without a reload, the injected script tag does not get executed.
+         * Notably, this currently triggers for satellite domain syncing.
+         */
+        if (typeof window !== 'undefined') {
+          window.location.reload();
+          return;
+        }
+
         return <Interstitial html={__clerk_ssr_interstitial_html} />;
       }
     }
