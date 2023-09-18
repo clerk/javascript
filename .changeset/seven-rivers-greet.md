41de12f46602fe4a60a6ee040e0ce4c751460c2c
Dimitris Klouvas <dimitris@clerk.dev>
2023-09-18 20:20:43 +0300

    feat(shared): Introduce deprecated/deprecatedProperty to report deprecation once

---
+++ .changeset/seven-rivers-greet.md
@@ -0,0 +1,5 @@
+---
+'@clerk/shared': minor
+---
+
+Introduce `deprecated()` and `deprecatedProperty()` helper methods to report deprecation warnings once on console
--- packages/shared/src/utils/index.ts
+++ packages/shared/src/utils/index.ts
@@ -23,3 +23,4 @@ export * from './isomorphicAtob';
 export * from './globs';
 export * from './loadScript';
 export { isDevelopmentEnvironment } from './runtimeEnvironment';
+export { deprecated, deprecatedProperty } from './deprecated';
