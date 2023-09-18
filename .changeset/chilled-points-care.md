80afc34583e82b71635960b043c0f638a123593d
Stefanos Anagnostou <stefanos@clerk.dev>
2023-09-18 13:37:35 +0300

    feat(remix): Refactor remix package to support Remix v2

---
+++ .changeset/chilled-points-care.md
@@ -0,0 +1,8 @@
+---
+'@clerk/remix': major
+---
+
+Refactor the remix package to support Remix v2
+
+Breaking changes:
+- Renamed the `V2_ClerkErrorBoundary` to `ClerkErrorBoundary`.
--- packages/remix/src/client/index.ts
+++ packages/remix/src/client/index.ts
@@ -1,6 +1,5 @@
 export * from './RemixClerkProvider';
 export { ClerkApp } from './ClerkApp';
-export * from './ClerkCatchBoundary';
-export { V2_ClerkErrorBoundary } from './V2_ClerkErrorBoundary';
+export { ClerkErrorBoundary } from './ClerkErrorBoundary';
 export { WithClerkState } from './types';
 export { SignIn, SignUp } from './uiComponents';
