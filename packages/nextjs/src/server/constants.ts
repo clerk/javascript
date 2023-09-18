1ac52c7add7b7bf56781d67cca12dfb94568043b
Dimitris Klouvas <dimitris@clerk.dev>
2023-09-19 02:50:27 +0300

    chore(nextjs): Warn about NEXT_PUBLIC_CLERK_FRONTEND_API environment variable deprecation

--- packages/nextjs/src/server/constants.ts
+++ packages/nextjs/src/server/constants.ts
@@ -19,7 +19,13 @@ if (API_KEY) {
   deprecated('CLERK_API_KEY', 'Use `CLERK_SECRET_KEY` environment variable instead.');
 }
 export const SECRET_KEY = process.env.CLERK_SECRET_KEY || '';
+/**
+ * @deprecated Use `PUBLISHABLE_KEY` instead.
+ */
 export const FRONTEND_API = process.env.NEXT_PUBLIC_CLERK_FRONTEND_API || '';
+if (FRONTEND_API) {
+  deprecated('NEXT_PUBLIC_CLERK_FRONTEND_API', 'Use `NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY` environment variable instead.');
+}
 export const PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || '';
 export const DOMAIN = process.env.NEXT_PUBLIC_CLERK_DOMAIN || '';
 export const PROXY_URL = process.env.NEXT_PUBLIC_CLERK_PROXY_URL || '';
