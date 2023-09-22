e71f40882e58a8dcc5a035d9c01617b0b8996028
Dimitris Klouvas <dimitris@clerk.dev>
2023-09-22 14:37:49 +0300

    chore(shared): Move share-able code from backend to shared package
    This is a preparation step to remove the share-able dependencies
    from backend package and import them from shared package

--- packages/shared/src/utils/url.ts
+++ packages/shared/src/utils/url.ts
@@ -1,3 +1,5 @@
+import { isStaging } from './instance';
+
 export function parseSearchParams(queryString = ''): URLSearchParams {
   if (queryString.startsWith('?')) {
     queryString = queryString.slice(1);
@@ -25,3 +27,28 @@ export function addClerkPrefix(str: string | undefined) {
   const stripped = str.replace(regex, '');
   return `clerk.${stripped}`;
 }
+
+export const getClerkJsMajorVersionOrTag = (frontendApi: string, pkgVersion?: string) => {
+  if (!pkgVersion && isStaging(frontendApi)) {
+    return 'staging';
+  }
+
+  if (!pkgVersion) {
+    return 'latest';
+  }
+
+  if (pkgVersion.includes('next')) {
+    return 'next';
+  }
+
+  return pkgVersion.split('.')[0] || 'latest';
+};
+
+export const getScriptUrl = (
+  frontendApi: string,
+  { pkgVersion, clerkJSVersion }: { pkgVersion?: string; clerkJSVersion?: string },
+) => {
+  const noSchemeFrontendApi = frontendApi.replace(/http(s)?:\/\//, '');
+  const major = getClerkJsMajorVersionOrTag(frontendApi, pkgVersion);
+  return `https://${noSchemeFrontendApi}/npm/@clerk/clerk-js@${clerkJSVersion || major}/dist/clerk.browser.js`;
+};
