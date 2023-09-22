07ede0f959f232f6cbecb596eb9352f8cb382cdc
panteliselef <panteliselef@outlook.com>
2023-09-22 11:16:05 +0300

    feat(shared): Introduce `invitations` and deprecate `invitationList` from useOrganization

--- packages/clerk-js/src/ui/components/OrganizationProfile/InvitedMembersList.tsx
+++ packages/clerk-js/src/ui/components/OrganizationProfile/InvitedMembersList.tsx
@@ -2,25 +2,15 @@ import type { OrganizationInvitationResource } from '@clerk/types';
 
 import { useCoreOrganization } from '../../contexts';
 import { localizationKeys, Td, Text } from '../../customizables';
-import { ThreeDotsMenu, useCardState, usePagination, UserPreview } from '../../elements';
+import { ThreeDotsMenu, useCardState, UserPreview } from '../../elements';
 import { handleError, roleLocalizationKey } from '../../utils';
 import { DataTable, RowContainer } from './MemberListTable';
 
-const ITEMS_PER_PAGE = 10;
-
 export const InvitedMembersList = () => {
   const card = useCardState();
-  const { page, changePage } = usePagination();
-  const { organization, invitationList, ...rest } = useCoreOrganization({
-    invitationList: { offset: (page - 1) * ITEMS_PER_PAGE, limit: ITEMS_PER_PAGE },
+  const { organization, invitations } = useCoreOrganization({
+    invitations: true,
   });
-
-  const mutateSwrState = () => {
-    const unstable__mutate = (rest as any).unstable__mutate;
-    if (unstable__mutate && typeof unstable__mutate === 'function') {
-      unstable__mutate();
-    }
-  };
 
   if (!organization) {
     return null;
@@ -28,19 +18,20 @@ export const InvitedMembersList = () => {
 
   const revoke = (invitation: OrganizationInvitationResource) => () => {
     return card
-      .runAsync(invitation.revoke)
-      .then(mutateSwrState)
-      .then(() => changePage(1))
+      .runAsync(async () => {
+        await invitation.revoke();
+        await (invitations as any).unstable__mutate?.();
+      })
       .catch(err => handleError(err, [], card.setError));
   };
 
   return (
     <DataTable
-      page={page}
-      onPageChange={changePage}
-      itemCount={organization.pendingInvitationsCount}
-      itemsPerPage={ITEMS_PER_PAGE}
-      isLoading={!invitationList}
+      page={invitations?.page || 1}
+      onPageChange={invitations?.fetchPage || (() => null)}
+      itemCount={invitations?.count || 0}
+      pageCount={invitations?.pageCount || 0}
+      isLoading={invitations?.isLoading}
       emptyStateLocalizationKey={localizationKeys('organizationProfile.membersPage.invitationsTab.table__emptyRow')}
       headers={[
         localizationKeys('organizationProfile.membersPage.activeMembersTab.tableHeader__user'),
@@ -48,7 +39,7 @@ export const InvitedMembersList = () => {
         localizationKeys('organizationProfile.membersPage.activeMembersTab.tableHeader__role'),
         localizationKeys('organizationProfile.membersPage.activeMembersTab.tableHeader__actions'),
       ]}
-      rows={(invitationList || []).map(i => (
+      rows={(invitations?.data || []).map(i => (
         <InvitationRow
           key={i.id}
           invitation={i}
