d9184c9e60370e60cd8a2171230a0ec67a0d3416
Dimitris Klouvas <dimitris@clerk.dev>
2023-09-25 15:23:47 +0300

    chore(backend): Warn about frontendApi deprecation

--- packages/backend/src/tokens/request.ts
+++ packages/backend/src/tokens/request.ts
@@ -1,7 +1,7 @@
 import { API_URL, API_VERSION, constants } from '../constants';
 import { assertValidSecretKey } from '../util/assertValidSecretKey';
 import { buildRequest, stripAuthorizationHeader } from '../util/IsomorphicRequest';
-import { isDevelopmentFromApiKey, parsePublishableKey } from '../util/shared';
+import { deprecated, isDevelopmentFromApiKey, parsePublishableKey } from '../util/shared';
 import type { RequestState } from './authStatus';
 import { AuthErrorReason, interstitial, signedOut, unknownState } from './authStatus';
 import type { TokenCarrier } from './errors';
@@ -119,6 +119,10 @@ function assertProxyUrlOrDomain(proxyUrlOrDomain: string | undefined) {
 
 export async function authenticateRequest(options: AuthenticateRequestOptions): Promise<RequestState> {
   const { cookies, headers, searchParams } = buildRequest(options?.request);
+
+  if (options.frontendApi) {
+    deprecated('frontentApi', 'Use `publishableKey` instead.');
+  }
 
   options = {
     ...options,
