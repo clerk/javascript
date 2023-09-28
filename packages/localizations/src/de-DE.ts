2a7214ea28c7791e07544354aa9953c98b97c20c
Tin Votan <tin.votan@votan-enterprise.com>
2023-09-28 18:09:52 +0200

    feat(localizations): Added German translation of deletePage section

--- packages/localizations/src/de-DE.ts
+++ packages/localizations/src/de-DE.ts
@@ -476,6 +476,13 @@ export const deDE: LocalizationResource = {
       actionLabel__download: 'Laden Sie .txt herunter',
       actionLabel__print: 'Drucken',
     },
+    deletePage: {
+      title: 'Konto löschen',
+      messageLine1: 'Sind Sie sich sicher, dass Sie ihr Konto löschen möchten?',
+      messageLine2: 'Dieser Vorgang ist dauerhaft und kann nicht rückgängig gemacht werden.',
+      actionDescription: 'Geben Sie "Konto löschen" ein, um fortzufahren.',
+      confirm: 'Konto löschen',
+    },
   },
   userButton: {
     action__manageAccount: 'Konto verwalten',
