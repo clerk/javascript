753f7bbda9bbb7444f96222a3b6cae815a09058f
panteliselef <panteliselef@outlook.com>
2023-09-28 12:48:13 +0300

    chore(shared): Remove patch for swr-devtools

--- packages/shared/src/hooks/clerk-swr.ts
+++ packages/shared/src/hooks/clerk-swr.ts
@@ -1,10 +1,3 @@
-export const disableSWRDevtools = () => {
-  if (typeof window !== 'undefined') {
-    // @ts-ignore
-    window.__SWR_DEVTOOLS_USE__ = undefined;
-  }
-};
-
 export * from 'swr';
 export { default as useSWR } from 'swr';
 export { default as useSWRInfinite } from 'swr/infinite';
--- packages/shared/src/hooks/usePagesOrInfinite.ts
+++ packages/shared/src/hooks/usePagesOrInfinite.ts
@@ -1,7 +1,5 @@
 import { useCallback, useMemo, useRef, useState } from 'react';
 
-import { disableSWRDevtools } from './clerk-swr';
-disableSWRDevtools();
 import { useSWR, useSWRInfinite } from './clerk-swr';
 import type { ValueOrSetter } from './types';
 import type { PaginatedResources } from './types';
