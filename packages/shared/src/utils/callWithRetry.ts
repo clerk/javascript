9626959479519e96cd5ec4c73a6bc919c2af34fc
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-09-29 22:12:35 +0300

    Revert "chore(shared): Add JSDoc to helpers moved to shared package"
    This reverts commit 6dbaa2f997b5b6a416b18a3e2e36d996db626d4a.

--- packages/shared/src/utils/callWithRetry.ts
+++ packages/shared/src/utils/callWithRetry.ts
@@ -4,12 +4,6 @@ function wait(ms: number) {
 
 const MAX_NUMBER_OF_RETRIES = 5;
 
-/**
- * Retry callback function every few hundred ms (with an exponential backoff
- * based on the current attempt) until the maximum attempts has reached or
- * the callback is executed successfully. The default number of maximum
- * attempts is 5 and retries are triggered when callback throws an error.
- */
 export async function callWithRetry<T>(
   fn: (...args: unknown[]) => Promise<T>,
   attempt = 1,
