00624ac61a7272d8fac0adad48ac0f79bb70f267
Bryce Kalow <bryce@clerk.dev>
2023-10-06 13:40:15 -0500

    chore(nextjs): Remove unintended change

--- packages/nextjs/src/app-beta/index.ts
+++ packages/nextjs/src/app-beta/index.ts
@@ -1,10 +1,3 @@
-import { deprecated } from '@clerk/shared';
-
-deprecated(
-  '@clerk/nextjs/app-beta',
-  'Use imports from `@clerk/nextjs` instead.\nFor more details, consult the middleware documentation: https://clerk.com/docs/nextjs/middleware',
-);
-
 export { auth } from './auth';
 export { currentUser } from './currentUser';
 export { ClerkProvider } from './ClerkProvider';
