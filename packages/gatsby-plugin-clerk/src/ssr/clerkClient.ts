260b2aaf3c98ab4a056dce550df467c611cdc820
Dimitris Klouvas <dimitris@clerk.dev>
2023-10-09 17:52:52 +0300

    chore(gatsby-plugin-clerk): Warn about CLERK_API_KEY environment variable deprecation

--- packages/gatsby-plugin-clerk/src/ssr/clerkClient.ts
+++ packages/gatsby-plugin-clerk/src/ssr/clerkClient.ts
@@ -1,9 +1,16 @@
 /* eslint-disable turbo/no-undeclared-env-vars */
 import { Clerk } from '@clerk/backend';
+import { deprecated } from '@clerk/shared';
 
 export const API_URL = process.env.CLERK_API_URL || 'https://api.clerk.dev';
 export const API_VERSION = process.env.CLERK_API_VERSION || 'v1';
+/**
+ * @deprecated Use `CLERK_SECRET_KEY` instead.
+ */
 export const API_KEY = process.env.CLERK_API_KEY || '';
+if (API_KEY) {
+  deprecated('CLERK_API_KEY', 'Use `CLERK_SECRET_KEY` environment variable instead.');
+}
 export const SECRET_KEY = process.env.CLERK_SECRET_KEY || '';
 export const FRONTEND_API = process.env.GATSBY_CLERK_FRONTEND_API || '';
 export const PUBLISHABLE_KEY = process.env.GATSBY_CLERK_PUBLISHABLE_KEY || '';
