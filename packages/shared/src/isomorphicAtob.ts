6477433dd63f87aa5b53792902b4fac509f6582a
LekoArts <lekoarts@gmail.com>
2023-10-12 12:23:30 +0200

    chore(shared): Move isomorphicAtob to root

--- packages/shared/src/isomorphicAtob.ts
+++ packages/shared/src/isomorphicAtob.ts
@@ -1,3 +1,7 @@
+/**
+ * A function that decodes a string of data which has been encoded using base-64 encoding.
+ * Uses `atob` if available, otherwise uses `Buffer` from `global`. If neither are available, returns the data as-is.
+ */
 export const isomorphicAtob = (data: string) => {
   if (typeof atob !== 'undefined' && typeof atob === 'function') {
     return atob(data);
