2ae0fa685210d1118796597c3495df902c18e231
LekoArts <lekoarts@gmail.com>
2023-10-12 15:54:18 +0200

    fix(fastify): Update imports

--- packages/fastify/src/constants.ts
+++ packages/fastify/src/constants.ts
@@ -1,6 +1,6 @@
 /* eslint-disable turbo/no-undeclared-env-vars */
 import { constants } from '@clerk/backend';
-import { deprecated } from '@clerk/shared';
+import { deprecated } from '@clerk/shared/deprecated';
 
 export const API_URL = process.env.CLERK_API_URL || 'https://api.clerk.dev';
 export const API_VERSION = process.env.CLERK_API_VERSION || 'v1';
