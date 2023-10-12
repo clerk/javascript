797c495efb160c65d88f25a8ae76401f8f3131b0
LekoArts <lekoarts@gmail.com>
2023-10-12 12:59:45 +0200

    chore(shared): Move file and remove mimeTypeExtensions

--- packages/shared/src/file.ts
+++ packages/shared/src/file.ts
@@ -16,3 +16,18 @@ export function readJSONFile(file: File): Promise<unknown> {
     reader.readAsText(file);
   });
 }
+
+const MimeTypeToExtensionMap = Object.freeze({
+  'image/png': 'png',
+  'image/jpeg': 'jpg',
+  'image/gif': 'gif',
+  'image/webp': 'webp',
+  'image/x-icon': 'ico',
+  'image/vnd.microsoft.icon': 'ico',
+} as const);
+
+export type SupportedMimeType = keyof typeof MimeTypeToExtensionMap;
+
+export const extension = (mimeType: SupportedMimeType): string => {
+  return MimeTypeToExtensionMap[mimeType];
+};
