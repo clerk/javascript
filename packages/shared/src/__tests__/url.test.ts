b6e25c2edcb7c102dd5176d742d54f45d6668556
LekoArts <lekoarts@gmail.com>
2023-10-20 16:23:06 +0200

    chore(shared): Improve test coverage

--- packages/shared/src/__tests__/url.test.ts
+++ packages/shared/src/__tests__/url.test.ts
@@ -1,4 +1,4 @@
-import { addClerkPrefix, parseSearchParams, stripScheme } from '../url';
+import { addClerkPrefix, getClerkJsMajorVersionOrTag, getScriptUrl, parseSearchParams, stripScheme } from '../url';
 
 describe('parseSearchParams(queryString)', () => {
   it('parses query string and returns a URLSearchParams object', () => {
@@ -54,5 +54,61 @@ describe('addClerkPrefix(str)', () => {
   ];
   it.each(cases)('attempts to add the prefix clerk. to %p', (urlInput, urlOutput) => {
     expect(addClerkPrefix(urlInput)).toBe(urlOutput);
+  });
+});
+
+describe('getClerkJsMajorVersionOrTag', () => {
+  const stagingFrontendApi = 'foobar.lclstage.dev';
+
+  it('returns staging if pkgVersion is not provided and frontendApi is staging', () => {
+    expect(getClerkJsMajorVersionOrTag(stagingFrontendApi)).toBe('staging');
+  });
+
+  it('returns latest if pkgVersion is not provided and frontendApi is not staging', () => {
+    expect(getClerkJsMajorVersionOrTag('foobar.dev')).toBe('latest');
+  });
+
+  it('returns next if pkgVersion contains next', () => {
+    expect(getClerkJsMajorVersionOrTag('foobar.dev', '1.2.3-next.4')).toBe('next');
+  });
+
+  it('returns the major version if pkgVersion is provided', () => {
+    expect(getClerkJsMajorVersionOrTag('foobar.dev', '1.2.3')).toBe('1');
+  });
+
+  it('returns latest if pkgVersion is empty string', () => {
+    expect(getClerkJsMajorVersionOrTag('foobar.dev', '')).toBe('latest');
+  });
+});
+
+describe('getScriptUrl', () => {
+  const frontendApi = 'https://foobar.dev';
+
+  it('returns URL using the clerkJSVersion if provided', () => {
+    expect(getScriptUrl(frontendApi, { clerkJSVersion: '1.2.3' })).toBe(
+      'https://foobar.dev/npm/@clerk/clerk-js@1.2.3/dist/clerk.browser.js',
+    );
+  });
+
+  it('returns URL using the latest version if clerkJSVersion & pkgVersion is not provided + frontendApi is not staging', () => {
+    expect(getScriptUrl(frontendApi, {})).toBe('https://foobar.dev/npm/@clerk/clerk-js@latest/dist/clerk.browser.js');
+  });
+
+  it('returns URL using the major version if only pkgVersion is provided', () => {
+    expect(getScriptUrl(frontendApi, { pkgVersion: '1.2.3' })).toBe(
+      'https://foobar.dev/npm/@clerk/clerk-js@1/dist/clerk.browser.js',
+    );
+  });
+
+  it('returns URL using the major version if only pkgVersion contains next', () => {
+    expect(getScriptUrl(frontendApi, { pkgVersion: '1.2.3-next.4' })).toBe(
+      'https://foobar.dev/npm/@clerk/clerk-js@next/dist/clerk.browser.js',
+    );
+  });
+
+  it('returns URL using the staging tag if frontendApi is staging', () => {
+    expect(getScriptUrl('https://foobar.lclstage.dev', {})).toBe(
+      'https://foobar.lclstage.dev/npm/@clerk/clerk-js@staging/dist/clerk.browser.js',
+    );
   });
 });
