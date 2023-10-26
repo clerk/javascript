b5242aad12f82782a65a9a2ce5b8eae7a9ebbc53
LekoArts <lekoarts@gmail.com>
2023-10-26 14:18:13 -0700

    chore(repo): Scripting

--- scripts/subpath-workaround.mjs
+++ scripts/subpath-workaround.mjs
@@ -20,39 +20,38 @@ async function run() {
   console.log(`Loading package.json for ${pkgName}`);
   const pkgFile = loadJSON(`../packages/${pkgName}/package.json`);
   const subpathHelperFile = await import(`../packages/${pkgName}/subpaths.mjs`);
-  
+
   console.log(
     `Found ${subpathHelperFile.subpathNames.length} subpaths and ${subpathHelperFile.subpathFoldersBarrel.length} subpath barrels`,
   );
 
   // Check if pkgFile.files already contains the subpaths. This means that the script has already been run and we should exit early
   const subpathsAlreadyAdded = subpathHelperFile.subpathNames.some(name => pkgFile.files.includes(name));
-  
+
   if (subpathsAlreadyAdded) {
     return console.log(`Subpaths already added to ${pkgName} package.json. Exiting early`);
   }
 
-  
   // Add all subpaths to the "files" property on package.json
   pkgFile.files = [...pkgFile.files, ...subpathHelperFile.subpathNames, ...subpathHelperFile.subpathFoldersBarrel];
-  
+
   writeJSON(`../packages/${pkgName}/package.json`, pkgFile);
-  
-  console.log(`Overwrote package.json for ${pkgName} with subpaths`)
-  
+
+  console.log(`Overwrote package.json for ${pkgName} with subpaths`);
+
   // Create directories for each subpath name using the pkgJsonPlaceholder
   subpathHelperFile.subpathNames.forEach(name => {
     fs.mkdirSync(new URL(`../packages/${pkgName}/${name}`, import.meta.url));
     writeJSON(`../packages/${pkgName}/${name}/package.json`, pkgJsonPlaceholder(name));
   });
-  
+
   // Create directories for each subpath barrel file using the pkgJsonBarrelPlaceholder
   subpathHelperFile.subpathFoldersBarrel.forEach(name => {
     fs.mkdirSync(new URL(`../packages/${pkgName}/${name}`, import.meta.url));
     writeJSON(`../packages/${pkgName}/${name}/package.json`, pkgJsonBarrelPlaceholder(name));
   });
-  
+
   console.log('Successfully created subpath directories with placeholder files');
 }
 
-await run()
+await run();
