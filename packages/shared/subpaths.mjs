efefbff8c727dce27e4c4d25f45fd230ae64cbc7
LekoArts <lekoarts@gmail.com>
2023-10-26 13:17:56 -0700

    chore(shared): Add script to shared

---
+++ packages/shared/subpaths.mjs
@@ -0,0 +1,27 @@
+// This file is a helper for the "subpath-workaround.mjs" script
+// We have to polyfill our "exports" subpaths :cry:
+
+export const subpathNames = [
+  'browser',
+  'callWithRetry',
+  'color',
+  'cookie',
+  'date',
+  'deprecated',
+  'error',
+  'file',
+  'globs',
+  'handleValueOrFn',
+  'isomorphicAtob',
+  'keys',
+  'loadScript',
+  'localStorageBroadcastChannel',
+  'poller',
+  'proxy',
+  'underscore',
+  'url',
+]
+
+export const subpathFoldersBarrel = [
+  'react'
+]
