02652fac571bc3fd4de84f99c54cd3e2d736071a
Vaggelis Yfantis <me@octoper.me>
2023-10-30 09:55:02 +0200

    chore(repo): Remove changes from NextJS playground

--- playground/nextjs/pages/create-organization/[[...index]].tsx
+++ playground/nextjs/pages/create-organization/[[...index]].tsx
@@ -1,13 +1,12 @@
 import { CreateOrganization } from '@clerk/nextjs';
 import type { NextPage } from 'next';
-import React from 'react';
 
 const CreateOrganizationPage: NextPage = () => {
   return (
     <div
       style={{ display: 'flex', flexDirection: 'column', gap: '2rem', justifyContent: 'center', alignItems: 'center' }}
     >
-      <CreateOrganization path='/' routing='virtual' />
+      <CreateOrganization />
     </div>
   );
 };
