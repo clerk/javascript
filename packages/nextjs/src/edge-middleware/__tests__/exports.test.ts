d9e999e5bf778fe94aed7e97e0ce3551b8dbce23
Dimitris Klouvas <dimitris@clerk.dev>
2023-11-01 16:20:35 +0200

    chore(nextjs): Drop `edge-middleware` subpath import

--- packages/nextjs/src/edge-middleware/__tests__/exports.test.ts
+++
@@ -1,7 +0,0 @@
-import * as publicExports from '../index';
-
-describe('/edge-middleware public exports', () => {
-  it('should not include a breaking change', () => {
-    expect(Object.keys(publicExports).sort()).toMatchSnapshot();
-  });
-});
