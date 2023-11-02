62f7df5e28235121265bf51b9dbabad72502b5d4
panteliselef <panteliselef@outlook.com>
2023-11-02 11:31:15 +0200

    chore(clerk-js): Improve types & add comments

--- packages/clerk-js/src/ui/hooks/useFetchRoles.ts
+++ packages/clerk-js/src/ui/hooks/useFetchRoles.ts
@@ -4,6 +4,10 @@ import { customRoleLocalizationKey, roleLocalizationKey } from '../utils';
 import { useFetch } from './useFetch';
 
 const getRolesParams = {
+  /**
+   * Fetch at most 20 roles, it is not expected for an app to have more.
+   * We also prevent the creation of more than 20 roles in dashboard.
+   */
   pageSize: 20,
 };
 export const useFetchRoles = () => {
