149da8be3348ccc00b07d06c861f695ff03bf207
George Desipris <george.desipris.56@gmail.com>
2023-11-03 13:12:20 +0200

    feat(fastify): Improve CLERK_API_URL default value

--- .changeset/rude-jobs-yawn.md
+++ .changeset/rude-jobs-yawn.md
@@ -3,6 +3,7 @@
 '@clerk/remix': patch
 'gatsby-plugin-clerk': patch
 '@clerk/shared': patch
+'@clerk/fastify': patch
 ---
 
 Improve the default value for `CLERK_API_URL` by utilizing the publishable key to differentiate between local, staging and prod environments.
--- packages/fastify/src/constants.ts
+++ packages/fastify/src/constants.ts
@@ -1,9 +1,10 @@
 import { constants } from '@clerk/backend';
+import { apiUrlFromPublishableKey } from '@clerk/shared/apiUrlFromPublishableKey';
 
-export const API_URL = process.env.CLERK_API_URL || 'https://api.clerk.com';
 export const API_VERSION = process.env.CLERK_API_VERSION || 'v1';
 export const SECRET_KEY = process.env.CLERK_SECRET_KEY || '';
 export const PUBLISHABLE_KEY = process.env.CLERK_PUBLISHABLE_KEY || '';
+export const API_URL = process.env.CLERK_API_URL || apiUrlFromPublishableKey(PUBLISHABLE_KEY);
 export const JWT_KEY = process.env.CLERK_JWT_KEY || '';
 
 export const { Cookies, Headers } = constants;
