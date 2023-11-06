852e9beab9eb0dea72b0a1ab44b5d8dce0a1b30f
Dimitris Klouvas <dimitris@clerk.dev>
2023-11-07 00:59:13 +0200

    chore(repo): Upgrade jest@^29.4.0 to fix subpath imports issue
    Also applied the following improvements:
    - ignore test path for  dist/, examples/, .turbo/, coverage/

--- packages/nextjs/jest.config.js
+++ packages/nextjs/jest.config.js
@@ -11,10 +11,5 @@ module.exports = {
   testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: { '^.+\\.m?tsx?$': 'ts-jest' },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testPathIgnorePatterns: ['/node_modules/', '/jest/'],
-  // Jest currently does not support package.json subpath imports
-  // so we manually map them to the actual files. See @clerk/backend/package.json
-  moduleNameMapper: {
-    '#crypto': '@clerk/backend/dist/runtime/node/crypto.js',
-  },
+  testPathIgnorePatterns: ['/node_modules/', '/jest/', '/.turbo', '/dist/', '/examples'],
 };
