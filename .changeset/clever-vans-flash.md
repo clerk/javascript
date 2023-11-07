b71da29e534f39936712d62f30e6e8229016ff8c
Mark Pitsilos <mark@clerk.dev>
2023-11-07 16:18:43 +0200

    fix(clerk-js): Use strict equality operator to check for lockout errors in handleRedirectCallback

---
+++ .changeset/clever-vans-flash.md
@@ -0,0 +1,5 @@
+---
+'@clerk/clerk-js': patch
+---
+
+Use strict equality operator to check for lockout errors in handleRedirectCallback
--- packages/clerk-js/src/core/clerk.ts
+++ packages/clerk-js/src/core/clerk.ts
@@ -1039,8 +1039,8 @@ export default class Clerk implements ClerkInterface {
       }
     }
 
-    const userLockedFromSignUp = su.externalAccountErrorCode == 'user_locked';
-    const userLockedFromSignIn = si.firstFactorVerificationErrorCode == 'user_locked';
+    const userLockedFromSignUp = su.externalAccountErrorCode === 'user_locked';
+    const userLockedFromSignIn = si.firstFactorVerificationErrorCode === 'user_locked';
 
     if (userLockedFromSignUp) {
       return navigateToSignUp();
