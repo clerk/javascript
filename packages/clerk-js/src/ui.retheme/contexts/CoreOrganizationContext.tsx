8006564198e1fb3460abd322f42e703f1394bb3b
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-11-10 16:18:17 +0200

    chore(*): Introduce retheme flags and preview build
    * chore(clerk-js): Support building with CLERK_RETHEME=1
    
    * chore(localizations): Support building with CLERK_RETHEME=1
    
    * chore(types): Support building with CLERK_RETHEME=1
    
    * chore(repo): Introduce retheme-preview action
    
    * chore(repo): Update ui.retheme dir
    
    * Create few-kids-design.md

---
+++ packages/clerk-js/src/ui.retheme/contexts/CoreOrganizationContext.tsx
@@ -0,0 +1,5 @@
+import { OrganizationProvider, useOrganization, useOrganizationList } from '@clerk/shared/react';
+
+export const CoreOrganizationProvider = OrganizationProvider;
+export const useCoreOrganization = useOrganization;
+export const useCoreOrganizationList = useOrganizationList;
