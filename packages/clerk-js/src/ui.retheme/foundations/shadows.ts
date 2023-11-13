57bd419da81f4aa1de85c4a054be48270db51a17
Marcel Cruz <12413903+marcelscruz@users.noreply.github.com>
2023-11-13 15:59:34 +0100

    feat(clerk-js): Add new design and arrow on primary button
    chore(clerk-js): Add changeset
    
    Remove content from changeset file
    
    chore(clerk-js): Move new arrow to separate file
    
    chore(clerk-js): Make button shadow a reusable util
    
    chore(clerk-js): Use theme values on arrow icon
    
    fix(clerk-js): Remove invalid color shades

--- packages/clerk-js/src/ui.retheme/foundations/shadows.ts
+++ packages/clerk-js/src/ui.retheme/foundations/shadows.ts
@@ -5,6 +5,8 @@ export const shadows = Object.freeze({
   fabShadow: '0px 12px 24px rgba(0, 0, 0, 0.32)',
   focusRing: '0 0 0 3px {{color}}',
   focusRingInput: '0 0 0 1px {{color}}',
+  buttonShadow:
+    '0px 0px 0px 1px {{color}}, 0px 1px 1px 0px rgba(255, 255, 255, 0.07) inset, 0px 2px 3px 0px rgba(34, 42, 53, 0.20), 0px 1px 1px 0px rgba(0, 0, 0, 0.24)',
   shadowShimmer: '1px 1px 2px rgba(0, 0, 0, 0.36)',
   sm: '0 1px 1px 0 rgb(0 0 0 / 0.05)',
 } as const);
