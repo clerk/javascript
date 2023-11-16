24fc5b2537ae3693183f8fdf65cc0395cd47830c
Dimitris Klouvas <dimitris@clerk.dev>
2023-11-16 20:20:46 +0200

    fix(repo): Exit integration tests on application error exit code

--- integration/scripts/waitForServer.ts
+++ integration/scripts/waitForServer.ts
@@ -1,8 +1,19 @@
+type WaitForServerArgsType = {
+  log;
+  delayInMs?: number;
+  maxAttempts?: number;
+  shouldExit?: () => boolean;
+};
+
 // Poll a url until it returns a 200 status code
-export const waitForServer = async (url: string, opts: { delayInMs?: number; maxAttempts?: number; log }) => {
-  const { delayInMs = 1000, maxAttempts = 20, log } = opts || {};
+export const waitForServer = async (url: string, opts: WaitForServerArgsType) => {
+  const { log, delayInMs = 1000, maxAttempts = 20, shouldExit = () => false } = opts;
   let attempts = 0;
   while (attempts < maxAttempts) {
+    if (shouldExit()) {
+      throw new Error(`Polling ${url} failed after ${maxAttempts} attempts (due to forced exit)`);
+    }
+
     try {
       log(`Polling ${url}...`);
       const res = await fetch(url);
@@ -15,5 +26,6 @@ export const waitForServer = async (url: string, opts: { delayInMs?: number; max
     attempts++;
     await new Promise(resolve => setTimeout(resolve, delayInMs));
   }
+
   throw new Error(`Polling ${url} failed after ${maxAttempts} attempts`);
 };
