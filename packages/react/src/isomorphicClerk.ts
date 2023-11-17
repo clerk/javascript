fa7bf97beb245575e17b3f58a2e8d3316363b8b6
Dimitris Klouvas <dimitris@clerk.dev>
2023-11-17 23:22:53 +0200

    fix(clerk-react): Drop unused import

--- packages/react/src/isomorphicClerk.ts
+++ packages/react/src/isomorphicClerk.ts
@@ -12,7 +12,6 @@ import type {
   HandleOAuthCallbackParams,
   ListenerCallback,
   OrganizationListProps,
-  OrganizationMembershipResource,
   OrganizationProfileProps,
   OrganizationResource,
   OrganizationSwitcherProps,
