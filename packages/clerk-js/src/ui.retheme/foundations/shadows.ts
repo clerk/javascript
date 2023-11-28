3b1cebb3a887ff45360616d1afe8a309fae7d878
George Desipris <george.desipris.56@gmail.com>
2023-11-28 12:24:15 +0200

    feat(clerk-js): Input border retheme

--- packages/clerk-js/src/ui.retheme/foundations/shadows.ts
+++ packages/clerk-js/src/ui.retheme/foundations/shadows.ts
@@ -3,9 +3,11 @@ export const shadows = Object.freeze({
   cardDropShadow: '0px 24px 48px rgba(0, 0, 0, 0.16)',
   boxShadow1: '0px 24px 48px rgba(0, 0, 0, 0.16)',
   fabShadow: '0px 12px 24px rgba(0, 0, 0, 0.32)',
-  focusRing: '0 0 0 3px {{color}}',
   buttonShadow:
     '0px 0px 0px 1px {{color}}, 0px 1px 1px 0px rgba(255, 255, 255, 0.07) inset, 0px 2px 3px 0px rgba(34, 42, 53, 0.20), 0px 1px 1px 0px rgba(0, 0, 0, 0.24)',
   shadowShimmer: '1px 1px 2px rgba(0, 0, 0, 0.36)',
   sm: '0 1px 1px 0 rgb(0 0 0 / 0.05)',
+  input: '0px 0px 0px 1px {{color1}}, 0px 1px 1px 0px {{color2}}',
+  inputHover: '0px 0px 0px 1px {{color1}}, 0px 1px 1px 0px {{color2}}',
+  focusRing: '0px 0px 0px 4px {{color}}',
 } as const);
