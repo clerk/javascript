2d383e413920fae1d1b303a669ca165035658639
Dimitris Klouvas <dimitris@clerk.dev>
2023-11-30 15:21:01 +0200

    feat(localizations): Allow usage of subpath exports (#2236)
    * feat(localizations): Allow usage of subpath exports
    
    The addition of `.gitignore` and `subpaths.mjs` is used to support
    Expo framework. This fix is also used in `@clerk/shared` package.
    
    original PR: https://github.com/clerk/javascript/pull/2046
    
    Co-authored-by: Andrew <slider7259@gmail.com>
    
    * update changeset
    
    ---------
    
    Co-authored-by: Andrew <slider7259@gmail.com>
    Co-authored-by: Lennart <lekoarts@gmail.com>

---
+++ packages/localizations/subpaths.mjs
@@ -0,0 +1,35 @@
+// This file is a helper for the "subpath-workaround.mjs" script
+// We have to polyfill our "exports" subpaths :cry:
+
+export const subpathNames = [
+  'ar-SA',
+  'cs-CZ',
+  'da-DK',
+  'de-DE',
+  'el-GR',
+  'en-US',
+  'es-ES',
+  'fr-FR',
+  'he-IL',
+  'it-IT',
+  'ja-JP',
+  'ko-KR',
+  'nb-NO',
+  'nl-NL',
+  'pl-PL',
+  'pt-BR',
+  'pt-PT',
+  'ro-RO',
+  'ru-RU',
+  'sk-SK',
+  'sv-SE',
+  'tr-TR',
+  'uk-UA',
+  'vi-VN',
+  'zh-CN',
+  'zh-TW',
+];
+
+export const subpathFoldersBarrel = [];
+
+export const ignoredFolders = [];
