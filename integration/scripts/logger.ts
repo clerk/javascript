46f5f38d7dd99b620fbe292c53b9815df20ce62a
LekoArts <lekoarts@gmail.com>
2023-12-01 11:15:58 +0100

    chore(repo): Use colored text & not bg

--- integration/scripts/logger.ts
+++ integration/scripts/logger.ts
@@ -1,23 +1,23 @@
 /* eslint-disable turbo/no-undeclared-env-vars */
 import { default as chalk } from 'chalk';
 
-const getRandomChalkBgColor = () => {
-  const colors = ['bgRed', 'bgGreen', 'bgYellow', 'bgBlue', 'bgMagenta', 'bgCyan', 'bgWhite'];
+const getRandomChalkColor = () => {
+  const colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white'];
   return colors[Math.floor(Math.random() * colors.length)];
 };
 
 type CreateLoggerOptions = { prefix: string; color?: string };
 export const createLogger = (opts: CreateLoggerOptions) => {
   const { color, prefix } = opts;
-  const prefixBgColor = color || getRandomChalkBgColor();
+  const prefixColor = color || getRandomChalkColor();
   return {
     info: (msg: string) => {
       if (process.env.DEBUG) {
-        console.info(`${chalk[prefixBgColor](`[${prefix}]`)} ${msg}`);
+        console.info(`${chalk[prefixColor](`[${prefix}]`)} ${msg}`);
       }
     },
     child: (childOpts: CreateLoggerOptions) => {
-      return createLogger({ prefix: `${prefix} :: ${childOpts.prefix}`, color: prefixBgColor });
+      return createLogger({ prefix: `${prefix} :: ${childOpts.prefix}`, color: prefixColor });
     },
   };
 };
