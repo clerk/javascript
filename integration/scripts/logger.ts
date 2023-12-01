821c780c17de64e19e2d119ab05bc117884d683e
LekoArts <lekoarts@gmail.com>
2023-12-01 11:21:44 +0100

    chore(repo): Remove white from choices

--- integration/scripts/logger.ts
+++ integration/scripts/logger.ts
@@ -2,7 +2,7 @@
 import { default as chalk } from 'chalk';
 
 const getRandomChalkColor = () => {
-  const colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white'];
+  const colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan'];
   return colors[Math.floor(Math.random() * colors.length)];
 };
 
