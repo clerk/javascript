bda54281dc6e24fdcb1478987e84f8bfbf9ff4a5
panteliselef <panteliselef@outlook.com>
2023-12-03 16:53:49 +0200

    feat(nextjs): Introduce `auth().protect()` for App Router
    Allow per page protection in app router. This utility will automatically throw a 404 error if user is not authorized or authenticated.
    When `auth().protect()` is called
    - inside a page or layout file it will render the nearest `not-found` component set by the developer
    - inside a route handler it will return empty response body with a 404 status code

--- packages/nextjs/src/app-router/server/auth.ts
+++ packages/nextjs/src/app-router/server/auth.ts
@@ -1,12 +1,82 @@
+import type { SignedInAuthObject, SignedOutAuthObject } from '@clerk/backend';
+import type {
+  CheckAuthorizationWithCustomPermissions,
+  OrganizationCustomPermission,
+  OrganizationCustomRole,
+} from '@clerk/types';
+import { notFound } from 'next/navigation';
+
 import { authAuthHeaderMissing } from '../../server/errors';
 import { buildClerkProps, createGetAuth } from '../../server/getAuth';
+import type { AuthObjectWithoutResources } from '../../server/types';
 import { buildRequestLike } from './utils';
 
 export const auth = () => {
-  return createGetAuth({
+  const authObject = createGetAuth({
     debugLoggerName: 'auth()',
     noAuthStatusMessage: authAuthHeaderMissing(),
-  })(buildRequestLike());
+  })(buildRequestLike()) as
+    | AuthObjectWithoutResources<
+        SignedInAuthObject & {
+          protect: (
+            params?:
+              | {
+                  role: OrganizationCustomRole;
+                  permission?: never;
+                }
+              | {
+                  role?: never;
+                  permission: OrganizationCustomPermission;
+                }
+              | ((has: CheckAuthorizationWithCustomPermissions) => boolean),
+          ) => AuthObjectWithoutResources<SignedInAuthObject>;
+        }
+      >
+    /**
+     * Add a comment
+     */
+    | AuthObjectWithoutResources<
+        SignedOutAuthObject & {
+          protect: never;
+        }
+      >;
+
+  authObject.protect = params => {
+    /**
+     * User is not authenticated
+     */
+    if (!authObject.userId) {
+      notFound();
+    }
+
+    /**
+     * User is authenticated
+     */
+    if (!params) {
+      return { ...authObject };
+    }
+
+    /**
+     * if a function is passed and returns false then throw not found
+     */
+    if (typeof params === 'function') {
+      if (params(authObject.has)) {
+        return { ...authObject };
+      }
+      return notFound();
+    }
+
+    /**
+     * Checking if user is authorized when permission or role is passed
+     */
+    if (authObject.has(params)) {
+      return { ...authObject };
+    }
+
+    notFound();
+  };
+
+  return authObject;
 };
 
 export const initialState = () => {
