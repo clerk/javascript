819b6a0d036df1324f3698134c7387fddda8c01c
panteliselef <panteliselef@outlook.com>
2023-12-05 21:54:58 +0200

    feat(types): Allow overriding of types for custom roles and permissions

--- packages/types/src/organizationMembership.ts
+++ packages/types/src/organizationMembership.ts
@@ -3,6 +3,15 @@ import type { ClerkResource } from './resource';
 import type { PublicUserData } from './session';
 import type { Autocomplete } from './utils';
 
+interface Base {
+  permission: string;
+  role: string;
+}
+
+declare global {
+  interface ClerkAuthorization {}
+}
+
 declare global {
   /**
    * If you want to provide custom types for the organizationMembership.publicMetadata
@@ -36,8 +45,15 @@ export interface OrganizationMembershipResource extends ClerkResource {
   update: (updateParams: UpdateOrganizationMembershipParams) => Promise<OrganizationMembershipResource>;
 }
 
-export type OrganizationCustomPermissionKey = string;
-export type OrganizationCustomRoleKey = string;
+export type OrganizationCustomPermissionKey = 'permission' extends keyof ClerkAuthorization
+  ? // @ts-expect-error Typescript cannot infer the existence of the `permission` key even if we checking it above
+    ClerkAuthorization['permission']
+  : Base['permission'];
+
+export type OrganizationCustomRoleKey = 'role' extends keyof ClerkAuthorization
+  ? // @ts-expect-error Typescript cannot infer the existence of the `role` key even if we checking it above
+    ClerkAuthorization['role']
+  : Base['role'];
 
 /**
  * @deprecated This type is deprecated and will be removed in the next major release.
@@ -59,7 +75,12 @@ export type OrganizationSystemPermissionKey =
  * OrganizationPermissionKey is a combination of system and custom permissions.
  * System permissions are only accessible from FAPI and client-side operations/utils
  */
-export type OrganizationPermissionKey = Autocomplete<OrganizationSystemPermissionKey>;
+export type OrganizationPermissionKey = 'permission' extends keyof ClerkAuthorization
+  ? // @ts-expect-error Typescript cannot infer the existence of the `permission` key even if we checking it above
+    // Disabling eslint rule because the error causes the type to become any when accessing a property that does not exist
+    // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
+    ClerkAuthorization['permission'] | OrganizationSystemPermissionKey
+  : Autocomplete<OrganizationSystemPermissionKey>;
 
 export type UpdateOrganizationMembershipParams = {
   role: MembershipRole;
