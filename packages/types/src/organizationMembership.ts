f810a7284a38a60e3bf88556e18e852b8920f57c
panteliselef <panteliselef@outlook.com>
2023-12-06 10:49:23 +0200

    Revert "chore(types): Approach 2"
    This reverts commit d9e2e4570b62fb3ae7c265e49c576b2c8d6345ce.

--- packages/types/src/organizationMembership.ts
+++ packages/types/src/organizationMembership.ts
@@ -3,6 +3,20 @@ import type { ClerkResource } from './resource';
 import type { PublicUserData } from './session';
 import type { Autocomplete } from './utils';
 
+interface Base {
+  permission: string;
+  role: string;
+}
+
+interface Placeholder {
+  permission: unknown;
+  role: unknown;
+}
+
+declare global {
+  interface ClerkAuthorization {}
+}
+
 declare global {
   /**
    * If you want to provide custom types for the organizationMembership.publicMetadata
@@ -21,9 +35,6 @@ declare global {
   interface OrganizationMembershipPrivateMetadata {
     [k: string]: unknown;
   }
-
-  type ClerkPermissionKey = string;
-  type ClerkRoleKey = string;
 }
 
 export interface OrganizationMembershipResource extends ClerkResource {
@@ -39,8 +50,16 @@ export interface OrganizationMembershipResource extends ClerkResource {
   update: (updateParams: UpdateOrganizationMembershipParams) => Promise<OrganizationMembershipResource>;
 }
 
-export type OrganizationCustomPermissionKey = ClerkPermissionKey;
-export type OrganizationCustomRoleKey = ClerkRoleKey;
+// @ts-expect-error
+export type OrganizationCustomPermissionKey = ClerkAuthorization['permission'] extends Placeholder['permission']
+  ? // @ts-expect-error
+    ClerkAuthorization['permission']
+  : Base['permission'];
+// @ts-expect-error
+export type OrganizationCustomRoleKey = ClerkAuthorization['role'] extends Placeholder['role']
+  ? // @ts-expect-error
+    ClerkAuthorization['role']
+  : Base['role'];
 
 /**
  * @deprecated This type is deprecated and will be removed in the next major release.
