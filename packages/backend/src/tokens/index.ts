e1f7eae87531b483564256f5456a31150caa469e
Tom Milewski <me@tm.codes>
2023-12-07 10:03:10 -0500

    chore(backend): Limit exposed token verification related errors (#2189)
    Co-authored-by: flawnn <fl4wn@protonmail.com>

--- packages/backend/src/tokens/index.ts
+++ packages/backend/src/tokens/index.ts
@@ -1,7 +1,7 @@
 export * from './authObjects';
 export { AuthStatus } from './authStatus';
 export type { RequestState } from './authStatus';
-export * from './errors';
+export { TokenVerificationError, TokenVerificationErrorReason } from './errors';
 export * from './factory';
 export { loadInterstitialFromLocal } from './interstitial';
 export { debugRequestState } from './request';
