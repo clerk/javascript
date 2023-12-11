6118d16bff96b2f081ba0e2354cecc95ba896ff5
Tom Milewski <me@tm.codes>
2023-12-11 12:13:10 -0500

    fix(elements): Update tsup config

--- packages/elements/tsup.config.ts
+++ packages/elements/tsup.config.ts
@@ -1,23 +1,26 @@
 import { defineConfig } from 'tsup';
 
-// @ts-expect-error - JSON file
+import { version as clerkJsVersion } from '../clerk-js/package.json';
 import { name, version } from './package.json';
 
 export default defineConfig(overrideOptions => {
   const isProd = overrideOptions.env?.NODE_ENV === 'production';
 
   return {
-    entry: ['src/index.tsx'],
-    minify: false,
     clean: true,
-    sourcemap: true,
-    format: ['cjs', 'esm'],
-    legacyOutput: true,
-    dts: true,
     define: {
       PACKAGE_NAME: `"${name}"`,
       PACKAGE_VERSION: `"${version}"`,
+      JS_PACKAGE_VERSION: `"${clerkJsVersion}"`,
       __DEV__: `${!isProd}`,
     },
+    dts: true,
+    entry: {
+      index: 'src/index.tsx',
+    },
+    external: ['react', 'react-dom'],
+    format: ['cjs', 'esm'],
+    minify: false,
+    sourcemap: true,
   };
 });
