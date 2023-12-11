7515d9586bc4ff5c4430f954f30caf4452d95e7b
panteliselef <panteliselef@outlook.com>
2023-12-11 21:52:31 +0200

    fix(types): Avoid using `ts-expect-error` as it may fail for hosting apps

--- packages/types/src/organizationMembership.ts
+++ packages/types/src/organizationMembership.ts
@@ -47,12 +47,12 @@ export interface OrganizationMembershipResource extends ClerkResource {
 }
 
 export type OrganizationCustomPermissionKey = 'permission' extends keyof ClerkAuthorization
-  ? // @ts-expect-error Typescript cannot infer the existence of the `permission` key even if we checking it above
+  ? // @ts-ignore Typescript cannot infer the existence of the `permission` key even if we checking it above
     ClerkAuthorization['permission']
   : Base['permission'];
 
 export type OrganizationCustomRoleKey = 'role' extends keyof ClerkAuthorization
-  ? // @ts-expect-error Typescript cannot infer the existence of the `role` key even if we checking it above
+  ? // @ts-ignore Typescript cannot infer the existence of the `role` key even if we checking it above
     ClerkAuthorization['role']
   : Base['role'];
 
@@ -63,7 +63,7 @@ export type OrganizationCustomRoleKey = 'role' extends keyof ClerkAuthorization
  * these types will no longer match a developer's custom logic.
  */
 export type MembershipRole = 'role' extends keyof ClerkAuthorization
-  ? // @ts-expect-error Typescript cannot infer the existence of the `role` key even if we checking it above
+  ? // @ts-ignore Typescript cannot infer the existence of the `role` key even if we checking it above
     // Disabling eslint rule because the error causes the type to become any when accessing a property that does not exist
     // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
     ClerkAuthorization['role'] | 'admin' | 'basic_member' | 'guest_member'
@@ -82,7 +82,7 @@ export type OrganizationSystemPermissionKey =
  * System permissions are only accessible from FAPI and client-side operations/utils
  */
 export type OrganizationPermissionKey = 'permission' extends keyof ClerkAuthorization
-  ? // @ts-expect-error Typescript cannot infer the existence of the `permission` key even if we checking it above
+  ? // @ts-ignore Typescript cannot infer the existence of the `permission` key even if we checking it above
     // Disabling eslint rule because the error causes the type to become any when accessing a property that does not exist
     // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
     ClerkAuthorization['permission'] | OrganizationSystemPermissionKey
