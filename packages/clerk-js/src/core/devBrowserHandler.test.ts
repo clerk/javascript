9b76632dcd946208b06048a1ac0b53d3b7decbb6
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-12-12 17:49:32 +0200

    chore(*): Fix linter

--- packages/clerk-js/src/core/devBrowserHandler.test.ts
+++ packages/clerk-js/src/core/devBrowserHandler.test.ts
@@ -1,25 +1,6 @@
-import type { CreateDevBrowserHandlerOptions } from './devBrowserHandler';
-import { createDevBrowserHandler } from './devBrowserHandler';
-
-describe('detBrowserHandler', () => {
-  // @ts-ignore
-  const { getDevBrowserJWT, setDevBrowserJWT, removeDevBrowserJWT } = createDevBrowserHandler(
-    {} as CreateDevBrowserHandlerOptions,
-  );
-
+describe.skip('detBrowserHandler', () => {
   // TODO: Add devbrowser tests
   describe('get', () => {
-    beforeEach(() => {
-      Object.defineProperty(window, 'localStorage', {
-        value: {
-          getItem: jest.fn(),
-          setItem: jest.fn(),
-          removeItem: jest.fn(),
-        },
-        writable: true,
-      });
-    });
-
     it('todo', () => {
       expect(true).toBeTruthy();
     });
