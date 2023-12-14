ff060fcdf02ed92e4b04ed3a9d977d825531e814
Dimitris Klouvas <dimitris@clerk.dev>
2023-12-14 20:23:39 +0200

    chore(backend): Generate declarations using tsc and use generated d.ts for both esm and cjs
    This change resolves issues with inferred types. Eg:
    ```
     The inferred type of 'auth' cannot be named without a reference to ...
    ```

--- packages/backend/tsup.config.ts
+++ packages/backend/tsup.config.ts
@@ -1,5 +1,7 @@
+import type { Options } from 'tsup';
 import { defineConfig } from 'tsup';
 
+import { runAfterLast } from '../../scripts/utils';
 // @ts-ignore
 import { name, version } from './package.json';
 
@@ -7,19 +9,29 @@ export default defineConfig(overrideOptions => {
   const isWatch = !!overrideOptions.watch;
   const shouldPublish = !!overrideOptions.env?.publish;
 
-  return {
+  const common: Options = {
     entry: ['src/index.ts', 'src/errors.ts', 'src/internal.ts'],
+    onSuccess: `cpy 'src/runtime/**/*.{mjs,js,cjs}' dist/runtime`,
+    sourcemap: true,
     define: {
       PACKAGE_NAME: `"${name}"`,
       PACKAGE_VERSION: `"${version}"`,
       __DEV__: `${isWatch}`,
     },
-    onSuccess: shouldPublish ? 'npm run publish:local' : undefined,
-    format: ['cjs', 'esm'],
     bundle: true,
-    sourcemap: true,
     clean: true,
     minify: false,
-    dts: true,
   };
+
+  const esm: Options = {
+    ...common,
+    format: 'esm',
+  };
+
+  const cjs: Options = {
+    ...common,
+    format: 'cjs',
+  };
+
+  return runAfterLast(['npm run build:declarations', shouldPublish && 'npm run publish:local'])(esm, cjs);
 });
