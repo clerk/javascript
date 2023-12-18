64b71c6378ccf20bdc5dab63fcb789e2a8bc8230
Nikos Douvlis <nikosdouvlis@gmail.com>
2023-12-18 19:47:11 +0200

    fix(backend): Export createClerkRequest under /internal

--- packages/backend/src/internal.ts
+++ packages/backend/src/internal.ts
@@ -20,3 +20,6 @@ export { AuthStatus } from './tokens/authStatus';
 export type { RequestState, SignedInState, SignedOutState } from './tokens/authStatus';
 
 export { decorateObjectWithResources, stripPrivateDataFromObject } from './util/decorateObjectWithResources';
+
+export { createClerkRequest } from './tokens/clerkRequest';
+export type { ClerkRequest } from './tokens/clerkRequest';
