84ea8ffb586176a661822088052fb354a3c43e29
panteliselef <panteliselef@outlook.com>
2023-12-19 15:44:21 +0200

    fix(clerk-js,clerk-react): Using missing MembershipRole type

--- packages/react/src/contexts/AuthContext.ts
+++ packages/react/src/contexts/AuthContext.ts
@@ -1,12 +1,12 @@
 import { createContextAndHook } from '@clerk/shared/react';
-import type { ActJWTClaim, MembershipRole, OrganizationCustomPermissionKey } from '@clerk/types';
+import type { ActJWTClaim, OrganizationCustomPermissionKey, OrganizationCustomRoleKey } from '@clerk/types';
 
 export const [AuthContext, useAuthContext] = createContextAndHook<{
   userId: string | null | undefined;
   sessionId: string | null | undefined;
   actor: ActJWTClaim | null | undefined;
   orgId: string | null | undefined;
-  orgRole: MembershipRole | null | undefined;
+  orgRole: OrganizationCustomRoleKey | null | undefined;
   orgSlug: string | null | undefined;
   orgPermissions: OrganizationCustomPermissionKey[] | null | undefined;
 }>('AuthContext');
