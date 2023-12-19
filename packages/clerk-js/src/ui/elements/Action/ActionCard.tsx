5a45ea62c9465257e8d6b5478545b3c130bd6bf9
Marcel Cruz <12413903+marcelscruz@users.noreply.github.com>
2023-12-19 10:52:59 -0300

    chore(clerk-js): Move <ActionCard> variant styles to a util

--- packages/clerk-js/src/ui/elements/Action/ActionCard.tsx
+++ packages/clerk-js/src/ui/elements/Action/ActionCard.tsx
@@ -1,22 +1,35 @@
 import type { ComponentProps } from 'react';
 
 import { Col } from '../../customizables';
+import type { InternalTheme } from '../../styledSystem';
 
 type ActionCardProps = ComponentProps<typeof Col> & {
   variant?: 'neutral' | 'destructive';
 };
 
+const styles = (t: InternalTheme) => ({
+  neutral: {
+    background: t.colors.$colorBackground,
+    boxShadow: t.shadows.$actionCardShadow,
+  },
+  destructive: {
+    background: t.colors.$blackAlpha50,
+    boxShadow: t.shadows.$actionCardDestructiveShadow,
+  },
+});
+
 export const ActionCard = (props: ActionCardProps) => {
   const { children, sx, variant = 'neutral', ...rest } = props;
+
   return (
     <Col
       sx={[
         t => ({
           gap: t.space.$4,
-          boxShadow: variant === 'destructive' ? t.shadows.$actionCardDestructiveShadow : t.shadows.$actionCardShadow,
           borderRadius: t.radii.$lg,
-          backgroundColor: variant === 'destructive' ? t.colors.$blackAlpha50 : t.colors.$colorBackground,
           padding: t.space.$6,
+          backgroundColor: styles(t)[variant].background,
+          boxShadow: styles(t)[variant].boxShadow,
         }),
         sx,
       ]}
