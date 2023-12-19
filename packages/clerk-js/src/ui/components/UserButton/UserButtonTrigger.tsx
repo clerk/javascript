d18cae5fd9ba80d9e396b433ea5e21b27bfdaf59
Marcel Cruz <12413903+marcelscruz@users.noreply.github.com>
2023-12-19 15:05:58 -0300

    fix(clerk-js): Make user name on <UserButton showName /> clickable (#2411)
    * fix(clerk-js): Make user name on <UserButton showName /> clickable
    
    * fix(clerk-js): Use smaller radius for UserButton button with name
    
    ---------
    
    Co-authored-by: Stefanos Anagnostou <stefanos@clerk.dev>

--- packages/clerk-js/src/ui/components/UserButton/UserButtonTrigger.tsx
+++ packages/clerk-js/src/ui/components/UserButton/UserButtonTrigger.tsx
@@ -1,9 +1,11 @@
 import { useUser } from '@clerk/shared/react';
 import { forwardRef } from 'react';
 
-import { Button, descriptors } from '../../customizables';
+import { useUserButtonContext } from '../../contexts';
+import { Button, descriptors, Flex } from '../../customizables';
 import { UserAvatar, withAvatarShimmer } from '../../elements';
 import type { PropsOfComponent } from '../../styledSystem';
+import { UserButtonTopLevelIdentifier } from './UserButtonTopLevelIdentifier';
 
 type UserButtonTriggerProps = PropsOfComponent<typeof Button> & {
   isOpen: boolean;
@@ -13,24 +15,33 @@ export const UserButtonTrigger = withAvatarShimmer(
   forwardRef<HTMLButtonElement, UserButtonTriggerProps>((props, ref) => {
     const { sx, ...rest } = props;
     const { user } = useUser();
+    const { showName } = useUserButtonContext();
 
     return (
       <Button
         elementDescriptor={descriptors.userButtonTrigger}
         variant='roundWrapper'
-        sx={[theme => ({ borderRadius: theme.radii.$circle }), sx]}
+        sx={[theme => ({ borderRadius: showName ? theme.radii.$md : theme.radii.$circle }), sx]}
         ref={ref}
         aria-label={`${props.isOpen ? 'Close' : 'Open'} user button`}
         aria-expanded={props.isOpen}
         aria-haspopup='dialog'
         {...rest}
       >
-        <UserAvatar
-          boxElementDescriptor={descriptors.userButtonAvatarBox}
-          imageElementDescriptor={descriptors.userButtonAvatarImage}
-          {...user}
-          size={theme => theme.sizes.$7}
-        />
+        <Flex
+          elementDescriptor={descriptors.userButtonBox}
+          isOpen={props.isOpen}
+          align='center'
+          gap={2}
+        >
+          <UserButtonTopLevelIdentifier showName={showName} />
+          <UserAvatar
+            boxElementDescriptor={descriptors.userButtonAvatarBox}
+            imageElementDescriptor={descriptors.userButtonAvatarImage}
+            {...user}
+            size={theme => theme.sizes.$7}
+          />
+        </Flex>
       </Button>
     );
   }),
