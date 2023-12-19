bd7335f6f495146e36aff73dc2f26f498e3abae5
Stefanos Anagnostou <stefanos@clerk.dev>
2023-12-19 15:05:29 +0200

    feat(nextjs): Add a hint to install WSL when developing on Windows

--- packages/nextjs/src/app-router/server/utils.ts
+++ packages/nextjs/src/app-router/server/utils.ts
@@ -5,13 +5,14 @@ export const buildRequestLike = () => {
   try {
     return new NextRequest('https://placeholder.com', { headers: headers() });
   } catch (e: any) {
-    if (
-      e &&
-      'message' in e &&
-      typeof e.message === 'string' &&
-      e.message.toLowerCase().includes('Dynamic server usage'.toLowerCase())
-    ) {
+    if (errorMessageIncludes(e, 'Dynamic server usage')) {
       throw e;
+    }
+
+    if (errorMessageIncludes(e, 'requestAsyncStorage')) {
+      throw new Error(
+        `Clerk: Are you developing on Windows?\nTry installing WSL: https://learn.microsoft.com/en-us/windows/wsl/install\nRelated issue: https://github.com/vercel/next.js/issues/52176\nOriginal error: ${e}`,
+      );
     }
 
     throw new Error(
@@ -19,3 +20,6 @@ export const buildRequestLike = () => {
     );
   }
 };
+
+const errorMessageIncludes = (e: any, message: string) =>
+  e && 'message' in e && typeof e.message === 'string' && e.message.toLowerCase().includes(message.toLowerCase());
