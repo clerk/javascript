4b85989a5a4097f9db473e0120b939f0736409d0
Tom Milewski <me@tm.codes>
2023-12-22 15:25:12 -0500

    feat(elements): Form and Field registration/submission (#2445)
    * feat(elements): Form and Field registration/submission
    
    * build(repo): List installed packages
    
    * build(repo): Test removing node_modules hard cache
    
    * build(repo): Use setup-node .npm cache
    
    * chore(repo): Finalize PR

---
+++ packages/elements/examples/nextjs/components/debug.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { SignedIn } from '@clerk/clerk-react';
+import { useSignInFlow } from '@clerk/elements';
+import { SignOutButton } from '@clerk/nextjs';
+
+export function Button(props: React.ComponentProps<'button'>) {
+  return (
+    <button
+      className='px-4 py-2 b-1 bg-blue-950 bg-opacity-20 hover:bg-opacity-10 active:bg-opacity-5 rounded-md dark:bg-opacity-100 dark:hover:bg-opacity-80 dark:active:bg-opacity-50 transition'
+      type='button'
+      {...props}
+    />
+  );
+}
+
+export function Debug() {
+  const ref = useSignInFlow();
+
+  return (
+    <div className='absolute text-xs flex p-4 gap-4 bottom-0 w-screen justify-center bg-secondary border-tertiary border-t'>
+      <Button
+        onClick={() => {
+          console.dir(ref.getSnapshot().context.fields);
+        }}
+      >
+        Log Fields
+      </Button>
+
+      <Button
+        onClick={() => {
+          console.dir(ref.getSnapshot().context);
+        }}
+      >
+        Log Context
+      </Button>
+
+      <SignedIn>
+        <SignOutButton>
+          <Button>Sign Out</Button>
+        </SignOutButton>
+      </SignedIn>
+    </div>
+  );
+}
--- packages/elements/src/internals/machines/sign-in.context.ts
+++ packages/elements/src/internals/machines/sign-in.context.ts
@@ -1,5 +1,94 @@
 import { createActorContext } from '@xstate/react';
+import { useCallback, useEffect } from 'react';
+import type { SnapshotFrom } from 'xstate';
 
 import { SignInMachine } from './sign-in.machine';
+import type { FieldDetails } from './sign-in.types';
 
-export const SignInActor = createActorContext(SignInMachine);
+export const {
+  Provider: SignInFlowProvider,
+  useActorRef: useSignInFlow,
+  useSelector: useSignInFlowSelector,
+} = createActorContext(SignInMachine);
+
+// TODO: Move selectors
+const fieldHasValueSelector = (type: string | undefined) => (state: SnapshotFrom<typeof SignInMachine>) =>
+  type ? Boolean(state.context.fields.get(type)?.value) : false;
+
+const fieldErrorSelector = (type: string | undefined) => (state: SnapshotFrom<typeof SignInMachine>) =>
+  type ? Boolean(state.context.fields.get(type)?.error) : undefined;
+
+const globalErrorSelector = (state: SnapshotFrom<typeof SignInMachine>) => state.context.error;
+
+export const useField = ({ type }: Partial<Pick<FieldDetails, 'type'>>) => {
+  const hasValue = useSignInFlowSelector(fieldHasValueSelector(type));
+  const error = useSignInFlowSelector(fieldErrorSelector(type));
+
+  const shouldBeHidden = false; // TODO: Implement clerk-js utils
+  const validity = error ? 'invalid' : 'valid';
+
+  return {
+    hasValue,
+    props: {
+      [`data-${validity}`]: true,
+      'data-hidden': shouldBeHidden ? true : undefined,
+      tabIndex: shouldBeHidden ? -1 : 0,
+    },
+  };
+};
+
+export const useInput = ({ type, value: initialValue }: Partial<Pick<FieldDetails, 'type' | 'value'>>) => {
+  const ref = useSignInFlow();
+  const hasValue = useSignInFlowSelector(fieldHasValueSelector(type));
+
+  useEffect(() => {
+    if (!type || ref.getSnapshot().context.fields.get(type)) return;
+
+    ref.send({ type: 'FIELD.ADD', field: { type, value: initialValue } });
+
+    return () => ref.send({ type: 'FIELD.REMOVE', field: { type } });
+  }, [ref]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const onChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (!type) return;
+      ref.send({ type: 'FIELD.UPDATE', field: { type, value: event.target.value } });
+    },
+    [ref, type],
+  );
+
+  // TODO: Implement clerk-js utils
+  const shouldBeHidden = false;
+
+  return {
+    hasValue,
+    props: {
+      'data-hidden': shouldBeHidden ? true : undefined,
+      'data-has-value': hasValue ? true : undefined,
+      onChange,
+      tabIndex: shouldBeHidden ? -1 : 0,
+    },
+  };
+};
+
+export const useForm = () => {
+  const ref = useSignInFlow();
+  const error = useSignInFlowSelector(globalErrorSelector);
+
+  const validity = error ? 'invalid' : 'valid';
+
+  const onSubmit = useCallback(
+    (event: React.FormEvent<Element>) => {
+      event.preventDefault();
+      ref.send({ type: 'SUBMIT' });
+    },
+    [ref],
+  );
+
+  return {
+    props: {
+      [`data-${validity}`]: true,
+      onSubmit,
+    },
+  };
+};
