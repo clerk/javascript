0f8cd04c02fb832d8fc1064cf41ad41e928d43e8
Tom Milewski <me@tm.codes>
2024-01-02 11:14:36 -0500

    chore(elements): Update example app (#2453)
    * chore(elements): Add helpful links to example app
    
    * chore(elements): Add changeset

--- packages/elements/examples/nextjs/app/layout.tsx
+++ packages/elements/examples/nextjs/app/layout.tsx
@@ -2,9 +2,10 @@ import './globals.css';
 
 import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Inter, Roboto_Mono } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+const mono = Roboto_Mono({ subsets: ['latin'], variable: '--font-roboto-mono' });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <ClerkProvider clerkJSVariant='headless'>
-        <body className={`${inter.variable} font-sans`}>{children}</body>
+        <body className={`${inter.variable} ${mono.variable} font-sans`}>{children}</body>
       </ClerkProvider>
     </html>
   );
