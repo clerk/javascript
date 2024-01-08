939a4a37e5dadcd1573c7ba1e9d2dde294364950
panteliselef <panteliselef@outlook.com>
2024-01-08 12:06:48 +0200

    fix(repo): Add redirectUrl to app-dir in nextjs playground

--- playground/nextjs/app/app-dir/sign-in/page.tsx
+++ playground/nextjs/app/app-dir/sign-in/page.tsx
@@ -1,5 +1,11 @@
 import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignIn path='/app-dir/sign-in' signUpUrl='/app-dir/sign-up' />
+  return (
+    <SignIn
+      path='/app-dir/sign-in'
+      signUpUrl='/app-dir/sign-up'
+      redirectUrl='/app-dir'
+    />
+  );
 }
