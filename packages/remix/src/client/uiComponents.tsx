b67f6ab799c7a508a2cccd196cf4c551d4f2222e
Dimitris Klouvas <dimitris@clerk.dev>
2024-01-08 17:51:52 +0200

    fix(nextjs,remix): CustomPages in remix & nextjs SDKs (#2508)

--- packages/remix/src/client/uiComponents.tsx
+++ packages/remix/src/client/uiComponents.tsx
@@ -17,17 +17,31 @@ import React from 'react';
 
 import { useClerkRemixOptions } from './RemixOptionsContext';
 
-export const UserProfile = (props: UserProfileProps) => {
-  return <BaseUserProfile {...useRoutingProps('UserProfile', props)} />;
-};
+// The assignment of UserProfile with BaseUserProfile props is used
+// to support the CustomPage functionality (eg UserProfile.Page)
+// Also the `typeof BaseUserProfile` is used to resolved the following error:
+// "The inferred type of 'UserProfile' cannot be named without a reference to ..."
+export const UserProfile: typeof BaseUserProfile = Object.assign(
+  (props: UserProfileProps) => {
+    return <BaseUserProfile {...useRoutingProps('UserProfile', props)} />;
+  },
+  { ...BaseUserProfile },
+);
 
 export const CreateOrganization = (props: CreateOrganizationProps) => {
   return <BaseCreateOrganization {...useRoutingProps('CreateOrganization', props)} />;
 };
 
-export const OrganizationProfile = (props: OrganizationProfileProps) => {
-  return <BaseOrganizationProfile {...useRoutingProps('OrganizationProfile', props)} />;
-};
+// The assignment of OrganizationProfile with BaseOrganizationProfile props is used
+// to support the CustomPage functionality (eg OrganizationProfile.Page)
+// Also the `typeof BaseOrganizationProfile` is used to resolved the following error:
+// "The inferred type of 'OrganizationProfile' cannot be named without a reference to ..."
+export const OrganizationProfile: typeof BaseOrganizationProfile = Object.assign(
+  (props: OrganizationProfileProps) => {
+    return <BaseOrganizationProfile {...useRoutingProps('OrganizationProfile', props)} />;
+  },
+  { ...BaseOrganizationProfile },
+);
 
 export const SignIn = (props: SignInProps) => {
   const { signInUrl } = useClerkRemixOptions();
