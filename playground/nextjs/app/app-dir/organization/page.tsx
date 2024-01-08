18cb1a913a5197f49e115778e8ef89c3c3589416
Vaggelis Yfantis <vaggelisyfantis@gmail.com>
2024-01-08 11:36:26 +0200

    chore(repo): Added path props to all components needed in app-dir version of NextJs playground (#2494)

--- playground/nextjs/app/app-dir/organization/page.tsx
+++ playground/nextjs/app/app-dir/organization/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div>
       <OrganizationSwitcher />
-      <OrganizationProfile />
+      <OrganizationProfile path='/app-dir/organization' />
     </div>
   );
 }
--- playground/nextjs/app/app-dir/sign-up/page.tsx
+++ playground/nextjs/app/app-dir/sign-up/page.tsx
@@ -1,5 +1,5 @@
 import { SignUp } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignUp />;
+  return <SignUp path='/app-dir/sign-up' signInUrl='/app-dir/sign-in' />;
 }
