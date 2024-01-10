6f45f070cfd35bf39490115d266e15afb03cb828
Jeff Escalante <jescalan@users.noreply.github.com>
2024-01-10 12:24:40 -0500

    fix(upgrade): Match against devDeps as well as dependencies

--- packages/upgrade/source/util/guess-framework.js
+++ packages/upgrade/source/util/guess-framework.js
@@ -3,11 +3,14 @@ import SDKS from '../constants/sdks.js';
 
 export default function guessFrameworks(dir) {
 	const pkg = readPackageSync({ cwd: dir });
-	if (!pkg.dependencies) return []; // no guessing if there are no deps
+
+	// no guessing if there are no deps
+	if (!pkg.dependencies && !pkg.devDependencies) return [];
 	const deps = Object.keys(pkg.dependencies);
+	const devDeps = Object.keys(pkg.devDependencies);
 
 	return SDKS.reduce((m, { label, value }) => {
-		if (deps.includes(label)) m.push({ label, value });
+		if (deps.includes(label) || devDeps.includes(label)) m.push({ label, value });
 		return m;
 	}, []);
 }
