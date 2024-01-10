0cfd8cf3f1bef0a8f34b7a4d2d782dd8fc2a7090
Nikos Douvlis <nikosdouvlis@gmail.com>
2024-01-10 14:49:24 +0200

    chore(repo): Force canary release even if in pre mode

--- scripts/canary.mjs
+++ scripts/canary.mjs
@@ -6,6 +6,17 @@ import { constants } from './common.mjs';
 
 await $`npx json -I -f ${constants.ChangesetConfigFile} -e "this.changelog = false"`;
 
+try {
+  // exit pre-release mode if we're in it
+  await $`npx changeset pre exit`;
+  // bump the version of all affected packages
+  // this will remove the prerelease versions
+  // but will also clear the changeset .md files
+  await $`npx changeset version`;
+} catch (e) {
+  // otherwise, do nothing
+}
+
 const res = await $`npx changeset version --snapshot canary`;
 const success = !res.stderr.includes('No unreleased changesets found');
 
