8c3df2b595cf35289658913314e7665396ccd0d8
panteliselef <panteliselef@outlook.com>
2024-01-11 19:11:28 +0200

    test(clerk-js): Remove Email

--- packages/clerk-js/src/ui/components/UserProfile/__tests__/EmailsSection.test.tsx
+++ packages/clerk-js/src/ui/components/UserProfile/__tests__/EmailsSection.test.tsx
@@ -1,4 +1,5 @@
 import { describe, it } from '@jest/globals';
+import { act } from '@testing-library/react';
 
 import { render, waitFor } from '../../../../testUtils';
 import { CardStateProvider } from '../../../elements';
@@ -11,17 +12,21 @@ const initConfig = createFixtures.config(f => {
   f.withUser({ username: 'georgeclerk' });
 });
 
+const emails = ['test@clerk.com', 'test2@clerk.com'];
+const withEmails = createFixtures.config(f => {
+  f.withEmailAddress();
+  f.withUser({
+    email_addresses: emails,
+  });
+});
+
+const getMenuItemFromText = (element: HTMLElement) => {
+  return element.parentElement?.parentElement?.parentElement?.children?.[1];
+};
+
 describe('EmailSection', () => {
   it('renders the section', async () => {
-    const emails = ['test@clerk.com', 'test2@clerk.com'];
-    const { wrapper, fixtures } = await createFixtures(f => {
-      f.withEmailAddress();
-      f.withUser({
-        email_addresses: emails,
-        first_name: 'George',
-        last_name: 'Clerk',
-      });
-    });
+    const { wrapper, fixtures } = await createFixtures(withEmails);
     fixtures.clerk.user!.getSessions.mockReturnValue(Promise.resolve([]));
 
     const { getByText } = render(
@@ -84,6 +89,98 @@ describe('EmailSection', () => {
     });
   });
 
-  // TODO-RETHEME: Test Removal
-  describe('Remove email', () => {});
+  describe('Remove email', () => {
+    it('Renders remove screen', async () => {
+      const { wrapper } = await createFixtures(withEmails);
+
+      const { getByText, userEvent, getByRole } = render(
+        <CardStateProvider>
+          <EmailsSection />
+        </CardStateProvider>,
+        { wrapper },
+      );
+
+      const item = getByText(emails[0]);
+      const menuButton = getMenuItemFromText(item);
+      await act(async () => {
+        await userEvent.click(menuButton!);
+      });
+
+      getByRole('menuitem', { name: /remove email/i });
+      await userEvent.click(getByRole('menuitem', { name: /remove email/i }));
+      await waitFor(() => getByRole('heading', { name: /remove email address/i }));
+    });
+
+    it('removes an email address', async () => {
+      const { wrapper, fixtures } = await createFixtures(withEmails);
+      const { getByText, userEvent, getByRole, queryByRole } = render(
+        <CardStateProvider>
+          <EmailsSection />
+        </CardStateProvider>,
+        { wrapper },
+      );
+
+      fixtures.clerk.user?.emailAddresses[0].destroy.mockResolvedValue();
+
+      const item = getByText(emails[0]);
+      const menuButton = getMenuItemFromText(item);
+      await act(async () => {
+        await userEvent.click(menuButton!);
+      });
+
+      getByRole('menuitem', { name: /remove email/i });
+      await userEvent.click(getByRole('menuitem', { name: /remove email/i }));
+      await waitFor(() => getByRole('heading', { name: /Remove email address/i }));
+
+      await userEvent.click(getByRole('button', { name: /save/i }));
+      expect(fixtures.clerk.user?.emailAddresses[0].destroy).toHaveBeenCalled();
+
+      await waitFor(() => expect(queryByRole('heading', { name: /Remove email address/i })).not.toBeInTheDocument());
+    });
+
+    describe('Form buttons', () => {
+      it('save button is not disabled by default', async () => {
+        const { wrapper } = await createFixtures(withEmails);
+        const { getByRole, userEvent, getByText } = render(
+          <CardStateProvider>
+            <EmailsSection />
+          </CardStateProvider>,
+          { wrapper },
+        );
+
+        const item = getByText(emails[0]);
+        const menuButton = getMenuItemFromText(item);
+        await act(async () => {
+          await userEvent.click(menuButton!);
+        });
+
+        getByRole('menuitem', { name: /remove email/i });
+        await userEvent.click(getByRole('menuitem', { name: /remove email/i }));
+        await waitFor(() => getByRole('heading', { name: /Remove email address/i }));
+        expect(getByRole('button', { name: /save$/i })).not.toHaveAttribute('disabled');
+      });
+
+      it('hides screen when when pressing cancel', async () => {
+        const { wrapper } = await createFixtures(withEmails);
+        const { getByRole, userEvent, getByText, queryByRole } = render(
+          <CardStateProvider>
+            <EmailsSection />
+          </CardStateProvider>,
+          { wrapper },
+        );
+
+        const item = getByText(emails[0]);
+        const menuButton = getMenuItemFromText(item);
+        await act(async () => {
+          await userEvent.click(menuButton!);
+        });
+
+        getByRole('menuitem', { name: /remove email/i });
+        await userEvent.click(getByRole('menuitem', { name: /remove email/i }));
+        await waitFor(() => getByRole('heading', { name: /Remove email address/i }));
+        await userEvent.click(getByRole('button', { name: /cancel$/i }));
+        expect(queryByRole('heading', { name: /Remove email address/i })).not.toBeInTheDocument();
+      });
+    });
+  });
 });
