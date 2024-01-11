84d1850a618767096c5545878b95c50bc5051fd2
Tom Milewski <me@tm.codes>
2024-01-11 14:05:25 -0500

    chore(elements): Update types & waitForClerk; Remove unused files (#2566)

--- packages/elements/src/internals/machines/shared.actors.ts
+++ packages/elements/src/internals/machines/shared.actors.ts
@@ -1,12 +1,18 @@
 import type { Clerk, LoadedClerk } from '@clerk/types';
 import { fromPromise } from 'xstate';
 
+import { ClerkElementsRuntimeError } from '~/internals/errors/error';
+
 export const waitForClerk = fromPromise<LoadedClerk, Clerk | LoadedClerk>(({ input: clerk }) => {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     if (clerk.loaded) {
       resolve(clerk as LoadedClerk);
-    } else {
+    } else if ('addOnLoaded' in clerk) {
+      // @ts-expect-error - Expects addOnLoaded from IsomorphicClerk.
+      // We don't want internals to rely on the @clerk/clerk-react package
       clerk.addOnLoaded(() => resolve(clerk as LoadedClerk));
+    } else {
+      reject(new ClerkElementsRuntimeError('Clerk client could not be loaded'));
     }
   });
 });
