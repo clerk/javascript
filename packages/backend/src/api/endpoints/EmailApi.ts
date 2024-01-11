2a615bf98a22250c9105671ce72bf5374602802f
Nikos Polykandriotis <nikos.polikandriotis@clerk.dev>
2024-01-11 17:13:48 +0200

    feat(backend): Deprecate createEmail (#2555)

--- packages/backend/src/api/endpoints/EmailApi.ts
+++ packages/backend/src/api/endpoints/EmailApi.ts
@@ -1,3 +1,5 @@
+import { deprecated } from '@clerk/shared/deprecated';
+
 import type { Email } from '../resources/Email';
 import { AbstractAPI } from './AbstractApi';
 
@@ -10,8 +12,18 @@ type EmailParams = {
 
 const basePath = '/emails';
 
+/**
+ * @deprecated This endpoint is no longer available and the function will be removed in the next major version.
+ */
 export class EmailAPI extends AbstractAPI {
+  /**
+   * @deprecated This endpoint is no longer available and the function will be removed in the next major version.
+   */
   public async createEmail(params: EmailParams) {
+    deprecated(
+      'EmailAPI.createEmail',
+      'This endpoint is no longer available and the function will be removed in the next major version.',
+    );
     return this.request<Email>({
       method: 'POST',
       path: basePath,
