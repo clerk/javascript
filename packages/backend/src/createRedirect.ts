aae0457a42b3e485b2d3785d45104e70f2ac1bbb
Nikos Douvlis <nikosdouvlis@gmail.com>
2024-01-18 19:58:26 +0200

    fix(nextjs,backend): Re-introduce top-level redirectToSignIn and redirectToSignUp helpers (#2630)

--- packages/backend/src/createRedirect.ts
+++ packages/backend/src/createRedirect.ts
@@ -1,13 +1,47 @@
 import { errorThrower, parsePublishableKey } from './util/shared';
 
 const buildUrl = (_baseUrl: string | URL, _targetUrl: string | URL, _returnBackUrl?: string | URL) => {
+  if (_baseUrl === '') {
+    return legacyBuildUrl(_targetUrl.toString(), _returnBackUrl?.toString());
+  }
+
   const baseUrl = new URL(_baseUrl);
   const returnBackUrl = _returnBackUrl ? new URL(_returnBackUrl, baseUrl) : undefined;
   const res = new URL(_targetUrl, baseUrl);
+
   if (returnBackUrl) {
     res.searchParams.set('redirect_url', returnBackUrl.toString());
   }
   return res.toString();
+};
+
+/**
+ * In v5, we deprecated the top-level redirectToSignIn and redirectToSignUp functions
+ * in favor of the new auth().redirectToSignIn helpers
+ * In order to allow for a smooth transition, we need to support the legacy redirectToSignIn for now
+ * as we will remove it in v6.
+ * In order to make sure that the legacy function works as expected, we will use legacyBuildUrl
+ * to build the url if baseUrl is not provided (which is the case for legacy redirectToSignIn)
+ * This function can be safely removed when we remove the legacy redirectToSignIn function
+ */
+const legacyBuildUrl = (targetUrl: string, redirectUrl?: string) => {
+  let url;
+  if (!targetUrl.startsWith('http')) {
+    if (!redirectUrl || !redirectUrl.startsWith('http')) {
+      throw new Error('destination url or return back url should be an absolute path url!');
+    }
+
+    const baseURL = new URL(redirectUrl);
+    url = new URL(targetUrl, baseURL.origin);
+  } else {
+    url = new URL(targetUrl);
+  }
+
+  if (redirectUrl) {
+    url.searchParams.set('redirect_url', redirectUrl);
+  }
+
+  return url.toString();
 };
 
 const buildAccountsBaseUrl = (frontendApi?: string) => {
