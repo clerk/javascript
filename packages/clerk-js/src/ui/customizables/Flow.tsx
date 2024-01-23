94b80d3ee0f8690b223a1e1f041ae6b1b40d7f85
George Desipris <george.desipris.56@gmail.com>
2024-01-24 00:05:31 +0200

    fix(clerk-js): Fix height issue in OS and UB

--- packages/clerk-js/src/ui/customizables/Flow.tsx
+++ packages/clerk-js/src/ui/customizables/Flow.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 import type { FlowMetadata } from '../elements';
 import { FlowMetadataProvider, InvisibleRootBox, useFlowMetadata } from '../elements';
+import type { ThemableCssProp } from '../styledSystem';
 import { InternalThemeProvider } from '../styledSystem';
 import { generateFlowClassname } from './classGeneration';
 import { descriptors } from './index';
 
-type FlowRootProps = React.PropsWithChildren & FlowMetadata;
+type FlowRootProps = React.PropsWithChildren & FlowMetadata & { sx?: ThemableCssProp };
 
 const Root = (props: FlowRootProps) => {
   return (
