fab3d0bc8a4313008c5fd39ed2a8ed63db46bd7b
Vaggelis Yfantis <me@octoper.me>
2024-02-05 21:13:27 +0200

    feat(clerk-js): Scroll action card into view when opens

--- packages/clerk-js/src/ui/elements/Action/ActionOpen.tsx
+++ packages/clerk-js/src/ui/elements/Action/ActionOpen.tsx
@@ -1,16 +1,29 @@
 import type { PropsWithChildren } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import { useActionContext } from './ActionRoot';
 
 type ActionOpenProps = PropsWithChildren<{ value: string }>;
 
+const ScrollWrapper = React.forwardRef<HTMLDivElement, PropsWithChildren>((props, ref) => {
+  return <div ref={ref}>{props.children}</div>;
+});
+
 export const ActionOpen = (props: ActionOpenProps) => {
   const { children, value } = props;
   const { active } = useActionContext();
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (active === value && ref.current) {
+      ref.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }, [active, value]);
 
   if (active !== value) {
     return null;
   }
 
-  return children;
+  // Make sure the child can accept a ref
+  return <ScrollWrapper ref={ref}>{children}</ScrollWrapper>;
 };
