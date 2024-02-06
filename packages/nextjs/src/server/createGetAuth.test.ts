cdd87d43963a8a4be51c889abc620e531db8e9cd
Nikos Douvlis <nikosdouvlis@gmail.com>
2024-02-06 23:39:43 +0200

    chore(nextjs): Improve createGetAuth test coverage
    (cherry picked from commit 79e9867b7ee3d7dc7dae1f82f61164ab1daf9f4a)

--- packages/nextjs/src/server/createGetAuth.test.ts
+++ packages/nextjs/src/server/createGetAuth.test.ts
@@ -5,6 +5,8 @@ import { createGetAuth, getAuth } from './createGetAuth';
 
 // { alg: 'HS256' }.{ sub: 'user-id' }.sig
 const mockToken = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyLWlkIn0.0u5CllULtDVD9DUUmUMdJLbBCSNcnv4j3hCaPz4dNr8';
+// { alg: 'HS256' }.{ sub: 'user-id-2' }.sig
+const mockToken2 = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyLWlkLTIifQ.K-mhz0Ber1Hfh2xCwmvsLwhZO_IKLtKt78KTHsecEas';
 
 describe('createGetAuth(opts)', () => {
   it('returns a getAuth function', () => {
@@ -36,6 +38,20 @@ describe('getAuth(req)', () => {
     });
 
     expect(getAuth(req).userId).toEqual(null);
+  });
+
+  it('prioritizes the token found in the auth header if a cookie token also exists', () => {
+    const req = new NextRequest('https://www.clerk.com', {
+      headers: new Headers({
+        [constants.Headers.AuthStatus]: AuthStatus.SignedIn,
+        [constants.Headers.AuthToken]: mockToken,
+        [constants.Headers.AuthMessage]: 'message',
+        [constants.Headers.AuthReason]: 'reason',
+        Cookie: `__session=${mockToken2};`,
+      }),
+    });
+
+    expect(getAuth(req).userId).toEqual('user-id');
   });
 
   it('throws if auth status is not found', () => {
