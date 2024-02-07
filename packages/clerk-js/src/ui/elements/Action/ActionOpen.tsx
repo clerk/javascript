aab7963e4a74a80196b7b363acfa9e6d6e2729c4
Vaggelis Yfantis <me@octoper.me>
2024-02-07 14:57:24 +0200

    fix(clerk-js): Fixes to scroll card into view

--- packages/clerk-js/src/ui/elements/Action/ActionOpen.tsx
+++ packages/clerk-js/src/ui/elements/Action/ActionOpen.tsx
@@ -6,25 +6,29 @@ import { useActionContext } from './ActionRoot';
 type ActionOpenProps = PropsWithChildren<{ value: string }>;
 
 const ScrollWrapper = React.forwardRef<HTMLDivElement, PropsWithChildren>((props, ref) => {
-  return <div ref={ref}>{props.children}</div>;
+  return (
+    <div
+      ref={ref}
+      {...props}
+    />
+  );
 });
 
 export const ActionOpen = (props: ActionOpenProps) => {
   const { children, value } = props;
   const { active } = useActionContext();
-  const ref = useRef<HTMLElement>(null);
+  const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const element = ref.current;
-    if (active === value && element) {
-      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    }
+    if (!element || active !== value) return;
+
+    element.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
   }, [active, value]);
 
   if (active !== value) {
     return null;
   }
 
-  // Make sure the child can accept a ref
   return <ScrollWrapper ref={ref}>{children}</ScrollWrapper>;
 };
