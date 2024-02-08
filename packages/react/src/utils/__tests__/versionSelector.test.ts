046b9d7595b8ca3e6b6ff9659a7591d11f5c5e2d
Nikos Douvlis <nikosdouvlis@gmail.com>
2024-02-08 15:27:25 +0200

    fix(clerk-react): Replace semver with custom regex in versionSelector
    The semver package doesn't provide exports map in its package.json. Vitest is ESM-only and relies more on these things whereas Jest is more forgiving. Instead of tweaking build config for clerk-react and given that we only rely on major() and prerelease() and the bundle-size considerations, we replaced semver with custom regexes

--- packages/react/src/utils/__tests__/versionSelector.test.ts
+++ packages/react/src/utils/__tests__/versionSelector.test.ts
@@ -5,6 +5,7 @@ describe('versionSelector', () => {
   it('should return the clerkJSVersion if it is provided', () => {
     expect(versionSelector('1.0.0')).toEqual('1.0.0');
   });
+
   it('should use the major version if there is no prerelease tag', () => {
     // @ts-ignore
     global.PACKAGE_VERSION = '1.0.0';
@@ -13,6 +14,7 @@ describe('versionSelector', () => {
 
     expect(versionSelector(undefined)).toEqual('1');
   });
+
   it('should use the prerelease tag when it is not snapshot', () => {
     // @ts-ignore
     global.PACKAGE_VERSION = '1.0.0-next.0';
@@ -21,6 +23,7 @@ describe('versionSelector', () => {
 
     expect(versionSelector(undefined)).toEqual('next');
   });
+
   it('should use the exact JS version if tag is snapshot', () => {
     // @ts-ignore
     global.PACKAGE_VERSION = '1.0.0-snapshot.0';
@@ -28,5 +31,26 @@ describe('versionSelector', () => {
     global.JS_PACKAGE_VERSION = '2.0.0-snapshot.0';
 
     expect(versionSelector(undefined)).toEqual('2.0.0-snapshot.0');
+  });
+
+  // We replaced semver with 2 custom regexes
+  // so we're testing the same cases as semver tests
+  // https://github.com/npm/node-semver/blob/main/test/functions/prerelease.js
+  // https://github.com/npm/node-semver/blob/main/test/functions/major.js
+  test.each([
+    ['1.2.3', 1],
+    [' 1.2.3 ', 1],
+    [' 2.2.3-4 ', 4],
+    [' 3.2.3-pre ', 'pre'],
+    ['v5.2.3', 5],
+    [' v8.2.3 ', 8],
+    ['\t13.2.3', 13],
+    ['1.2.2-alpha.1', 'alpha'],
+    ['1.2.2-beta.1', 'beta'],
+    ['1.2.2-rc.1', 'rc'],
+    ['1.2.2', 1],
+    ['1.2.2-pre', 'pre'],
+  ])('versionSelector(%s) should return %i', (version, expected) => {
+    expect(versionSelector(undefined, version)).toEqual(expected.toString());
   });
 });
