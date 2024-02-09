4a05f2e90fff3aa23c66521f29469a80631a7262
Clerk Cookie <136073014+clerk-cookie@users.noreply.github.com>
2024-02-09 04:17:13 -0500

    fix(clerk-react): Properly fire onLoad event when clerk-js is already loaded (#2757) (#2767)
    In case of mounting-unmounting-mounting the `IsomorphicClerk.addOnLoaded` would not fire correctly, hence `derivedState` would return the initialState instead of the client side state.
    
    In addition to the above fix, we are now cleaning up the IsomorphicClerk instance when the provider is unmounted.
    
    (cherry picked from commit 6ac9e717a7ce8f09c1604f324add5e7e02041c07)
    
    Co-authored-by: panteliselef <panteliselef@outlook.com>

--- packages/react/src/contexts/ClerkContextProvider.tsx
+++ packages/react/src/contexts/ClerkContextProvider.tsx
@@ -107,5 +107,11 @@ const useLoadedIsomorphicClerk = (options: IsomorphicClerkOptions) => {
     isomorphicClerk.addOnLoaded(() => setLoaded(true));
   }, []);
 
+  React.useEffect(() => {
+    return () => {
+      IsomorphicClerk.clearInstance();
+    };
+  }, []);
+
   return { isomorphicClerk, loaded };
 };
