9263da869343e26d06f87f4f013e95b583cf3b38
George Desipris <73396808+desiprisg@users.noreply.github.com>
2024-02-09 14:19:03 +0200

    fix(clerk-js): Use theme for polished outline button shadow (#2772)

--- packages/clerk-js/src/ui/polishedAppearance.ts
+++ packages/clerk-js/src/ui/polishedAppearance.ts
@@ -33,8 +33,7 @@ export const polishedAppearance = {
         },
         '&[data-variant="outline"]': {
           border: 0,
-          boxShadow:
-            '0px 2px 3px -1px rgba(0, 0, 0, 0.08), 0px 1px 0px 0px rgba(0, 0, 0, 0.02), 0px 0px 0px 1px rgba(0, 0, 0, 0.08)',
+          boxShadow: `0px 2px 3px -1px rgba(0, 0, 0, 0.08), 0px 1px 0px 0px rgba(0, 0, 0, 0.02), 0px 0px 0px 1px ${theme.colors.$neutralAlpha100}`,
         },
       },
       badge: {
