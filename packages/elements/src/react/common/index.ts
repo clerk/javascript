1dc8073ac685ff296767839c6a29e33330d28a87
LekoArts <lekoarts@gmail.com>
2024-02-14 14:07:35 +0100

    fix(elements): Don't export Form from /common

--- packages/elements/src/react/common/index.ts
+++ packages/elements/src/react/common/index.ts
@@ -1,2 +1,2 @@
-export { Field, FieldError, FieldState, Form, GlobalError, Input, Label, Submit } from '~/react/common/form';
+export { Field, FieldError, FieldState, GlobalError, Input, Label, Submit } from '~/react/common/form';
 export { SocialProviderIcon } from '~/react/common/third-party-providers/social-provider';
