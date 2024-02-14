506b1ded444d4e9a14da4c3c13bfa0018eed9409
Lennart <lekoarts@gmail.com>
2024-02-14 16:11:07 +0100

    fix(elements): Misc stuff (#2797)
    * fix(elements): Don't export Form from /common
    
    * fix(elements): Make GlobalError a div
    
    * fix(elements): Don't export SocialProviderIcon from /common
    
    * chore(repo): Add empty changeset
    
    * chore(elements): Bump package version

--- packages/elements/src/react/common/index.ts
+++ packages/elements/src/react/common/index.ts
@@ -1,2 +1,1 @@
-export { Field, FieldError, FieldState, Form, GlobalError, Input, Label, Submit } from '~/react/common/form';
-export { SocialProviderIcon } from '~/react/common/third-party-providers/social-provider';
+export { Field, FieldError, FieldState, GlobalError, Input, Label, Submit } from '~/react/common/form';
