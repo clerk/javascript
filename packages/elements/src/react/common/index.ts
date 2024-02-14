edc74ef96b0d0013905622ec76daa57ba7fa6628
LekoArts <lekoarts@gmail.com>
2024-02-14 14:51:45 +0100

    fix(elements): Don't export SocialProviderIcon from /common

--- packages/elements/src/react/common/index.ts
+++ packages/elements/src/react/common/index.ts
@@ -1,2 +1,1 @@
 export { Field, FieldError, FieldState, GlobalError, Input, Label, Submit } from '~/react/common/form';
-export { SocialProviderIcon } from '~/react/common/third-party-providers/social-provider';
