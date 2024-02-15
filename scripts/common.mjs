58e88c152c604fc093201a10f89e20f672803677
LekoArts <lekoarts@gmail.com>
2024-02-15 11:51:25 +0100

    chore(repo): WIP

--- scripts/common.mjs
+++ scripts/common.mjs
@@ -2,10 +2,7 @@ export const constants = {
   ChangesetConfigFile: '.changeset/config.json',
   ElementsPackageJson: 'packages/elements/package.json',
   ElementsPeerDeps: {
-    '@clerk/clerk-react': '^4.30.5 || ^5.0.0-beta-v5 || ^5.0.0-canary',
-    '@clerk/shared': '^1.3.1 || ^2.0.0-beta-v5 || ^2.0.0-canary',
-    next: '>=13.0.4',
-    react: '>=18',
-    'react-dom': '>=18',
+    react: '^4.30.5 || ^5.0.0-beta-v5 || ^5.0.0-canary',
+    shared: '^1.3.1 || ^2.0.0-beta-v5 || ^2.0.0-canary',
   },
 };
