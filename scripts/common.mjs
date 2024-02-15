303174dd798bb05e257b54d5dc1acca3a278c0bf
LekoArts <lekoarts@gmail.com>
2024-02-15 11:58:24 +0100

    chore(repo): Finish

--- scripts/common.mjs
+++ scripts/common.mjs
@@ -1,8 +1,4 @@
 export const constants = {
   ChangesetConfigFile: '.changeset/config.json',
   ElementsPackageJson: 'packages/elements/package.json',
-  ElementsPeerDeps: {
-    react: '^4.30.5 || ^5.0.0-beta-v5 || ^5.0.0-canary',
-    shared: '^1.3.1 || ^2.0.0-beta-v5 || ^2.0.0-canary',
-  },
 };
