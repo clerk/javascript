1766292b519a30c5165b2fd0ed3ad66dfd518b54
Tom Milewski <me@tm.codes>
2024-02-20 16:11:18 -0500

    chore(elements): Clean up old code; Update XState deps

--- packages/elements/src/react/sign-up/context/sign-up.context.ts
+++
@@ -1,8 +0,0 @@
-import { createActorContext } from '@xstate/react';
-import type { SnapshotFrom } from 'xstate';
-
-import { SignUpMachine } from '~/internals/machines/sign-up/sign-up.machine';
-
-export type SnapshotState = SnapshotFrom<typeof SignUpMachine>;
-
-export const SignUpCtx = createActorContext(SignUpMachine);
