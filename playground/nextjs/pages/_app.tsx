318d4eb37245480690fec1fb5ef7ec7ddb25adeb
Stefanos Anagnostou <stefanos@clerk.dev>
2024-02-23 19:56:50 +0200

    fix(themes): Rename the `simple` theme to `__unstable_simple`

--- playground/nextjs/pages/_app.tsx
+++ playground/nextjs/pages/_app.tsx
@@ -10,7 +10,7 @@ import {
   SignOutButton,
   UserButton,
 } from '@clerk/nextjs';
-import { dark, neobrutalism, shadesOfPurple, simple } from '@clerk/themes';
+import { __unstable_simple, dark, neobrutalism, shadesOfPurple } from '@clerk/themes';
 import Link from 'next/link';
 import React, { FunctionComponent, useEffect, useState } from 'react';
 
@@ -56,7 +56,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider
       appearance={{
-        baseTheme: styleReset ? [simple, themes[selectedTheme]] : themes[selectedTheme],
+        baseTheme: styleReset ? [__unstable_simple, themes[selectedTheme]] : themes[selectedTheme],
         variables: {
           colorPrimary: primaryColor,
         },
