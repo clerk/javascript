f58b6c3cf12bd62832fb47109c09ac8572103578
Stefanos Anagnostou <anagstef@users.noreply.github.com>
2024-02-24 00:35:01 +0200

    fix(themes): Rename the `simple` theme to `__unstable_simple` (#2852)

--- packages/themes/src/themes/simple.ts
+++ packages/themes/src/themes/simple.ts
@@ -1,6 +1,6 @@
 import { experimental_createTheme } from '../createTheme';
 
-export const simple = experimental_createTheme({
+export const __unstable_simple = experimental_createTheme({
   //@ts-expect-error not public api
   simpleStyles: true,
 });
