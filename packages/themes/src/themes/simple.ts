a4775bf7c9093b7c1316ed63ee22f3356fad5234
Stefanos Anagnostou <anagstef@users.noreply.github.com>
2024-02-26 23:10:39 +0200

    chore(themes): Rename __unstable_simple to experimental__simple (#2860)

--- packages/themes/src/themes/simple.ts
+++ packages/themes/src/themes/simple.ts
@@ -1,6 +1,6 @@
 import { experimental_createTheme } from '../createTheme';
 
-export const __unstable_simple = experimental_createTheme({
+export const experimental__simple = experimental_createTheme({
   //@ts-expect-error not public api
   simpleStyles: true,
 });
