86eb8017a4349d98c0f84ce080e6f893248a09ff
Nikos Douvlis <nikosdouvlis@gmail.com>
2024-03-01 02:28:24 +0200

    fix(nextjs,clerk-js): Make useAwaitableNavigate handle navigations between pages reliably
    We need to use window to store the reference to the buffer, as ClerkProvider might be unmounted and remounted during navigations. If we use a ref, it will be reset when ClerkProvider is unmounted

--- packages/nextjs/src/app-router/client/ClerkProvider.tsx
+++ packages/nextjs/src/app-router/client/ClerkProvider.tsx
@@ -1,19 +1,19 @@
 'use client';
 import { ClerkProvider as ReactClerkProvider } from '@clerk/clerk-react';
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import React, { useEffect, useTransition } from 'react';
 
 import { ClerkNextOptionsProvider } from '../../client-boundary/NextOptionsContext';
 import { useSafeLayoutEffect } from '../../client-boundary/useSafeLayoutEffect';
 import type { NextClerkProviderProps } from '../../types';
 import { mergeNextClerkPropsWithEnv } from '../../utils/mergeNextClerkPropsWithEnv';
-import { invalidateCacheAction } from './invalidateCacheAction';
 import { useAwaitableNavigate } from './useAwaitableNavigate';
 
 declare global {
   export interface Window {
     __clerk_nav_await: Array<(value: void) => void>;
     __clerk_nav: (to: string) => Promise<void>;
+    __clerk_internal_invalidateCachePromise: () => void | undefined;
   }
 }
 
@@ -21,10 +21,41 @@ export const ClientClerkProvider = (props: NextClerkProviderProps) => {
   const { __unstable_invokeMiddlewareOnAuthStateChange = true } = props;
   const router = useRouter();
   const navigate = useAwaitableNavigate();
+  const [isPending, startTransition] = useTransition();
+
+  useEffect(() => {
+    if (!isPending) {
+      window.__clerk_internal_invalidateCachePromise?.();
+    }
+  }, [isPending]);
 
   useSafeLayoutEffect(() => {
     window.__unstable__onBeforeSetActive = () => {
-      return invalidateCacheAction();
+      /**
+       * We need to invalidate the cache in case the user is navigating to a page that
+       * was previously cached using the auth state that was active at the time.
+       *
+       *  We also need to await for the invalidation to happen before we navigate,
+       * otherwise the navigation will use the cached page.
+       *
+       * For example, if we did not invalidate the flow, the following scenario would be broken:
+       * - The middleware is configured in such a way that it redirects you back to the same page if a certain condition is true (eg, you need to pick an org)
+       * - The user has a <Link href=/> component in the page
+       * - The UB is mounted with afterSignOutUrl=/
+       * - The user clicks the Link. A nav to / happens, a 307 to the current page is returned so a navigation does not take place. The / navigation is now cached as a 307 to the current page
+       * - The user clicks sign out
+       * - We call router.refresh()
+       * - We navigate to / but its cached and instead, we 'redirect' to the current page
+       *
+       *  For more information on cache invalidation, see:
+       * https://nextjs.org/docs/app/building-your-application/caching#invalidation-1
+       */
+      return new Promise(res => {
+        window.__clerk_internal_invalidateCachePromise = res;
+        startTransition(() => {
+          router.refresh();
+        });
+      });
     };
 
     window.__unstable__onAfterSetActive = () => {
