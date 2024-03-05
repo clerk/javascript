39981c45d21da238187ad7d6e7483db0981185ae
George Desipris <george.desipris.56@gmail.com>
2024-03-05 12:23:12 +0200

    fix(clerk-js): Show counter of members/invitations/requests even if it is 0

--- packages/clerk-js/src/ui/common/NotificationCountBadge.tsx
+++ packages/clerk-js/src/ui/common/NotificationCountBadge.tsx
@@ -1,35 +1,26 @@
 import { formatToCompactNumber } from '../../ui/utils/intl';
 import { Flex, localizationKeys, NotificationBadge, useLocalizations } from '../customizables';
-import { useDelayedVisibility, usePrefersReducedMotion } from '../hooks';
+import { usePrefersReducedMotion } from '../hooks';
 import type { PropsOfComponent, ThemableCssProp } from '../styledSystem';
 import { animations } from '../styledSystem';
 
 type NotificationCountBadgeProps = PropsOfComponent<typeof NotificationBadge> & {
   notificationCount: number;
-  shouldDelayVisibility?: boolean;
   containerSx?: ThemableCssProp;
 };
 
 export const NotificationCountBadge = (props: NotificationCountBadgeProps) => {
-  const { notificationCount, containerSx, shouldDelayVisibility, ...restProps } = props;
+  const { notificationCount, containerSx, ...restProps } = props;
   const prefersReducedMotion = usePrefersReducedMotion();
-  const delayVisibility = shouldDelayVisibility || notificationCount > 0;
-  const showNotification = useDelayedVisibility(delayVisibility, 350) || false;
   const { t } = useLocalizations();
   const localeKey = t(localizationKeys('locale'));
-  const formatedNotificationCount = formatToCompactNumber(notificationCount, localeKey);
+  const formattedNotificationCount = formatToCompactNumber(notificationCount, localeKey);
 
   const enterExitAnimation: ThemableCssProp = t => ({
     animation: prefersReducedMotion
       ? 'none'
-      : `${showNotification ? animations.notificationAnimation : animations.outAnimation} ${
-          t.transitionDuration.$textField
-        } ${t.transitionTiming.$slowBezier} 0s 1 normal forwards`,
+      : `${animations.notificationAnimation} ${t.transitionDuration.$textField} ${t.transitionTiming.$slowBezier} 0s 1 normal forwards`,
   });
-
-  if (!showNotification) {
-    return null;
-  }
 
   return (
     <Flex
@@ -46,7 +37,7 @@ export const NotificationCountBadge = (props: NotificationCountBadgeProps) => {
         sx={enterExitAnimation}
         {...restProps}
       >
-        {formatedNotificationCount}
+        {formattedNotificationCount}
       </NotificationBadge>
     </Flex>
   );
