a25feba25c4ae69d65303e7961678c8d3318fa39
Vaggelis Yfantis <me@octoper.me>
2024-03-08 16:30:19 +0200

    fix(remix): Revert ClerkApp to the previous state

--- packages/remix/src/client/ClerkApp.tsx
+++ packages/remix/src/client/ClerkApp.tsx
@@ -1,7 +1,6 @@
-import { useLoaderData, useRouteError } from '@remix-run/react';
+import { useLoaderData } from '@remix-run/react';
 import React from 'react';
 
-import { ClerkErrorBoundary } from '.';
 import type { RemixClerkProviderProps } from './RemixClerkProvider';
 import { ClerkProvider } from './RemixClerkProvider';
 
@@ -11,12 +10,6 @@ export function ClerkApp(App: () => JSX.Element, opts: ClerkAppOptions = {}) {
   return () => {
     // @ts-expect-error - we know clerkState is in the loader data
     const { clerkState } = useLoaderData();
-    const error = useRouteError();
-
-    if (error) {
-      return ClerkErrorBoundary();
-    }
-
     return (
       // @ts-expect-error
       <ClerkProvider
