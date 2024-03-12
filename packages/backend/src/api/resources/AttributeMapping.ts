de00783c3eb1a71ae6f126413ab8e5ce49195210
Emmanouela <emmanouelapothitou@gmail.com>
2024-03-12 13:55:46 +0200

    feat(backend): Create SamlConnectionApi and SamlConnection resource

--- packages/backend/src/api/resources/AttributeMapping.ts
+++ packages/backend/src/api/resources/AttributeMapping.ts
@@ -2,10 +2,10 @@ import type { AttributeMappingJSON } from './JSON';
 
 export class AttributeMapping {
   constructor(
-    readonly userId: string,
-    readonly emailAddress: string,
-    readonly firstName: string,
-    readonly lastName: string,
+    readonly user_id: string,
+    readonly email_address: string,
+    readonly first_name: string,
+    readonly last_name: string,
   ) {}
 
   static fromJSON(data: AttributeMappingJSON): AttributeMapping {
