5fac1984fc352e21b337b18ccf928a9e4c638cc0
Emmanouela <emmanouelapothitou@gmail.com>
2024-03-13 14:03:28 +0200

    feat(backend): Use camelCase in class properties

--- packages/backend/src/api/resources/AttributeMapping.ts
+++ packages/backend/src/api/resources/AttributeMapping.ts
@@ -2,10 +2,10 @@ import type { AttributeMappingJSON } from './JSON';
 
 export class AttributeMapping {
   constructor(
-    readonly user_id: string,
-    readonly email_address: string,
-    readonly first_name: string,
-    readonly last_name: string,
+    readonly userId: string,
+    readonly emailAddress: string,
+    readonly firstName: string,
+    readonly lastName: string,
   ) {}
 
   static fromJSON(data: AttributeMappingJSON): AttributeMapping {
