aeee4e0ecea3bc4100e19ff92953ecf2dc37c786
Stefanos Anagnostou <stefanos@clerk.dev>
2024-03-14 19:52:18 +0200

    test(e2e): Add tests for Account Portal flows with both clerk v4 and v5

--- integration/templates/next-app-router-quickstart/src/app/page.tsx
+++ integration/templates/next-app-router-quickstart/src/app/page.tsx
@@ -1,12 +1,15 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
     <header>
       <SignedOut>
+        <p>signed-out-state</p>
         <SignInButton />
+        <SignUpButton />
       </SignedOut>
       <SignedIn>
+        <p>signed-in-state</p>
         <UserButton />
       </SignedIn>
     </header>
---
+++ integration/tests/next-account-portal/common.ts
@@ -0,0 +1,2 @@
+export const CLERK_DB_JWT_COOKIE_NAME = '__clerk_db_jwt';
+export const CLERK_SESSION_COOKIE_NAME = '__session';
