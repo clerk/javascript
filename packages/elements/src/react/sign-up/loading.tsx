c521446de466b5f954c979af53421aa1a3bff188
LekoArts <lekoarts@gmail.com>
2024-03-15 14:06:02 +0100

    chore(elements): Add JSDoc comments

--- packages/elements/src/react/sign-up/loading.tsx
+++ packages/elements/src/react/sign-up/loading.tsx
@@ -29,6 +29,26 @@ function mapScopeToStrategy(scope: Extract<LoadingScope, `provider:${string}`>):
   return `oauth_${scopeWithoutPrefix}`;
 }
 
+/**
+ * Access the loading state of a chosen scope. Scope can refer to a step, a provider, or the global loading state. The global loading state is `true` when any of the other scopes are loading.
+ *
+ * @param scope - Specify which loading state to access. Can be a step, a provider, or the global loading state.
+ * @param {Function} children - A render prop function that receives `isLoading` as an argument. `isLoading` is a boolean that indicates if the current scope is loading or not.
+ *
+ * @example
+ * <Loading scope="global">
+ *  {({ isLoading }) => isLoading && <Spinner />}
+ * </Loading>
+ *
+ * @example
+ * <Step name="start">
+ *  <Submit>
+ *    <Loading scope="step:start">
+ *      {({ isLoading }) => isLoading ? "Loading..." : "Submit"}
+ *    </Loading>
+ *  </Submit>
+ * </Step>
+ */
 export function Loading({ children, scope }: LoadingProps) {
   const routerRef = SignUpRouterCtx.useActorRef();
   const [isLoading, { step, strategy }] = useLoading(routerRef);
