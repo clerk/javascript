124669eeba98a7e596cb5272481d02c0bd60969f
Tom Milewski <me@tm.codes>
2024-03-20 12:07:56 -0400

    fix(elements): Routing for Next 14.1

--- packages/elements/src/react/router/next.ts
+++ packages/elements/src/react/router/next.ts
@@ -1,5 +1,7 @@
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
+import { NEXT_ROUTING_CHANGE_VERSION } from '~/internals/constants';
+
 import type { ClerkHostRouter } from './router';
 
 /**
@@ -11,9 +13,11 @@ export const useNextRouter = (): ClerkHostRouter => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
+  const shallow = typeof window === 'undefined' || !window.next || window.next.version < NEXT_ROUTING_CHANGE_VERSION;
+
   return {
-    push: (path: string) => router.push(path),
-    replace: (path: string) => router.replace(path),
+    push: (path: string) => (shallow ? router.push(path) : window.history.pushState(null, '', path)),
+    replace: (path: string) => (shallow ? router.replace(path) : window.history.replaceState(null, '', path)),
     pathname: () => pathname,
     searchParams: () => searchParams,
   };
