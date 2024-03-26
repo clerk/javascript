02bf18ad12f47b6817a83e07b819cbd77d2a75a7
Vaggelis Yfantis <me@octoper.me>
2024-03-26 18:50:06 +0200

    test(e2e): Added more test for sign up flow

--- integration/tests/sign-up-flow.test.ts
+++ integration/tests/sign-up-flow.test.ts
@@ -27,7 +27,7 @@ testAgainstRunningApps({ withEnv: [appConfigs.envs.withEmailCodes] })('sign up f
     await fakeUser.deleteIfExists();
   });
 
-  test('cant sign up with weak password', async ({ page, context }) => {
+  test("can't sign up with weak password", async ({ page, context }) => {
     const u = createTestUtils({ app, page, context });
     const fakeUser = u.services.users.createFakeUser({
       fictionalEmail: true,
@@ -44,6 +44,27 @@ testAgainstRunningApps({ withEnv: [appConfigs.envs.withEmailCodes] })('sign up f
 
     await u.po.expect.toBeSignedOut();
 
+    await fakeUser.deleteIfExists();
+  });
+
+  test('can sign up with phone number', async ({ page, context }) => {
+    const u = createTestUtils({ app, page, context });
+    const fakeUser = u.services.users.createFakeUser({
+      fictionalEmail: true,
+      withPhoneNumber: true,
+      withUsername: true,
+    });
+    await u.po.signUp.goTo();
+    await u.po.signUp.signUp({
+      email: fakeUser.email,
+      phoneNumber: fakeUser.phoneNumber,
+      password: fakeUser.password,
+    });
+    // Verify email
+    await u.po.signUp.enterTestOtpCode();
+    // Verify phone number
+    await u.po.signUp.enterTestOtpCode();
+    await u.po.expect.toBeSignedIn();
     await fakeUser.deleteIfExists();
   });
 });
