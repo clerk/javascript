af293b14b6ef337742d2b23f4957604eae240332
nikospapcom <admin@nikospap.com>
2024-03-29 15:55:48 +0200

    chore(clerk-js): Init billing resource and user billing page

--- packages/clerk-js/src/ui/constants.ts
+++ packages/clerk-js/src/ui/constants.ts
@@ -1,9 +1,11 @@
 export const USER_PROFILE_NAVBAR_ROUTE_ID = {
   ACCOUNT: 'account',
   SECURITY: 'security',
+  BILLING: 'billing',
 };
 
 export const ORGANIZATION_PROFILE_NAVBAR_ROUTE_ID = {
   GENERAL: 'general',
   MEMBERS: 'members',
+  BILLING: 'billing',
 };
