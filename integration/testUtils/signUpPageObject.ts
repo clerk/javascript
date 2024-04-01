5f7fbfbe06c78c0c9944253d621dad80a32693c4
Vaggelis Yfantis <me@octoper.me>
2024-04-01 14:52:55 +0300

    test(e2e): Add sign up flow tests (#3056)
    * test(e2e): Add sign up flow tests
    
    * test(e2e): Added more test for sign up flow
    
    * test(e2e): Added more test for sign up flow
    
    * feat(e2e): Updated sign up page object
    
    * feat(e2e): Updated sign up tests
    
    * test(e2e): Added a test to sign up flow
    
    * chore(repo): Add Changeset
    
    * chore(e2e): Skip a test in sign up flow
    
    * chore(e2e): Update Sign up flow test
    
    * chore(e2e): Check if error for password is visible

--- integration/testUtils/signUpPageObject.ts
+++ integration/testUtils/signUpPageObject.ts
@@ -1,6 +1,15 @@
 import { common } from './commonPageObject';
 import type { TestArgs } from './signInPageObject';
 
+type SignUpFormInputs = {
+  email?: string;
+  password?: string;
+  firstName?: string;
+  lastName?: string;
+  username?: string;
+  phoneNumber?: string;
+};
+
 export const createSignUpComponentPageObject = (testArgs: TestArgs) => {
   const { page } = testArgs;
 
@@ -16,10 +25,34 @@ export const createSignUpComponentPageObject = (testArgs: TestArgs) => {
     signUpWithOauth: (provider: string) => {
       return page.getByRole('button', { name: new RegExp(`continue with ${provider}`, 'gi') });
     },
-    signUpWithEmailAndPassword: async (opts: { email: string; password: string }) => {
-      await self.getEmailAddressInput().fill(opts.email);
-      await self.setPassword(opts.password);
+    signUp: async (opts: SignUpFormInputs) => {
+      if (opts.firstName) {
+        await self.getFirstNameInput().fill(opts.lastName);
+      }
+
+      if (opts.lastName) {
+        await self.getLastNameInput().fill(opts.firstName);
+      }
+
+      if (opts.email) {
+        await self.getEmailAddressInput().fill(opts.email);
+      }
+
+      if (opts.username) {
+        await self.getUsernameInput().fill(opts.username);
+      }
+
+      if (opts.phoneNumber) {
+        await self.getPhoneNumberInput().fill(opts.phoneNumber);
+      }
+
+      if (opts.password) {
+        await self.getPasswordInput().fill(opts.password);
+      }
       await self.continue();
+    },
+    signUpWithEmailAndPassword: async (opts: Pick<SignUpFormInputs, 'email' | 'password'>) => {
+      await self.signUp({ email: opts.email, password: opts.password });
     },
     waitForEmailVerificationScreen: async () => {
       await page.waitForURL(/verify/);
