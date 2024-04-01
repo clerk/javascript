4430d04d81ff91f6c54818fd433e7afd67c7cdde
nikospapcom <admin@nikospap.com>
2024-04-01 12:41:34 +0300

    chore(clerk-js): Add createBillingPortalSession endpoint for users

--- packages/types/src/user.ts
+++ packages/types/src/user.ts
@@ -179,3 +179,7 @@ export type GetUserOrganizationMembershipParams = ClerkPaginationParams;
 export type GetOrganizationMemberships = (
   params?: GetUserOrganizationMembershipParams,
 ) => Promise<ClerkPaginatedResponse<OrganizationMembershipResource>>;
+
+export type CreateBillingPortalSessionParams = {
+  redirectUrl?: string;
+};
