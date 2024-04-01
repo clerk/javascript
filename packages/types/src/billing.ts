35ffb09f71a5de03fa8cf906c44ab84a57a4d63c
nikospapcom <admin@nikospap.com>
2024-04-01 12:51:41 +0300

    chore(clerk-js): Remove BillingPortalSessionResource

--- packages/types/src/billing.ts
+++ packages/types/src/billing.ts
@@ -13,7 +13,3 @@ export interface BillingPlanResource extends ClerkResource {
   createdAt: Date;
   updatedAt: Date;
 }
-
-export interface BillingPortalSessionResource extends ClerkResource {
-  redirectUrl: string;
-}
