6d3b422c8ac2fb2df66d20f4a5ca55c5e20a5f5d
panteliselef <panteliselef@outlook.com>
2024-04-03 11:48:29 +0300

    fix(clerk-js): Avoid depending on `count` as it can be zero but invitations may still exist (#3092)

--- packages/clerk-js/src/ui/components/OrganizationSwitcher/UserInvitationSuggestionList.tsx
+++ packages/clerk-js/src/ui/components/OrganizationSwitcher/UserInvitationSuggestionList.tsx
@@ -219,11 +219,11 @@ export const UserInvitationSuggestionList = (props: UserInvitationSuggestionList
   const { ref, userSuggestions, userInvitations } = useFetchInvitations();
   const isLoading = userInvitations.isLoading || userSuggestions.isLoading;
   const hasNextPage = userInvitations.hasNextPage || userSuggestions.hasNextPage;
-  const hasAnyData = !!(userInvitations.count || userSuggestions.count);
 
   // Solve weird bug with swr while running unit tests
-  const userInvitationsData = userInvitations.data?.filter(a => !!a);
-  const userSuggestionsData = userSuggestions.data?.filter(a => !!a);
+  const userInvitationsData = userInvitations.data?.filter(a => !!a) || [];
+  const userSuggestionsData = userSuggestions.data?.filter(a => !!a) || [];
+  const hasAnyData = userInvitationsData.length > 0 || userSuggestionsData.length > 0;
 
   if (!hasAnyData && !isLoading) {
     return null;
