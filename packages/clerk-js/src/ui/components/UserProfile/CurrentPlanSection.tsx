ebd0b81c4d3accc284803a112faee11016c1f255
nikospapcom <admin@nikospap.com>
2024-04-03 18:20:48 +0300

    chore(clerk-js): Display currentPlan data

--- packages/clerk-js/src/ui/components/UserProfile/CurrentPlanSection.tsx
+++ packages/clerk-js/src/ui/components/UserProfile/CurrentPlanSection.tsx
@@ -1,8 +1,19 @@
-import { localizationKeys, Text } from '../../customizables';
-import { ProfileSection } from '../../elements';
+import { useUser } from '@clerk/shared/react';
+
+import { Box, localizationKeys, Text } from '../../customizables';
+import { FullHeightLoader, ProfileSection } from '../../elements';
+import { useFetch } from '../../hooks';
 import { mqu } from '../../styledSystem';
+import { centsToUnit } from '../../utils';
 
 export const CurrentPlanSection = () => {
+  const { user } = useUser();
+  const { data: currentPlan, isLoading } = useFetch(user?.getCurrentPlan, 'user-current-plan');
+
+  if (!user) {
+    return null;
+  }
+
   return (
     <ProfileSection.Root
       title={localizationKeys('userProfile.start.currentPlanSection.title')}
@@ -10,32 +21,40 @@ export const CurrentPlanSection = () => {
       centered={false}
       sx={{ [mqu.md]: { alignItems: 'flex-start' } }}
     >
-      <ProfileSection.Item id='currentPlan'>
-        <div>
-          <Text
-            variant='subtitle'
-            sx={t => ({ marginBottom: t.space.$2 })}
-          >
-            Business
-          </Text>
-          <Text sx={t => ({ fontWeight: t.fontWeights.$semibold, fontSize: t.fontSizes.$lg })}>
-            $200.00{' '}
+      {isLoading || !currentPlan ? (
+        <FullHeightLoader />
+      ) : (
+        <ProfileSection.Item id='currentPlan'>
+          <Box>
             <Text
-              as='span'
-              sx={t => ({ color: t.colors.$colorTextSecondary, fontSize: t.fontSizes.$md })}
+              variant='subtitle'
+              sx={t => ({ marginBottom: t.space.$2 })}
             >
-              per month
+              {currentPlan.name}
             </Text>
-          </Text>
-          <Text sx={t => ({ color: t.colors.$colorTextSecondary, fontSize: t.fontSizes.$sm })}>
-            Renews on 28 April 2024
-          </Text>
-        </div>
-        <ProfileSection.Button
-          id='currentPlan'
-          localizationKey={localizationKeys('userProfile.start.currentPlanSection.primaryButton')}
-        />
-      </ProfileSection.Item>
+            <Text sx={t => ({ fontWeight: t.fontWeights.$semibold, fontSize: t.fontSizes.$lg })}>
+              {centsToUnit(currentPlan.priceInCents)}{' '}
+              <Text
+                as='span'
+                colorScheme='secondary'
+                sx={t => ({ fontSize: t.fontSizes.$md })}
+              >
+                per month
+              </Text>
+            </Text>
+            <Text
+              colorScheme='secondary'
+              sx={t => ({ fontSize: t.fontSizes.$sm })}
+            >
+              Renews on 28 April 2024
+            </Text>
+          </Box>
+          <ProfileSection.Button
+            id='currentPlan'
+            localizationKey={localizationKeys('userProfile.start.currentPlanSection.primaryButton')}
+          />
+        </ProfileSection.Item>
+      )}
     </ProfileSection.Root>
   );
 };
