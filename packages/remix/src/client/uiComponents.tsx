d87a2f074e8b09ce7cf055e8cad265e9dad77166
Vaggelis Yfantis <me@octoper.me>
2024-04-05 00:13:03 +0300

    fix(clerk-js): Add usePathnameWithoutSplatRouteParams to handle splat routes

--- packages/remix/src/client/uiComponents.tsx
+++ packages/remix/src/client/uiComponents.tsx
@@ -13,8 +13,9 @@ import type {
   SignUpProps,
   UserProfileProps,
 } from '@clerk/types';
-import { useLocation } from '@remix-run/react';
 import React from 'react';
+
+import { usePathnameWithoutSplatRouteParams } from './usePathnameWithoutSplatRouteParams';
 
 // The assignment of UserProfile with BaseUserProfile props is used
 // to support the CustomPage functionality (eg UserProfile.Page)
@@ -22,14 +23,14 @@ import React from 'react';
 // "The inferred type of 'UserProfile' cannot be named without a reference to ..."
 export const UserProfile: typeof BaseUserProfile = Object.assign(
   (props: UserProfileProps) => {
-    const { pathname: path } = useLocation();
+    const path = usePathnameWithoutSplatRouteParams();
     return <BaseUserProfile {...useRoutingProps('UserProfile', props, { path })} />;
   },
   { ...BaseUserProfile },
 );
 
 export const CreateOrganization = (props: CreateOrganizationProps) => {
-  const { pathname: path } = useLocation();
+  const path = usePathnameWithoutSplatRouteParams();
   return <BaseCreateOrganization {...useRoutingProps('CreateOrganization', props, { path })} />;
 };
 
@@ -39,18 +40,18 @@ export const CreateOrganization = (props: CreateOrganizationProps) => {
 // "The inferred type of 'OrganizationProfile' cannot be named without a reference to ..."
 export const OrganizationProfile: typeof BaseOrganizationProfile = Object.assign(
   (props: OrganizationProfileProps) => {
-    const { pathname: path } = useLocation();
+    const path = usePathnameWithoutSplatRouteParams();
     return <BaseOrganizationProfile {...useRoutingProps('OrganizationProfile', props, { path })} />;
   },
   { ...BaseOrganizationProfile },
 );
 
 export const SignIn = (props: SignInProps) => {
-  const { pathname: path } = useLocation();
+  const path = usePathnameWithoutSplatRouteParams();
   return <BaseSignIn {...useRoutingProps('SignIn', props, { path })} />;
 };
 
 export const SignUp = (props: SignUpProps) => {
-  const { pathname: path } = useLocation();
+  const path = usePathnameWithoutSplatRouteParams();
   return <BaseSignUp {...useRoutingProps('SignUp', props, { path })} />;
 };
