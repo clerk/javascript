575280dea13c37fc520f831335f17138142e7fcf
Alexandros Ntitoras <47026269+AlexNti@users.noreply.github.com>
2024-04-05 12:40:00 +0300

    Feat/connect manage plans with plan and billing (#3124)
    * feat(clerk-js,localizations,types): Create CurrentPlanSection
    
    * feat(clerk-js,localizations): Create PaymentMethodSection
    
    * fix(types): Add type definitions for currentPlanSection & paymentMethodSection
    
    * chore(clerk-js): Display currentPlan data
    
    * chore(clerk-js): Align Manage Billing Info in top right
    
    * chore(clerk-js): Add billingCycle types to currentPlan
    
    * chore(clerk-js,types): Introduce payment method section
    
    * chore(clerk-js,types): Fix payment method type
    
    * chore(clerk-js,types,localizations): Current plan localization fixes
    
    * refactor(clerk-js): Move CurrentPlanSection and PaymentMethodSection to common
    
    * chore(clerk-js,types,localizations): Localize more strings and add CardAlert component
    
    * chore(clerk-js): Billing page improvements
    
    * chore(clerk-js): Add billing provider for Billing
    
    * chore(clerk-js): Remove unused code from common billing
    
    * chore(clerk-js): Make planAndBilling default page
    
    * chore(clerk-js): Add actions for navigation buttons
    
    * chore(clerk-js): Move all related billing files inside an isolated folder in common
    
    * chore(clerk-js): Add manage plan button
    
    * chore(clerk-js): Change type of current plan to CurrentBillingPlanResource
    
    ---------
    
    Co-authored-by: nikospapcom <admin@nikospap.com>

--- packages/clerk-js/src/ui/components/UserProfile/BillingPage.tsx
+++ packages/clerk-js/src/ui/components/UserProfile/BillingPage.tsx
@@ -1,63 +1,33 @@
 import { useUser } from '@clerk/shared/react';
-import React from 'react';
 
-import { CurrentPlanSection, PaymentMethodSection } from '../../common';
-import { Col, descriptors, localizationKeys } from '../../customizables';
-import { Card, FullHeightLoader, Header, useCardState, withCardStateProvider } from '../../elements';
+import { Billing } from '../../common';
+import { FullHeightLoader, withCardStateProvider } from '../../elements';
 import { useFetch } from '../../hooks';
-import { handleError } from '../../utils';
 
 export const BillingPage = withCardStateProvider(() => {
-  const [isLoadingPortalSession, setIsLoadingPortalSession] = React.useState(false);
   const { user } = useUser();
-  const card = useCardState();
+  const { data: currentPlan, isLoading: isLoadingCurrentPlan } = useFetch(user?.getCurrentPlan, 'user-current-plan');
+  const { data: availablePlans, isLoading: isLoadingAvailablePlans } = useFetch(
+    user?.getAvailablePlans,
+    'user-available-plans',
+  );
 
-  const { data: currentPlan, isLoading } = useFetch(user?.getCurrentPlan, 'user-current-plan');
+  if (isLoadingCurrentPlan || isLoadingAvailablePlans || !user) {
+    return <FullHeightLoader />;
+  }
 
-  const handleStartPortalSession = async () => {
-    setIsLoadingPortalSession(true);
-    try {
-      const res = await user?.createPortalSession({ returnUrl: window.location.href });
-      window.location.href = res?.redirectUrl || '';
-    } catch (e) {
-      handleError(e, [], card.setError);
-    } finally {
-      setIsLoadingPortalSession(false);
-    }
-  };
+  if (!currentPlan) {
+    return null;
+  }
 
   return (
-    <Col
-      elementDescriptor={descriptors.page}
-      sx={t => ({ gap: t.space.$8, color: t.colors.$colorText })}
+    <Billing.Root
+      createPortalSession={user?.createPortalSession}
+      changePlan={user?.changePlan}
+      currentPlan={currentPlan}
+      availablePlans={availablePlans?.data || []}
     >
-      <Col
-        elementDescriptor={descriptors.profilePage}
-        elementId={descriptors.profilePage.setId('billing')}
-      >
-        <Header.Root>
-          <Header.Title
-            localizationKey={localizationKeys('billing.start.headerTitle__billing')}
-            sx={t => ({ marginBottom: t.space.$4 })}
-            textVariant='h2'
-          />
-        </Header.Root>
-
-        <Card.Alert>{card.error}</Card.Alert>
-
-        {isLoading ? (
-          <FullHeightLoader />
-        ) : (
-          <>
-            <CurrentPlanSection currentPlan={currentPlan} />
-            <PaymentMethodSection
-              currentPlan={currentPlan}
-              onClickManageBilling={handleStartPortalSession}
-              isLoadingPortalSession={isLoadingPortalSession}
-            />
-          </>
-        )}
-      </Col>
-    </Col>
+      <Billing />
+    </Billing.Root>
   );
 });
