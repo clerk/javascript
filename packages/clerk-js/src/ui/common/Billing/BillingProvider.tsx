c2a1224fa6238fe0be485fbba756a1ab6d36f8a0
Alexandros Ntitoras <47026269+AlexNti@users.noreply.github.com>
2024-04-05 12:40:00 +0300

    Feat/connect manage plans with plan and billing (#3124)
    * feat(clerk-js,localizations,types): Create CurrentPlanSection
    
    * feat(clerk-js,localizations): Create PaymentMethodSection
    
    * fix(types): Add type definitions for currentPlanSection & paymentMethodSection
    
    * chore(clerk-js): Display currentPlan data
    
    * chore(clerk-js): Align Manage Billing Info in top right
    
    * chore(clerk-js): Add billingCycle types to currentPlan
    
    * chore(clerk-js,types): Introduce payment method section
    
    * chore(clerk-js,types): Fix payment method type
    
    * chore(clerk-js,types,localizations): Current plan localization fixes
    
    * refactor(clerk-js): Move CurrentPlanSection and PaymentMethodSection to common
    
    * chore(clerk-js,types,localizations): Localize more strings and add CardAlert component
    
    * chore(clerk-js): Billing page improvements
    
    * chore(clerk-js): Add billing provider for Billing
    
    * chore(clerk-js): Remove unused code from common billing
    
    * chore(clerk-js): Make planAndBilling default page
    
    * chore(clerk-js): Add actions for navigation buttons
    
    * chore(clerk-js): Move all related billing files inside an isolated folder in common
    
    * chore(clerk-js): Add manage plan button
    
    * chore(clerk-js): Change type of current plan to CurrentBillingPlanResource
    
    ---------
    
    Co-authored-by: nikospapcom <admin@nikospap.com>

---
+++ packages/clerk-js/src/ui/common/Billing/BillingProvider.tsx
@@ -0,0 +1,47 @@
+import { createContextAndHook } from '@clerk/shared/react';
+import type {
+  BillingPlanResource,
+  ChangePlanParams,
+  CheckoutSessionResource,
+  CreatePortalSessionParams,
+  CurrentBillingPlanResource,
+  PortalSessionResource,
+} from '@clerk/types';
+import React from 'react';
+
+type BillingProviderValue = {
+  availablePlans: BillingPlanResource[];
+  currentPlan: CurrentBillingPlanResource;
+  createPortalSession: (params: CreatePortalSessionParams) => Promise<PortalSessionResource>;
+  changePlan: (params: ChangePlanParams) => Promise<CheckoutSessionResource>;
+  currentPage: BillingPages;
+  goToPlanAndBilling: () => void;
+  goToManageBillingPlan: () => void;
+};
+
+type BillingProviderProps = Omit<
+  BillingProviderValue,
+  'goToPlanAndBilling' | 'goToManageBillingPlan' | 'currentPage'
+> & { children: React.ReactNode };
+
+export const [BillingContext, useBillingContext, _] = createContextAndHook<BillingProviderValue>('BillingContext');
+
+type BillingPages = 'planAndBilling' | 'manageBillingPlan';
+
+export const BillingProvider = (params: BillingProviderProps) => {
+  const [currentPage, setCurrentPage] = React.useState<BillingPages>('planAndBilling');
+
+  const goToPlanAndBilling = () => {
+    setCurrentPage('planAndBilling');
+  };
+
+  const goToManageBillingPlan = () => {
+    setCurrentPage('manageBillingPlan');
+  };
+
+  return (
+    <BillingContext.Provider value={{ value: { ...params, currentPage, goToPlanAndBilling, goToManageBillingPlan } }}>
+      {params.children}
+    </BillingContext.Provider>
+  );
+};
---
+++ packages/clerk-js/src/ui/common/Billing/PlanAndBillingScreen.tsx
@@ -0,0 +1,152 @@
+import React from 'react';
+
+import { Box, Col, descriptors, localizationKeys, Text, useLocalizations } from '../../customizables';
+import { Card, Header, ProfileSection, useCardState } from '../../elements';
+import { mqu } from '../../styledSystem';
+import { centsToUnit, getRelativeToNowDateKey, handleError } from '../../utils';
+import { useBillingContext } from './BillingProvider';
+
+const ManagePaymentMethodButton = () => {
+  const { createPortalSession } = useBillingContext();
+  const [isLoadingPortalSession, setIsLoadingPortalSession] = React.useState(false);
+  const card = useCardState();
+
+  const handleStartPortalSession = async () => {
+    setIsLoadingPortalSession(true);
+    try {
+      const res = await createPortalSession({ returnUrl: window.location.href });
+      window.location.href = res?.redirectUrl || '';
+    } catch (e) {
+      handleError(e, [], card.setError);
+    } finally {
+      setIsLoadingPortalSession(false);
+    }
+  };
+
+  return (
+    <ProfileSection.Button
+      id='paymentMethod'
+      localizationKey={localizationKeys('billing.paymentMethodSection.primaryButton__manageBillingInfo')}
+      onClick={handleStartPortalSession}
+      isLoading={isLoadingPortalSession}
+    />
+  );
+};
+
+export const PaymentMethodSection = () => {
+  const { t } = useLocalizations();
+  const { currentPlan } = useBillingContext();
+
+  if (!currentPlan) {
+    return null;
+  }
+
+  return (
+    <ProfileSection.Root
+      title={localizationKeys('billing.paymentMethodSection.title')}
+      id='paymentMethod'
+      centered={false}
+      sx={{ [mqu.md]: { alignItems: 'flex-start' } }}
+    >
+      <ProfileSection.Item id='paymentMethod'>
+        {currentPlan.paymentMethod ? (
+          <>
+            <Box>
+              <Text>•••• {currentPlan.paymentMethod.card.last4}</Text>
+              <Text sx={t => ({ color: t.colors.$colorTextSecondary, fontSize: t.fontSizes.$sm })}>
+                {t(localizationKeys('billing.paymentMethodSection.expires'))} {currentPlan.paymentMethod.card.expMonth}/
+                {currentPlan.paymentMethod.card.expYear}
+              </Text>
+            </Box>
+            <ManagePaymentMethodButton />
+          </>
+        ) : (
+          <ProfileSection.ArrowButton
+            id='paymentMethod'
+            localizationKey={localizationKeys('billing.paymentMethodSection.primaryButton')}
+          />
+        )}
+      </ProfileSection.Item>
+    </ProfileSection.Root>
+  );
+};
+
+const CurrentPlanSection = () => {
+  const { t, locale } = useLocalizations();
+  const { goToManageBillingPlan, currentPlan } = useBillingContext();
+
+  if (!currentPlan) {
+    return null;
+  }
+
+  return (
+    <ProfileSection.Root
+      title={localizationKeys('billing.currentPlanSection.title')}
+      id='currentPlan'
+      centered={false}
+      sx={{ [mqu.md]: { alignItems: 'flex-start' } }}
+    >
+      <ProfileSection.Item id='currentPlan'>
+        <Box>
+          <Text
+            variant='subtitle'
+            sx={t => ({ marginBottom: t.space.$2 })}
+          >
+            {currentPlan.name}
+          </Text>
+          <Text sx={t => ({ fontWeight: t.fontWeights.$semibold, fontSize: t.fontSizes.$lg })}>
+            {centsToUnit({ cents: currentPlan.priceInCents, locale })}{' '}
+            <Text
+              as='span'
+              colorScheme='secondary'
+              sx={t => ({ fontSize: t.fontSizes.$md })}
+            >
+              {t(localizationKeys('billing.currentPlanSection.perMonth'))}
+            </Text>
+          </Text>
+          <Text
+            colorScheme='secondary'
+            sx={t => ({ fontSize: t.fontSizes.$sm })}
+          >
+            {t(localizationKeys('billing.currentPlanSection.renewsOn'))}{' '}
+            {t(getRelativeToNowDateKey(currentPlan.billingCycle?.endDate))}
+          </Text>
+        </Box>
+        <ProfileSection.Button
+          onClick={goToManageBillingPlan}
+          id='currentPlan'
+          localizationKey={localizationKeys('billing.currentPlanSection.primaryButton')}
+        />
+      </ProfileSection.Item>
+    </ProfileSection.Root>
+  );
+};
+
+export const PlanAndBillingScreen = () => {
+  const card = useCardState();
+
+  return (
+    <Col
+      elementDescriptor={descriptors.page}
+      sx={t => ({ gap: t.space.$8, color: t.colors.$colorText })}
+    >
+      <Col
+        elementDescriptor={descriptors.profilePage}
+        elementId={descriptors.profilePage.setId('billing')}
+      >
+        <Header.Root>
+          <Header.Title
+            localizationKey={localizationKeys('billing.start.headerTitle__billing')}
+            sx={t => ({ marginBottom: t.space.$4 })}
+            textVariant='h2'
+          />
+        </Header.Root>
+
+        <Card.Alert>{card.error}</Card.Alert>
+
+        <CurrentPlanSection />
+        <PaymentMethodSection />
+      </Col>
+    </Col>
+  );
+};
