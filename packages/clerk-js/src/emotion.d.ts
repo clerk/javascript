249b8d69830d1bd006c5647d8dbf33744e303c60
Nikos Douvlis <nikosdouvlis@gmail.com>
2024-04-08 15:59:56 +0300

    fix(clerk-js): Remove ~ui path alias
    We no longer have a use case for it and with the current bundling configuration it is causing issues when clerk-js is imported as a ESM or CJS package.

--- packages/clerk-js/src/emotion.d.ts
+++ packages/clerk-js/src/emotion.d.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-restricted-imports
 import '@emotion/react';
 
-import type { InternalTheme } from '~ui/foundations';
+import type { InternalTheme } from './ui/foundations';
 
 declare module '@emotion/react' {
   export interface Theme extends InternalTheme {}
