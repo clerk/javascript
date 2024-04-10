ef491eefa328185725d77ef84b3f968f5519fa33
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-04-10 15:17:36 -0400

    chore(elements): Add docs link

--- packages/elements/src/internals/machines/sign-in/machines/verification.machine.ts
+++ packages/elements/src/internals/machines/sign-in/machines/verification.machine.ts
@@ -73,7 +73,7 @@ const SignInVerificationMachine = setup({
             .map(s => s)
             .join(
               ', ',
-            )}. Before deploying your app, make sure to render a <Strategy> component for each supported strategy. For more information, visit the documentation: <link>`,
+            )}. Before deploying your app, make sure to render a <Strategy> component for each supported strategy. For more information, visit the documentation: https://clerk.com/docs/elements/reference/sign-in#strategy`,
         );
       }
 
@@ -82,7 +82,7 @@ const SignInVerificationMachine = setup({
         !context.registeredStrategies.has(context.currentFactor?.strategy as unknown as SignInFactor)
       ) {
         throw new ClerkElementsRuntimeError(
-          `Your sign-in attempt is missing a ${context.currentFactor?.strategy} strategy. Make sure <Strategy name="${context.currentFactor?.strategy}"> is rendered in your flow. For more information, visit the documentation: <link>`,
+          `Your sign-in attempt is missing a ${context.currentFactor?.strategy} strategy. Make sure <Strategy name="${context.currentFactor?.strategy}"> is rendered in your flow. For more information, visit the documentation: https://clerk.com/docs/elements/reference/sign-in#strategy`,
         );
       }
     },
