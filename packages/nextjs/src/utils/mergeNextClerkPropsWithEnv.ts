7759e852005bdc25c09e4bd57886422129b94651
Nikos Douvlis <nikosdouvlis@gmail.com>
2024-04-19 15:08:19 +0300

    fix(nextjs): Correctly read legacy NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL and NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL env variables

--- packages/nextjs/src/utils/mergeNextClerkPropsWithEnv.ts
+++ packages/nextjs/src/utils/mergeNextClerkPropsWithEnv.ts
@@ -20,15 +20,11 @@ export const mergeNextClerkPropsWithEnv = (props: Omit<NextClerkProviderProps, '
     signUpForceRedirectUrl:
       props.signUpForceRedirectUrl || process.env.NEXT_PUBLIC_CLERK_SIGN_UP_FORCE_REDIRECT_URL || '',
     signInFallbackRedirectUrl:
-      props.signInFallbackRedirectUrl ||
-      process.env.NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL ||
-      process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL ||
-      '',
+      props.signInFallbackRedirectUrl || process.env.NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL || '',
     signUpFallbackRedirectUrl:
-      props.signUpFallbackRedirectUrl ||
-      process.env.NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL ||
-      process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL ||
-      '',
+      props.signUpFallbackRedirectUrl || process.env.NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL || '',
+    afterSignInUrl: props.afterSignInUrl || process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL || '',
+    afterSignUpUrl: props.afterSignUpUrl || process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL || '',
     telemetry: props.telemetry ?? {
       disabled: isTruthy(process.env.NEXT_PUBLIC_CLERK_TELEMETRY_DISABLED),
       debug: isTruthy(process.env.NEXT_PUBLIC_CLERK_TELEMETRY_DEBUG),
