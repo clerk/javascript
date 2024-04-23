bf38a93b5c0678a854e5ec08f1bfb5eaf2b11d0c
LekoArts <lekoarts@gmail.com>
2024-04-23 13:04:32 +0200

    fix(elements): Remove some public exports

--- packages/elements/src/react/sign-in/index.ts
+++ packages/elements/src/react/sign-in/index.ts
@@ -1,15 +1,10 @@
 // Mark as a client-only package. This will error if you try to import it in a React Server Component.
 import 'client-only';
 
-import { SignInRouterCtx } from './context';
-
 export { SignInRoot as SignIn, SignInRoot as Root } from './root';
 export { SignInStep as Step } from './step';
 export { SignInAction as Action } from './action';
-export {
-  SignInChooseStrategy as ChooseStrategy,
-  SignInSupportedStrategy as SupportedStrategy,
-} from './choose-strategy';
+export { SignInSupportedStrategy as SupportedStrategy } from './choose-strategy';
 
 export {
   SignInFirstFactor as FirstFactor,
@@ -18,9 +13,3 @@ export {
 } from './verifications';
 
 export { SignInSafeIdentifier as SafeIdentifier, SignInSalutation as Salutation } from './identifiers';
-
-/** @internal Internal use only */
-export const useSignInActorRef_internal = SignInRouterCtx.useActorRef;
-
-/** @internal Internal use only */
-export const useSignInSelector_internal = SignInRouterCtx.useSelector;
