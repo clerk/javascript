5b408cf815f091ab4fe13a1672f7c380ba7d58a2
Emmanouela <emmanouelapothitou@gmail.com>
2024-04-24 16:28:30 +0300

    fix(clerk-js): Check if legacy property has value before display warning

--- packages/clerk-js/src/utils/assertNoLegacyProp.ts
+++ packages/clerk-js/src/utils/assertNoLegacyProp.ts
@@ -2,7 +2,7 @@ export function assertNoLegacyProp(props: Record<string, any>) {
   const legacyProps = ['redirectUrl', 'afterSignInUrl', 'afterSignUpUrl', 'after_sign_in_url', 'after_sign_up_url'];
   const legacyProp = Object.keys(props).find(key => legacyProps.includes(key));
 
-  if (legacyProp) {
+  if (legacyProp && props[legacyProp]) {
     // TODO: @nikos update with the docs link
     console.warn(
       `Clerk: The prop "${legacyProp}" is deprecated and should be replaced with the new "fallbackRedirectUrl" or "forceRedirectUrl" props instead.`,
