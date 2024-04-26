a4b18809347fd9e2a1c139b23e1b9017ababa64a
Dimitris Klouvas <dimitris@clerk.dev>
2024-04-26 15:21:21 +0300

    chore(backend): Rename assertValidSecretKey file to optionsAssertions
    The optionsAssertions module will include assertion function for options
    used across our package

--- packages/backend/src/util/optionsAssertions.ts
+++ packages/backend/src/util/optionsAssertions.ts
@@ -1,7 +1,13 @@
+import { parsePublishableKey } from '@clerk/shared/keys';
+
 export function assertValidSecretKey(val: unknown): asserts val is string {
   if (!val || typeof val !== 'string') {
     throw Error('Missing Clerk Secret Key. Go to https://dashboard.clerk.com and get your key for your instance.');
   }
 
   //TODO: Check if the key is invalid and throw error
+}
+
+export function assertValidPublishableKey(val: unknown): asserts val is string {
+  parsePublishableKey(val as string | undefined, { fatal: true });
 }
