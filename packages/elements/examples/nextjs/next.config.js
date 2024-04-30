a8629586d032c21dbaa2facefe83436d36bda5de
Bryce Kalow <bryce@clerk.dev>
2024-04-30 16:30:41 -0500

    fix(elements): Only call useSearchParams() on the client

--- packages/elements/examples/nextjs/next.config.js
+++ packages/elements/examples/nextjs/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   // Disable React strict mode when using the state machine inspector
   reactStrictMode: process.env['NEXT_PUBLIC_CLERK_ELEMENTS_DEBUG'] !== 'true',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
