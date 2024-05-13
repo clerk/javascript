7540b0522d90afda98a2be074d350712dcadd8c4
Tom Milewski <me@tm.codes>
2024-05-13 03:03:43 -0400

    fix(chrome-extension): Remove Origin from API mutation request headers (#3363)
    * fix(chrome-extension): Remove `origin` from API mutation request headers [SDK-1736]
    
    * fix(chrome-extension): Remove `Origin` from API mutation request headers [SDK-1736]
    
    * chore(chrome-extension): Loosen regex filter
    
    ---------
    
    Co-authored-by: Lennart <lekoarts@gmail.com>

--- packages/chrome-extension/src/index.ts
+++ packages/chrome-extension/src/index.ts
@@ -1,9 +1,36 @@
+// Override Clerk React error thrower to show that errors come from @clerk/chrome-extension
+import { setErrorThrowerOptions } from '@clerk/clerk-react/internal';
+import type { DeclarativeNetRequest } from 'webextension-polyfill';
+import browser from 'webextension-polyfill';
+
+setErrorThrowerOptions({ packageName: PACKAGE_NAME });
+
+browser.runtime.onInstalled.addListener(() => {
+  const rules: DeclarativeNetRequest.Rule[] = [
+    {
+      id: 654321,
+      action: {
+        type: 'modifyHeaders',
+        requestHeaders: [{ header: 'Origin', operation: 'remove' }],
+      },
+      condition: {
+        initiatorDomains: [browser.runtime.id],
+        isUrlFilterCaseSensitive: true,
+        regexFilter: '.+_is_native=1.*',
+        requestMethods: ['post', 'put', 'delete'],
+        resourceTypes: ['xmlhttprequest'],
+      },
+    },
+  ];
+
+  void browser.declarativeNetRequest.updateDynamicRules({
+    removeRuleIds: rules.map(r => r.id),
+    addRules: rules,
+  });
+});
+
 export * from '@clerk/clerk-react';
 export type { StorageCache } from './utils/storage';
 
 // The order matters since we want override @clerk/clerk-react ClerkProvider
 export { ClerkProvider } from './ClerkProvider';
-
-// Override Clerk React error thrower to show that errors come from @clerk/chrome-extension
-import { setErrorThrowerOptions } from '@clerk/clerk-react/internal';
-setErrorThrowerOptions({ packageName: PACKAGE_NAME });
