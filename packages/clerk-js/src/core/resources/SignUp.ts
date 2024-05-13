3984bd90339e94b0502a11cb5ad2674d88685295
Stefanos Anagnostou <stefanos@clerk.dev>
2024-05-13 17:03:09 +0300

    fix(clerk-js): Remove the generic CAPTCHA console error

--- packages/clerk-js/src/core/resources/SignUp.ts
+++ packages/clerk-js/src/core/resources/SignUp.ts
@@ -82,7 +82,6 @@ export class SignUp extends BaseResource implements SignUpResource {
         paramsWithCaptcha.captchaToken = captchaToken;
         paramsWithCaptcha.captchaWidgetType = captchaWidgetTypeUsed;
       } catch (e) {
-        console.error('Clerk CAPTCHA Error: ', e.captchaError || e.message || e);
         if (e.captchaError) {
           paramsWithCaptcha.captchaError = e.captchaError;
         } else {
