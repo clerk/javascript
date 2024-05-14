235e559d2575b6418ab8ab7ce010e6c713838f61
panteliselef <panteliselef@outlook.com>
2024-05-14 19:53:42 +0300

    feat(clerk-js,clerk-react): Reuse initialized GIS instance

--- packages/clerk-js/src/ui/components/GoogleOneTap/one-tap-start.tsx
+++ packages/clerk-js/src/ui/components/GoogleOneTap/one-tap-start.tsx
@@ -53,23 +53,30 @@ function _OneTapStart(): JSX.Element | null {
   const environmentClientID = environment.displayConfig.googleOneTapClientId;
   const shouldLoadGIS = !user?.id && !!environmentClientID;
 
+  async function initializeGIS() {
+    const google = await loadGIS();
+    google.accounts.id.initialize({
+      client_id: environmentClientID!,
+      callback: oneTapCallback,
+      itp_support: ctx.itpSupport,
+      cancel_on_tap_outside: ctx.cancelOnTapOutside,
+      auto_select: false,
+      use_fedcm_for_prompt: ctx.fedCmSupport,
+    });
+    return google;
+  }
+
   /**
    * Prevent GIS from initializing multiple times
    */
-  useFetch(shouldLoadGIS ? loadGIS : undefined, 'google-identity-services-script', {
-    // Mark as stale, in order for the onSuccess callback to be called.
-    staleTime: 100,
-    onSuccess(google) {
-      google.accounts.id.initialize({
-        client_id: environmentClientID!,
-        callback: oneTapCallback,
-        itp_support: ctx.itpSupport,
-        cancel_on_tap_outside: ctx.cancelOnTapOutside,
-        auto_select: false,
-        use_fedcm_for_prompt: ctx.fedCmSupport,
-      });
+  const { data: initializedGoogle } = useFetch(
+    shouldLoadGIS ? initializeGIS : undefined,
+    'google-identity-services-script',
+  );
 
-      google.accounts.id.prompt(notification => {
+  useEffect(() => {
+    if (initializedGoogle && !user?.id && !isPromptedRef.current) {
+      initializedGoogle.accounts.id.prompt(notification => {
         // Close the modal, when the user clicks outside the prompt or cancels
         if (notification.getMomentType() === 'skipped') {
           // Unmounts the component will cause the useEffect cleanup function from below to be called
@@ -77,18 +84,18 @@ function _OneTapStart(): JSX.Element | null {
         }
       });
       isPromptedRef.current = true;
-    },
-  });
+    }
+  }, [clerk, initializedGoogle, user?.id]);
 
   // Trigger only on mount/unmount. Above we handle the logic for the initial fetch + initialization
   useEffect(() => {
     return () => {
-      if (window.google && isPromptedRef.current) {
+      if (initializedGoogle && isPromptedRef.current) {
         isPromptedRef.current = false;
-        window.google.accounts.id.cancel();
+        initializedGoogle.accounts.id.cancel();
       }
     };
-  }, []);
+  }, [initializedGoogle]);
 
   return null;
 }
