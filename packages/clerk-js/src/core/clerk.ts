28d01fd98e2ae199c42a60afe24ee864d4585435
Vaggelis Yfantis <me@octoper.me>
2024-05-15 14:01:52 +0300

    fix(clerk-js): Use TokenUpdate event to remove the session token

--- packages/clerk-js/src/core/clerk.ts
+++ packages/clerk-js/src/core/clerk.ts
@@ -728,7 +728,7 @@ export class Clerk implements ClerkInterface {
     const shouldSignOutSession = this.session && newSession === null;
     if (shouldSignOutSession) {
       this.#broadcastSignOutEvent();
-      removeSessionCookie();
+      eventBus.dispatch(events.TokenUpdate, { token: null });
     }
 
     await onBeforeSetActive();
