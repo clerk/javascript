1d48655603c32a864b124f73601dd3e32872c129
Dimitris Klouvas <dimitris@clerk.dev>
2024-05-17 13:50:37 +0300

    fix(clerk-js): Support get/set/remove into both suffixed/un-suffixed cookies
    This change is required to support setting both suffixed/un-suffixed
    cookies using part of the publishableKey to support Multiple apps running
    on the same eTLD+1 domain or localhost.
    Setting both suffixed/un-suffixed cookies is used to support backwards compatibility.

--- packages/clerk-js/src/core/auth/cookies/session.ts
+++ packages/clerk-js/src/core/auth/cookies/session.ts
@@ -2,6 +2,7 @@ import { createCookieHandler } from '@clerk/shared/cookie';
 import { addYears } from '@clerk/shared/date';
 
 import { inCrossOriginIframe } from '../../../utils';
+import { getSuffixedCookieName } from '../utils';
 
 const SESSION_COOKIE_NAME = '__session';
 
@@ -15,21 +16,22 @@ export type SessionCookieHandler = {
  * The cookie is used by the Clerk backend SDKs to identify
  * the authenticated user.
  */
-export const createSessionCookie = (): SessionCookieHandler => {
+export const createSessionCookie = (publishableKey: string): SessionCookieHandler => {
   const sessionCookie = createCookieHandler(SESSION_COOKIE_NAME);
+  const suffixedSessionCookie = createCookieHandler(getSuffixedCookieName(SESSION_COOKIE_NAME, publishableKey));
 
-  const remove = () => sessionCookie.remove();
+  const remove = () => {
+    suffixedSessionCookie.remove();
+    sessionCookie.remove();
+  };
 
   const set = (token: string) => {
     const expires = addYears(Date.now(), 1);
     const sameSite = inCrossOriginIframe() ? 'None' : 'Lax';
     const secure = window.location.protocol === 'https:';
 
-    return sessionCookie.set(token, {
-      expires,
-      sameSite,
-      secure,
-    });
+    suffixedSessionCookie.set(token, { expires, sameSite, secure });
+    sessionCookie.set(token, { expires, sameSite, secure });
   };
 
   return {
