80ee1e74872c6e9d3f1eef9fd7cdcd11b29e743a
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-05-24 11:04:34 -0400

    fix TS

--- packages/tailwindcss-transformer/src/index.ts
+++ packages/tailwindcss-transformer/src/index.ts
@@ -1,15 +1,20 @@
+import { createHash } from 'node:crypto';
+
+import postcss, { type Plugin } from 'postcss';
 import recast from 'recast';
 import tsParser from 'recast/parsers/babel-ts.js';
-import postcss from 'postcss';
-import tailwindcss from 'tailwindcss';
-import valueParser from 'postcss-value-parser';
-import { createHash } from 'node:crypto';
 import type { Config } from 'tailwindcss';
+import tailwindcss from 'tailwindcss';
+
+import { replaceVariableScope } from './replace-variable-scope';
 
 /**
  * A map of hashed classnames from Tailwind CSS classes and their original values
  */
 type StyleCache = Map<string, string>;
+type Ctx = { styleCache: StyleCache };
+
+const clRegex = /^cl-[a-z0-9]{8}$/;
 
 function isBinaryExpression(node: recast.types.namedTypes.BinaryExpression) {
   return recast.types.namedTypes.BinaryExpression.check(node);
@@ -34,46 +39,44 @@ function generateHashedClassName(value: string) {
   return 'cl-' + createHash('sha256').update(value, 'utf8').digest('hex').slice(0, 8);
 }
 
-let clRegex = /^cl-[a-z0-9]{8}$/;
+function visitNode(node: recast.types.ASTNode, ctx: Ctx) {
+  recast.visit(node, {
+    visitStringLiteral(path) {
+      if (clRegex.test(path.node.value)) {
+        return false;
+      }
+      if (isBinaryExpression(path.parentPath.node)) {
+        return false;
+      }
+      if (isLogicalExpression(path.parentPath.node) && !isRightmostOperand(path)) {
+        return false;
+      }
+      const cn = generateHashedClassName(path.node.value);
+      ctx.styleCache.set(cn, path.node.value);
+      path.node.value = cn;
+      return false;
+    },
+  });
+}
 
-export async function transform(code: string, ctx: { styleCache: StyleCache }) {
+export function transform(code: string, ctx: Ctx) {
   try {
-    let ast = recast.parse(code, { parser: tsParser });
-
-    function visitNode(node: recast.types.ASTNode) {
-      recast.visit(node, {
-        visitStringLiteral(path) {
-          if (clRegex.test(path.node.value)) {
-            return false;
-          }
-          if (isBinaryExpression(path.parentPath.node)) {
-            return false;
-          }
-          if (isLogicalExpression(path.parentPath.node) && !isRightmostOperand(path)) {
-            return false;
-          }
-          let cn = generateHashedClassName(path.node.value);
-          ctx.styleCache.set(cn, path.node.value);
-          path.node.value = cn;
-          return false;
-        },
-      });
-    }
+    const ast = recast.parse(code, { parser: tsParser });
 
     recast.visit(ast, {
       // visit className attributes containing TW classes
       visitJSXAttribute(path) {
-        let node = path.node;
+        const node = path.node;
         if (path.node.name.name === 'className') {
-          visitNode(node);
+          visitNode(node, ctx);
         }
         this.traverse(path);
       },
       // visit cn function calls containing TW classes
       visitCallExpression(path) {
-        let node = path.node;
+        const node = path.node;
         if (node.callee.type === 'Identifier' && node.callee.name === 'cn') {
-          visitNode(node);
+          visitNode(node, ctx);
         }
         this.traverse(path);
       },
@@ -107,29 +110,9 @@ export async function generateStylesheet(
     stylesheet += `html :where(.${cn}) { @apply ${value} }\n`;
   }
 
-  // @ts-ignore
-  let result = await postcss([
-    tailwindcss(ctx.tailwindConfig),
-    {
-      postcssPlugin: 'Replace variable scope',
-      Declaration(decl) {
-        if (decl.prop.startsWith('--tw-')) {
-          decl.prop = decl.prop.replace('--tw-', '--cl-');
-        }
-        let value = valueParser(decl.value);
-        value.walk(function (node) {
-          if (node.type === 'function' && node.value === 'var') {
-            node.nodes.forEach(n => {
-              if (n.type === 'word' && n.value.startsWith('--tw-')) {
-                n.value = n.value.replace('--tw-', '--cl-');
-              }
-            });
-          }
-        });
-        decl.value = value.toString();
-      },
-    },
-  ]).process(stylesheet, { from: undefined });
+  const result = await postcss([tailwindcss(ctx.tailwindConfig) as Plugin, replaceVariableScope]).process(stylesheet, {
+    from: undefined,
+  });
 
   return result.css;
 }
