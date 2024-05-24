0eb2bdad76093a29f001818aa81cb2548e3e7300
panteliselef <panteliselef@outlook.com>
2024-05-24 19:17:17 +0300

    feat(clerk-js): Standalone OrganizationSwitcher

--- packages/clerk-js/src/ui/components/OrganizationSwitcher/OrganizationSwitcher.tsx
+++ packages/clerk-js/src/ui/components/OrganizationSwitcher/OrganizationSwitcher.tsx
@@ -1,4 +1,5 @@
-import { useId } from 'react';
+import type { ReactElement } from 'react';
+import { cloneElement, useId } from 'react';
 
 import { AcceptedInvitationsProvider, useOrganizationSwitcherContext, withCoreUserGuard } from '../../contexts';
 import { Flow } from '../../customizables';
@@ -7,8 +8,9 @@ import { usePopover } from '../../hooks';
 import { OrganizationSwitcherPopover } from './OrganizationSwitcherPopover';
 import { OrganizationSwitcherTrigger } from './OrganizationSwitcherTrigger';
 
-const _OrganizationSwitcher = withFloatingTree(() => {
-  const { defaultOpen, __experimental_open, __experimental_onOpenChanged } = useOrganizationSwitcherContext();
+const OrganizationSwitcherWithFloatingTree = withFloatingTree<{ children: ReactElement }>(({ children }) => {
+  const { __experimental_onOpenChanged, __experimental_open, defaultOpen } = useOrganizationSwitcherContext();
+
   const { floating, reference, styles, toggle, isOpen, nodeId, context } = usePopover({
     defaultOpen,
     open: __experimental_open,
@@ -20,33 +22,49 @@ const _OrganizationSwitcher = withFloatingTree(() => {
   const switcherButtonMenuId = useId();
 
   return (
+    <>
+      <OrganizationSwitcherTrigger
+        ref={reference}
+        onClick={toggle}
+        isOpen={isOpen}
+        aria-controls={isOpen ? switcherButtonMenuId : undefined}
+        aria-expanded={isOpen}
+      />
+      <Popover
+        nodeId={nodeId}
+        context={context}
+        isOpen={isOpen}
+      >
+        {cloneElement(children, {
+          id: switcherButtonMenuId,
+          close: toggle,
+          ref: floating,
+          style: styles,
+        })}
+      </Popover>
+    </>
+  );
+});
+
+const _OrganizationSwitcher = () => {
+  const { __experimental_hideTrigger, __experimental_onActionEnded } = useOrganizationSwitcherContext();
+
+  return (
     <Flow.Root
       flow='organizationSwitcher'
       sx={{ display: 'inline-flex' }}
     >
       <AcceptedInvitationsProvider>
-        <OrganizationSwitcherTrigger
-          ref={reference}
-          onClick={toggle}
-          isOpen={isOpen}
-          aria-controls={isOpen ? switcherButtonMenuId : undefined}
-          aria-expanded={isOpen}
-        />
-        <Popover
-          nodeId={nodeId}
-          context={context}
-          isOpen={isOpen}
-        >
-          <OrganizationSwitcherPopover
-            id={switcherButtonMenuId}
-            close={toggle}
-            ref={floating}
-            style={{ ...styles }}
-          />
-        </Popover>
+        {__experimental_hideTrigger ? (
+          <OrganizationSwitcherPopover close={__experimental_onActionEnded} />
+        ) : (
+          <OrganizationSwitcherWithFloatingTree>
+            <OrganizationSwitcherPopover />
+          </OrganizationSwitcherWithFloatingTree>
+        )}
       </AcceptedInvitationsProvider>
     </Flow.Root>
   );
-});
+};
 
 export const OrganizationSwitcher = withCoreUserGuard(withCardStateProvider(_OrganizationSwitcher));
--- packages/clerk-js/src/ui/components/OrganizationSwitcher/OrganizationSwitcherPopover.tsx
+++ packages/clerk-js/src/ui/components/OrganizationSwitcher/OrganizationSwitcherPopover.tsx
@@ -21,11 +21,12 @@ import { useRouter } from '../../router';
 import type { PropsOfComponent, ThemableCssProp } from '../../styledSystem';
 import { OrganizationActionList } from './OtherOrganizationActions';
 
-type OrganizationSwitcherPopoverProps = { close: () => void } & PropsOfComponent<typeof PopoverCard.Root>;
+type OrganizationSwitcherPopoverProps = { close?: () => void } & PropsOfComponent<typeof PopoverCard.Root>;
 
 export const OrganizationSwitcherPopover = React.forwardRef<HTMLDivElement, OrganizationSwitcherPopoverProps>(
   (props, ref) => {
-    const { close, ...rest } = props;
+    const { close: undefinedClose, ...rest } = props;
+    const close = () => undefinedClose?.();
     const card = useCardState();
     const { openOrganizationProfile, openCreateOrganization } = useClerk();
     const { organization: currentOrg } = useOrganization();
