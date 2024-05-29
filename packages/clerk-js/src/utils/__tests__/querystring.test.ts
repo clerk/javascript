5c385f878b43467939d97b6e9096142729b8b7cb
Emmanouela <emmanouelapothitou@gmail.com>
2024-05-29 16:47:38 +0300

    chore(clerk-js): Update querystring.ts file and add more tests

--- packages/clerk-js/src/utils/__tests__/querystring.test.ts
+++ packages/clerk-js/src/utils/__tests__/querystring.test.ts
@@ -1,18 +1,55 @@
+import { camelToSnake } from '@clerk/shared';
+
 import { getQueryParams, stringifyQueryParams } from '../querystring';
 
 describe('getQueryParams(string)', () => {
-  it('parses a querystring', () => {
-    expect(getQueryParams('')).toEqual({});
-    expect(getQueryParams('foo=42&bar=43')).toEqual({ foo: '42', bar: '43' });
-    expect(getQueryParams('?foo=42&bar=43')).toEqual({ foo: '42', bar: '43' });
+  it('parses an emtpy querystring', () => {
+    const res = getQueryParams('');
+    expect(res).toEqual({});
+  });
+
+  it('parses a querystring into a URLSearchParams instance', () => {
+    const res = getQueryParams('foo=42&bar=43');
+    expect(res).toEqual({ foo: '42', bar: '43' });
+  });
+
+  it('parses a querystring into a URLSearchParams instance even when prefixed with ?', () => {
+    const res = getQueryParams('?foo=42&bar=43');
+    expect(res).toEqual({ foo: '42', bar: '43' });
+  });
+
+  it('parses multiple occurances of the same key as an array', () => {
+    const res = getQueryParams('?foo=42&foo=43&bar=1');
+    expect(res).toEqual({ foo: ['42', '43'], bar: '1' });
   });
 });
 
 describe('stringifyQueryParams(object)', () => {
+  // it('converts an object to querystring', () => {
+  //   expect(stringifyQueryParams({})).toEqual('');
+  //   expect(stringifyQueryParams({ foo: '42', bar: '43' })).toBe('foo=42&bar=43');
+  //   expect(stringifyQueryParams({ foo: ['42', '22'], bar: '43' })).toBe('foo=42&foo=22&bar=43');
+  //   expect(stringifyQueryParams({ foo: ['42', '22'], bar: undefined })).toBe('foo=42&foo=22');
+  //   expect(stringifyQueryParams({ unsafeMetadata: { bar: '1' } })).toBe('unsafe_metadata=%7B%22bar%22%3A%221%22%7D');
+  // });
+
   it('converts an object to querystring', () => {
-    expect(stringifyQueryParams({})).toEqual('');
     expect(stringifyQueryParams({ foo: '42', bar: '43' })).toBe('foo=42&bar=43');
+  });
+  it('converts an object to querystring when value is an array', () => {
     expect(stringifyQueryParams({ foo: ['42', '22'], bar: '43' })).toBe('foo=42&foo=22&bar=43');
+  });
+  it('converts an object to querystring when value is undefined', () => {
     expect(stringifyQueryParams({ foo: ['42', '22'], bar: undefined })).toBe('foo=42&foo=22');
+  });
+  it('converts an object to querystring when value is an object', () => {
+    expect(stringifyQueryParams({ unsafe_metadata: { bar: '1' } })).toBe('unsafe_metadata=%7B%22bar%22%3A%221%22%7D');
+  });
+
+  it('converts an object to querystring when key is camelCase', () => {
+    expect(stringifyQueryParams({ barFoo: '1' }, { encoder: camelToSnake })).toBe('bar_foo=1');
+    expect(stringifyQueryParams({ unsafeMetadata: { bar: '1' } }, { encoder: camelToSnake })).toBe(
+      'unsafe_metadata=%7B%22bar%22%3A%221%22%7D',
+    );
   });
 });
--- packages/clerk-js/src/utils/querystring.ts
+++ packages/clerk-js/src/utils/querystring.ts
@@ -1,25 +1,44 @@
-export const getQueryParams = (queryString: string): URLSearchParams => {
-  const queryParamsObject = new URLSearchParams();
+export const getQueryParams = (queryString: string) => {
+  const queryParamsObject: { [key: string]: string | string[] } = {};
   const queryParams = new URLSearchParams(queryString);
   queryParams.forEach((value, key) => {
-    queryParamsObject.set(key, value);
+    if (key in queryParamsObject) {
+      // If the key already exists, we need to handle it as an array
+      const existingValue = queryParamsObject[key];
+      if (Array.isArray(existingValue)) {
+        existingValue.push(value);
+      } else {
+        queryParamsObject[key] = [existingValue, value];
+      }
+    } else {
+      queryParamsObject[key] = value;
+    }
   });
-
-  return queryParamsObject;
+  return queryParamsObject as Record<string, string>;
 };
 
-export const stringifyQueryParams = (params: Record<string, unknown> | Array<unknown>, encoder?) => {
+type StringifyQueryParamsOptions = {
+  encoder?: (key: string) => string;
+};
+
+export const stringifyQueryParams = (
+  params: Record<string, string | undefined | null | object | Array<string | undefined | null>>,
+  opts: StringifyQueryParamsOptions = {},
+) => {
   const queryParams = new URLSearchParams();
+
   if (params && typeof params === 'object') {
     Object.keys(params).forEach(key => {
-      const encodedKey = encoder ? encoder(key) : key;
+      const encodedKey = opts.encoder ? opts.encoder(key) : key;
       const value = params[key];
       if (Array.isArray(value)) {
-        value.forEach(item => queryParams.append(encodedKey, item));
+        value.forEach(v => v !== undefined && queryParams.append(encodedKey, v || ''));
       } else if (value === undefined) {
         return;
+      } else if (typeof value === 'object' && value !== null) {
+        queryParams.append(encodedKey, JSON.stringify(value));
       } else {
-        queryParams.append(encodedKey, value);
+        queryParams.append(encodedKey, value || '');
       }
     });
   }
