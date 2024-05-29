02c398e5bdbd951d5a73ac27a2fa86b2e072de84
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-05-29 13:03:11 -0400

    render generated theme in dialog

--- packages/ui/theme-builder/app/theme-builder.tsx
+++ packages/ui/theme-builder/app/theme-builder.tsx
@@ -194,42 +194,9 @@ export function ThemeBuilder() {
               )}
               aria-hidden='true'
             />
-<<<<<<< HEAD
             {componnents[selectedComponent]}
           </figure>
         </div>
-=======
-            <Card.Root>
-              <Card.Content>
-                <Card.Header>
-                  <Card.Title>Sign in to Acme Co</Card.Title>
-                  <Card.Description>Welcome back! Please sign in to continue</Card.Description>
-                </Card.Header>
-                <Card.Body>
-                  <Connection.Root>
-                    <Connection.Button>Google</Connection.Button>
-                    <Connection.Button>GitHub</Connection.Button>
-                  </Connection.Root>
-                  <Seperator>or</Seperator>
-                  <Field.Root>
-                    <Field.Label>Email address</Field.Label>
-                    <Field.Input />
-                    <Field.Message>Identifier is invalid.</Field.Message>
-                  </Field.Root>
-                  <Button>Continue</Button>
-                </Card.Body>
-              </Card.Content>
-              <Card.Footer>
-                <Card.FooterAction>
-                  <Card.FooterActionText>
-                    Don&apos;t have an account? <Card.FooterActionLink href='/sign-up'>Sign up</Card.FooterActionLink>
-                  </Card.FooterActionText>
-                </Card.FooterAction>
-              </Card.Footer>
-            </Card.Root>
-          </div>
-        </figure>
->>>>>>> 4ee36de20 (Convert field to compound component)
       </div>
     </>
   );
