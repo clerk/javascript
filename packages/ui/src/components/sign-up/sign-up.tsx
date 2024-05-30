d939d9b7aac74c8bb42fed1c7158b371fa31a0ed
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-05-30 09:33:41 -0400

    Install elements

---
+++ packages/ui/src/components/sign-up/sign-up.tsx
@@ -0,0 +1,80 @@
+import * as Common from '@clerk/elements/common';
+import * as SignUp from '@clerk/elements/sign-up';
+
+import { Button } from '~/primitives/button';
+import * as Card from '~/primitives/card';
+import * as Connection from '~/primitives/connection';
+import * as Field from '~/primitives/field';
+import { Seperator } from '~/primitives/seperator';
+
+export function SignUpComponent() {
+  return (
+    <SignUp.Root exampleMode>
+      <SignUp.Step name='start'>
+        <Card.Root>
+          <Card.Content>
+            <Card.Header>
+              <Card.Title>Create your account</Card.Title>
+              <Card.Description>Welcome! Please fill in the details to get started.</Card.Description>
+            </Card.Header>
+            <Card.Body>
+              <Connection.Root>
+                <Common.Connection
+                  name='google'
+                  asChild
+                >
+                  <Connection.Button>
+                    <Common.Icon className='size-4' />
+                    Google
+                  </Connection.Button>
+                </Common.Connection>
+                <Common.Connection
+                  name='github'
+                  asChild
+                >
+                  <Connection.Button>
+                    <Common.Icon className='size-4' />
+                    GitHub
+                  </Connection.Button>
+                </Common.Connection>
+              </Connection.Root>
+              <Seperator>or</Seperator>
+              <Common.Field
+                name='emailAddress'
+                asChild
+              >
+                <Field.Root>
+                  <Common.Label asChild>
+                    <Field.Label>Email address</Field.Label>
+                  </Common.Label>
+                  <Common.Input asChild>
+                    <Field.Input />
+                  </Common.Input>
+                  <Common.FieldError>
+                    {({ message }) => {
+                      return <Field.Message intent='error'>{message}</Field.Message>;
+                    }}
+                  </Common.FieldError>
+                </Field.Root>
+              </Common.Field>
+
+              <SignUp.Action
+                submit
+                asChild
+              >
+                <Button>Continue</Button>
+              </SignUp.Action>
+            </Card.Body>
+          </Card.Content>
+          <Card.Footer>
+            <Card.FooterAction>
+              <Card.FooterActionText>
+                Have an account? <Card.FooterActionLink href='/sign-in'>Sign in</Card.FooterActionLink>
+              </Card.FooterActionText>
+            </Card.FooterAction>
+          </Card.Footer>
+        </Card.Root>
+      </SignUp.Step>
+    </SignUp.Root>
+  );
+}
