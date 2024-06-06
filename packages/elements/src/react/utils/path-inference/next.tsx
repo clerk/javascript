0455851c168100d1b19dcf7e585836ab87c167f3
LekoArts <lekoarts@gmail.com>
2024-06-06 14:52:51 +0200

    app router only

--- packages/elements/src/react/utils/path-inference/next.tsx
+++ packages/elements/src/react/utils/path-inference/next.tsx
@@ -1,39 +1,10 @@
-import { useRouter } from 'next/compat/router';
+import { useParams, usePathname } from 'next/navigation';
 import React from 'react';
-
-import { removeOptionalCatchAllSegment } from './utils';
 
 // Adapted from packages/nextjs/src/client-boundary/hooks/usePathnameWithoutCatchAll.tsx
 
 export const usePathnameWithoutCatchAll = () => {
   const pathRef = React.useRef<string>();
-
-  /**
-   * The compat version of useRouter returns null instead of throwing an error when used inside App router.
-   * Use it to detect if the component is used inside pages or app router
-   * TODO: Should we skip this compat layer since in the router we do not use compat?
-   */
-  const pagesRouter = useRouter();
-
-  if (pagesRouter) {
-    if (pathRef.current) {
-      return pathRef.current;
-    } else {
-      // The optional catch all route is included in the pathname in pages router. It starts with [[... and we can just remove it
-      pathRef.current = removeOptionalCatchAllSegment(pagesRouter.pathname);
-      return pathRef.current;
-    }
-  }
-
-  /**
-   * Require is used to avoid importing next/navigation when the pages router is used, as it will throw an error.
-   * Can't use dynamic import as the hook needs to be sync
-   */
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const usePathname = require('next/navigation').usePathname;
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const useParams = require('next/navigation').useParams;
-
   /**
    * Get the pathname that includes any named or catch all params.
    * @example
