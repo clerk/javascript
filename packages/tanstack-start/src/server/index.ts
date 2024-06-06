ac1f2a6e90089838029abed294fa7f05aae8b472
Vaggelis Yfantis <me@octoper.me>
2024-06-06 19:37:42 +0300

    refactor(tanstack-start): Refactor clerkMiddlewareHandler

--- packages/tanstack-start/src/server/index.ts
+++ packages/tanstack-start/src/server/index.ts
@@ -10,29 +10,26 @@ export type RequestHandler<TRouter extends AnyRouter> = (ctx: {
 }) => Promise<Response>;
 export type CustomizeRequestHandler<TRouter extends AnyRouter> = (cb: RequestHandler<TRouter>) => EventHandler;
 
-export function clerkMiddlewareHandler<TRouter extends AnyRouter>(cb: RequestHandler<TRouter>) {
-  // TODO: Needs improvement
-  return async ({ request, router, responseHeaders }: Parameters<RequestHandler<TRouter>>[0]) => {
-    try {
-      const authContext = await rootAuthBeforeLoader(request);
+export function clerkMiddlewareHandler<TRouter extends AnyRouter>(eventHandler: CustomizeRequestHandler<TRouter>) {
+  return (cb: RequestHandler<TRouter>): EventHandler => {
+    return eventHandler(async ({ request, router, responseHeaders }) => {
+      try {
+        const authContext = await rootAuthBeforeLoader(request);
 
-      router.update({
-        context: authContext || {},
-      });
+        router.update({
+          context: authContext || {},
+        });
 
-      await router.load();
-    } catch (error) {
-      if (error instanceof Response) {
-        return error;
+        await router.load();
+      } catch (error) {
+        if (error instanceof Response) {
+          return error;
+        }
+
+        throw error;
       }
 
-      throw error;
-    }
-
-    return await cb({
-      request,
-      router,
-      responseHeaders,
+      return cb({ request, router, responseHeaders });
     });
   };
 }
