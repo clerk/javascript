283f8d4e6e6cb0990c82116572594543df6c0a67
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-06-07 11:36:20 -0400

    feat(ui): Implement `<PhoneNumberField />` component (#3524)

--- packages/ui/src/primitives/icon.tsx
+++ packages/ui/src/primitives/icon.tsx
@@ -16,6 +16,7 @@ function createIcon({ displayName, viewBox, path }: { displayName: string; viewB
         fill='none'
         xmlns='http://www.w3.org/2000/svg'
         className={cn('size-[1em]', rtlIcons.includes(displayName) && 'rtl:rotate-180', className)}
+        aria-hidden
         {...props}
       >
         {path}
@@ -664,5 +665,30 @@ export const Envelope = createIcon({
         d='m15 6.984-5.924 3.4a2.406 2.406 0 0 1-2.152 0L1 6.983V11.5a1.75 1.75 0 0 0 1.75 1.75h10.5A1.75 1.75 0 0 0 15 11.5V6.984Z'
       />
     </>
+  ),
+});
+
+export const ChevronUpDown = createIcon({
+  displayName: 'IconChevronUpDown',
+  viewBox: '0 0 16 16',
+  path: (
+    <path
+      fill='currentColor'
+      d='M5.53 9.22a.75.75 0 0 0-1.06 1.06l3 3a.75.75 0 0 0 1.06 0l3-3a.75.75 0 1 0-1.06-1.06L8 11.69 5.53 9.22ZM5.53 6.78a.75.75 0 0 1-1.06-1.06l3-3a.75.75 0 0 1 1.06 0l3 3a.75.75 0 0 1-1.06 1.06L8 4.31 5.53 6.78Z'
+    />
+  ),
+});
+
+export const Checkmark = createIcon({
+  displayName: 'IconCheckmark',
+  viewBox: '0 0 10 10',
+  path: (
+    <path
+      d='m1 6 3 3 5-8'
+      stroke='currentColor'
+      strokeWidth='1.25'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
   ),
 });
