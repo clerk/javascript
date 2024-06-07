e6fe663705e7debf5a680ffaf9668888ce4c8b78
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-06-07 17:14:24 -0400

    feat(ui): Implement connection layout rendering

--- packages/ui/src/primitives/connection.tsx
+++ packages/ui/src/primitives/connection.tsx
@@ -3,20 +3,37 @@ import * as React from 'react';
 
 import { Spinner } from './spinner';
 
-export const Root = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(function Root(
-  { children, className, ...props },
-  forwardedRef,
-) {
+function getColumnCount<T>(items: T[], maxCols: number = 6): number {
+  const numRows = Math.ceil(items.length / maxCols);
+  const itemsPerRow = Math.ceil(items.length / numRows);
+  const rows: T[][] = Array.from({ length: numRows }, () => []);
+  let currentArrayIndex = 0;
+  for (const item of items) {
+    rows[currentArrayIndex].push(item);
+    if (rows[currentArrayIndex].length === itemsPerRow) {
+      currentArrayIndex++;
+    }
+  }
+  return Math.max(...rows.map(row => row.length));
+}
+
+export const Root = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & {
+    columns?: number;
+  }
+>(function Root({ children, className, ...props }, forwardedRef) {
+  const cols = getColumnCount(React.Children.toArray(children));
+  console.log(cols);
   return (
     <div
       ref={forwardedRef}
       {...props}
       className={cn(
-        '[--cl-connection-gap:theme(spacing.2)]',
-        '[--cl-connection-cols:2]',
-        'flex gap-[--cl-connection-gap] flex-wrap items-center justify-center',
+        'flex items-center justify-center flex-wrap [--cl-connection-gap:theme(spacing.2)] gap-y-[--cl-connection-gap] -mx-1',
         className,
       )}
+      style={{ '--cl-connection-cols': cols } as React.CSSProperties}
     >
       {children}
     </div>
@@ -35,20 +52,21 @@ export const Button = React.forwardRef(function Button(
   forwardedRef: React.ForwardedRef<HTMLButtonElement>,
 ) {
   return (
-    // eslint-disable-next-line react/button-has-type
-    <button
-      ref={forwardedRef}
-      {...props}
-      className={cn(
-        'w-[calc((100%/var(--cl-connection-cols))-var(--cl-connection-gap))] ',
-        'min-w-0 flex items-center justify-center gap-2 bg-transparent text-gray-12 font-medium rounded-md bg-clip-padding border border-gray-a6 shadow-sm shadow-gray-a3 py-1.5 px-2.5 outline-none focus-visible:ring-[0.1875rem] focus-visible:ring-gray-a3 focus-visible:border-gray-a8 text-base',
-        busy ? 'cursor-wait' : disabled ? 'disabled:cursor-not-allowed disabled:opacity-50' : 'hover:bg-gray-a2',
-        className,
-      )}
-      disabled={busy || disabled}
-    >
-      {icon ? <span className='text-base'>{busy ? <Spinner>Loading…</Spinner> : <span>{icon}</span>}</span> : null}
-      {children}
-    </button>
+    <div className='w-[calc(100%/var(--cl-connection-cols))] px-[calc(var(--cl-connection-gap)/2)]'>
+      {/* eslint-disable react/button-has-type */}
+      <button
+        ref={forwardedRef}
+        {...props}
+        className={cn(
+          'min-w-0 w-full flex items-center justify-center gap-2 bg-transparent text-gray-12 font-medium rounded-md bg-clip-padding border border-gray-a6 shadow-sm shadow-gray-a3 py-1.5 px-2.5 outline-none focus-visible:ring-[0.1875rem] focus-visible:ring-gray-a3 focus-visible:border-gray-a8 text-base',
+          busy ? 'cursor-wait' : disabled ? 'disabled:cursor-not-allowed disabled:opacity-50' : 'hover:bg-gray-a2',
+          className,
+        )}
+        disabled={busy || disabled}
+      >
+        {icon ? <span className='text-base'>{busy ? <Spinner>Loading…</Spinner> : <span>{icon}</span>}</span> : null}
+        <span className='sr-only'>{children}</span>
+      </button>
+    </div>
   );
 });
