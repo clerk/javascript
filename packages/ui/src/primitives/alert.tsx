ca4651553e6ee8a750716ce1f9261ce098ea99b9
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-06-07 09:28:53 -0400

    update icons

--- packages/ui/src/primitives/alert.tsx
+++ packages/ui/src/primitives/alert.tsx
@@ -6,9 +6,9 @@ import * as Icon from './icon';
 export const Alert = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement> & {
-    intent?: 'warning' | 'danger';
+    intent?: 'warning' | 'error';
   }
->(function Alert({ children, className, intent = 'danger', ...props }, forwardedRef) {
+>(function Alert({ children, className, intent = 'error', ...props }, forwardedRef) {
   return (
     <div
       ref={forwardedRef}
@@ -17,13 +17,18 @@ export const Alert = React.forwardRef<
         'text-base leading-small py-3 px-4 rounded-md border',
         {
           warning: 'text-warning bg-warning/10 border-warning/20',
-          danger: 'text-danger bg-danger/10 border-danger/20',
+          error: 'text-danger bg-danger/10 border-danger/20',
         }[intent],
         className,
       )}
     >
       <div className='flex gap-x-2'>
-        <Icon.ExclamationTriangleSm className='size-4 shrink-0' />
+        {
+          {
+            error: <Icon.ExclamationOctagonSm className='size-4 shrink-0' />,
+            warning: <Icon.ExclamationTriangleSm className='size-4 shrink-0' />,
+          }[intent]
+        }
         {children}
       </div>
     </div>
