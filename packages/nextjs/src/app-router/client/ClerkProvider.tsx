f17124a4924e46421c8b3230960ae9823936f329
George Desipris <george.desipris.56@gmail.com>
2024-06-11 15:28:48 +0300

    fix(nextjs): Invalidate via server action for nextjs 14 and above

--- packages/nextjs/src/app-router/client/ClerkProvider.tsx
+++ packages/nextjs/src/app-router/client/ClerkProvider.tsx
@@ -57,7 +57,12 @@ export const ClientClerkProvider = (props: NextClerkProviderProps) => {
       return new Promise(res => {
         window.__clerk_internal_invalidateCachePromise = res;
         startTransition(() => {
-          invalidateCacheAction();
+          //@ts-expect-error next exitsts on window
+          if (window.next?.version && typeof window.next.version === 'string' && window.next.version.startsWith('13')) {
+            router.refresh();
+          } else {
+            invalidateCacheAction();
+          }
         });
       });
     };
