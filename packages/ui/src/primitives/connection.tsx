9000aca1b96f7a2ffaee9d2a12275ba5195fabe0
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-06-11 09:47:23 -0400

    document usage

--- packages/ui/src/primitives/connection.tsx
+++ packages/ui/src/primitives/connection.tsx
@@ -3,6 +3,47 @@ import * as React from 'react';
 
 import { Spinner } from './spinner';
 
+/**
+ * Calculates the number of columns given the total number of items and the maximum columns allowed per row.
+ *
+ * @param itemsLength - The total number of items.
+ * @param maxCols - The maximum number of columns allowed per row (default is 6).
+ * @returns The calculated number of columns.
+ *
+ * Example output for item counts from 1 to 24 with maxCols = 6:
+ *
+ *  1:  [ 1 ]
+ *  2:  [ 1, 2 ]
+ *  3:  [ 1, 2, 3 ]
+ *  4:  [ 1, 2, 3, 4 ]
+ *  5:  [ 1, 2, 3, 4, 5 ]
+ *  6:  [ 1, 2, 3, 4, 5, 6 ]
+ *  7:  [ [1, 2, 3, 4], [5, 6, 7] ]
+ *  8:  [ [1, 2, 3, 4], [5, 6, 7, 8] ]
+ *  9:  [ [1, 2, 3, 4, 5], [6, 7, 8, 9] ]
+ * 10:  [ [1, 2, 3, 4, 5], [6, 7, 8, 9, 10] ]
+ * 11:  [ [1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11] ]
+ * 12:  [ [1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12] ]
+ * 13:  [ [1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13] ]
+ * 14:  [ [1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14] ]
+ * 15:  [ [1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11], [12, 13, 14, 15] ]
+ * 16:  [ [1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12], [13, 14, 15, 16] ]
+ * 17:  [ [1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12], [13, 14, 15, 16, 17] ]
+ * 18:  [ [1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18] ]
+ * 19:  [ [1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19] ]
+ * 20:  [ [1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20] ]
+ * 21:  [ [1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11], [12, 13, 14, 15, 16], [17, 18, 19, 20, 21] ]
+ * 22:  [ [1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11], [12, 13, 14, 15, 16], [17, 18, 19, 20, 21, 22] ]
+ * 23:  [ [1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12], [13, 14, 15, 16, 17], [18, 19, 20, 21, 22, 23] ]
+ * 24:  [ [1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12], [13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24] ]
+ *
+ * Examples:
+ * ```
+ * getColumnCount(1); // 1
+ * getColumnCount(7); // 4
+ * getColumnCount(15); // 6
+ * ```
+ */
 function getColumnCount(itemsLength: number, maxCols: number = 6): number {
   const numRows = Math.ceil(itemsLength / maxCols);
   return Math.ceil(itemsLength / numRows);
