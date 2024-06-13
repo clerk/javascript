793297c0d656d94d03b660e446d0adacd3cac1b0
Bryce Kalow <bryce@clerk.dev>
2024-06-13 12:55:02 -0500

    Delete playground/app-router/src/pages/api/api.js

--- playground/app-router/src/pages/api/api.js
+++
@@ -1,9 +0,0 @@
-import { requireAuth } from "@clerk/nextjs/api";
- 
-export default requireAuth(async function handler(
-  req,
-  res
-) {
-  // Load any data your application needs for the API route
-  return res.status(200).json({});
-})
