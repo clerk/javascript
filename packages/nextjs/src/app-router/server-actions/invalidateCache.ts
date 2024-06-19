0ad388866d6b0992f37f1cffa3d0bf1fe8598531
Bryce Kalow <bryce@clerk.dev>
2024-06-18 22:04:26 -0500

    fix(nextjs): Make exported server action async

--- packages/nextjs/src/app-router/server-actions/invalidateCache.ts
+++ packages/nextjs/src/app-router/server-actions/invalidateCache.ts
@@ -2,6 +2,6 @@
 
 import { cookies } from 'next/headers';
 
-export function invalidateCacheAction() {
+export async function invalidateCacheAction() {
   return cookies().delete(`__clerk_invalidate_cache_cookie_${Date.now()}`);
 }
