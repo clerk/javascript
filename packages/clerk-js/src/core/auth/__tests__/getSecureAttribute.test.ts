ce7b2a39dfb37b0ed83f8f1e3080347b56883f3e
Stefanos Anagnostou <stefanos@clerk.dev>
2024-07-01 14:56:59 +0300

    Add tests

---
+++ packages/clerk-js/src/core/auth/__tests__/getSecureAttribute.test.ts
@@ -0,0 +1,73 @@
+import { getSecureAttribute } from '../getSecureAttribute';
+
+const oldWindowLocation = window.location;
+const oldWindowSafari = (window as any).safari;
+const oldWindowSecureContext = window.isSecureContext;
+
+describe('getSecureAttribute', () => {
+  afterAll(() => {
+    Object.defineProperty(global.window, 'location', {
+      value: oldWindowLocation,
+    });
+    Object.defineProperty(global.window, 'safari', {
+      value: oldWindowSafari,
+    });
+    Object.defineProperty(global.window, 'isSecureContext', {
+      value: oldWindowSecureContext,
+    });
+  });
+
+  it('returns true if the protocol is https', () => {
+    const mockWindowLocation = new URL('https://www.clerk.com') as any as Window['location'];
+    Object.defineProperty(global.window, 'location', { value: mockWindowLocation });
+    expect(getSecureAttribute('None')).toBe(true);
+  });
+
+  it('returns false if the protocol is not https and the browser is Safari', () => {
+    const mockWindowLocation = new URL('http://www.clerk.com') as any as Window['location'];
+    Object.defineProperty(global.window, 'location', { value: mockWindowLocation });
+    Object.defineProperty(global.window, 'safari', { value: { dummyValue: true } });
+    Object.defineProperty(global.window, 'isSecureContext', { value: undefined });
+    expect(getSecureAttribute('None')).toBe(false);
+  });
+
+  it('returns true if isSecureContext is true', () => {
+    const mockWindowLocation = new URL('http://www.clerk.com') as any as Window['location'];
+    Object.defineProperty(global.window, 'location', { value: mockWindowLocation });
+    Object.defineProperty(global.window, 'safari', { value: undefined });
+    Object.defineProperty(global.window, 'isSecureContext', { value: true });
+    expect(getSecureAttribute('None')).toBe(true);
+  });
+
+  it('returns false if isSecureContext is false', () => {
+    const mockWindowLocation = new URL('http://www.clerk.com') as any as Window['location'];
+    Object.defineProperty(global.window, 'location', { value: mockWindowLocation });
+    Object.defineProperty(global.window, 'safari', { value: undefined });
+    Object.defineProperty(global.window, 'isSecureContext', { value: false });
+    expect(getSecureAttribute('None')).toBe(false);
+  });
+
+  it('returns true if the protocol is http and the hostname is localhost and sameSite is None', () => {
+    const mockWindowLocation = new URL('http://localhost') as any as Window['location'];
+    Object.defineProperty(global.window, 'location', { value: mockWindowLocation });
+    Object.defineProperty(global.window, 'safari', { value: undefined });
+    Object.defineProperty(global.window, 'isSecureContext', { value: undefined });
+    expect(getSecureAttribute('None')).toBe(true);
+  });
+
+  it('returns false if the protocol is http and the hostname is localhost and sameSite is not None', () => {
+    const mockWindowLocation = new URL('http://localhost') as any as Window['location'];
+    Object.defineProperty(global.window, 'location', { value: mockWindowLocation });
+    Object.defineProperty(global.window, 'safari', { value: undefined });
+    Object.defineProperty(global.window, 'isSecureContext', { value: undefined });
+    expect(getSecureAttribute('Lax')).toBe(false);
+  });
+
+  it('returns false if the protocol is http and the hostname is not localhost', () => {
+    const mockWindowLocation = new URL('http://www.clerk.com') as any as Window['location'];
+    Object.defineProperty(global.window, 'location', { value: mockWindowLocation });
+    Object.defineProperty(global.window, 'safari', { value: undefined });
+    Object.defineProperty(global.window, 'isSecureContext', { value: undefined });
+    expect(getSecureAttribute('None')).toBe(false);
+  });
+});
--- packages/clerk-js/src/core/auth/getSecureAttribute.ts
+++ packages/clerk-js/src/core/auth/getSecureAttribute.ts
@@ -4,17 +4,22 @@ type SameSiteAttributeType = 'None' | 'Lax' | 'Strict';
 // Also, localhost is considered secure for testing purposes by Chrome and Firefox
 // Safari does not support the Secure attribute on localhost, although it returns true for isSecureContext
 // ref: https://bugs.webkit.org/show_bug.cgi?id=232088#c8
-export const getSecureAttribute = (sameSite: SameSiteAttributeType) => {
-  const httpsProtocol = window.location.protocol === 'https:';
-
-  // Return true on Safari only for `https` protocol
-  if (typeof (window as any).safari !== 'undefined') {
-    return httpsProtocol;
+export const getSecureAttribute = (sameSite: SameSiteAttributeType): boolean => {
+  if (window.location.protocol === 'https:') {
+    return true;
   }
 
+  // This is because Safari does not support the Secure attribute on localhost
+  if (typeof (window as any).safari !== 'undefined') {
+    return false;
+  }
+
+  // This is a useful way to check if the current context is secure
+  // This is needed for example in environments like Firefox Remote Control
+  // where the `localhost` is not considered secure
   if (typeof window.isSecureContext !== 'undefined') {
     return window.isSecureContext;
   }
 
-  return httpsProtocol || (window.location.hostname === 'localhost' && sameSite === 'None');
+  return window.location.hostname === 'localhost' && sameSite === 'None';
 };
