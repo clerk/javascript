17181109305f9c3702dcb84da718908762d964b2
panteliselef <panteliselef@outlook.com>
2024-07-04 19:57:05 +0300

    WIP: debug CI for astro without offline @clerk/astro

--- integration/presets/astro.ts
+++ integration/presets/astro.ts
@@ -2,8 +2,8 @@ import { applicationConfig } from '../models/applicationConfig';
 import { templates } from '../templates';
 
 // const clerkAstroLocal = `file:${process.cwd()}/packages/astro`;
-// const clerkTypesLocal = `file:${process.cwd()}/packages/types`;
-// const clerkLocalizationLocal = `file:${process.cwd()}/packages/localizations`;
+const clerkTypesLocal = `file:${process.cwd()}/packages/types`;
+const clerkLocalizationLocal = `file:${process.cwd()}/packages/localizations`;
 
 const astroNode = applicationConfig()
   .setName('astro-node')
@@ -14,8 +14,8 @@ const astroNode = applicationConfig()
   .addScript('build', 'npm run build')
   .addScript('serve', 'npm run preview')
   .addDependency('@clerk/astro', '0.0.1-canary.ve8a2af5')
-  .addDependency('@clerk/types', '4.6.1')
-  .addDependency('@clerk/localizations', '2.4.6');
+  .addDependency('@clerk/types', clerkTypesLocal)
+  .addDependency('@clerk/localizations', clerkLocalizationLocal);
 
 export const astro = {
   node: astroNode,
