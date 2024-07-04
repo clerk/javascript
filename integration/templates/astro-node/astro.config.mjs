7b4df7d4779cb23bfc81b7b869871a6dcf517123
panteliselef <panteliselef@outlook.com>
2024-07-04 16:43:06 +0300

    chore(astro,e2e): Cleanup

--- integration/templates/astro-node/astro.config.mjs
+++ integration/templates/astro-node/astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import clerk from '@clerk/astro';
-import { loadEnv } from 'vite';
 import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
@@ -12,12 +11,6 @@ export default defineConfig({
     mode: 'standalone',
   }),
   integrations: [clerk(), react(), tailwind()],
-  // server: {
-  //   port:  (() => {
-  //     console.log("-asstro",loadEnv(process.env.NODE_ENV, process.cwd(), "").PORT)
-  //     return 39333
-  //   })()
-  // }
   server: {
     port: Number(process.env.PORT),
   },
