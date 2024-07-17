496e324b9502910d77745f6dc2a7f032ee9b2070
panteliselef <panteliselef@outlook.com>
2024-07-17 14:41:08 +0300

    chore(astro): Remove assertions from `windowNavigate` as it is handled in clerk-js

--- packages/astro/src/internal/create-clerk-instance.ts
+++ packages/astro/src/internal/create-clerk-instance.ts
@@ -10,18 +10,9 @@ let initOptions: ClerkOptions | undefined;
 
 // TODO-SHARED: copied from `clerk-js`
 export const CLERK_BEFORE_UNLOAD_EVENT = 'clerk:beforeunload';
-export const ALLOWED_PROTOCOLS = ['http:', 'https:'];
 
 function windowNavigate(to: URL | string): void {
-  let toURL = new URL(to, window.location.href);
-
-  if (!ALLOWED_PROTOCOLS.includes(toURL.protocol)) {
-    console.warn(
-      `Clerk: "${toURL.protocol}" is not a valid protocol. Redirecting to "/" instead. If you think this is a mistake, please open an issue.`,
-    );
-    toURL = new URL('/', window.location.href);
-  }
-
+  const toURL = new URL(to, window.location.href);
   window.dispatchEvent(new CustomEvent(CLERK_BEFORE_UNLOAD_EVENT));
   window.location.href = toURL.href;
 }
