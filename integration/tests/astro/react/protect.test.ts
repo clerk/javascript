17485746090fa266652df220e7f498faa5c9db98
panteliselef <panteliselef@outlook.com>
2024-07-18 18:35:10 +0300

    identify problematic tests 11

--- integration/tests/astro/react/protect.test.ts
+++ integration/tests/astro/react/protect.test.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 import type { FakeOrganization, FakeUser } from '../../../testUtils';
 import { createTestUtils, testAgainstRunningApps } from '../../../testUtils';
@@ -24,30 +24,30 @@ testAgainstRunningApps({
     await app.teardown();
   });
 
-  test('dummy test', async ({ page, context }) => {
-    createTestUtils({ app, page, context });
+  // test('dummy test', async ({ page, context }) => {
+  //   createTestUtils({ app, page, context });
+  // });
+
+  test.skip('only admin', async ({ page, context }) => {
+    const u = createTestUtils({ app, page, context });
+    await u.page.goToRelative('/react/sign-in#/?redirect_url=/react');
+    await u.po.signIn.waitForMounted();
+    await u.po.signIn.signInWithEmailAndInstantPassword({ email: fakeAdmin.email, password: fakeAdmin.password });
+    await u.po.expect.toBeSignedIn();
+    await u.po.organizationSwitcher.waitForMounted();
+    await u.po.organizationSwitcher.toggleTrigger();
+    await u.page.locator('.cl-organizationSwitcherPreviewButton').click();
+    await u.po.organizationSwitcher.waitForAnOrganizationToSelected();
+    await u.page.goToRelative('/react/only-admins');
+    await expect(u.page.getByText("I'm an admin")).toBeVisible();
   });
 
-  // test.skip('only admin', async ({ page, context }) => {
-  //   const u = createTestUtils({ app, page, context });
-  //   await u.page.goToRelative('/react/sign-in#/?redirect_url=/react');
-  //   await u.po.signIn.waitForMounted();
-  //   await u.po.signIn.signInWithEmailAndInstantPassword({ email: fakeAdmin.email, password: fakeAdmin.password });
-  //   await u.po.expect.toBeSignedIn();
-  //   await u.po.organizationSwitcher.waitForMounted();
-  //   await u.po.organizationSwitcher.toggleTrigger();
-  //   await u.page.locator('.cl-organizationSwitcherPreviewButton').click();
-  //   await u.po.organizationSwitcher.waitForAnOrganizationToSelected();
-  //   await u.page.goToRelative('/react/only-admins');
-  //   await expect(u.page.getByText("I'm an admin")).toBeVisible();
-  // });
-  //
-  // test.skip('only member', async ({ page, context }) => {
-  //   const u = createTestUtils({ app, page, context });
-  //   await u.page.goToRelative('/react/sign-in#/?redirect_url=/react/only-members');
-  //   await u.po.signIn.waitForMounted();
-  //   await u.po.signIn.signInWithEmailAndInstantPassword({ email: fakeAdmin.email, password: fakeAdmin.password });
-  //   await u.po.expect.toBeSignedIn();
-  //   await expect(u.page.getByText('Not a member')).toBeVisible();
-  // });
+  test.skip('only member', async ({ page, context }) => {
+    const u = createTestUtils({ app, page, context });
+    await u.page.goToRelative('/react/sign-in#/?redirect_url=/react/only-members');
+    await u.po.signIn.waitForMounted();
+    await u.po.signIn.signInWithEmailAndInstantPassword({ email: fakeAdmin.email, password: fakeAdmin.password });
+    await u.po.expect.toBeSignedIn();
+    await expect(u.page.getByText('Not a member')).toBeVisible();
+  });
 });
