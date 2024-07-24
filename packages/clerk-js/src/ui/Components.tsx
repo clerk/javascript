33c13cea5673fc84498dca80d20de6f2f56fa833
Emmanouela Pothitou <68468183+EmmanouelaPothitou@users.noreply.github.com>
2024-07-24 14:46:57 +0300

    Update packages/clerk-js/src/ui/Components.tsx
    Co-authored-by: panteliselef <panteliselef@outlook.com>

--- packages/clerk-js/src/ui/Components.tsx
+++ packages/clerk-js/src/ui/Components.tsx
@@ -285,7 +285,7 @@ const Components = (props: ComponentsProps) => {
       flowName={'userProfile'}
       onClose={() => componentsControls.closeModal('userProfile')}
       onExternalNavigate={() => componentsControls.closeModal('userProfile')}
-      startPath={buildVirtualRouterUrl({ base: '/user', path: userProfileModal?.path || urlStateParam?.path })}
+      startPath={buildVirtualRouterUrl({ base: '/user', path: userProfileModal?.__experimental_startPath || urlStateParam?.path })}
       componentName={'UserProfileModal'}
       modalContainerSx={{ alignItems: 'center' }}
       modalContentSx={t => ({ height: `min(${t.sizes.$176}, calc(100% - ${t.sizes.$12}))`, margin: 0 })}
