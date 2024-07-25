156c73699e7c28f755850d503f820e4ba51f88f9
Vaggelis Yfantis <me@octoper.me>
2024-07-25 21:46:25 +0300

    chore(clerk-expo): Update JSDOC of getClerkInstance

--- packages/expo/src/provider/singleton/createClerkInstance.ts
+++ packages/expo/src/provider/singleton/createClerkInstance.ts
@@ -14,24 +14,6 @@ const KEY = '__clerk_client_jwt';
 export let clerk: HeadlessBrowserClerk | BrowserClerk;
 let __internal_clerk: HeadlessBrowserClerk | BrowserClerk | undefined;
 
-/**
- * Access or create a Clerk instance outside of React.
- * @example
- * import { ClerkProvider, getClerkInstance } from "@clerk/expo"
- *
- * const clerkInstance = getClerkInstance({ publishableKey: 'xxxx' })
- *
- * // Always pass the `publishableKey` to `ClerkProvider`
- * <ClerkProvider publishableKey={'xxxx'}>
- *     ...
- * </ClerkProvider>
- *
- * // Somewhere in your code, outside of React you can do
- * const token = await clerkInstance.session?.getToken();
- * fetch('http://example.com/', {headers: {Authorization: token })
- * @throws MissingPublishableKeyError publishableKey is missing and Clerk has not been initialized yet
- * @returns HeadlessBrowserClerk
- */
 export function createClerkInstance(ClerkClass: typeof Clerk) {
   return (options?: BuildClerkOptions): HeadlessBrowserClerk | BrowserClerk => {
     const {
