3414e6fe533e80682f5d393ec508f89db3ab7b44
Robert Soriano <sorianorobertc@gmail.com>
2024-07-29 11:36:56 -0700

    chore(clerk-js): Update organization ID utility function description
    Co-authored-by: Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>

--- packages/clerk-js/src/utils/organization.ts
+++ packages/clerk-js/src/utils/organization.ts
@@ -1,5 +1,7 @@
 /**
- * Checks and assumes a string is an organization ID if it starts with 'org_'.
+ * Checks and assumes a string is an organization ID if it starts with 'org_', specifically for
+ disambiguating with slugs. `_` is a disallowed character in slug names, so slugs cannot 
+ start with `org_`.
  */
 export function isOrganizationId(id: string) {
   return id.startsWith('org_');
