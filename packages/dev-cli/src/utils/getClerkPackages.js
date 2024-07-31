aa64eb69e98e09e5563ddf76dda28bc947724609
Bryce Kalow <bryce@clerk.dev>
2024-07-31 14:03:22 -0500

    Fix monorepo root detection for installed package case

--- packages/dev-cli/src/utils/getClerkPackages.js
+++ packages/dev-cli/src/utils/getClerkPackages.js
@@ -1,14 +1,16 @@
 import { readFile } from 'node:fs/promises';
-import { dirname, join, posix, resolve } from 'node:path';
+import { dirname, posix } from 'node:path';
 
 import { globby } from 'globby';
+
+import { getMonorepoRoot } from './getMonorepoRoot.js';
 
 /**
  * Generates an object with keys of package names and values of absolute paths to the package folder.
  * @returns {Promise<Record<string, string>>}
  */
 export async function getClerkPackages() {
-  const monorepoRoot = resolve(join(import.meta.dirname, '..', '..', '..', '..'));
+  const monorepoRoot = await getMonorepoRoot();
   /** @type {Record<string, string>} */
   const packages = {};
   const clerkPackages = await globby([posix.join(monorepoRoot, 'packages', '*', 'package.json'), '!*node_modules*']);
