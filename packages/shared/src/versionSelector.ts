883e7341266edca36d68ea278c3384d2d474b1b1
wobsoriano <sorianorobertc@gmail.com>
2024-07-30 17:24:59 -0700

    chore(shared): Improve version selector explanation

--- packages/shared/src/versionSelector.ts
+++ packages/shared/src/versionSelector.ts
@@ -1,9 +1,9 @@
 /**
  * This version selector is a bit complicated, so here is the flow:
  * 1. Use the clerkJSVersion prop on the provider
- * 2. Use the exact `@clerk/clerk-js` version if tag is snapshot
- * 3. Use the prerelease tag of `@clerk/clerk-js`
- * 4. Fallback to the major version of `@clerk/clerk-js`
+ * 2. Use the exact `@clerk/clerk-js` version if it is a `@snapshot` prerelease
+ * 3. Use the prerelease tag of `@clerk/clerk-js` or the package version provided
+ * 4. Fallback to the major version of `@clerk/clerk-js` or the package version provided
  * @param clerkJSVersion - The optional clerkJSVersion prop on the provider
  * @param packageVersion - The version of `@clerk/clerk-js` that will be used if an explicit version is not provided
  * @returns The npm tag, version or major version to use
