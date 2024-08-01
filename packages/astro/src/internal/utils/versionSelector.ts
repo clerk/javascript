af076d391dcd20001cd066daf4538a395577b42c
wobsoriano <sorianorobertc@gmail.com>
2024-07-31 17:07:09 -0700

    chore(astro): Remove unused file

--- packages/astro/src/internal/utils/versionSelector.ts
+++
@@ -1,9 +0,0 @@
-const HARDCODED_LATEST_CLERK_JS_VERSION = '5';
-
-export const versionSelector = (clerkJSVersion: string | undefined): string => {
-  if (clerkJSVersion) {
-    return clerkJSVersion;
-  }
-
-  return HARDCODED_LATEST_CLERK_JS_VERSION;
-};
