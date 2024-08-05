943fa2223d0b57fcf7d736afc8a0a40a0cc4b141
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-08-05 11:54:00 -0400

    feat(ui): Add support for `socialButtonsVariant` prop

--- packages/ui/src/common/connections.tsx
+++ packages/ui/src/common/connections.tsx
@@ -1,13 +1,16 @@
 import * as Common from '@clerk/elements/common';
 
 import { PROVIDERS } from '~/constants/providers';
+import { useAppearance } from '~/hooks/use-appearance';
 import { useEnabledConnections } from '~/hooks/use-enabled-connections';
 import * as Connection from '~/primitives/connection';
 import * as Icon from '~/primitives/icon';
 
 export function Connections(props: { disabled?: boolean }) {
   const enabledConnections = useEnabledConnections();
+  const { layout } = useAppearance();
   const hasConnection = enabledConnections.length > 0;
+  const textVisuallyHidden = enabledConnections.length > 2 || layout?.socialButtonsVariant === 'iconButton';
 
   return hasConnection ? (
     <Connection.Root>
@@ -30,7 +33,7 @@ export function Connections(props: { disabled?: boolean }) {
                     busy={isConnectionLoading}
                     disabled={props?.disabled || isConnectionLoading}
                     icon={IconComponent ? <IconComponent className='text-base' /> : null}
-                    textVisuallyHidden={enabledConnections.length > 2}
+                    textVisuallyHidden={textVisuallyHidden}
                   >
                     {connection?.name || c.provider}
                   </Connection.Button>
