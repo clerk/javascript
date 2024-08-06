07763245c771a9b383913fba3fc15e2d7ac754d5
Joe Bell <7349341+joe-bell@users.noreply.github.com>
2024-08-06 11:05:36 +0300

    remove connection primitive, extend upon button

--- packages/ui/src/primitives/button.tsx
+++ packages/ui/src/primitives/button.tsx
@@ -1,5 +1,5 @@
 import type { VariantProps } from 'cva';
-import { cva } from 'cva';
+import { cva, cx } from 'cva';
 import * as React from 'react';
 
 import { Spinner } from './spinner';
@@ -10,6 +10,7 @@ const button = cva({
     'appearance-none relative isolate select-none',
     'text-base font-medium',
     'px-[calc(theme(spacing.3)-var(--button-border-width))] py-[calc(theme(spacing[1.5])-var(--button-border-width))]',
+    'min-h-[1.875rem]',
     'inline-flex w-full items-center justify-center gap-3',
     'border-[length:--button-border-width] rounded-md bg-clip-padding',
     'outline-none focus-visible:ring',
@@ -18,12 +19,16 @@ const button = cva({
   variants: {
     intent: {
       primary: [
+        '[--button-icon-color:currentColor]',
+        '[--button-icon-opacity:0.6]',
         'text-accent-contrast bg-accent-9 border-accent-9 shadow-[0_1px_1px_0_theme(colors.white/.07)_inset]',
         'before:absolute before:inset-0 before:rounded-[calc(theme(borderRadius.md)-1px)] before:shadow-[0_1px_1px_0_theme(colors.white/.07)_inset]',
         'after:pointer-events-none after:absolute after:inset-0 after:-z-10 after:rounded-[calc(theme(borderRadius.md)-1px)] after:bg-gradient-to-b after:from-white/10 after:to-transparent',
         'focus-visible:ring-accent-a7',
       ],
       secondary: [
+        '[--button-icon-color:theme(colors.gray.12)]',
+        '[--button-icon-opacity:1]',
         'text-gray-12 border-gray-a6 bg-gray-surface shadow-sm shadow-gray-a3',
         'focus-visible:border-gray-a8 focus-visible:ring-accent-a3',
       ],
@@ -61,13 +66,15 @@ export const Button = React.forwardRef(function Button(
     intent,
     type = 'button',
     spinnerWhenBusy,
+    textVisuallyHidden,
     ...props
   }: React.ButtonHTMLAttributes<HTMLButtonElement> &
     // Omit types supplied via ButtonHTMLAttributes
     Omit<VariantProps<typeof button>, 'disabled'> & {
-      spinnerWhenBusy?: boolean;
       iconStart?: React.ReactNode;
       iconEnd?: React.ReactNode;
+      spinnerWhenBusy?: boolean;
+      textVisuallyHidden?: boolean;
     },
   forwardedRef: React.ForwardedRef<HTMLButtonElement>,
 ) {
@@ -85,9 +92,23 @@ export const Button = React.forwardRef(function Button(
         <Spinner className='shrink-0 text-[1.125rem]'>Loading…</Spinner>
       ) : (
         <>
-          {iconStart ? <span className='shrink-0 text-inherit opacity-60'>{iconStart}</span> : null}
-          {children ? <span className='truncate'>{children}</span> : null}
-          {iconEnd ? <span className='shrink-0 text-inherit opacity-60'>{iconEnd}</span> : null}
+          {iconStart ? (
+            <span
+              data-button-icon=''
+              className='shrink-0 text-[--button-icon-color] opacity-[--button-icon-opacity]'
+            >
+              {iconStart}
+            </span>
+          ) : null}
+          {children ? <span className={cx('truncate', textVisuallyHidden && 'sr-only')}>{children}</span> : null}
+          {iconEnd ? (
+            <span
+              data-button-icon=''
+              className='shrink-0 text-[--button-icon-color] opacity-[--button-icon-opacity]'
+            >
+              {iconEnd}
+            </span>
+          ) : null}
         </>
       )}
     </button>
