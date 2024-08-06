7e1f0d865c36c6ed1889febd2bc33f4f47781dba
wobsoriano <sorianorobertc@gmail.com>
2024-08-06 13:47:21 -0700

    test(astro): Add initial CSR e2e test

---
+++ integration/tests/astro/static.test.ts
@@ -0,0 +1,47 @@
+import { expect, test } from '@playwright/test';
+
+import type { FakeOrganization, FakeUser } from '../../testUtils';
+import { createTestUtils, testAgainstRunningApps } from '../../testUtils';
+
+testAgainstRunningApps({ withPattern: ['astro.static.withCustomRoles'] })(
+  'basic flows for @astro static output',
+  ({ app }) => {
+    test.describe.configure({ mode: 'serial' });
+
+    let fakeAdmin: FakeUser;
+    let fakeOrganization: FakeOrganization;
+    let fakeAdmin2: FakeUser;
+    let fakeOrganization2: FakeOrganization;
+
+    test.beforeAll(async () => {
+      const m = createTestUtils({ app });
+      fakeAdmin = m.services.users.createFakeUser();
+      const admin = await m.services.users.createBapiUser(fakeAdmin);
+      fakeOrganization = await m.services.users.createFakeOrganization(admin.id);
+
+      fakeAdmin2 = m.services.users.createFakeUser();
+      const admin2 = await m.services.users.createBapiUser(fakeAdmin2);
+      fakeOrganization2 = await m.services.users.createFakeOrganization(admin2.id);
+    });
+
+    test.afterAll(async () => {
+      await fakeOrganization.delete();
+      await fakeAdmin.deleteIfExists();
+
+      await fakeOrganization2.delete();
+      await fakeAdmin2.deleteIfExists();
+      await app.teardown();
+    });
+
+    test('Clerk client loads on first visit and Sign In button renders', async ({ page, context }) => {
+      const u = createTestUtils({ app, page, context });
+      await u.page.goToAppHome();
+
+      await u.page.waitForClerkJsLoaded();
+
+      await u.po.expect.toBeSignedOut();
+
+      await expect(u.page.getByRole('link', { name: /Login/i })).toBeVisible();
+    });
+  },
+);
