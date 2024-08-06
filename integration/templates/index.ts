604bec8841efd3d77e63ae53cc692f75d97e1c91
wobsoriano <sorianorobertc@gmail.com>
2024-08-06 13:53:09 -0700

    test(astro): Add astro-static to template map

--- integration/templates/index.ts
+++ integration/templates/index.ts
@@ -12,6 +12,7 @@ export const templates = {
   'remix-node': resolve(__dirname, './remix-node'),
   'elements-next': resolve(__dirname, './elements-next'),
   'astro-node': resolve(__dirname, './astro-node'),
+  'astro-static': resolve(__dirname, './astro-static'),
   'expo-web': resolve(__dirname, './expo-web'),
 } as const;
 
