29e23ed71df77274d82c12589ba6e7dbe15a0d87
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-08-19 14:08:13 -0400

    revert signin override

--- packages/ui/theme-builder/app/sign-in/[[...sign-in]]/page.tsx
+++ packages/ui/theme-builder/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,13 +2,5 @@
 import { SignIn } from '@clerk/ui/sign-in';
 
 export default function Page() {
-  return (
-    <SignIn
-      appearance={{
-        layout: {
-          logoImageUrl: '/clerk-logo-dark-accent.svg',
-        },
-      }}
-    />
-  );
+  return <SignIn />;
 }
