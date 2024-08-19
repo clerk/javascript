5083de9c9869a5892421a8d21815c55a75ca9404
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-08-19 15:45:52 -0400

    add comment

--- packages/ui/src/hooks/use-card.ts
+++ packages/ui/src/hooks/use-card.ts
@@ -2,6 +2,41 @@ import { useAppearance } from '~/contexts';
 
 import { useDisplayConfig } from './use-display-config';
 
+/**
+ * Access card rendering values for the `<Card.Logo />` and `<Card.Footer />`
+ * based on the display config and parsed appearance.
+ *
+ * @example
+ * ```tsx
+ * import { useCard } from './use-card';
+ *
+ * function MyCardComponent() {
+ *   const { logoProps, footerProps } = useCard();
+ *
+ *   return (
+ *     <Card>
+ *       <Card.Logo {...logoProps} />
+ *       <Card.Footer {...footerProps} />
+ *     </Card>
+ *   );
+ * }
+ * ```
+ *
+ * @returns {{
+ *   logoProps: {
+ *     href: string;
+ *     src: string;
+ *     alt: string;
+ *   };
+ *   footerProps: {
+ *     branded: boolean;
+ *     helpPageUrl: string | undefined;
+ *     privacyPageUrl: string | undefined;
+ *     termsPageUrl: string | undefined;
+ *   };
+ * }}
+ */
+
 export function useCard() {
   const { layout } = useAppearance().parsedAppearance;
   const { applicationName, branded, logoImageUrl, homeUrl } = useDisplayConfig();
