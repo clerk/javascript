fa723e4eaae823b6b50f0f15b506f27465630d8b
wobsoriano <sorianorobertc@gmail.com>
2024-08-21 09:07:29 -0700

    chore(astro): Add JSDoc to vite plugin

--- packages/astro/src/integration/vite-plugin-astro-config.ts
+++ packages/astro/src/integration/vite-plugin-astro-config.ts
@@ -2,6 +2,13 @@ import type { AstroConfig } from 'astro';
 
 type VitePlugin = Required<AstroConfig['vite']>['plugins'][number];
 
+/**
+ * This Vite module exports a `isStaticOutput` function that is imported inside our control components
+ * to determine which components to use depending on the Astro config output option.
+ *
+ * @param {AstroConfig} astroConfig - The Astro configuration object
+ * @returns {VitePlugin} A Vite plugin
+ */
 export function vitePluginAstroConfig(astroConfig: AstroConfig): VitePlugin {
   const virtualModuleId = 'virtual:@clerk/astro/config';
   const resolvedVirtualModuleId = '\0' + virtualModuleId;
