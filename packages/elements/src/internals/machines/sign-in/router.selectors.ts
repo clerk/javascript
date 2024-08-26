644a32c90d1b199d7947208302d46830d43f8221
Tom Milewski <me@tm.codes>
2024-08-26 16:03:02 -0400

    chore(elements): Remove commented code

--- packages/elements/src/internals/machines/sign-in/router.selectors.ts
+++ packages/elements/src/internals/machines/sign-in/router.selectors.ts
@@ -3,32 +3,6 @@ import { formatSalutation } from '~/internals/machines/utils/formatters';
 
 import type { SignInRouterSnapshot } from './router.types';
 
-// export function SignInSafeIdentifierSelectorForStrategy(
-//   strategy: SignInStrategyName | undefined,
-// ): (s: SignInRouterSnapshot) => string {
-//   return (s: SignInRouterSnapshot) => {
-//     const signIn = s.context.clerk?.client.signIn;
-
-//     if (strategy) {
-//       const matchingFactors = [
-//         ...(signIn.supportedFirstFactors ?? []),
-//         ...(signIn.supportedSecondFactors ?? []),
-//       ].filter(f => f.strategy === strategy);
-
-//       const matchingFactor =
-//         signIn.identifier && matchingFactors.length > 0
-//           ? matchingFactors.find(f => 'safeIdentifier' in f && f.safeIdentifier === signIn.identifier)
-//           : matchingFactors[0];
-
-//       if (matchingFactor && 'safeIdentifier' in matchingFactor) {
-//         return matchingFactor.safeIdentifier;
-//       }
-//     }
-
-//     return signIn.identifier || '';
-//   };
-// }
-
 export function SignInSafeIdentifierSelectorForStrategy(
   strategy: SignInStrategyName | undefined,
 ): (s: SignInRouterSnapshot) => string {
