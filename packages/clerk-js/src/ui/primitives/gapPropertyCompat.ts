8a0580813e4787b93404185576a7c0df8c43df66
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-08-27 17:02:16 -0400

    bring back comment

--- packages/clerk-js/src/ui/primitives/gapPropertyCompat.ts
+++ packages/clerk-js/src/ui/primitives/gapPropertyCompat.ts
@@ -1,4 +1,6 @@
 export const createFlexGapPropertyIosCompat = (val: string, dir: 'col' | 'row') => {
+  // Resolve gap property compatibility issue on iOS 13
+  // This is not needed for css grid, only for flex
   if (navigator?.userAgent?.match(/(iphone|ipad).+(os).*(\s13).+safari/i)) {
     return {
       '& > *:not([hidden]):not([style*="visibility: hidden"]) + *:not([hidden]):not([style*="visibility: hidden"])': {
