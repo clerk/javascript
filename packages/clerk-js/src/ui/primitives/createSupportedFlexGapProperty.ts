a6bf229cd6d5deca4b5c75b2a4c34a3301731ef1
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-08-27 14:23:44 -0400

    update to selector supports

--- packages/clerk-js/src/ui/primitives/createSupportedFlexGapProperty.ts
+++ packages/clerk-js/src/ui/primitives/createSupportedFlexGapProperty.ts
@@ -1,9 +1,14 @@
+/**
+ * @supports selector() was released roughly at the same point as flexbox gap,
+ * so we use that to check support since flexbox gap check is not supported.
+ * @see https://birdsong.dev/blog/sloppy-supports-for-flexbox-gap/
+ */
 export const createSupportedFlexGapProperty = (val: string, dir: 'col' | 'row') => {
   return {
-    '@supports (gap: 1rem)': {
+    '@supports selector(:first-child)': {
       gap: val,
     },
-    '@supports not (gap: 1rem)': {
+    '@supports not selector(:first-child)': {
       '& > *:not([hidden]):not([style*="visibility: hidden"]) + *:not([hidden]):not([style*="visibility: hidden"])': {
         marginLeft: dir === 'row' ? val : undefined,
         marginTop: dir === 'col' ? val : undefined,
