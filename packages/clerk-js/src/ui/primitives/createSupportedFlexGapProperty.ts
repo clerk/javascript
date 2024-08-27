c07abbb50903b40b16adec785f4d028e8cf1945b
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-08-27 16:52:10 -0400

    fix navigator check

--- packages/clerk-js/src/ui/primitives/createSupportedFlexGapProperty.ts
+++ packages/clerk-js/src/ui/primitives/createSupportedFlexGapProperty.ts
@@ -1,18 +1,11 @@
-/**
- * @supports selector() was released roughly at the same point as flexbox gap,
- * so we use that to check support since flexbox gap check is not supported.
- * @see https://birdsong.dev/blog/sloppy-supports-for-flexbox-gap/
- */
 export const createSupportedFlexGapProperty = (val: string, dir: 'col' | 'row') => {
-  return {
-    '@supports selector(:first-child)': {
-      gap: val,
-    },
-    '@supports not selector(:first-child)': {
+  if (navigator?.userAgent?.match(/(iphone|ipad).+(os).*(\s13).+safari/i)) {
+    return {
       '& > *:not([hidden]):not([style*="visibility: hidden"]) + *:not([hidden]):not([style*="visibility: hidden"])': {
         marginLeft: dir === 'row' ? val : undefined,
         marginTop: dir === 'col' ? val : undefined,
       },
-    },
-  };
+    };
+  }
+  return { gap: val };
 };
