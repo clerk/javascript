4453bb942e6a30d76b79f03421b07b661e5d7196
panteliselef <panteliselef@outlook.com>
2024-09-02 18:48:42 +0300

    chore(e2e): WIP

--- integration/tests/protect.test.ts
+++ integration/tests/protect.test.ts
@@ -114,10 +114,12 @@ testAgainstRunningApps({ withEnv: [appConfigs.envs.withCustomRoles] })('authoriz
     await u.page.goToRelative('/settings/auth-protect');
     await expect(u.page.getByText(/this page could not be found/i)).toBeVisible();
 
-    // route handler
-    await u.page.goToRelative('/api/settings/');
+    // Route Handler
+    await u.page.goToRelative('/api/settings/').catch(() => {});
 
     // Result of 404 response with empty body
-    expect(await u.page.content()).toEqual('<html><head></head><body></body></html>');
+    expect(await u.page.content()).toMatch(/^(<!DOCTYPE html>)/);
+    expect(await u.page.content()).toMatch(/(No web page was found)/);
+    expect(await u.page.content()).toMatch(/(HTTP ERROR 404)/);
   });
 });
