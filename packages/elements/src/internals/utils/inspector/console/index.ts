95ac67a143c263bef0c1f589728566ab8f95768d
Bryce Kalow <bryce@clerk.dev>
2024-09-04 15:35:28 -0500

    feat(elements): Remove direct reliance on next and clerk-react (#4064)

--- packages/elements/src/internals/utils/inspector/console/index.ts
+++ packages/elements/src/internals/utils/inspector/console/index.ts
@@ -1,16 +1,20 @@
 import { isTruthy } from '@clerk/shared/underscore';
 
+import { safeAccess } from '~/utils/safe-access';
+
 import { createConsoleInspector } from './console';
 
 export function getInspector() {
   if (
     __DEV__ &&
     process.env.NODE_ENV === 'development' &&
-    isTruthy(process.env.NEXT_PUBLIC_CLERK_ELEMENTS_DEBUG ?? process.env.CLERK_ELEMENTS_DEBUG)
+    isTruthy(
+      safeAccess(() => process.env.NEXT_PUBLIC_CLERK_ELEMENTS_DEBUG_UI ?? process.env.CLERK_ELEMENTS_DEBUG_UI, false),
+    )
   ) {
     return createConsoleInspector({
       enabled: true,
-      debugServer: isTruthy(process.env.CLERK_ELEMENTS_DEBUG_SERVER),
+      debugServer: isTruthy(safeAccess(() => process.env.CLERK_ELEMENTS_DEBUG_SERVER, false)),
     });
   }
   return undefined;
