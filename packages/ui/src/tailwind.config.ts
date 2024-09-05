af84fe75d4603253fdda97b77f69d02eae964991
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-09-05 15:01:19 -0400

    chore(ui): Add ios-support conditions for inputs to prevent zoom

--- packages/ui/src/tailwind.config.ts
+++ packages/ui/src/tailwind.config.ts
@@ -115,7 +115,7 @@ const config = {
     },
   },
   plugins: [
-    plugin(function ({ addBase, theme }) {
+    plugin(function ({ addBase, addVariant, theme }) {
       /**
        * Extends Tailwind's built-in `addBase` function by scoping styles to
        * only affect components within the `@clerk/ui` package.
@@ -469,6 +469,11 @@ const config = {
           fontFamily: 'var(--cl-font-family)',
         },
       });
+
+      /**
+       * Conditionally apply CSS to ios devices
+       */
+      addVariant('supports-ios', '@supports (-webkit-touch-callout: none)');
     }),
   ],
 } satisfies Config;
