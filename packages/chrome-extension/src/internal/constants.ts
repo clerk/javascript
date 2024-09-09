06fa37e13922e09f6d786c3daf8e8e64926229ea
Tom Milewski <me@tm.codes>
2024-09-09 15:11:40 -0400

    chore(chrome-extension): Interim commit

--- packages/chrome-extension/src/internal/constants.ts
+++ packages/chrome-extension/src/internal/constants.ts
@@ -1,6 +1,5 @@
 export const CLIENT_JWT_KEY = '__client';
 export const STORAGE_KEY_CLIENT_JWT = '__clerk_client_jwt';
-export const VALID_HOST_PERMISSION_REGEX = /(https?:\/\/[\w.-]+)/;
 export const DEFAULT_LOCAL_HOST_PERMISSION = 'http://localhost';
 export const AUTH_HEADER = {
   production: 'Authorization',
