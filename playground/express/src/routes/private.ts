88cd23769c31c13227d91927d8ee1308e5d1f36d
wobsoriano <sorianorobertc@gmail.com>
2024-09-13 13:10:57 -0700

    chore(express): Update playground to use correct error handlers

--- playground/express/src/routes/private.ts
+++ playground/express/src/routes/private.ts
@@ -1,23 +1,36 @@
-import type { Response } from 'express';
+import type { NextFunction, Response } from 'express';
 
-import { getAuth, requireAuth } from '@clerk/express';
+import { getAuth, requireAuth, UnauthorizedError, ForbiddenError } from '@clerk/express';
 import { Router, Request as ExpressRequest } from 'express';
 
 const router = Router();
 
 router.use(requireAuth);
 
-router.get('/me', async (req: ExpressRequest, reply: Response) => {
-  return reply.json({ auth: getAuth(req) });
+router.get('/me', async (req: ExpressRequest, res: Response) => {
+  return res.json({ auth: getAuth(req) });
 });
 
-router.get('/private', async (req, reply: Response) => {
+router.get('/admin-only', async (req: ExpressRequest, res: Response, next: NextFunction) => {
   const auth = getAuth(req);
-  if (!auth.userId) {
-    return reply.status(403).end();
+
+  if (!auth.has({ role: 'admin' })) {
+    return next(new ForbiddenError());
   }
 
-  return reply.json({ hello: 'world', auth });
-});
+  return res.json({ message: 'Hello admin!', auth });
+})
+
+router.use((err: Error, _req: ExpressRequest, res: Response, next: NextFunction) => {
+  if (err instanceof UnauthorizedError) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  if (err instanceof ForbiddenError) {
+    return res.status(403).json({ error: 'Forbidden' });
+  }
+
+  return next(err)
+})
 
 export const privateRoutes = router;
