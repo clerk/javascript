c7faf610753eb2f0862973482fd64f4b6e96384f
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-09-16 13:35:32 -0400

    refactor(ui): Add `<Separator />` descriptors (#4165)

--- packages/ui/src/primitives/separator.tsx
+++ packages/ui/src/primitives/separator.tsx
@@ -1,19 +1,38 @@
-import { cx } from 'cva';
 import * as React from 'react';
 
-export const Separator = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLParagraphElement>>(
-  function Separator({ children, className, ...props }, forwardedRef) {
-    return (
-      <p
-        ref={forwardedRef}
-        {...props}
-        className={cx(
-          'before:bg-gray-a4 after:bg-gray-a4 text-gray-a11 flex items-center gap-x-4 text-base before:h-px before:flex-1 after:h-px after:flex-1',
-          className,
-        )}
-      >
-        {children}
-      </p>
-    );
+import {
+  mergeDescriptors,
+  type ParsedDescriptor,
+  type ParsedElementsFragment,
+  useAppearance,
+} from '~/contexts/AppearanceContext';
+
+export const layoutStyle = {
+  separator: {
+    className: 'flex items-center gap-x-4 before:h-px before:flex-1 after:h-px after:flex-1',
   },
-);
+} satisfies ParsedElementsFragment;
+
+export const visualStyle = {
+  separator: {
+    className: 'before:bg-gray-a4 after:bg-gray-a4 text-gray-a11 text-base ',
+  },
+} satisfies ParsedElementsFragment;
+
+export const Separator = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & {
+    descriptors?: ParsedDescriptor[];
+  }
+>(function Separator({ children, descriptors, ...props }, forwardedRef) {
+  const { elements } = useAppearance().parsedAppearance;
+  return (
+    <div
+      ref={forwardedRef}
+      {...props}
+      {...mergeDescriptors(elements.separator, ...(descriptors ?? []))}
+    >
+      {children}
+    </div>
+  );
+});
