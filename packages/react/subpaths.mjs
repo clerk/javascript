3d58b06759a016103a6b796bf844b649911db589
Lennart <lekoarts@gmail.com>
2024-09-17 16:04:22 +0200

    chore(repo): QoL for subpaths script (#4180)

--- packages/react/subpaths.mjs
+++ packages/react/subpaths.mjs
@@ -1,10 +1,13 @@
-import packageJson from './package.json' assert { type: 'json' };
-// This file is a helper for the "subpath-workaround.mjs" script
-// We have to polyfill our "exports" subpaths :cry:
+import packageJson from './package.json' with { type: 'json' };
 
-export const subpathNames = Object.keys(packageJson.exports)
-  .filter(k => k.startsWith('./') && k !== './' && k !== './package.json')
-  .map(k => k.replace('./', ''));
+/**
+ * This file is a helper for the "subpath-workaround.mjs" script.
+ * Add your new subpath to package.json#files.
+ *
+ * When you add an entry to the package.json "files" field, a subfolder will be automatically created with a package.json pointing to that file
+ */
+
+export const subpathNames = packageJson.files.filter(k => k !== 'dist');
 
 export const subpathFoldersBarrel = [];
 
