a5da675026b0b67270a0603a735d4329e048dbbb
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-09-18 15:12:45 -0400

    update comment

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -18,7 +18,7 @@ export const STAGING_API_URL = 'https://api.clerkstage.dev';
 export const PROD_API_URL = 'https://api.clerk.com';
 
 /**
- * Returns the URL for a static SVG image
+ * Returns the URL for a static image
  * using the new img.clerk.com service
  */
 export function iconImageUrl(id: string, format: 'svg' | 'jpeg' = 'svg'): string {
