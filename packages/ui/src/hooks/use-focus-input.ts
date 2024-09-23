34927f344097ce8a0af0a7abfe7168912e5e0bb3
Alex Carpenter <im.alexcarpenter@gmail.com>
2024-09-23 14:25:08 -0400

    wrap in useCallback

--- packages/ui/src/hooks/use-focus-input.ts
+++ packages/ui/src/hooks/use-focus-input.ts
@@ -19,12 +19,12 @@ import * as React from 'react';
 
 export function useFocusInput(): [React.RefObject<HTMLInputElement>, () => void] {
   const ref = React.useRef<HTMLInputElement>(null);
-  const setFocus = () => {
+  const setFocus = React.useCallback(() => {
     requestAnimationFrame(() => {
       if (ref.current) {
         ref.current.focus();
       }
     });
-  };
+  }, [ref]);
   return [ref, setFocus];
 }
