7d2cdb514db37206e4edb0c4b079fa9608ab5116
wobsoriano <sorianorobertc@gmail.com>
2024-09-26 14:57:19 -0700

    chore(express): Remove unused error classes

--- packages/express/src/index.ts
+++ packages/express/src/index.ts
@@ -5,4 +5,4 @@ export { clerkClient } from './clerkClient';
 export type { ExpressRequestWithAuth } from './types';
 export { clerkMiddleware } from './clerkMiddleware';
 export { getAuth } from './getAuth';
-export { requireAuth, ForbiddenError, UnauthorizedError } from './requireAuth';
+export { requireAuth } from './requireAuth';
--- packages/express/src/requireAuth.ts
+++ packages/express/src/requireAuth.ts
@@ -4,64 +4,6 @@ import { authenticateAndDecorateRequest } from './authenticateRequest';
 import type { ClerkMiddlewareOptions } from './types';
 
 /**
- * This error is typically thrown by the `requireAuth` middleware when
- * a request is made to a protected route without proper authentication.
- *
- * @class
- * @extends Error
- *
- * @example
- * // This error is usually handled in an Express error handling middleware
- * app.use((err, req, res, next) => {
- *   if (err instanceof UnauthorizedError) {
- *     res.status(401).send('Unauthorized');
- *   } else {
- *     next(err);
- *   }
- * });
- */
-export class UnauthorizedError extends Error {
-  constructor() {
-    super('Unauthorized');
-    this.name = 'UnauthorizedError';
-  }
-}
-
-/**
- * This error is typically used when a user is authenticated but lacks the necessary permissions
- * to access a resource or perform an action.
- *
- * @class
- * @extends Error
- *
- * @example
- * // This error can be used in custom authorization middleware
- * const checkPermission = (req, res, next) => {
- *   const auth = getAuth(req)
- *   if (!auth.has({ permission: 'permission' })) {
- *     return next(new ForbiddenError());
- *   }
- *   next();
- * };
- *
- * @example
- * // This error is usually handled in an Express error handling middleware
- * app.use((err, req, res, next) => {
- *   if (err instanceof ForbiddenError) {
- *     res.status(403).send('Forbidden');
- *   } else {
- *     next(err);
- *   }
- * });
- */
-export class ForbiddenError extends Error {
-  constructor() {
-    super('Forbidden');
-    this.name = 'ForbiddenError';
-  }
-}
-
-/**
  * Middleware to require authentication for user requests.
  * Redirects unauthenticated requests to the sign-in page.
  *
