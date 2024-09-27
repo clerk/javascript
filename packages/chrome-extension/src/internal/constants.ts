4f2da7e1803caa40f6378c7245cc50880ebdbeff
Tom Milewski <me@tm.codes>
2024-09-27 14:29:04 -0400

    fix(chrome-extention): update req/res handlers

--- packages/chrome-extension/src/internal/constants.ts
+++ packages/chrome-extension/src/internal/constants.ts
@@ -2,3 +2,5 @@ export const CLIENT_JWT_KEY = '__client';
 export const STORAGE_KEY_CLIENT_JWT = '__clerk_client_jwt';
 export const DEFAULT_LOCAL_HOST_PERMISSION = 'http://localhost';
 export const AUTH_HEADER = 'Authorization';
+export const AUTH_HEADER_DEV = '__clerk_db_jwt';
+export const SESSION_ID_HEADER_DEV = '_clerk_session_id';
--- packages/chrome-extension/src/internal/utils/request-handler.ts
+++ packages/chrome-extension/src/internal/utils/request-handler.ts
@@ -4,25 +4,39 @@ import { AUTH_HEADER } from '../constants';
 import type { JWTHandler } from './jwt-handler';
 
 type Handler = Parameters<Clerk['__unstable__onBeforeRequest']>[0];
+type Req = Parameters<Handler>[0];
 
 /** Save the appropriate JWT from the response to storage */
-export function requestHandler(jwtHandler: JWTHandler) {
+export function requestHandler(jwtHandler: JWTHandler, { isProd }: { isProd: boolean }) {
   const handler: Handler = async requestInit => {
     requestInit.credentials = 'omit';
 
     const currentJWT = await jwtHandler.get();
 
     if (!currentJWT) {
-      requestInit.url?.searchParams.append('_is_native', '1');
-      return;
+      unauthenticatedHandler(requestInit);
+    } else if (isProd) {
+      prodHandler(requestInit, currentJWT);
+      console.log('requestHandler (authHeader):', AUTH_HEADER, `Bearer ${currentJWT}`);
+    } else {
+      devHandler(requestInit, currentJWT);
     }
 
-    requestInit.url?.searchParams.append('_is_native', '1');
-    (requestInit.headers as Headers).set(AUTH_HEADER, `Bearer ${currentJWT}`);
-
     console.log('requestHandler (searchParams):', requestInit.url?.searchParams.toString());
-    console.log('requestHandler (authHeader):', AUTH_HEADER, `Bearer ${currentJWT}`);
   };
 
   return handler;
+}
+
+function unauthenticatedHandler(requestInit: Req) {
+  requestInit.url?.searchParams.append('_is_native', '1');
+}
+
+function devHandler(requestInit: Req, jwt: string) {
+  requestInit.url?.searchParams.append('__clerk_db_jwt', jwt);
+}
+
+function prodHandler(requestInit: Req, jwt: string) {
+  requestInit.url?.searchParams.append('_is_native', '1');
+  (requestInit.headers as Headers).set(AUTH_HEADER, `Bearer ${jwt}`);
 }
