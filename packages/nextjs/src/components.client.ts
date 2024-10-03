be216defc82fc49676108608b1623ed213700cc8
panteliselef <panteliselef@outlook.com>
2024-10-03 12:29:49 +0300

    remove unused code

--- packages/nextjs/src/components.client.ts
+++ packages/nextjs/src/components.client.ts
@@ -1,8 +1,2 @@
 export { ClerkProvider } from './client-boundary/ClerkProvider';
-export {
-  SignedIn,
-  SignedOut,
-  Protect,
-  // __experimental_protect,
-  __experimental_protectComponent,
-} from './client-boundary/controlComponents';
+export { SignedIn, SignedOut, Protect, __experimental_protectComponent } from './client-boundary/controlComponents';
