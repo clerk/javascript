5c6391b2d8e23aedd471d997360390ede9a6834a
Dylan Staley <88163+dstaley@users.noreply.github.com>
2024-10-17 13:43:02 -0700

    feat(ui): Descriptor Variance Authority (#4343)
    Co-authored-by: Alex Carpenter <im.alexcarpenter@gmail.com>

--- packages/ui/src/themes/layout.ts
+++ packages/ui/src/themes/layout.ts
@@ -1,7 +1,15 @@
 import { layoutStyle as alertLayoutStyle } from '~/primitives/alert';
+import { layoutStyle as buttonStyle } from '~/primitives/button';
 import { layoutStyle as cardLayoutStyle } from '~/primitives/card';
 import { layoutStyle as separatorStyle } from '~/primitives/separator';
+import { layoutStyle as layoutSpinnerStyle } from '~/primitives/spinner';
 
 import { buildTheme } from './buildTheme';
 
-export const layoutTheme = buildTheme({ ...alertLayoutStyle, ...cardLayoutStyle, ...separatorStyle });
+export const layoutTheme = buildTheme({
+  ...alertLayoutStyle,
+  ...buttonStyle,
+  ...cardLayoutStyle,
+  ...separatorStyle,
+  ...layoutSpinnerStyle,
+});
