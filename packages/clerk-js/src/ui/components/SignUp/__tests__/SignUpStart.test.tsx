27dc15f4d6c38caa15be0d8959c72d93353065fa
Vaggelis Yfantis <me@octoper.me>
2024-10-17 20:19:35 +0300

    tests(clerk-js): Fix SignUpContinue tests

--- packages/clerk-js/src/ui/components/SignUp/__tests__/SignUpStart.test.tsx
+++ packages/clerk-js/src/ui/components/SignUp/__tests__/SignUpStart.test.tsx
@@ -259,6 +259,7 @@ describe('SignUpStart', () => {
   describe('Legal consent', () => {
     it('shows sign up component with legal consent checkbox', async () => {
       const { wrapper } = await createFixtures(f => {
+        f.withLegalConsent();
         f.withTermsPrivacyPolicyUrls({
           privacyPolicy: 'https://clerk.dev/privacy',
           termsOfService: 'https://clerk.dev/tos',
