cf8fe36f786cb1e36e36eae86847f89a4972f410
Alexandros Ntitoras <a.ntitoras@gmail.com>
2024-10-17 18:05:21 +0300

    feat(expo): Allow clerk provider to receive passkey functions as props

--- packages/expo/src/provider/singleton/types.ts
+++ packages/expo/src/provider/singleton/types.ts
@@ -1,6 +1,23 @@
+import type {
+  CredentialReturn,
+  PublicKeyCredentialCreationOptionsWithoutExtensions,
+  PublicKeyCredentialRequestOptionsWithoutExtensions,
+  PublicKeyCredentialWithAuthenticatorAssertionResponse,
+  PublicKeyCredentialWithAuthenticatorAttestationResponse,
+} from '@clerk/types';
+
 import type { TokenCache } from '../../cache/types';
 
 export type BuildClerkOptions = {
   publishableKey?: string;
   tokenCache?: TokenCache;
+  passkeysFunc?: {
+    get: (
+      publicKeyCredential: PublicKeyCredentialRequestOptionsWithoutExtensions,
+    ) => Promise<CredentialReturn<PublicKeyCredentialWithAuthenticatorAssertionResponse>>;
+    create: (
+      publicKeyCredential: PublicKeyCredentialCreationOptionsWithoutExtensions,
+    ) => Promise<CredentialReturn<PublicKeyCredentialWithAuthenticatorAttestationResponse>>;
+    isSupported: () => boolean;
+  };
 };
