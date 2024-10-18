dba4becfaff2a31508fc1c441ef7554453e3f9eb
Dylan Staley <88163+dstaley@users.noreply.github.com>
2024-10-18 11:21:32 -0700

    feat(ui): Add descriptors

--- packages/ui/src/descriptors.ts
+++ packages/ui/src/descriptors.ts
@@ -1,5 +1,4 @@
-// type AlertDescriptorIdentifier = 'alert' | 'alert__error' | 'alert__warning' | 'alertRoot' | 'alertIcon';
-// type SeparatorDescriptorIdentifier = 'separator';
+import type { OAuthProvider, Web3Provider } from '@clerk/types';
 
 export const DESCRIPTORS = [
   // Alert
@@ -8,11 +7,57 @@ export const DESCRIPTORS = [
   'alert__warning',
   'alertIcon',
 
+  // Button
+  'button',
+  'buttonPrimary',
+  'buttonSecondary',
+  'buttonConnection',
+  'buttonPrimaryDefault',
+  'buttonSecondaryDefault',
+  'buttonConnectionDefault',
+  'buttonDisabled',
+  'buttonBusy',
+  'buttonText',
+  'buttonTextVisuallyHidden',
+  'buttonIcon',
+  'buttonIconStart',
+  'buttonIconEnd',
+  'buttonSpinner',
+
+  // Connection
+  'connectionList',
+  'connectionListItem',
+
   // Separator
   'separator',
+
+  // Spinner
+  'spinner',
+
+  // Card
+  'cardRoot',
+  'cardRootDefault',
+  'cardRootInner',
+  'cardHeader',
+  'cardContent',
+  'cardTitle',
+  'cardDescription',
+  'cardBody',
+  'cardActions',
+  'cardFooter',
+  'cardFooterAction',
+  'cardFooterActionText',
+  'cardFooterActionLink',
+  'cardFooterActionButton',
+  'cardFooterActionPageLink',
+  'cardLogoBox',
+  'cardLogoLink',
+  'cardLogoImage',
 ] as const;
+
+type Provider = OAuthProvider | Web3Provider;
 
 /**
  * Union of all valid descriptors used throughout the components.
  */
-export type DescriptorIdentifier = (typeof DESCRIPTORS)[number];
+export type DescriptorIdentifier = (typeof DESCRIPTORS)[number] | `buttonConnection__${Provider}`;
