290e375a4ae390a939a4e237716b720539959a7d
Dylan Staley <88163+dstaley@users.noreply.github.com>
2024-10-18 11:52:09 -0700

    fix(ui): Add missing descriptors when building theme (#4368)

--- packages/ui/src/themes/buildTheme.ts
+++ packages/ui/src/themes/buildTheme.ts
@@ -1,22 +1,53 @@
-import type { PartialTheme, ParsedElements } from '~/contexts/AppearanceContext';
+import type { ParsedElements, PartialTheme } from '~/contexts/AppearanceContext';
+import { DESCRIPTORS } from '~/descriptors';
 
 /**
- * Given a complete theme object minus descriptors, returns a full ParsedElements object with generated descriptors.
+ * Given an object containing partial descriptors, returns a full ParsedElements object with generated descriptors.
  */
 export function buildTheme(p: PartialTheme): ParsedElements {
   const theme: Partial<ParsedElements> = {};
+  const suppliedDescriptors = new Set<string>();
+
+  // Setup base theme containing empty objects for each descriptor.
+  DESCRIPTORS.forEach(descriptor => {
+    theme[descriptor] = {
+      descriptor: `cl-${descriptor}`,
+      className: '',
+      style: {},
+    };
+  });
 
   for (const descriptor in p) {
-    if (p[descriptor as keyof PartialTheme]) {
-      const { className, style } = p[descriptor as keyof PartialTheme]!;
-      theme[descriptor as keyof ParsedElements] = {
-        descriptor: `cl-${descriptor}`,
-        className: className ?? '',
-        style: style ?? {},
-      };
+    const key = descriptor as keyof ParsedElements;
+
+    if (p[key]) {
+      if (!(key in theme)) {
+        console.warn(`Clerk: Unknown descriptor: ${descriptor}`);
+        continue;
+      }
+
+      // These non-null assertions are okay since we confirmed that theme contains the descriptor above.
+      const { className, style } = p[key]!;
+      if (className) {
+        theme[key]!.className = className;
+      }
+
+      if (style) {
+        theme[key]!.style = style;
+      }
+
+      suppliedDescriptors.add(descriptor);
     }
   }
 
+  const missingDescriptors = DESCRIPTORS.filter(d => !suppliedDescriptors.has(d));
+  if (missingDescriptors.length > 0) {
+    console.warn(
+      `Clerk: Missing style configuration for the following descriptors:\n- ${missingDescriptors.join('\n- ')}`,
+    );
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
   return theme as ParsedElements;
 }
 
@@ -33,4 +64,21 @@ export function mergeTheme(a: ParsedElements, b: ParsedElements): ParsedElements
   }
 
   return mergedTheme;
+}
+
+if (import.meta.vitest) {
+  const { it, expect, describe } = import.meta.vitest;
+
+  describe('buildTheme', () => {
+    it('returns a theme containing all descriptors', () => {
+      const theme = buildTheme({});
+      expect(Object.keys(theme).sort()).toStrictEqual([...DESCRIPTORS].sort());
+
+      for (const [k, v] of Object.entries(theme)) {
+        expect(v.descriptor).toEqual(`cl-${k}`);
+        expect(v.className).toEqual('');
+        expect(v.style).toStrictEqual({});
+      }
+    });
+  });
 }
