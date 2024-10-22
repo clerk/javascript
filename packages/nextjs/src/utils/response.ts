1b32955ea663b9a133322aa930839a6d577ee5ff
Bryce Kalow <bryce@clerk.dev>
2024-10-21 23:59:52 -0500

    chore(nextjs)!: Remove deprecated nextjs APIs (#4356)
    Co-authored-by: Jacek Radko <jacekradko@gmail.com>

--- packages/nextjs/src/utils/response.ts
+++ packages/nextjs/src/utils/response.ts
@@ -1,43 +1,4 @@
-import { NextResponse } from 'next/server';
-
 import { constants as nextConstants } from '../constants';
-
-/**
- * A function that merges two Response objects into a single response.
- * The final response respects the body and the status of the last response,
- * but the cookies and headers of all responses are merged.
- */
-export const mergeResponses = (...responses: (NextResponse | Response | null | undefined | void)[]) => {
-  const normalisedResponses = responses.filter(Boolean).map(res => {
-    // If the response is a NextResponse, we can just return it
-    if (res instanceof NextResponse) {
-      return res;
-    }
-
-    return new NextResponse(res!.body, res!);
-  });
-
-  if (normalisedResponses.length === 0) {
-    return;
-  }
-
-  const lastResponse = normalisedResponses[normalisedResponses.length - 1];
-  const finalResponse = new NextResponse(lastResponse.body, lastResponse);
-
-  for (const response of normalisedResponses) {
-    response.headers.forEach((value: string, name: string) => {
-      finalResponse.headers.set(name, value);
-    });
-
-    response.cookies.getAll().forEach(cookie => {
-      const { name, value, ...options } = cookie;
-
-      finalResponse.cookies.set(name, value, options);
-    });
-  }
-
-  return finalResponse;
-};
 
 export const isRedirect = (res: Response) => {
   return res.headers.get(nextConstants.Headers.NextRedirect);
