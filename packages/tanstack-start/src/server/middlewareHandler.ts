81f84d10382634fd0cf6ff8d3f9939fd957822e8
Vaggelis Yfantis <me@octoper.me>
2024-10-29 15:10:57 +0200

    wip

--- packages/tanstack-start/src/server/middlewareHandler.ts
+++ packages/tanstack-start/src/server/middlewareHandler.ts
@@ -28,10 +28,10 @@ export function createClerkHandler<TRouter extends AnyRouter>(
 
         // Merging the TanStack router context with the Clerk context and loading the router
         router.update({
-          context: {  ...router.options.context, ...clerkInitialState },
+          context: { ...router.options.context, ...clerkInitialState },
         });
 
-        // await router.load();
+        await router.load();
       } catch (error) {
         if (error instanceof Response) {
           // returning the response
