1b8d68af25f03458bfa606e0807f91571a142824
Tom Milewski <me@tm.codes>
2024-10-30 15:34:50 -0400

    fix: Out-of-sync background clients

--- packages/chrome-extension/src/background/clerk.ts
+++ packages/chrome-extension/src/background/clerk.ts
@@ -8,16 +8,9 @@ import { SCOPE } from '../types';
 
 export type CreateClerkClientOptions = Omit<_CreateClerkClientOptions, 'scope'>;
 
-export let clerk: Clerk;
-
 export async function createClerkClient(opts: CreateClerkClientOptions): Promise<Clerk> {
-  if (clerk) {
-    return clerk;
-  }
-
   Clerk.mountComponentRenderer = undefined;
-  clerk = await _createClerkClient({ ...opts, scope: SCOPE.BACKGROUND });
+  const clerk = await _createClerkClient({ ...opts, scope: SCOPE.BACKGROUND });
   await clerk.load({ standardBrowser: false });
-
   return clerk;
 }
