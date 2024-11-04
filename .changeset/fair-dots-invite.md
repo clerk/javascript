cbaeaed1d473948052d431ecfc194ba3b9c4a740
Bryce Kalow <bryce@clerk.dev>
2024-11-04 11:15:47 -0600

    fix(nextjs): Do not set cookie during cache invalidation (#4478)

---
+++ .changeset/fair-dots-invite.md
@@ -0,0 +1,5 @@
+---
+'@clerk/nextjs': patch
+---
+
+Fixes an issue where Next.js cache invalidation was incorrectly setting a cookie.
--- packages/nextjs/src/app-router/server-actions.ts
+++ packages/nextjs/src/app-router/server-actions.ts
@@ -1,11 +1,11 @@
 'use server';
 
-import { cookie } from 'ezheaders';
+import { getCookies } from 'ezheaders';
 
 // This function needs to be async as we'd like to support next versions in the range of [14.1.2,14.2.0)
 // These versions required 'use server' files to export async methods only. This check was later relaxed
 // and the async is no longer required in newer next versions.
 // ref: https://github.com/vercel/next.js/pull/62821
 export async function invalidateCacheAction(): Promise<void> {
-  await cookie(`__clerk_invalidate_cache_cookie_${Date.now()}`, '');
+  void (await getCookies()).delete(`__clerk_invalidate_cache_cookie_${Date.now()}`);
 }
