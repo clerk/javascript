5a81a835a63bc1d4c5944488e8d570848bd9881a
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-11-05 10:43:32 -0300

    Check for user permissions rather than skipping

--- packages/clerk-js/src/ui/components/OrganizationProfile/OrganizationProfile.tsx
+++ packages/clerk-js/src/ui/components/OrganizationProfile/OrganizationProfile.tsx
@@ -13,8 +13,6 @@ import { OrganizationProfileRoutes } from './OrganizationProfileRoutes';
 const _OrganizationProfile = (_: OrganizationProfileProps) => {
   const { organization } = useOrganization();
 
-  console.log('Trigger integration tests');
-
   if (!organization) {
     return null;
   }
