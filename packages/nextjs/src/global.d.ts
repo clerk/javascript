cdd5a0ac074194f4d0d6a493b2f0888787d60c4e
Jacek Radko <jacek@clerk.dev>
2024-11-04 21:25:27 -0600

    fix(nextjs): Ambient global.d.ts (#4485)

--- packages/nextjs/src/global.d.ts
+++ packages/nextjs/src/global.d.ts
@@ -1,28 +1,23 @@
-declare global {
-  interface Window {
-    __unstable__onBeforeSetActive: () => void | Promise<void>;
-    __unstable__onAfterSetActive: () => void | Promise<void>;
+declare namespace NodeJS {
+  interface ProcessEnv {
+    CLERK_SECRET_KEY: string | undefined;
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string | undefined;
+    NEXT_PUBLIC_CLERK_PROXY_URL: string | undefined;
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: string | undefined;
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: string | undefined;
+    NEXT_PUBLIC_CLERK_SIGN_IN_FORCE_REDIRECT_URL: string | undefined;
+    NEXT_PUBLIC_CLERK_SIGN_UP_FORCE_REDIRECT_URL: string | undefined;
+    NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL: string | undefined;
+    NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL: string | undefined;
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: string | undefined;
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: string | undefined;
   }
 }
 
-declare global {
-  const PACKAGE_NAME: string;
-  const PACKAGE_VERSION: string;
-  namespace NodeJS {
-    interface ProcessEnv {
-      CLERK_SECRET_KEY: string | undefined;
-      NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string | undefined;
-      NEXT_PUBLIC_CLERK_PROXY_URL: string | undefined;
-      NEXT_PUBLIC_CLERK_SIGN_IN_URL: string | undefined;
-      NEXT_PUBLIC_CLERK_SIGN_UP_URL: string | undefined;
-      NEXT_PUBLIC_CLERK_SIGN_IN_FORCE_REDIRECT_URL: string | undefined;
-      NEXT_PUBLIC_CLERK_SIGN_UP_FORCE_REDIRECT_URL: string | undefined;
-      NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL: string | undefined;
-      NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL: string | undefined;
-      NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: string | undefined;
-      NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: string | undefined;
-    }
-  }
+interface Window {
+  __unstable__onBeforeSetActive: () => void | Promise<void>;
+  __unstable__onAfterSetActive: () => void | Promise<void>;
 }
 
-export {};
+declare const PACKAGE_NAME: string;
+declare const PACKAGE_VERSION: string;
