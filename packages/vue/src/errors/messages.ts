99862b5a5a6da60534a4b235838a12a4b47ed808
Robert Soriano <sorianorobertc@gmail.com>
2024-11-06 10:16:01 -0800

    chore: apply suggested edits
    Co-authored-by: Bryce Kalow <bryce@clerk.dev>

--- packages/vue/src/errors/messages.ts
+++ packages/vue/src/errors/messages.ts
@@ -5,4 +5,4 @@ export const invalidStateError =
   'Invalid state. Feel free to submit a bug or reach out to support here: https://clerk.com/support';
 
 export const useAuthHasRequiresRoleOrPermission =
-  'Missing parameters. `has` from `useAuth` requires a permission or role key to be passed. Example usage: `has({permission: "org:posts:edit"`';
+  'Missing parameters. `has` from `useAuth` requires a permission or role key to be passed. Example usage: `has({permission: "org:posts:edit"})`';
