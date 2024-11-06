e199037b8f484abdeeb9fc24455a1b4b8c31c8dd
Alexandros Ntitoras <47026269+AlexNti@users.noreply.github.com>
2024-11-06 17:50:38 +0200

    feat(clerk-expo): Support expo passkeys (#4352)
    Co-authored-by: Stefanos Anagnostou <anagstef@users.noreply.github.com>
    Co-authored-by: Stefanos Anagnostou <stefanos@clerk.dev>

--- packages/expo/src/provider/singleton/types.ts
+++ packages/expo/src/provider/singleton/types.ts
@@ -1,6 +1,32 @@
+import type {
+  CredentialReturn,
+  PublicKeyCredentialCreationOptionsWithoutExtensions,
+  PublicKeyCredentialRequestOptionsWithoutExtensions,
+  PublicKeyCredentialWithAuthenticatorAssertionResponse,
+  PublicKeyCredentialWithAuthenticatorAttestationResponse,
+} from '@clerk/types';
+
 import type { TokenCache } from '../../cache/types';
 
 export type BuildClerkOptions = {
   publishableKey?: string;
   tokenCache?: TokenCache;
+  /**
+   * Note: Passkey support in Expo is currently in a limited rollout phase.
+   * If you're interested in using this feature, please contact us for early access or additional details.
+   *
+   * @experimental This API is experimental and may change at any moment.
+   */
+  __experimental_passkeys?: {
+    get: ({
+      publicKeyOptions,
+    }: {
+      publicKeyOptions: PublicKeyCredentialRequestOptionsWithoutExtensions;
+    }) => Promise<CredentialReturn<PublicKeyCredentialWithAuthenticatorAssertionResponse>>;
+    create: (
+      publicKeyCredential: PublicKeyCredentialCreationOptionsWithoutExtensions,
+    ) => Promise<CredentialReturn<PublicKeyCredentialWithAuthenticatorAttestationResponse>>;
+    isSupported: () => boolean;
+    isAutoFillSupported: () => Promise<boolean>;
+  };
 };
