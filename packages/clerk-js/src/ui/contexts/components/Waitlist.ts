fa1ae0d547a4eb55fc3983c121cd251f37baf9d3
Dylan Staley <88163+dstaley@users.noreply.github.com>
2024-11-08 14:29:13 -0800

    fix(clerk-js): Split component contexts into separate files (#4520)
    Co-authored-by: Stefanos Anagnostou <anagstef@users.noreply.github.com>

---
+++ packages/clerk-js/src/ui/contexts/components/Waitlist.ts
@@ -0,0 +1,33 @@
+import { createContext, useContext } from 'react';
+
+import { buildURL } from '../../../utils';
+import { useEnvironment, useOptions } from '../../contexts';
+import type { WaitlistCtx } from '../../types';
+
+export type WaitlistContextType = WaitlistCtx & {
+  signInUrl: string;
+  redirectUrl?: string;
+};
+
+export const WaitlistContext = createContext<WaitlistCtx | null>(null);
+
+export const useWaitlistContext = (): WaitlistContextType => {
+  const context = useContext(WaitlistContext);
+  const { displayConfig } = useEnvironment();
+  const options = useOptions();
+
+  if (!context || context.componentName !== 'Waitlist') {
+    throw new Error('Clerk: useWaitlistContext called outside Waitlist.');
+  }
+
+  const { componentName, ...ctx } = context;
+
+  let signInUrl = ctx.signInUrl || options.signInUrl || displayConfig.signInUrl;
+  signInUrl = buildURL({ base: signInUrl }, { stringify: true });
+
+  return {
+    ...ctx,
+    componentName,
+    signInUrl,
+  };
+};
