83f5ea2db9648207edd6988687e31066fd0e8a6d
wobsoriano <sorianorobertc@gmail.com>
2024-11-12 20:46:45 -0800

    chore(nuxt): Add comment about bundling

--- packages/nuxt/tsup.config.ts
+++ packages/nuxt/tsup.config.ts
@@ -7,6 +7,8 @@ export default defineConfig(() => {
     clean: true,
     entry: ['./src/module.ts', './src/runtime/plugin.ts', './src/runtime/server/*.ts'],
     format: ['esm'],
+    // Make sure to not bundle the imports
+    // or else the Nuxt module will not be able to resolve them
     bundle: false,
     sourcemap: true,
     minify: false,
