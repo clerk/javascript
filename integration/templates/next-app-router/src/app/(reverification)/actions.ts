571e98324fa7b344cdf15966d3ffe535ba955bb0
panteliselef <panteliselef@outlook.com>
2024-11-14 12:06:31 +0200

    chore(types,shared): Update reverification config values to snake_case

--- integration/templates/next-app-router/src/app/(reverification)/actions.ts
+++ integration/templates/next-app-router/src/app/(reverification)/actions.ts
@@ -2,14 +2,15 @@
 
 import { auth } from '@clerk/nextjs/server';
 import { __experimental_reverificationMismatch as reverificationMismatch } from '@clerk/shared/authorization-errors';
+import { __experimental_ReverificationConfig } from '@clerk/types';
 
 const logUserIdActionReverification = async () => {
   const { userId, has } = await auth.protect();
 
   const config = {
-    level: 'secondFactor',
+    level: 'second_factor',
     afterMinutes: 1,
-  } as const;
+  } satisfies __experimental_ReverificationConfig;
 
   const userNeedsReverification = !has({
     __experimental_reverification: config,
