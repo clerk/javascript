cec0186064cd99310a20566f51450316d6a9dfd9
panteliselef <panteliselef@outlook.com>
2024-11-15 18:23:53 +0200

    Add Type tests

--- packages/react/src/hooks/__tests__/useReverification.test.ts
+++ packages/react/src/hooks/__tests__/useReverification.test.ts
@@ -1,121 +1,60 @@
-import { __experimental_useReverification as useReverification } from '@clerk/shared/react';
+import { __experimental_reverificationMismatch } from '@clerk/shared/authorization-errors';
+import type { __experimental_useReverification as useReverification } from '@clerk/shared/react';
 import { expectTypeOf } from 'expect-type';
-// type HasFunction = Exclude<ReturnType<typeof __experimental_useReverification>['has'], undefined>;
-// type ParamsOfHas = Parameters<HasFunction>[0];
+
+type ExcludeClerkError<T> = T extends { clerk_error: any } ? never : T;
+
+const fetcher = async (key: string, options: { id: string }) => {
+  return {
+    key,
+    options,
+  };
+};
+
+const fetcherWithHelper = async (key: string, options: { id: string }) => {
+  if (key == 'a') {
+    return __experimental_reverificationMismatch();
+  }
+
+  return {
+    key,
+    options,
+  };
+};
+
+type Fetcher = typeof fetcherWithHelper;
 
 describe('useReverification type tests', () => {
-  const fetcher = async (key: string, options: { id: string }) => {
-    return {
-      key,
-      options,
-    };
-  };
+  it('allow pass through types', () => {
+    type UseReverificationWithFetcher = typeof useReverification<typeof fetcher, object>;
+    type VerifiedFetcher = ReturnType<UseReverificationWithFetcher>[0];
+    expectTypeOf(fetcher).toEqualTypeOf<VerifiedFetcher>();
+  });
 
-  const [verifiedFetcher] = useReverification(fetcher);
+  it('returned callback with clerk error excluded and possible null in case of cancelled flow', () => {
+    type UseReverificationWithFetcherHelper = typeof useReverification<typeof fetcherWithHelper, object>;
+    type VerifiedFetcherHelper = ReturnType<UseReverificationWithFetcherHelper>[0];
 
-  describe('has', () => {
-    it('allow pass through types', () => {
-      expectTypeOf({} as const).toMatchTypeOf<ParamsOfHas>();
-    });
+    expectTypeOf(fetcherWithHelper).not.toEqualTypeOf<VerifiedFetcherHelper>();
 
-    it('has({randomKey}) is not allowed', () => {
-      expectTypeOf({
-        randomKey: '',
-      }).not.toMatchTypeOf<ParamsOfHas>();
-    });
+    expectTypeOf<Parameters<Fetcher>>().toEqualTypeOf<Parameters<VerifiedFetcherHelper>>();
+    expectTypeOf<ReturnType<Fetcher>>().not.toEqualTypeOf<ReturnType<VerifiedFetcherHelper>>();
+    expectTypeOf<ExcludeClerkError<Awaited<ReturnType<Fetcher>>> | null>().toEqualTypeOf<
+      Awaited<ReturnType<VerifiedFetcherHelper>>
+    >();
+  });
 
-    it('has({role: string}) is allowed', () => {
-      expectTypeOf({ role: 'org:admin' }).toMatchTypeOf<ParamsOfHas>();
-    });
-
-    it('has({role: string, permission: string}) is NOT allowed', () => {
-      expectTypeOf({ role: 'org:admin', permission: 'some-perm' }).not.toMatchTypeOf<ParamsOfHas>();
-    });
-
-    it('has with role and assurance is allowed', () => {
-      expectTypeOf({
-        role: 'org:admin',
-        __experimental_reverification: {
-          level: 'first_factor',
-          afterMinutes: 10,
-        },
-      } as const).toMatchTypeOf<ParamsOfHas>();
-    });
-
-    it('has with permission and reverification is allowed', () => {
-      expectTypeOf({
-        permission: 'org:edit:posts',
-        __experimental_reverification: {
-          level: 'first_factor',
-          afterMinutes: 10,
-        },
-      } as const).toMatchTypeOf<ParamsOfHas>();
-    });
-
-    it('has({reverification: {level, maxAge}}) is allowed', () => {
-      expectTypeOf({
-        __experimental_reverification: {
-          level: 'first_factor',
-          afterMinutes: 10,
-        },
-      } as const).toMatchTypeOf<ParamsOfHas>();
-    });
-
-    it('reverification with other values as maxAge should throw', () => {
-      expectTypeOf({
-        __experimental_reverification: {
-          level: 'first_factor',
-          afterMinutes: '10',
-        },
-      } as const).not.toMatchTypeOf<ParamsOfHas>();
-    });
-
-    it('veryStrict reverification is allowed', () => {
-      expectTypeOf({
-        __experimental_reverification: 'strict_mfa',
-      } as const).toMatchTypeOf<ParamsOfHas>();
-    });
-
-    it('strict reverification is allowed', () => {
-      expectTypeOf({
-        __experimental_reverification: 'strict',
-      } as const).toMatchTypeOf<ParamsOfHas>();
-    });
-
-    it('moderate reverification is allowed', () => {
-      expectTypeOf({
-        __experimental_reverification: 'moderate',
-      } as const).toMatchTypeOf<ParamsOfHas>();
-    });
-
-    it('lax reverification is allowed', () => {
-      expectTypeOf({
-        __experimental_reverification: 'lax',
-      } as const).toMatchTypeOf<ParamsOfHas>();
-    });
-
-    it('random reverification is not allowed', () => {
-      expectTypeOf({
-        __experimental_reverification: 'random',
-      } as const).not.toMatchTypeOf<ParamsOfHas>();
-    });
-
-    it('reverification with other strings as level should throw', () => {
-      expectTypeOf({
-        __experimental_reverification: {
-          level: 'some-factor',
-          afterMinutes: 10,
-        },
-      } as const).not.toMatchTypeOf<ParamsOfHas>();
-    });
-
-    it('reverification with number as level should throw', () => {
-      expectTypeOf({
-        __experimental_reverification: {
-          level: 2,
-          afterMinutes: 10,
-        },
-      } as const).not.toMatchTypeOf<ParamsOfHas>();
-    });
+  it('returned callback with clerk error excluded but without null since we throw', () => {
+    type UseReverificationWithFetcherHelperThrow = typeof useReverification<
+      typeof fetcherWithHelper,
+      {
+        throwOnCancel: true;
+      }
+    >;
+    type VerifiedFetcherHelperThrow = ReturnType<UseReverificationWithFetcherHelperThrow>[0];
+    expectTypeOf(fetcherWithHelper).not.toEqualTypeOf<VerifiedFetcherHelperThrow>();
+    expectTypeOf<ExcludeClerkError<Awaited<ReturnType<Fetcher>>>>().toEqualTypeOf<
+      Awaited<ReturnType<VerifiedFetcherHelperThrow>>
+    >();
   });
 });
