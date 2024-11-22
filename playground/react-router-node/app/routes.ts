fc2ddc315362994e23d1e64af84d8f25ffc7b053
LekoArts <lekoarts@gmail.com>
2024-11-22 15:33:57 +0100

    use declarative routes instead of fs

--- playground/react-router-node/app/routes.ts
+++ playground/react-router-node/app/routes.ts
@@ -1,4 +1,12 @@
-import { flatRoutes } from "@react-router/fs-routes";
-import type { RouteConfig } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  route,
+  index,
+} from "@react-router/dev/routes";
 
-export default flatRoutes() satisfies RouteConfig;
+export default [
+  index('./routes/index.tsx'),
+  route('/sign-in/*', './routes/sign-in.tsx'),
+  route('/sign-up/*', './routes/sign-up.tsx'),
+  route('/protected', './routes/protected.tsx'),
+] satisfies RouteConfig
