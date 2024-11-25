9a6da5d7d5583e43b8e87f564f940002c7ac8219
Ryan Steil <ryan@predictionlab.ai>
2024-11-25 00:34:24 -0600

    fix: remove server-action from ClerkProvider

--- packages/nextjs/src/app-router/client/ClerkProvider.tsx
+++ packages/nextjs/src/app-router/client/ClerkProvider.tsx
@@ -1,4 +1,5 @@
 'use client';
+// eslint-disable-next-line import/no-unresolved
 import { ClerkProvider as ReactClerkProvider } from '@clerk/clerk-react';
 import { useRouter } from 'next/navigation';
 import React, { useEffect, useTransition } from 'react';
@@ -8,7 +9,6 @@ import { ClerkNextOptionsProvider, useClerkNextOptions } from '../../client-boun
 import type { NextClerkProviderProps } from '../../types';
 import { ClerkJSScript } from '../../utils/clerk-js-script';
 import { mergeNextClerkPropsWithEnv } from '../../utils/mergeNextClerkPropsWithEnv';
-import { invalidateCacheAction } from '../server-actions';
 import { useAwaitablePush } from './useAwaitablePush';
 import { useAwaitableReplace } from './useAwaitableReplace';
 
@@ -44,35 +44,17 @@ export const ClientClerkProvider = (props: NextClerkProviderProps) => {
 
   useSafeLayoutEffect(() => {
     window.__unstable__onBeforeSetActive = () => {
-      /**
-       * We need to invalidate the cache in case the user is navigating to a page that
-       * was previously cached using the auth state that was active at the time.
-       *
-       *  We also need to await for the invalidation to happen before we navigate,
-       * otherwise the navigation will use the cached page.
-       *
-       * For example, if we did not invalidate the flow, the following scenario would be broken:
-       * - The middleware is configured in such a way that it redirects you back to the same page if a certain condition is true (eg, you need to pick an org)
-       * - The user has a <Link href=/> component in the page
-       * - The UB is mounted with afterSignOutUrl=/
-       * - The user clicks the Link. A nav to / happens, a 307 to the current page is returned so a navigation does not take place. The / navigation is now cached as a 307 to the current page
-       * - The user clicks sign out
-       * - We call router.refresh()
-       * - We navigate to / but its cached and instead, we 'redirect' to the current page
-       *
-       *  For more information on cache invalidation, see:
-       * https://nextjs.org/docs/app/building-your-application/caching#invalidation-1
-       */
       return new Promise(res => {
         window.__clerk_internal_invalidateCachePromise = res;
 
-        // NOTE: the following code will allow `useReverification()` to work properly when `handlerReverification` is called inside `startTransition`
         if (window.next?.version && typeof window.next.version === 'string' && window.next.version.startsWith('13')) {
           startTransition(() => {
             router.refresh();
           });
         } else {
-          void invalidateCacheAction().then(() => res());
+          const cookieName = `__clerk_invalidate_cache_cookie_${Date.now()}`;
+          document.cookie = `${cookieName}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
+          res();
         }
       });
     };
