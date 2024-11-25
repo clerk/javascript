353489709d69af78d6337d42cac91d605723cb2c
wobsoriano <sorianorobertc@gmail.com>
2024-11-25 15:21:40 -0800

    chore: Record middleware telemetry

--- packages/nuxt/src/runtime/server/clerkMiddleware.ts
+++ packages/nuxt/src/runtime/server/clerkMiddleware.ts
@@ -1,6 +1,7 @@
 import type { AuthObject } from '@clerk/backend';
 import type { AuthenticateRequestOptions } from '@clerk/backend/internal';
 import { AuthStatus, constants } from '@clerk/backend/internal';
+import { eventMethodCalled } from '@clerk/shared/telemetry';
 import type { EventHandler } from 'h3';
 import { createError, eventHandler, isEventHandler, setResponseHeader } from 'h3';
 
@@ -79,6 +80,14 @@ export const clerkMiddleware: ClerkMiddleware = (...args: unknown[]) => {
   const [handler, options] = parseHandlerAndOptions(args);
   return eventHandler(async event => {
     const clerkRequest = toWebRequest(event);
+
+    clerkClient(event).telemetry.record(
+      eventMethodCalled('clerkMiddleware', {
+        handler: Boolean(handler),
+        satellite: Boolean(options.isSatellite),
+        proxy: Boolean(options.proxyUrl),
+      }),
+    );
 
     const requestState = await clerkClient(event).authenticateRequest(clerkRequest, options);
 
