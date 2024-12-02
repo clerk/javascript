ad85446fb1a9beb0cc8d8d4c1be7270caa50907b
wobsoriano <sorianorobertc@gmail.com>
2024-12-02 13:18:16 -0800

    chore(vue): Add custom menu items

--- packages/vue/src/errors/messages.ts
+++ packages/vue/src/errors/messages.ts
@@ -6,3 +6,23 @@ export const invalidStateError =
 
 export const useAuthHasRequiresRoleOrPermission =
   'Missing parameters. `has` from `useAuth` requires a permission or role key to be passed. Example usage: `has({permission: "org:posts:edit"})`';
+
+export const userButtonMenuActionRenderedError =
+  '<UserButton.Action /> component needs to be a direct child of `<UserButton.MenuItems />`.';
+
+export const userButtonMenuLinkRenderedError =
+  '<UserButton.Link /> component needs to be a direct child of `<UserButton.MenuItems />`.';
+
+export const userButtonMenuItemLinkWrongProps =
+  'Missing props. <UserButton.Link /> component requires the following props: href, label and labelIcon.';
+
+export const userButtonMenuItemsActionWrongsProps =
+  'Missing props. <UserButton.Action /> component requires the following props: label.';
+
+export const customPageWrongProps = (componentName: string) =>
+  `Missing props. <${componentName}.Page /> component requires the following props: url, label, labelIcon, alongside with children to be rendered inside the page.`;
+
+export const customLinkWrongProps = (componentName: string) =>
+  `Missing props. <${componentName}.Link /> component requires the following props: url, label and labelIcon.`;
+
+export const userButtonIgnoredComponent = `<UserButton /> can only accept <UserButton.UserProfilePage />, <UserButton.UserProfileLink /> and <UserButton.MenuItems /> as its children. Any other provided component will be ignored.`;
