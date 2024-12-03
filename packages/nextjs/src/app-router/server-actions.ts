7369441f75a0040226c79245f3deb8f776964a3f
Jacek <jacek@clerk.dev>
2024-12-03 13:22:36 -0600

    feat: no ezheaders

--- packages/nextjs/src/app-router/server-actions.ts
+++ packages/nextjs/src/app-router/server-actions.ts
@@ -1,11 +1,11 @@
 'use server';
 
-import { getCookies } from 'ezheaders';
+import { cookies } from 'next/headers';
 
 // This function needs to be async as we'd like to support next versions in the range of [14.1.2,14.2.0)
 // These versions required 'use server' files to export async methods only. This check was later relaxed
 // and the async is no longer required in newer next versions.
 // ref: https://github.com/vercel/next.js/pull/62821
 export async function invalidateCacheAction(): Promise<void> {
-  void (await getCookies()).delete(`__clerk_invalidate_cache_cookie_${Date.now()}`);
+  void (await cookies()).delete(`__clerk_invalidate_cache_cookie_${Date.now()}`
 }
