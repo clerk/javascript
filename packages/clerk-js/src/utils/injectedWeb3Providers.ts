e253b148df32d0b167e82a4be0f7674db2349be0
Haris Chaniotakis <haris@clerk.dev>
2024-12-03 08:54:26 +0200

    feat(clerk-js,elements,clerk-react,types): Support OKW Wallet Web3 provider and authentication strategy

--- packages/clerk-js/src/utils/injectedWeb3Providers.ts
+++ packages/clerk-js/src/utils/injectedWeb3Providers.ts
@@ -1,4 +1,4 @@
-import type { MetamaskWeb3Provider } from '@clerk/types';
+import type { MetamaskWeb3Provider, OKXWalletWeb3Provider } from '@clerk/types';
 
 //https://eips.ethereum.org/EIPS/eip-6963
 
@@ -27,12 +27,13 @@ interface EIP6963ProviderDetail {
 }
 
 type EIP6963AnnounceProviderEvent = CustomEvent<EIP6963ProviderDetail>;
-type InjectedWeb3Provider = MetamaskWeb3Provider;
+type InjectedWeb3Provider = MetamaskWeb3Provider | OKXWalletWeb3Provider;
 
 class InjectedWeb3Providers {
   #providers: EIP6963ProviderDetail[] = [];
   #providerIdMap: Record<InjectedWeb3Provider, string> = {
     metamask: 'MetaMask',
+    okx_wallet: 'OKX Wallet',
   } as const;
   static #instance: InjectedWeb3Providers | null = null;
 
