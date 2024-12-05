206bb4f02e27e2050c58257bd2a89d9fc8a72f8e
panteliselef <panteliselef@outlook.com>
2024-12-05 17:28:42 +0200

    use next/headers

--- packages/nextjs/src/app-router/keyless-actions.ts
+++ packages/nextjs/src/app-router/keyless-actions.ts
@@ -1,6 +1,6 @@
 'use server';
 import type { AccountlessApplication } from '@clerk/backend';
-import { getCookies } from 'ezheaders';
+import { cookies } from 'next/headers';
 import { redirect, RedirectType } from 'next/navigation';
 
 import { getKeylessCookieName } from '../server/keyless';
@@ -8,7 +8,7 @@ import { canUseKeyless__server } from '../utils/feature-flags';
 
 export async function syncKeylessConfigAction(args: AccountlessApplication & { returnUrl: string }): Promise<void> {
   const { claimUrl, publishableKey, secretKey, returnUrl } = args;
-  void (await getCookies()).set(getKeylessCookieName(), JSON.stringify({ claimUrl, publishableKey, secretKey }), {
+  void (await cookies()).set(getKeylessCookieName(), JSON.stringify({ claimUrl, publishableKey, secretKey }), {
     secure: true,
     httpOnly: true,
   });
@@ -32,7 +32,7 @@ export async function createOrReadKeylessAction(): Promise<null | Omit<Accountle
 
   const { claimUrl, publishableKey, secretKey } = result;
 
-  void (await getCookies()).set(getKeylessCookieName(), JSON.stringify({ claimUrl, publishableKey, secretKey }), {
+  void (await cookies()).set(getKeylessCookieName(), JSON.stringify({ claimUrl, publishableKey, secretKey }), {
     secure: false,
     httpOnly: false,
   });
