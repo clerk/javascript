0f7ddba4db753cbc88dddcb1c08720e3805faba4
Tom Milewski <me@tm.codes>
2024-12-05 11:13:15 -0500

    chore(localizations): Add disclaimer

--- packages/localizations/src/id-ID.ts
+++ packages/localizations/src/id-ID.ts
@@ -1,3 +1,15 @@
+/*
+ * =====================================================================================
+ * DISCLAIMER:
+ * =====================================================================================
+ * This localization file is a community contribution and is not officially maintained
+ * by Clerk. It has been provided by the community and may not be fully aligned
+ * with the current or future states of the main application. Clerk does not guarantee
+ * the accuracy, completeness, or timeliness of the translations in this file.
+ * Use of this file is at your own risk and discretion.
+ * =====================================================================================
+ */
+
 import type { LocalizationResource } from "@clerk/types";
 
 export const idID: LocalizationResource = {
