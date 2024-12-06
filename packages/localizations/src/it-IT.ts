c3cbcfa8f1911be6e973d0f7c4c9a335c733ef11
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2024-12-06 07:34:53 -0300

    Fix gap between provider and email address

--- packages/localizations/src/it-IT.ts
+++ packages/localizations/src/it-IT.ts
@@ -684,14 +684,12 @@ export const itIT: LocalizationResource = {
     },
     emailAddressPage: {
       emailCode: {
-        formHint: 'Una email contenente un codice di verifica è stata inviata a questo indirizzo email.',
         formSubtitle: 'Inserisci il codice di verifica inviato a {{identifier}}',
         formTitle: 'Codice di verifica',
         resendButton: 'Rinvia codice',
         successMessage: "L'indirizzo email {{identifier}} è stato aggiunto al tuo account.",
       },
       emailLink: {
-        formHint: 'Una email contenente un link di verifica sarà inviata a questo indirizzo email.',
         formSubtitle: 'Clicca sul link di verifica nella email inviata a {{identifier}}',
         formTitle: 'Link di verifica',
         resendButton: 'Rinvia link',
