1fd65546902cc0a4c45c1b862cdb0159655d1a4c
LekoArts <lekoarts@gmail.com>
2024-12-10 15:22:12 +0100

    improve test

--- integration/tests/react-router/basic.test.ts
+++ integration/tests/react-router/basic.test.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 import { appConfigs } from '../../presets';
 import type { FakeUser } from '../../testUtils';
@@ -48,6 +48,45 @@ testAgainstRunningApps({ withEnv: [appConfigs.envs.withEmailCodes], withPattern:
       await u.po.userButton.waitForPopover();
 
       await u.po.userButton.toHaveVisibleMenuItems([/Manage account/i, /Sign out$/i]);
+    });
+
+    test('redirects to sign-in when unauthenticated', async ({ page, context }) => {
+      const u = createTestUtils({ app, page, context });
+
+      await u.page.goToRelative('/protected');
+      await u.page.waitForURL(`${app.serverUrl}/sign-in`);
+      await u.po.signIn.waitForMounted();
+    });
+
+    test('renders control components contents', async ({ page, context }) => {
+      const u = createTestUtils({ app, page, context });
+
+      await u.page.goToAppHome();
+      await expect(u.page.getByText('SignedOut')).toBeVisible();
+
+      await u.page.goToRelative('/sign-in');
+      await u.po.signIn.waitForMounted();
+      await u.po.signIn.signInWithEmailAndInstantPassword({ email: fakeUser.email, password: fakeUser.password });
+      await u.po.expect.toBeSignedIn();
+      await expect(u.page.getByText('SignedIn')).toBeVisible();
+    });
+
+    test('renders user profile with SSR data', async ({ page, context }) => {
+      const u = createTestUtils({ app, page, context });
+
+      await u.page.goToRelative('/sign-in');
+      await u.po.signIn.waitForMounted();
+      await u.po.signIn.signInWithEmailAndInstantPassword({ email: fakeUser.email, password: fakeUser.password });
+      await u.po.expect.toBeSignedIn();
+
+      await u.po.userButton.waitForMounted();
+      await u.page.goToRelative('/protected');
+      await u.po.userProfile.waitForMounted();
+
+      // Fetched from an API endpoint (/api/me), which is server-rendered.
+      // This also verifies that the server middleware is working.
+      await expect(u.page.getByText(`First name: ${fakeUser.firstName}`)).toBeVisible();
+      await expect(u.page.getByText(`Email: ${fakeUser.email}`)).toBeVisible();
     });
   },
 );
