f3a0440ffa19f880575f6897ed70501073bc1b66
Vaggelis Yfantis <me@octoper.me>
2024-12-10 02:08:07 +0200

    fix(tanstack-start): Fix ENV variable loading

--- packages/tanstack-start/src/server/constants.ts
+++ packages/tanstack-start/src/server/constants.ts
@@ -2,24 +2,27 @@ import { apiUrlFromPublishableKey } from '@clerk/shared/apiUrlFromPublishableKey
 
 import { getEnvVariable, getPublicEnvVariables } from '../utils/env';
 
-export const CLERK_JS_VERSION = getPublicEnvVariables().clerkJsVersion || '';
-export const CLERK_JS_URL = getPublicEnvVariables().clerkJsUrl || '';
-export const API_VERSION = getEnvVariable('CLERK_API_VERSION') || 'v1';
-export const SECRET_KEY = getEnvVariable('CLERK_SECRET_KEY') || '';
-export const PUBLISHABLE_KEY = getPublicEnvVariables().publishableKey || '';
-export const ENCRYPTION_KEY = getEnvVariable('CLERK_ENCRYPTION_KEY') || '';
-export const API_URL = getEnvVariable('CLERK_API_URL') || apiUrlFromPublishableKey(PUBLISHABLE_KEY);
-export const DOMAIN = getPublicEnvVariables().domain || '';
-export const PROXY_URL = getPublicEnvVariables().proxyUrl || '';
-export const CLERK_JWT_KEY = getEnvVariable('CLERK_JWT_KEY') || '';
-export const IS_SATELLITE = getPublicEnvVariables().isSatellite || false;
-export const SIGN_IN_URL = getPublicEnvVariables().signInUrl || '';
-export const SIGN_UP_URL = getPublicEnvVariables().signUpUrl || '';
-export const SDK_METADATA = {
-  name: PACKAGE_NAME,
-  version: PACKAGE_VERSION,
-  environment: getEnvVariable('NODE_ENV'),
-};
-
-export const TELEMETRY_DISABLED = getPublicEnvVariables().telemetryDisabled;
-export const TELEMETRY_DEBUG = getPublicEnvVariables().telemetryDebug;
+// @ts-expect-error - TODO: Improve types
+export const commonEnvs = () =>
+  ({
+    CLERK_JS_VERSION: getPublicEnvVariables().clerkJsVersion || '',
+    CLERK_JS_URL: getPublicEnvVariables().clerkJsUrl || '',
+    API_VERSION: getEnvVariable('CLERK_API_VERSION') || 'v1',
+    SECRET_KEY: getEnvVariable('CLERK_SECRET_KEY') || '',
+    PUBLISHABLE_KEY: getPublicEnvVariables().publishableKey || '',
+    ENCRYPTION_KEY: getEnvVariable('CLERK_ENCRYPTION_KEY') || '',
+    API_URL: getEnvVariable('CLERK_API_URL') || apiUrlFromPublishableKey(commonEnvs().PUBLISHABLE_KEY),
+    DOMAIN: getPublicEnvVariables().domain || '',
+    PROXY_URL: getPublicEnvVariables().proxyUrl || '',
+    CLERK_JWT_KEY: getEnvVariable('CLERK_JWT_KEY') || '',
+    IS_SATELLITE: getPublicEnvVariables().isSatellite || false,
+    SIGN_IN_URL: getPublicEnvVariables().signInUrl || '',
+    SIGN_UP_URL: getPublicEnvVariables().signUpUrl || '',
+    SDK_METADATA: {
+      name: PACKAGE_NAME,
+      version: PACKAGE_VERSION,
+      environment: getEnvVariable('NODE_ENV'),
+    },
+    TELEMETRY_DISABLED: getPublicEnvVariables().telemetryDisabled,
+    TELEMETRY_DEBUG: getPublicEnvVariables().telemetryDebug,
+  }) as const;
