82e8f404f5b468b564ef48392b36bfa3e40ea91e
LekoArts <lekoarts@gmail.com>
2024-12-10 10:30:56 +0100

    improve env var loading

--- packages/react-router/src/ssr/loadOptions.ts
+++ packages/react-router/src/ssr/loadOptions.ts
@@ -13,12 +13,13 @@ export const loadOptions = (args: LoaderFunctionArgs, overrides: RootAuthLoaderO
   const { request, context } = args;
   const clerkRequest = createClerkRequest(patchRequest(request));
 
-  // Fetch environment variables across Remix runtime.
-  // 1. First check if the user passed the key in the getAuth function or the rootAuthLoader.
+  // Fetch environment variables across React Router runtime.
+  // 1. First check if the user passed the key in the getAuth() function or the rootAuthLoader().
   // 2. Then try from process.env if exists (Node).
-  // 3. Then try from globalThis (Cloudflare Workers).
-  // 4. Then from loader context (Cloudflare Pages).
-  const secretKey = overrides.secretKey || getEnvVariable('CLERK_SECRET_KEY', context) || '';
+  // 3. Then try from import.meta.env if exists (Vite).
+  // 4. Then try from globalThis (Cloudflare Workers).
+  // 5. Then from loader context (Cloudflare Pages).
+  const secretKey = overrides.secretKey || getEnvVariable('CLERK_SECRET_KEY', context);
   const publishableKey = overrides.publishableKey || getPublicEnvVariables(context).publishableKey;
   const jwtKey = overrides.jwtKey || getEnvVariable('CLERK_JWT_KEY', context);
   const apiUrl = getEnvVariable('CLERK_API_URL', context) || apiUrlFromPublishableKey(publishableKey);
@@ -33,13 +34,13 @@ export const loadOptions = (args: LoaderFunctionArgs, overrides: RootAuthLoaderO
   const signInUrl = overrides.signInUrl || getPublicEnvVariables(context).signInUrl;
   const signUpUrl = overrides.signUpUrl || getPublicEnvVariables(context).signUpUrl;
   const signInForceRedirectUrl =
-    overrides.signInForceRedirectUrl || getEnvVariable('CLERK_SIGN_IN_FORCE_REDIRECT_URL', context) || '';
+    overrides.signInForceRedirectUrl || getPublicEnvVariables(context).signInForceRedirectUrl;
   const signUpForceRedirectUrl =
-    overrides.signUpForceRedirectUrl || getEnvVariable('CLERK_SIGN_UP_FORCE_REDIRECT_URL', context) || '';
+    overrides.signUpForceRedirectUrl || getPublicEnvVariables(context).signUpForceRedirectUrl;
   const signInFallbackRedirectUrl =
-    overrides.signInFallbackRedirectUrl || getEnvVariable('CLERK_SIGN_IN_FALLBACK_REDIRECT_URL', context) || '';
+    overrides.signInFallbackRedirectUrl || getPublicEnvVariables(context).signInFallbackRedirectUrl;
   const signUpFallbackRedirectUrl =
-    overrides.signUpFallbackRedirectUrl || getEnvVariable('CLERK_SIGN_UP_FALLBACK_REDIRECT_URL', context) || '';
+    overrides.signUpFallbackRedirectUrl || getPublicEnvVariables(context).signUpFallbackRedirectUrl;
   const afterSignInUrl = overrides.afterSignInUrl || getPublicEnvVariables(context).afterSignInUrl;
   const afterSignUpUrl = overrides.afterSignUpUrl || getPublicEnvVariables(context).afterSignUpUrl;
 
