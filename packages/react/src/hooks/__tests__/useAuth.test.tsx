bf6388ca4c7dc69e79fdae03060a7de85b931da7
Jacek <jacek@clerk.dev>
2024-12-12 13:37:41 -0600

    happy path test

--- packages/react/src/hooks/__tests__/useAuth.test.tsx
+++ packages/react/src/hooks/__tests__/useAuth.test.tsx
@@ -1,6 +1,9 @@
+import { ClerkInstanceContext } from '@clerk/shared/react';
+import type { LoadedClerk } from '@clerk/types';
 import { render } from '@testing-library/react';
 import React from 'react';
 
+import { AuthContext } from '../../contexts/AuthContext';
 import { useAuth } from '../useAuth';
 
 const TestComponent = () => {
@@ -20,5 +23,17 @@ describe('useAuth', () => {
     }).toThrow(
       '@clerk/clerk-react: useAuth can only be used within the <ClerkProvider /> component. Learn more: https://clerk.com/docs/components/clerk-provider',
     );
+  });
+
+  test('renders the correct values when wrapped in <ClerkProvider>', () => {
+    expect(() => {
+      render(
+        <ClerkInstanceContext.Provider value={{ value: {} as LoadedClerk }}>
+          <AuthContext.Provider value={{ value: {} as any }}>
+            <TestComponent />
+          </AuthContext.Provider>
+        </ClerkInstanceContext.Provider>,
+      );
+    }).not.toThrow();
   });
 });
