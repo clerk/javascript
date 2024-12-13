9101b6a8716dece089ac8a1115150fa58774ba13
Jakob Evangelista <jakobevangelista@gmail.com>
2024-12-12 16:44:52 -0800

    added export to internal

--- packages/backend/src/internal.ts
+++ packages/backend/src/internal.ts
@@ -10,7 +10,13 @@ export { debugRequestState } from './tokens/request';
 export type { AuthenticateRequestOptions, OrganizationSyncOptions } from './tokens/types';
 
 export type { SignedInAuthObjectOptions, SignedInAuthObject, SignedOutAuthObject } from './tokens/authObjects';
-export { makeAuthObjectSerializable, signedOutAuthObject, signedInAuthObject } from './tokens/authObjects';
+export {
+  makeAuthObjectSerializable,
+  signedOutAuthObject,
+  signedInAuthObject,
+  authenticatedMachineObject,
+  unauthenticatedMachineObject,
+} from './tokens/authObjects';
 
 export { AuthStatus } from './tokens/authStatus';
 export type {
