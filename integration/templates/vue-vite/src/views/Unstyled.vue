d8f8c21e2190db31c07bbcc6b27312979832c262
wobsoriano <sorianorobertc@gmail.com>
2024-12-17 10:10:39 -0800

    chore: fix formatting

--- integration/templates/vue-vite/src/views/Unstyled.vue
+++ integration/templates/vue-vite/src/views/Unstyled.vue
@@ -3,6 +3,12 @@ import { SignInButton, SignUpButton } from '@clerk/vue';
 </script>
 
 <template>
-  <SignInButton mode="modal" fallbackRedirectUrl="/" />
-  <SignUpButton mode="modal" fallbackRedirectUrl="/" />
+  <SignInButton
+    mode="modal"
+    fallbackRedirectUrl="/"
+  />
+  <SignUpButton
+    mode="modal"
+    fallbackRedirectUrl="/"
+  />
 </template>
