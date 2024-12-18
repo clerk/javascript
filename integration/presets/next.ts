a9234f28b53455c3be5517c9e1098fa525ee0dd4
Jacek Radko <jacek@clerk.dev>
2024-12-18 10:49:33 -0600

    fix(repo): Link local packages for e2e (#4800)

--- integration/presets/next.ts
+++ integration/presets/next.ts
@@ -1,6 +1,7 @@
 import { constants } from '../constants';
 import { applicationConfig } from '../models/applicationConfig.js';
 import { templates } from '../templates/index.js';
+import { linkPackage } from './utils';
 
 const appRouter = applicationConfig()
   .setName('next-app-router')
@@ -13,9 +14,9 @@ const appRouter = applicationConfig()
   .addDependency('next', constants.E2E_NEXTJS_VERSION)
   .addDependency('react', constants.E2E_REACT_VERSION)
   .addDependency('react-dom', constants.E2E_REACT_DOM_VERSION)
-  .addDependency('@clerk/nextjs', constants.E2E_CLERK_VERSION || '*')
-  .addDependency('@clerk/shared', '*')
-  .addDependency('@clerk/types', '*');
+  .addDependency('@clerk/nextjs', constants.E2E_CLERK_VERSION || linkPackage('nextjs'))
+  .addDependency('@clerk/shared', linkPackage('shared'))
+  .addDependency('@clerk/types', linkPackage('types'));
 
 const appRouterTurbo = appRouter.clone().setName('next-app-router-turbopack').addScript('dev', 'pnpm dev');
 
