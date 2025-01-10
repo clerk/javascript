75207355df978d92ec700ab413bb9dde3ae67201
Jacek <jacek@clerk.dev>
2025-01-10 10:42:01 -0600

    chore(repo): more comprehensive nuke script

---
+++ scripts/nuke.mjs
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+
+import { access, constants, readdir } from 'node:fs/promises';
+import { join, resolve } from 'node:path';
+
+import { $ } from 'zx';
+
+const $$ = $({
+  env: { NODE_ENV: 'production' },
+  // eslint-disable-next-line turbo/no-undeclared-env-vars
+  verbose: !!process.env.VERBOSE,
+});
+
+const DIRECTORIES_TO_CLEAN = ['.coverage', '.turbo', 'coverage', 'dist', 'node_modules'];
+
+// Iterate over `packages/*`
+try {
+  const packagesDir = resolve('packages');
+  await access(packagesDir, constants.R_OK);
+
+  try {
+    const packages = await readdir(packagesDir);
+    const promises = packages.map(
+      dir =>
+        $$({
+          // eslint-disable-next-line turbo/no-undeclared-env-vars
+          verbose: !!process.env.VERBOSE,
+        })`rm -rf ${DIRECTORIES_TO_CLEAN.map(directory => join(join(packagesDir, dir), directory))}`,
+    );
+
+    await Promise.all(promises).then(() => void console.log(`Cleaned ${promises.length} packages`));
+  } catch (error) {
+    console.error(error);
+  }
+} catch {
+  console.log('Cannot access packages directory');
+}
+
+// Iterate over `playground/*`
+try {
+  const playgroundDir = resolve('playground');
+  await access(playgroundDir);
+  try {
+    const playgrounds = await readdir(playgroundDir);
+    for (const dir of playgrounds) {
+      // Remove `yalc` packages
+      await $$({
+        cwd: join(playgroundDir, dir),
+      })`rm -rf .yalc`.then(() => console.log('Removed all yalc packages'));
+    }
+    const promises = playgrounds.map(
+      dir => $$`rm -rf ${DIRECTORIES_TO_CLEAN.map(directory => join(join(playgroundDir, dir), directory))}`,
+    );
+
+    await Promise.allSettled(promises).then(() => void console.log(`Cleaned ${promises.length} playgrounds`));
+  } catch (error) {
+    console.error(error);
+  }
+} catch {
+  console.log('Cannot access playground directory');
+}
+
+// Remove `.turbo` directory
+await $`rm -rf .turbo`.then(() => console.log('Removed root .turbo directory'));
+
+// Remove root `node_modules`
+await $`rm -rf node_modules`.then(() => console.log('Removed root node_modules'));
