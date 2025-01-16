f7609f0e5e8e312d1521771af0c8abbfe67b865f
Jonah <jonah.allibone@gmail.com>
2025-01-16 17:38:19 -0500

    added integrity

--- packages/shared/src/loadScript.ts
+++ packages/shared/src/loadScript.ts
@@ -8,11 +8,12 @@ type LoadScriptOptions = {
   defer?: boolean;
   crossOrigin?: 'anonymous' | 'use-credentials';
   nonce?: string;
+  integrity?: string;
   beforeLoad?: (script: HTMLScriptElement) => void;
 };
 
 export async function loadScript(src = '', opts: LoadScriptOptions): Promise<HTMLScriptElement> {
-  const { async, defer, beforeLoad, crossOrigin, nonce } = opts || {};
+  const { async, defer, beforeLoad, crossOrigin, nonce, integrity } = opts || {};
 
   const load = () => {
     return new Promise<HTMLScriptElement>((resolve, reject) => {
@@ -27,6 +28,7 @@ export async function loadScript(src = '', opts: LoadScriptOptions): Promise<HTM
       const script = document.createElement('script');
 
       crossOrigin && script.setAttribute('crossorigin', crossOrigin);
+      integrity && script.setAttribute('integrity', integrity);
       script.async = async || false;
       script.defer = defer || false;
 
