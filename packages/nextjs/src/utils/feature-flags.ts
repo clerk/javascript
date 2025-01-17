1c2d7a06781bc3eb2eb22be7b40a2f8e3afcf21d
panteliselef <panteliselef@outlook.com>
2025-01-17 16:32:01 +0200

    fix(nextjs): Building with missing envs on keyless should fail

--- packages/nextjs/src/utils/feature-flags.ts
+++ packages/nextjs/src/utils/feature-flags.ts
@@ -4,6 +4,6 @@ import { KEYLESS_DISABLED } from '../server/constants';
 import { isNextWithUnstableServerActions } from './sdk-versions';
 
 const canUseKeyless__server = !isNextWithUnstableServerActions && isDevelopmentEnvironment() && !KEYLESS_DISABLED;
-const canUseKeyless__client = !isNextWithUnstableServerActions && !KEYLESS_DISABLED;
+const canUseKeyless__client = !isNextWithUnstableServerActions && isDevelopmentEnvironment() && !KEYLESS_DISABLED;
 
 export { canUseKeyless__client, canUseKeyless__server };
