743018cd4df26277bcfb0bb765bc49c6850b3ea9
Jacek <jacek@clerk.dev>
2025-01-16 19:55:31 -0600

    fix(repo): Standardize ambient global.d.ts

--- packages/react/src/global.d.ts
+++ packages/react/src/global.d.ts
@@ -1,3 +1,6 @@
 declare const PACKAGE_NAME: string;
 declare const PACKAGE_VERSION: string;
 declare const __DEV__: boolean;
+declare const JS_PACKAGE_VERSION: string;
+
+declare const __BUILD_DISABLE_RHC__: boolean;
