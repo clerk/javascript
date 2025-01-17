9eef7713212378351e8e01628611eaa18de250e8
Jacek Radko <jacek@clerk.dev>
2025-01-17 11:50:03 -0600

    fix(repo): Standardize ambient global.d.ts (#4919)

--- packages/react-router/src/global.d.ts
+++ packages/react-router/src/global.d.ts
@@ -1,4 +1,2 @@
 declare const PACKAGE_NAME: string;
 declare const PACKAGE_VERSION: string;
-declare const JS_PACKAGE_VERSION: string;
-declare const __DEV__: boolean;
--- packages/react/src/global.d.ts
+++ packages/react/src/global.d.ts
@@ -2,3 +2,8 @@ declare const PACKAGE_NAME: string;
 declare const PACKAGE_VERSION: string;
 declare const JS_PACKAGE_VERSION: string;
 declare const __DEV__: boolean;
+
+declare module globalThis {
+  // eslint-disable-next-line no-var
+  var __BUILD_DISABLE_RHC__: boolean;
+}
