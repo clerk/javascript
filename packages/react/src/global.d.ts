a55385e4e7b83f79cd9c9def939e4c5d693ca618
Jacek <jacek@clerk.dev>
2025-01-17 08:15:19 -0600

    WIP

--- packages/react/src/global.d.ts
+++ packages/react/src/global.d.ts
@@ -1,6 +1,9 @@
 declare const PACKAGE_NAME: string;
 declare const PACKAGE_VERSION: string;
-declare const __DEV__: boolean;
 declare const JS_PACKAGE_VERSION: string;
+declare const __DEV__: boolean;
 
-declare const __BUILD_DISABLE_RHC__: boolean;
+declare module globalThis {
+  // eslint-disable-next-line no-var
+  var __BUILD_DISABLE_RHC__: boolean;
+}
