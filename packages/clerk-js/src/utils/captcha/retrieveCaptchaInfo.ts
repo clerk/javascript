27fb70a2534ecfc6d2573ba65fb32be2786b9d30
Nikos Douvlis <nikosdouvlis@gmail.com>
2025-01-17 12:44:51 +0200

    fix(clerk-js): Do not use CF proxy as fallback for challenges.cloudflare.com (#4922)

--- packages/clerk-js/src/utils/captcha/retrieveCaptchaInfo.ts
+++ packages/clerk-js/src/utils/captcha/retrieveCaptchaInfo.ts
@@ -1,9 +1,7 @@
 import type { Clerk } from '../../core/clerk';
-import { createFapiClient } from '../../core/fapiClient';
 
 export const retrieveCaptchaInfo = (clerk: Clerk) => {
   const _environment = clerk.__unstable__environment;
-  const fapiClient = createFapiClient(clerk);
   const captchaProvider = _environment ? _environment.displayConfig.captchaProvider : 'turnstile';
 
   return {
@@ -12,12 +10,5 @@ export const retrieveCaptchaInfo = (clerk: Clerk) => {
     captchaProvider,
     captchaPublicKeyInvisible: _environment ? _environment.displayConfig.captchaPublicKeyInvisible : null,
     canUseCaptcha: _environment ? _environment.userSettings.signUp.captcha_enabled && clerk.isStandardBrowser : null,
-    captchaURL: fapiClient
-      .buildUrl({
-        path: 'cloudflare/turnstile/v0/api.js',
-        pathPrefix: '',
-        search: '?render=explicit',
-      })
-      .toString(),
   };
 };
