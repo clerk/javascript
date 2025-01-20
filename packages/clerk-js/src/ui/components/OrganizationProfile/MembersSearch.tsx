b6ce5c786ed825f5d5bcd8fb7b11721e17b56882
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2025-01-20 14:55:34 -0300

    Introduce custom debounce logic

--- packages/clerk-js/src/ui/components/OrganizationProfile/MembersSearch.tsx
+++ packages/clerk-js/src/ui/components/OrganizationProfile/MembersSearch.tsx
@@ -1,39 +1,68 @@
 import { useOrganization } from '@clerk/shared/react';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { Flex, localizationKeys, useLocalizations } from '../../../ui/customizables';
 import { Animated, InputWithIcon } from '../../../ui/elements';
-import { useDebounce } from '../../../ui/hooks';
 import { MagnifyingGlass } from '../../../ui/icons';
 import { Spinner } from '../../../ui/primitives';
 
 export const MembersSearch = () => {
   const { t } = useLocalizations();
   const [search, setSearch] = useState('');
-  const query = useDebounce(search, 500);
+  const [query, setQuery] = useState('');
 
-  // TODO (perf) -> Only trigger query once user stops typing
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const debounceTimer = useRef<NodeJS.Timeout | undefined>();
+
   const { memberships } = useOrganization({
     memberships: {
       query,
     },
   });
 
-  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(event.target.value);
   };
+
+  // Updates query state to trigger query only once user stops typing
+  useEffect(() => {
+    if (!inputRef.current) {
+      return;
+    }
+
+    const controller = new AbortController();
+
+    inputRef.current.addEventListener(
+      'keyup',
+      () => {
+        clearTimeout(debounceTimer.current);
+
+        debounceTimer.current = setTimeout(() => {
+          setQuery(search);
+        }, 500);
+      },
+      {
+        signal: controller.signal,
+      },
+    );
+
+    return () => {
+      controller.abort();
+    };
+  }, [search]);
 
   return (
     <Animated asChild>
       <Flex sx={{ width: '100%' }}>
         <InputWithIcon
+          ref={inputRef}
           type='search'
           autoCapitalize='none'
           spellCheck={false}
           aria-label='Search'
           placeholder={t(localizationKeys('organizationProfile.membersPage.action__search'))}
           leftIcon={memberships?.isFetching ? <Spinner /> : <MagnifyingGlass />}
-          onChange={handleSearch}
+          onChange={handleChange}
           containerSx={{ width: '100%' }}
         />
       </Flex>
