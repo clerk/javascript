c6880d4a6f1b653b1c8aa6eb198eff9c3ee81337
Bryce Kalow <bryce@clerk.dev>
2025-01-21 22:27:58 -0600

    Make the option actually work

--- packages/clerk-js/src/core/clerk.ts
+++ packages/clerk-js/src/core/clerk.ts
@@ -326,7 +326,7 @@ export class Clerk implements ClerkInterface {
     }
 
     // Log a development mode warning once
-    if (this.#instanceType === 'development' && !this.#options.disableDevelopmentModeWarning) {
+    if (this.#instanceType === 'development' && !options?.disableDevelopmentModeWarning) {
       logger.warnOnce(
         'Clerk: Clerk has been loaded with development keys. Development instances have strict usage limits and should not be used when deploying your application to production. Learn more: https://clerk.com/docs/deployments/overview',
       );
