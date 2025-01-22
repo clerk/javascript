f87ede848265d75ea1e880a3ab80c53a250f42cf
Tom Milewski <me@tm.codes>
2025-01-22 12:05:15 -0500

    fix(shared): Remove `handleValueOrFn` from subpaths (#4972)

---
+++ packages/shared/src/handleValueOrFn.ts
@@ -0,0 +1,6 @@
+import { handleValueOrFn as origHandleValueOrFn } from './utils/handleValueOrFn';
+
+/**
+ * @deprecated - use `handleValueOrFn` from `@clerk/shared/utils` instead
+ */
+export const handleValueOrFn = origHandleValueOrFn;
