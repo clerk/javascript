0eef54094bbfdcc543176a43c1084e3b662cdc2c
Jacek <jacek@clerk.dev>
2025-01-24 13:39:44 -0600

    fix(astro): Correct typing for #async-local-storage

--- packages/astro/src/global.d.ts
+++ packages/astro/src/global.d.ts
@@ -1,2 +1,8 @@
 declare const PACKAGE_NAME: string;
 declare const PACKAGE_VERSION: string;
+
+declare module '#async-local-storage' {
+  import type { AsyncLocalStorage } from 'node:async_hooks';
+
+  export const authAsyncStorage: AsyncLocalStorage<unknown>;
+}
