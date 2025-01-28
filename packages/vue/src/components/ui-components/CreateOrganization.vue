0e20737a0d7387494f79aff5c64bdf43db89f28a
Robert Soriano <sorianorobertc@gmail.com>
2025-01-28 07:34:08 -0800

    Update packages/vue/src/components/ui-components/CreateOrganization.vue
    Co-authored-by: Lennart <lekoarts@gmail.com>

--- packages/vue/src/components/ui-components/CreateOrganization.vue
+++ packages/vue/src/components/ui-components/CreateOrganization.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ClerkHostRenderer } from '../ClerkHostRenderer';
 import { useClerk } from '../../composables';
-import { CreateOrganizationProps } from '@clerk/types';
+import type { CreateOrganizationProps } from '@clerk/types';
 
 const clerk = useClerk();
 const props = defineProps<CreateOrganizationProps>();
