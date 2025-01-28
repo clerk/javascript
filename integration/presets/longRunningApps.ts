0bf4534d47a66b437a502e8f3829d6bf889524a5
Alex Carpenter <alex.carpenter@clerk.dev>
2025-01-28 09:04:19 -0500

    add test

--- integration/presets/longRunningApps.ts
+++ integration/presets/longRunningApps.ts
@@ -37,6 +37,11 @@ export const createLongRunningApps = () => {
       config: next.appRouter,
       env: envs.withSignInOrUpEmailLinksFlow,
     },
+    {
+      id: 'next.appRouter.withSignInOrUpwithRestrictedModeFlow',
+      config: next.appRouter,
+      env: envs.withSignInOrUpwithRestrictedModeFlow,
+    },
     { id: 'quickstart.next.appRouter', config: next.appRouterQuickstart, env: envs.withEmailCodesQuickstart },
     { id: 'elements.next.appRouter', config: elements.nextAppRouter, env: envs.withEmailCodes },
     { id: 'astro.node.withCustomRoles', config: astro.node, env: envs.withCustomRoles },
