299b92e128a8da7b05ed8f1430dffefc8f8bd3f7
Stefanos Anagnostou <stefanos@clerk.dev>
2025-01-30 19:07:34 +0200

    fix(clerk-js): Refactor how we handle the style on the captcha element

--- packages/clerk-js/src/ui/elements/CaptchaElement.tsx
+++ packages/clerk-js/src/ui/elements/CaptchaElement.tsx
@@ -1,13 +1,47 @@
+import { useEffect, useRef } from 'react';
+
 import { CAPTCHA_ELEMENT_ID } from '../../utils/captcha';
 import { Box } from '../customizables';
 
-type CaptchaElementProps = {
-  maxHeight?: string;
-};
+export const CaptchaElement = () => {
+  const elementRef = useRef(null);
+  const maxHeightValueRef = useRef('0');
+  const minHeightValueRef = useRef('unset');
+  const marginBottomValueRef = useRef('unset');
 
-export const CaptchaElement = ({ maxHeight }: CaptchaElementProps) => (
-  <Box
-    id={CAPTCHA_ELEMENT_ID}
-    sx={{ display: 'block', alignSelf: 'center', maxHeight }}
-  />
-);
+  useEffect(() => {
+    if (!elementRef.current) return;
+
+    const observer = new MutationObserver(mutations => {
+      mutations.forEach(mutation => {
+        const target = mutation.target as HTMLDivElement;
+        if (mutation.type === 'attributes' && mutation.attributeName === 'style' && elementRef.current) {
+          maxHeightValueRef.current = target.style.maxHeight || '0';
+          minHeightValueRef.current = target.style.minHeight || 'unset';
+          marginBottomValueRef.current = target.style.marginBottom || 'unset';
+        }
+      });
+    });
+
+    observer.observe(elementRef.current, {
+      attributes: true,
+      attributeFilter: ['style'],
+    });
+
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <Box
+      ref={elementRef}
+      id={CAPTCHA_ELEMENT_ID}
+      style={{
+        display: 'block',
+        alignSelf: 'center',
+        maxHeight: maxHeightValueRef.current,
+        minHeight: minHeightValueRef.current,
+        marginBottom: marginBottomValueRef.current,
+      }}
+    />
+  );
+};
