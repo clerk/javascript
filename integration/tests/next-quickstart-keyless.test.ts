42b52e08df6cba6adad1722dcf4e84b239233881
panteliselef <panteliselef@outlook.com>
2025-01-30 16:24:49 +0200

    cleanup 2

--- integration/tests/next-quickstart-keyless.test.ts
+++ integration/tests/next-quickstart-keyless.test.ts
@@ -1,3 +1,4 @@
+import type { Page } from '@playwright/test';
 import { expect, test } from '@playwright/test';
 
 import type { Application } from '../models/application';
@@ -5,6 +6,21 @@ import { appConfigs } from '../presets';
 import { createTestUtils } from '../testUtils';
 
 const commonSetup = appConfigs.next.appRouterQuickstart.clone();
+
+const mockClaimedInstanceEnvironmentCall = async (page: Page) => {
+  await page.route('*/**/v1/environment*', async route => {
+    const response = await route.fetch();
+    const json = await response.json();
+    const newJson = {
+      ...json,
+      auth_config: {
+        ...json.auth_config,
+        claimed_at: Date.now(),
+      },
+    };
+    await route.fulfill({ response, json: newJson });
+  });
+};
 
 test.describe('Keyless mode @quickstart', () => {
   test.describe.configure({ mode: 'serial' });
@@ -27,9 +43,8 @@ test.describe('Keyless mode @quickstart', () => {
     await u.page.waitForClerkJsLoaded();
     await u.po.expect.toBeSignedOut();
 
-    await u.page.waitForAppUrl('/');
-
     await u.po.keylessPopover.waitForMounted();
+
     expect(await u.po.keylessPopover.isExpanded()).toBe(false);
     await u.po.keylessPopover.toggle();
     expect(await u.po.keylessPopover.isExpanded()).toBe(true);
@@ -50,25 +65,14 @@ test.describe('Keyless mode @quickstart', () => {
     page,
     context,
   }) => {
-    await page.route('*/**/v1/environment*', async route => {
-      const response = await route.fetch();
-      const json = await response.json();
-      const newJson = {
-        ...json,
-        auth_config: {
-          ...json.auth_config,
-          claimed_at: Date.now(),
-        },
-      };
-      await route.fulfill({ response, json: newJson });
-    });
-
+    await mockClaimedInstanceEnvironmentCall(page);
     const u = createTestUtils({ app, page, context });
     await u.page.goToAppHome();
     await u.page.waitForClerkJsLoaded();
+
     await u.po.keylessPopover.waitForMounted();
     expect(await u.po.keylessPopover.isExpanded()).toBe(true);
-    await expect(await u.po.keylessPopover.promptToUseClaimedKeys()).toBeVisible();
+    await expect(u.po.keylessPopover.promptToUseClaimedKeys()).toBeVisible();
 
     const [newPage] = await Promise.all([
       context.waitForEvent('page'),
@@ -87,25 +91,22 @@ test.describe('Keyless mode @quickstart', () => {
     page,
     context,
   }) => {
-    await page.route('*/**/v1/environment*', async route => {
-      const response = await route.fetch();
-      const json = await response.json();
-      const newJson = {
-        ...json,
-        auth_config: {
-          ...json.auth_config,
-          claimed_at: Date.now(),
-        },
-      };
-      await route.fulfill({ response, json: newJson });
-    });
-
+    await mockClaimedInstanceEnvironmentCall(page);
     const u = createTestUtils({ app, page, context });
     await u.page.goToAppHome();
+
     await u.po.keylessPopover.waitForMounted();
     await expect(await u.po.keylessPopover.promptToUseClaimedKeys()).toBeVisible();
+
+    /**
+     * Copy keys from `.clerk/.tmp/keyless.json to `.env`
+     */
     await app.keylessToEnv();
+    /**
+     * wait a bit for the server to load the new env file
+     */
     await page.waitForTimeout(5_000);
+
     await page.reload();
     await u.po.keylessPopover.waitForMounted();
     await u.po.keylessPopover.promptToDismiss().click();
