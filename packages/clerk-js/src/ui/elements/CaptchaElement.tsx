e79e8ac969261f2cd4f090e655e8cf41808ae204
Stefanos Anagnostou <stefanos@clerk.dev>
2025-01-31 12:48:27 +0200

    add comments

--- packages/clerk-js/src/ui/elements/CaptchaElement.tsx
+++ packages/clerk-js/src/ui/elements/CaptchaElement.tsx
@@ -3,6 +3,12 @@ import { useEffect, useRef } from 'react';
 import { CAPTCHA_ELEMENT_ID } from '../../utils/captcha';
 import { Box } from '../customizables';
 
+/**
+ * This component uses a MutationObserver to listen for DOM changes made by our Turnstile logic,
+ * which operates outside the React lifecycle. It stores the observed state in ref to ensure that
+ * any external style changes, such as updates to max-height, min-height, or margin-bottom persist across re-renders,
+ * preventing unwanted layout resets.
+ */
 export const CaptchaElement = () => {
   const elementRef = useRef(null);
   const maxHeightValueRef = useRef('0');
