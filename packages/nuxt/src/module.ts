c7e00327783e8d917ced1f18b563e8d8c041eb09
Robert Soriano <sorianorobertc@gmail.com>
2025-02-03 07:05:10 -0800

    Update module.ts
    Co-authored-by: Alex Carpenter <im.alexcarpenter@gmail.com>

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -129,7 +129,7 @@ export default defineNuxtModule<ModuleOptions>({
       'RedirectToCreateOrganization',
       'SignedIn',
       'SignedOut',
-      'Waitlist'
+      'Waitlist',
     ];
     components.forEach(component => {
       void addComponent({
