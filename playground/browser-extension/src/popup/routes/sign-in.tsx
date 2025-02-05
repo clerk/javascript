755ba247b5aa9953314e1f72b57a54a9006bc46a
Tom Milewski <me@tm.codes>
2025-02-04 20:14:51 -0500

    Update the browser extension playground (#5080)

--- playground/browser-extension/src/popup/routes/sign-in.tsx
+++ playground/browser-extension/src/popup/routes/sign-in.tsx
@@ -2,9 +2,15 @@ import { SignIn } from "@clerk/chrome-extension";
 
 export const SignInPage = () => {
   return (
-    <>
-      <p>Sign In</p>
-      <SignIn routing="virtual" />
-    </>
+    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-[543px] plasmo-overflow-y-auto">
+      <SignIn
+        appearance={{
+          elements: {
+            socialButtons: "plasmo-hidden",
+            dividerRow: "plasmo-hidden"
+          }
+        }}
+      />
+    </div>
   );
 };
