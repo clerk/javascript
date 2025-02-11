3cd5a0e092b2a59791694e57ba351c96f0b1f479
wobsoriano <sorianorobertc@gmail.com>
2025-02-10 19:02:53 -0800

    feat: Add organization custom pages and links via OrganizationSwitcher

--- integration/templates/vue-vite/src/views/Home.vue
+++ integration/templates/vue-vite/src/views/Home.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-import { useAuth } from '@clerk/vue';
+import { SignedIn, OrganizationSwitcher, useAuth } from '@clerk/vue';
 
 const { isSignedIn } = useAuth();
 </script>
 
 <template>
+  <SignedIn>
+    <OrganizationSwitcher />
+  </SignedIn>
   <div>
     <ul>
       <li v-if="isSignedIn"><RouterLink to="/profile">Profile</RouterLink></li>
--- integration/templates/vue-vite/src/views/custom-pages/OrganizationProfile.vue
+++ integration/templates/vue-vite/src/views/custom-pages/OrganizationProfile.vue
@@ -1,9 +1,41 @@
 <script setup lang="ts">
-import { OrganizationProfile } from '@clerk/vue';
+import { OrganizationProfile, OrganizationSwitcher } from '@clerk/vue';
+import { useRoute } from 'vue-router';
+
+// Added a dedicatedPage query param to conditionally render the OrganizationProfile
+// as for some reason, the menu items in the OrganizationSwitcher
+// goes out of bounds in test environment.
+const route = useRoute();
+const dedicatedPage = route.query.dedicatedPage === 'true';
 </script>
 
 <template>
-  <OrganizationProfile>
+  <div>is dedicated page: {{ dedicatedPage }}</div>
+  <OrganizationSwitcher>
+    <OrganizationSwitcher.OrganizationProfilePage
+      v-if="!dedicatedPage"
+        label="Terms"
+        url="terms"
+      >
+        <template #labelIcon>
+          <div>Icon</div>
+        </template>
+        <div>
+          <h1>Custom Terms Page</h1>
+          <p>This is the custom terms page</p>
+        </div>
+      </OrganizationSwitcher.OrganizationProfilePage>
+      <OrganizationSwitcher.OrganizationProfileLink
+        v-if="!dedicatedPage"
+        label="Homepage"
+        url="/"
+      >
+        <template #labelIcon>
+          <div>Icon</div>
+        </template>
+      </OrganizationSwitcher.OrganizationProfileLink>
+  </OrganizationSwitcher>
+  <OrganizationProfile v-if="dedicatedPage">
     <OrganizationProfile.Page
       label="Terms"
       url="terms"
