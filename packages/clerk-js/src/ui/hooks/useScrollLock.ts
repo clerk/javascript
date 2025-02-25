1c0612df3f73e592742b3906960bb34e7de36495
Vaggelis Yfantis <me@octoper.me>
2025-02-25 20:50:26 +0200

    fix(clerk-js): Early return

--- packages/clerk-js/src/ui/hooks/useScrollLock.ts
+++ packages/clerk-js/src/ui/hooks/useScrollLock.ts
@@ -9,24 +9,22 @@ let oldOverflow: string;
 export const useScrollLock = <T extends HTMLElement>(el: T) => {
   const disableScroll = () => {
     preventScrollCount++;
-    if (preventScrollCount === 1) {
-      oldPaddingRightPx = getComputedStyle(el).paddingRight;
-      oldOverflow = getComputedStyle(el).overflow;
-      const oldWidth = el.clientWidth;
-      el.style.overflow = 'hidden';
-      const currentWidth = el.clientWidth;
-      const oldPaddingRight = Number.parseInt(oldPaddingRightPx.replace('px', ''));
-      el.style.paddingRight = `${currentWidth - oldWidth + oldPaddingRight}px`;
-    }
+    if (preventScrollCount !== 1) return;
+    oldPaddingRightPx = getComputedStyle(el).paddingRight;
+    oldOverflow = getComputedStyle(el).overflow;
+    const oldWidth = el.clientWidth;
+    el.style.overflow = 'hidden';
+    const currentWidth = el.clientWidth;
+    const oldPaddingRight = Number.parseInt(oldPaddingRightPx.replace('px', ''));
+    el.style.paddingRight = `${currentWidth - oldWidth + oldPaddingRight}px`;
   };
 
   const enableScroll = () => {
     preventScrollCount--;
-    if (preventScrollCount === 0) {
-      el.style.overflow = oldOverflow;
-      if (oldPaddingRightPx) {
-        el.style.paddingRight = oldPaddingRightPx;
-      }
+    if (preventScrollCount !== 0) return; 
+    el.style.overflow = oldOverflow;
+    if (oldPaddingRightPx) {
+      el.style.paddingRight = oldPaddingRightPx;
     }
   };
 
