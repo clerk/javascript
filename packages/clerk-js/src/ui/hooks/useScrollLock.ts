d65dde27b90f5dfab463c51faf1bd88a511194ac
Vaggelis Yfantis <me@octoper.me>
2025-02-25 18:02:40 +0200

    fix(clerk-js): Correctly handle scroll lock for multiple modals

--- packages/clerk-js/src/ui/hooks/useScrollLock.ts
+++ packages/clerk-js/src/ui/hooks/useScrollLock.ts
@@ -1,26 +1,32 @@
+let preventScrollCount = 0;
+let oldPaddingRightPx: string;
+let oldOverflow: string;
 /**
  * Disables scroll for an element.
  * Adds extra padding to prevent layout shifting
  * caused by hiding the scrollbar.
  */
 export const useScrollLock = <T extends HTMLElement>(el: T) => {
-  let oldPaddingRightPx: string;
-  let oldOverflow: string;
-
   const disableScroll = () => {
-    oldPaddingRightPx = getComputedStyle(el).paddingRight;
-    oldOverflow = getComputedStyle(el).overflow;
-    const oldWidth = el.clientWidth;
-    el.style.overflow = 'hidden';
-    const currentWidth = el.clientWidth;
-    const oldPaddingRight = Number.parseInt(oldPaddingRightPx.replace('px', ''));
-    el.style.paddingRight = `${currentWidth - oldWidth + oldPaddingRight}px`;
+    preventScrollCount++;
+    if (preventScrollCount === 1) {
+      oldPaddingRightPx = getComputedStyle(el).paddingRight;
+      oldOverflow = getComputedStyle(el).overflow;
+      const oldWidth = el.clientWidth;
+      el.style.overflow = 'hidden';
+      const currentWidth = el.clientWidth;
+      const oldPaddingRight = Number.parseInt(oldPaddingRightPx.replace('px', ''));
+      el.style.paddingRight = `${currentWidth - oldWidth + oldPaddingRight}px`;
+    }
   };
 
   const enableScroll = () => {
-    el.style.overflow = oldOverflow;
-    if (oldPaddingRightPx) {
-      el.style.paddingRight = oldPaddingRightPx;
+    preventScrollCount--;
+    if (preventScrollCount === 0) {
+      el.style.overflow = oldOverflow;
+      if (oldPaddingRightPx) {
+        el.style.paddingRight = oldPaddingRightPx;
+      }
     }
   };
 
