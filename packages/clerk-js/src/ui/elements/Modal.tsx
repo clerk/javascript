953878805b153dea39e37f0a3bee904d3fc57f5e
Alex Carpenter <alex.carpenter@clerk.dev>
2025-02-27 11:07:17 -0500

    init

--- packages/clerk-js/src/ui/elements/Modal.tsx
+++ packages/clerk-js/src/ui/elements/Modal.tsx
@@ -1,4 +1,4 @@
-import { createContextAndHook } from '@clerk/shared/react';
+import { createContextAndHook, useSafeLayoutEffect } from '@clerk/shared/react';
 import { FloatingOverlay } from '@floating-ui/react';
 import React, { useRef } from 'react';
 
@@ -21,6 +21,9 @@ type ModalProps = React.PropsWithChildren<{
   style?: React.CSSProperties;
 }>;
 
+let cleanup = () => {};
+let lockCount = 0;
+
 export const Modal = withFloatingTree((props: ModalProps) => {
   const { handleClose, handleOpen, contentSx, containerSx, canCloseModal, id, style } = props;
   const overlayRef = useRef<HTMLDivElement>(null);
@@ -38,6 +41,21 @@ export const Modal = withFloatingTree((props: ModalProps) => {
       handleOpen?.();
     }
   }, [isOpen]);
+
+  useSafeLayoutEffect(() => {
+    lockCount++;
+
+    if (lockCount === 1) {
+      cleanup = enableScrollLock();
+    }
+
+    return () => {
+      lockCount--;
+      if (lockCount === 0) {
+        cleanup();
+      }
+    };
+  });
 
   const modalCtx = React.useMemo(() => ({ value: canCloseModal === false ? {} : { toggle } }), [toggle, canCloseModal]);
 
@@ -97,3 +115,70 @@ export const Modal = withFloatingTree((props: ModalProps) => {
     </Popover>
   );
 });
+
+interface NavigatorUAData {
+  brands: Array<{ brand: string; version: string }>;
+  mobile: boolean;
+  platform: string;
+}
+
+// Avoid Chrome DevTools blue warning.
+export function getPlatform(): string {
+  const uaData = (navigator as any).userAgentData as NavigatorUAData | undefined;
+
+  if (uaData?.platform) {
+    return uaData.platform;
+  }
+
+  return navigator.platform;
+}
+
+function enableScrollLock() {
+  const isIOS = /iP(hone|ad|od)|iOS/.test(getPlatform());
+  const bodyStyle = document.body.style;
+  // RTL <body> scrollbar
+  const scrollbarX =
+    Math.round(document.documentElement.getBoundingClientRect().left) + document.documentElement.scrollLeft;
+  const paddingProp = scrollbarX ? 'paddingLeft' : 'paddingRight';
+  const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+  const scrollX = bodyStyle.left ? parseFloat(bodyStyle.left) : window.scrollX;
+  const scrollY = bodyStyle.top ? parseFloat(bodyStyle.top) : window.scrollY;
+
+  bodyStyle.overflow = 'hidden';
+
+  if (scrollbarWidth) {
+    bodyStyle[paddingProp] = `${scrollbarWidth}px`;
+  }
+
+  // Only iOS doesn't respect `overflow: hidden` on document.body, and this
+  // technique has fewer side effects.
+  if (isIOS) {
+    // iOS 12 does not support `visualViewport`.
+    const offsetLeft = window.visualViewport?.offsetLeft || 0;
+    const offsetTop = window.visualViewport?.offsetTop || 0;
+
+    Object.assign(bodyStyle, {
+      position: 'fixed',
+      top: `${-(scrollY - Math.floor(offsetTop))}px`,
+      left: `${-(scrollX - Math.floor(offsetLeft))}px`,
+      right: '0',
+    });
+  }
+
+  return () => {
+    Object.assign(bodyStyle, {
+      overflow: '',
+      [paddingProp]: '',
+    });
+
+    if (isIOS) {
+      Object.assign(bodyStyle, {
+        position: '',
+        top: '',
+        left: '',
+        right: '',
+      });
+      window.scrollTo(scrollX, scrollY);
+    }
+  };
+}
