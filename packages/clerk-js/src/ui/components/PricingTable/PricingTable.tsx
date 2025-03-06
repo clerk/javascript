db2cb09874ef3a4f82f15140efef6bdb776bf546
Alex Carpenter <alex.carpenter@clerk.dev>
2025-03-06 10:17:22 -0500

    add comments

--- packages/clerk-js/src/ui/components/PricingTable/PricingTable.tsx
+++ packages/clerk-js/src/ui/components/PricingTable/PricingTable.tsx
@@ -36,14 +36,19 @@ export const __experimental_PricingTable = (props: __experimental_PricingTablePr
       <Box
         elementDescriptor={descriptors.pricingTable}
         sx={t => ({
+          // Sets the minimum width a column can be before wrapping
           '--grid-min-size': isCompact ? '11.75rem' : '20rem',
+          // Set a max amount of columns before they start wrapping to new rows.
           '--grid-max-columns': 'infinity',
+          // Set the default gap, use `--grid-gap-y` to override the row gap
           '--grid-gap': t.space.$4,
+          // Derived from the maximum column size based on the grid configuration
           '--max-column-width': '100% / var(--grid-max-columns, infinity) - var(--grid-gap)',
+          // Derived from `--max-column-width` and ensures it respects the minimum size and maximum width constraints
           '--column-width': 'max(var(--max-column-width), min(var(--grid-min-size, 10rem), 100%))',
           display: 'grid',
           gridTemplateColumns: 'repeat(auto-fit, minmax(var(--column-width), 1fr))',
-          gap: `var(--grid-gap-vertical, var(--grid-gap, ${t.space.$4})) var(--grid-gap, ${t.space.$4})`,
+          gap: `var(--grid-gap-y, var(--grid-gap, ${t.space.$4})) var(--grid-gap, ${t.space.$4})`,
           alignItems: 'start',
           width: '100%',
           minWidth: '0',
