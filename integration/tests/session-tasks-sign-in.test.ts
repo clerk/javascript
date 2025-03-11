f05f96429a567e415e7f5ca0e1da565b40a0e0bd
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2025-03-11 10:37:33 -0300

    Refactor integration tests to use use long running apps

--- integration/tests/session-tasks-sign-in.test.ts
+++ integration/tests/session-tasks-sign-in.test.ts
@@ -1,41 +1,38 @@
 import { expect, test } from '@playwright/test';
 
-import type { Application } from '../models/application';
 import { appConfigs } from '../presets';
 import type { FakeUser } from '../testUtils';
-import { createTestUtils } from '../testUtils';
+import { createTestUtils, testAgainstRunningApps } from '../testUtils';
 
-test.describe('session tasks after sign-in flow @nextjs', () => {
-  test.describe.configure({ mode: 'serial' });
-  let app: Application;
-  let fakeUser: FakeUser;
+testAgainstRunningApps({ withEnv: [appConfigs.envs.withSessionTasks] })(
+  'session tasks after sign-in flow @nextjs',
+  ({ app }) => {
+    test.describe.configure({ mode: 'serial' });
 
-  test.beforeAll(async () => {
-    app = await appConfigs.next.appRouter.clone().commit();
-    await app.setup();
-    await app.withEnv(appConfigs.envs.withSessionTasks);
-    await app.dev();
+    let fakeUser: FakeUser;
 
-    const m = createTestUtils({ app });
-    fakeUser = m.services.users.createFakeUser();
-    await m.services.users.createBapiUser(fakeUser);
-  });
+    test.beforeAll(async () => {
+      const u = createTestUtils({ app });
+      fakeUser = u.services.users.createFakeUser();
+      await u.services.users.createBapiUser(fakeUser);
+    });
 
-  test.afterAll(async () => {
-    await fakeUser.deleteIfExists();
-    await app.teardown();
-  });
+    test.afterAll(async () => {
+      await fakeUser.deleteIfExists();
+      await app.teardown();
+    });
 
-  test('navigate to task on after sign-in', async ({ page, context }) => {
-    const u = createTestUtils({ app, page, context });
-    await u.po.signIn.goTo();
-    await u.po.signIn.setIdentifier(fakeUser.email);
-    await u.po.signIn.continue();
-    await u.po.signIn.setPassword(fakeUser.password);
-    await u.po.signIn.continue();
-    await u.po.expect.toBeSignedIn();
+    test('navigate to task on after sign-in', async ({ page, context }) => {
+      const u = createTestUtils({ app, page, context });
+      await u.po.signIn.goTo();
+      await u.po.signIn.setIdentifier(fakeUser.email);
+      await u.po.signIn.continue();
+      await u.po.signIn.setPassword(fakeUser.password);
+      await u.po.signIn.continue();
+      await u.po.expect.toBeSignedIn();
 
-    await expect(u.page.getByRole('button', { name: /create organization/i })).toBeVisible();
-    expect(page.url()).toContain('add-organization');
-  });
-});
+      await expect(u.page.getByRole('button', { name: /create organization/i })).toBeVisible();
+      expect(page.url()).toContain('add-organization');
+    });
+  },
+);
--- integration/tests/session-tasks-sign-up.test.ts
+++ integration/tests/session-tasks-sign-up.test.ts
@@ -1,40 +1,34 @@
 import { expect, test } from '@playwright/test';
 
-import type { Application } from '../models/application';
 import { appConfigs } from '../presets';
-import { createTestUtils } from '../testUtils';
+import { createTestUtils, testAgainstRunningApps } from '../testUtils';
 
-test.describe('session tasks after sign-up flow @nextjs', () => {
-  test.describe.configure({ mode: 'serial' });
-  let app: Application;
+testAgainstRunningApps({ withEnv: [appConfigs.envs.withSessionTasks] })(
+  'session tasks after sign-up flow @nextjs',
+  ({ app }) => {
+    test.describe.configure({ mode: 'serial' });
 
-  test.beforeAll(async () => {
-    app = await appConfigs.next.appRouter.clone().commit();
-    await app.setup();
-    await app.withEnv(appConfigs.envs.withSessionTasks);
-    await app.dev();
-  });
-
-  test.afterAll(async () => {
-    await app.teardown();
-  });
-
-  test('navigate to task on after sign-up', async ({ page, context }) => {
-    const u = createTestUtils({ app, page, context });
-    const fakeUser = u.services.users.createFakeUser({
-      fictionalEmail: true,
-      withPhoneNumber: true,
-      withUsername: true,
-    });
-    await u.po.signUp.goTo();
-    await u.po.signUp.signUpWithEmailAndPassword({
-      email: fakeUser.email,
-      password: fakeUser.password,
+    test.afterAll(async () => {
+      await app.teardown();
     });
 
-    await expect(u.page.getByRole('button', { name: /create organization/i })).toBeVisible();
-    expect(page.url()).toContain('add-organization');
+    test('navigate to task on after sign-up', async ({ page, context }) => {
+      const u = createTestUtils({ app, page, context });
+      const fakeUser = u.services.users.createFakeUser({
+        fictionalEmail: true,
+        withPhoneNumber: true,
+        withUsername: true,
+      });
+      await u.po.signUp.goTo();
+      await u.po.signUp.signUpWithEmailAndPassword({
+        email: fakeUser.email,
+        password: fakeUser.password,
+      });
 
-    await fakeUser.deleteIfExists();
-  });
-});
+      await expect(u.page.getByRole('button', { name: /create organization/i })).toBeVisible();
+      expect(page.url()).toContain('add-organization');
+
+      await fakeUser.deleteIfExists();
+    });
+  },
+);
