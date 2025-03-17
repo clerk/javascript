32dc6f2336a803fe4dcf510fb6f2742f427c34aa
wobsoriano <sorianorobertc@gmail.com>
2025-03-17 10:57:04 -0700

    feat(clerk-expo): Export token cache with SecureStore implementation

---
+++ packages/expo/src/token-cache/index.ts
@@ -0,0 +1,40 @@
+import * as SecureStore from 'expo-secure-store';
+import { Platform } from 'react-native';
+
+import type { TokenCache } from '../cache';
+
+/**
+ * Create a token cache using Expo's SecureStore
+ */
+const createTokenCache = (): TokenCache => {
+  return {
+    getToken: async (key: string) => {
+      try {
+        const item = await SecureStore.getItemAsync(key);
+        return item;
+      } catch (error) {
+        console.error('secure store get item error: ', error);
+        await SecureStore.deleteItemAsync(key);
+        return null;
+      }
+    },
+    saveToken: (key: string, token: string) => {
+      return SecureStore.setItemAsync(key, token);
+    },
+  };
+};
+
+/**
+ * Secure token cache implementation for Expo apps.
+ *
+ * Clerk stores the active user's session token in memory by default. In Expo apps, the
+ * recommended way to store sensitive data, such as tokens, is by using `expo-secure-store`
+ * which encrypts the data before storing it.
+ *
+ * To implement your own token cache, create an object that implements the `TokenCache` interface:
+ * - `getToken(key: string): Promise<string | null>`
+ * - `saveToken(key: string, token: string): Promise<void>`
+ *
+ * @returns {TokenCache | undefined} Object with `getToken` and `saveToken` methods, undefined on web
+ */
+export const tokenCache = Platform.OS !== 'web' ? createTokenCache() : undefined;
