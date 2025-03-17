896a794a3520bdc6227b8689a9a0f0a0da55f56a
LekoArts <lekoarts@gmail.com>
2025-03-17 11:32:43 +0100

    typedoc improvements

--- .typedoc/custom-theme.mjs
+++ .typedoc/custom-theme.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { ReflectionKind } from 'typedoc';
+import { ArrayType, IntersectionType, ReflectionKind, ReflectionType, UnionType } from 'typedoc';
 import { MarkdownTheme, MarkdownThemeContext } from 'typedoc-plugin-markdown';
 
 /**
@@ -234,6 +234,114 @@ class ClerkMarkdownThemeContext extends MarkdownThemeContext {
         }
 
         md.push(this.partials.body(model, { headingLevel: options.headingLevel }));
+
+        return md.join('\n\n');
+      },
+      /**
+       * Copied from default theme / source code. This hides the "Type parameters" section and the declaration title from the output
+       * https://github.com/typedoc2md/typedoc-plugin-markdown/blob/e798507a3c04f9ddf7710baf4cc7836053e438ff/packages/typedoc-plugin-markdown/src/theme/context/partials/member.declaration.ts
+       * @param {import('typedoc').DeclarationReflection} model
+       * @param {{ headingLevel: number, nested?: boolean }} options
+       */
+      declaration: (model, options = { headingLevel: 2, nested: false }) => {
+        const md = [];
+
+        const opts = {
+          nested: false,
+          ...options,
+        };
+
+        if (!opts.nested && model.sources && !this.options.getValue('disableSources')) {
+          md.push(this.partials.sources(model));
+        }
+
+        if (model?.documents) {
+          md.push(
+            this.partials.documents(model, {
+              headingLevel: options.headingLevel,
+            }),
+          );
+        }
+
+        /**
+         * @type any
+         */
+        const modelType = model.type;
+        /**
+         * @type {import('typedoc').DeclarationReflection}
+         */
+        let typeDeclaration = modelType?.declaration;
+
+        if (model.type instanceof ArrayType && model.type?.elementType instanceof ReflectionType) {
+          typeDeclaration = model.type?.elementType?.declaration;
+        }
+
+        const hasTypeDeclaration =
+          Boolean(typeDeclaration) ||
+          (model.type instanceof UnionType && model.type?.types.some(type => type instanceof ReflectionType));
+
+        if (model.comment) {
+          md.push(
+            this.partials.comment(model.comment, {
+              headingLevel: opts.headingLevel,
+              showSummary: true,
+              showTags: false,
+            }),
+          );
+        }
+
+        if (model.type instanceof IntersectionType) {
+          model.type?.types?.forEach(intersectionType => {
+            if (intersectionType instanceof ReflectionType && !intersectionType.declaration.signatures) {
+              if (intersectionType.declaration.children) {
+                md.push(heading(opts.headingLevel, this.i18n.theme_type_declaration()));
+
+                md.push(
+                  this.partials.typeDeclaration(intersectionType.declaration, {
+                    headingLevel: opts.headingLevel,
+                  }),
+                );
+              }
+            }
+          });
+        }
+
+        if (hasTypeDeclaration) {
+          if (model.type instanceof UnionType) {
+            if (this.helpers.hasUsefulTypeDetails(model.type)) {
+              md.push(heading(opts.headingLevel, this.i18n.theme_type_declaration()));
+
+              model.type.types.forEach(type => {
+                if (type instanceof ReflectionType) {
+                  md.push(this.partials.someType(type, { forceCollapse: true }));
+                  md.push(this.partials.typeDeclarationContainer(model, type.declaration, options));
+                } else {
+                  md.push(`${this.partials.someType(type)}`);
+                }
+              });
+            }
+          } else {
+            const useHeading =
+              typeDeclaration?.children?.length &&
+              (model.kind !== ReflectionKind.Property || this.helpers.useTableFormat('properties'));
+            if (useHeading) {
+              md.push(heading(opts.headingLevel, this.i18n.theme_type_declaration()));
+            }
+            md.push(this.partials.typeDeclarationContainer(model, typeDeclaration, options));
+          }
+        }
+        if (model.comment) {
+          md.push(
+            this.partials.comment(model.comment, {
+              headingLevel: opts.headingLevel,
+              showSummary: false,
+              showTags: true,
+              showReturns: true,
+            }),
+          );
+        }
+
+        md.push(this.partials.inheritance(model, { headingLevel: opts.headingLevel }));
 
         return md.join('\n\n');
       },
