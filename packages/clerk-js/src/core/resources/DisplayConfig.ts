b9da1b92c0ebb095ccefbef502ea8db1df082cf4
Jacek <jacek@clerk.dev>
2025-03-17 11:29:04 -0500

    displayconfig

--- packages/clerk-js/src/core/resources/DisplayConfig.ts
+++ packages/clerk-js/src/core/resources/DisplayConfig.ts
@@ -63,42 +63,54 @@ export class DisplayConfig extends BaseResource implements DisplayConfigResource
       return this;
     }
 
-    this.afterCreateOrganizationUrl = data.after_create_organization_url ?? this.afterCreateOrganizationUrl;
-    this.afterJoinWaitlistUrl = data.after_join_waitlist_url ?? this.afterJoinWaitlistUrl;
-    this.afterLeaveOrganizationUrl = data.after_leave_organization_url ?? this.afterLeaveOrganizationUrl;
-    this.afterSignInUrl = data.after_sign_in_url ?? this.afterSignInUrl;
-    this.afterSignOutAllUrl = data.after_sign_out_all_url ?? this.afterSignOutAllUrl;
-    this.afterSignOutOneUrl = data.after_sign_out_one_url ?? this.afterSignOutOneUrl;
-    this.afterSignUpUrl = data.after_sign_up_url ?? this.afterSignUpUrl;
-    this.afterSwitchSessionUrl = data.after_switch_session_url ?? this.afterSwitchSessionUrl;
-    this.applicationName = data.application_name ?? this.applicationName;
-    this.branded = data.branded ?? this.branded;
-    this.captchaHeartbeat = data.captcha_heartbeat ?? this.captchaHeartbeat;
-    this.captchaHeartbeatIntervalMs = data.captcha_heartbeat_interval_ms ?? this.captchaHeartbeatIntervalMs;
-    this.captchaOauthBypass = data.captcha_oauth_bypass ?? this.captchaOauthBypass;
-    this.captchaProvider = data.captcha_provider ?? this.captchaProvider;
-    this.captchaPublicKey = data.captcha_public_key ?? this.captchaPublicKey;
-    this.captchaPublicKeyInvisible = data.captcha_public_key_invisible ?? this.captchaPublicKeyInvisible;
-    this.captchaWidgetType = data.captcha_widget_type ?? this.captchaWidgetType;
-    this.clerkJSVersion = data.clerk_js_version ?? this.clerkJSVersion;
-    this.createOrganizationUrl = data.create_organization_url ?? this.createOrganizationUrl;
-    this.faviconImageUrl = data.favicon_image_url ?? this.faviconImageUrl;
-    this.googleOneTapClientId = data.google_one_tap_client_id ?? this.googleOneTapClientId;
-    this.homeUrl = data.home_url ?? this.homeUrl;
-    this.id = data.id ?? this.id;
-    this.instanceEnvironmentType = data.instance_environment_type ?? this.instanceEnvironmentType;
-    this.logoImageUrl = data.logo_image_url ?? this.logoImageUrl;
-    this.organizationProfileUrl = data.organization_profile_url ?? this.organizationProfileUrl;
-    this.preferredSignInStrategy = data.preferred_sign_in_strategy ?? this.preferredSignInStrategy;
-    this.privacyPolicyUrl = data.privacy_policy_url ?? this.privacyPolicyUrl;
-    this.showDevModeWarning = data.show_devmode_warning ?? this.showDevModeWarning;
-    this.signInUrl = data.sign_in_url ?? this.signInUrl;
-    this.signUpUrl = data.sign_up_url ?? this.signUpUrl;
-    this.supportEmail = data.support_email ?? this.supportEmail;
-    this.termsUrl = data.terms_url ?? this.termsUrl;
-    this.theme = data.theme ?? this.theme;
-    this.userProfileUrl = data.user_profile_url ?? this.userProfileUrl;
-    this.waitlistUrl = data.waitlist_url ?? this.waitlistUrl;
+    this.afterCreateOrganizationUrl = this.withDefault(
+      data.after_create_organization_url,
+      this.afterCreateOrganizationUrl,
+    );
+    this.afterJoinWaitlistUrl = this.withDefault(data.after_join_waitlist_url, this.afterJoinWaitlistUrl);
+    this.afterLeaveOrganizationUrl = this.withDefault(
+      data.after_leave_organization_url,
+      this.afterLeaveOrganizationUrl,
+    );
+    this.afterSignInUrl = this.withDefault(data.after_sign_in_url, this.afterSignInUrl);
+    this.afterSignOutAllUrl = this.withDefault(data.after_sign_out_all_url, this.afterSignOutAllUrl);
+    this.afterSignOutOneUrl = this.withDefault(data.after_sign_out_one_url, this.afterSignOutOneUrl);
+    this.afterSignUpUrl = this.withDefault(data.after_sign_up_url, this.afterSignUpUrl);
+    this.afterSwitchSessionUrl = this.withDefault(data.after_switch_session_url, this.afterSwitchSessionUrl);
+    this.applicationName = this.withDefault(data.application_name, this.applicationName);
+    this.branded = this.withDefault(data.branded, this.branded);
+    this.captchaHeartbeat = this.withDefault(data.captcha_heartbeat, this.captchaHeartbeat);
+    this.captchaHeartbeatIntervalMs = this.withDefault(
+      data.captcha_heartbeat_interval_ms,
+      this.captchaHeartbeatIntervalMs,
+    );
+    this.captchaOauthBypass = this.withDefault(data.captcha_oauth_bypass, this.captchaOauthBypass);
+    this.captchaProvider = this.withDefault(data.captcha_provider, this.captchaProvider);
+    this.captchaPublicKey = this.withDefault(data.captcha_public_key, this.captchaPublicKey);
+    this.captchaPublicKeyInvisible = this.withDefault(
+      data.captcha_public_key_invisible,
+      this.captchaPublicKeyInvisible,
+    );
+    this.captchaWidgetType = this.withDefault(data.captcha_widget_type, this.captchaWidgetType);
+    this.clerkJSVersion = this.withDefault(data.clerk_js_version, this.clerkJSVersion);
+    this.createOrganizationUrl = this.withDefault(data.create_organization_url, this.createOrganizationUrl);
+    this.faviconImageUrl = this.withDefault(data.favicon_image_url, this.faviconImageUrl);
+    this.googleOneTapClientId = this.withDefault(data.google_one_tap_client_id, this.googleOneTapClientId);
+    this.homeUrl = this.withDefault(data.home_url, this.homeUrl);
+    this.id = this.withDefault(data.id, this.id);
+    this.instanceEnvironmentType = this.withDefault(data.instance_environment_type, this.instanceEnvironmentType);
+    this.logoImageUrl = this.withDefault(data.logo_image_url, this.logoImageUrl);
+    this.organizationProfileUrl = this.withDefault(data.organization_profile_url, this.organizationProfileUrl);
+    this.preferredSignInStrategy = this.withDefault(data.preferred_sign_in_strategy, this.preferredSignInStrategy);
+    this.privacyPolicyUrl = this.withDefault(data.privacy_policy_url, this.privacyPolicyUrl);
+    this.showDevModeWarning = this.withDefault(data.show_devmode_warning, this.showDevModeWarning);
+    this.signInUrl = this.withDefault(data.sign_in_url, this.signInUrl);
+    this.signUpUrl = this.withDefault(data.sign_up_url, this.signUpUrl);
+    this.supportEmail = this.withDefault(data.support_email, this.supportEmail);
+    this.termsUrl = this.withDefault(data.terms_url, this.termsUrl);
+    this.theme = this.withDefault(data.theme, this.theme);
+    this.userProfileUrl = this.withDefault(data.user_profile_url, this.userProfileUrl);
+    this.waitlistUrl = this.withDefault(data.waitlist_url, this.waitlistUrl);
 
     return this;
   }
