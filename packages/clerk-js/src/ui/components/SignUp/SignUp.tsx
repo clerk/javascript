a25c190b4513bb707fd35495d0dc845800e9f06d
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2025-03-18 11:20:06 -0300

    Fix internal navigation context to support virtual routing

--- packages/clerk-js/src/ui/components/SignUp/SignUp.tsx
+++ packages/clerk-js/src/ui/components/SignUp/SignUp.tsx
@@ -28,15 +28,15 @@ function RedirectToSignUp() {
 
 function SignUpRoutes(): JSX.Element {
   const { __internal_setComponentNavigationContext } = useClerk();
-  const { navigate, basePath } = useRouter();
+  const { navigate, indexPath } = useRouter();
   const signUpContext = useSignUpContext();
 
   // `experimental.withSessionTasks` will be removed soon in favor of checking via environment response
   usePreloadSessionTask(signUpContext.withSessionTasks);
 
   React.useEffect(() => {
-    return __internal_setComponentNavigationContext?.({ basePath, navigate });
-  }, [basePath, navigate]);
+    return __internal_setComponentNavigationContext?.({ indexPath, navigate });
+  }, [indexPath, navigate]);
 
   return (
     <Flow.Root flow='signUp'>
