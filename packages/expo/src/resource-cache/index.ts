45e352d87f8c36f337ec71a77b433e1fa89de83b
wobsoriano <sorianorobertc@gmail.com>
2025-03-18 07:51:17 -0700

    chore: put deprecation directly in secure-store export

--- packages/expo/src/resource-cache/index.ts
+++ packages/expo/src/resource-cache/index.ts
@@ -1,8 +1,1 @@
-import { createResourceCacheStore } from './resource-cache';
-
-/**
- * @deprecated Use `resourceCache` from `@clerk/clerk-expo/resource-cache` instead.
- */
-const secureStore = createResourceCacheStore;
-
-export { secureStore, createResourceCacheStore as resourceCache };
+export { createResourceCacheStore as resourceCache } from './resource-cache';
--- packages/expo/src/secure-store/index.ts
+++ packages/expo/src/secure-store/index.ts
@@ -1,1 +1,8 @@
-export { secureStore } from '../resource-cache';
+import { resourceCache } from '../resource-cache';
+
+/**
+ * @deprecated Use `resourceCache` from `@clerk/clerk-expo/resource-cache` instead.
+ */
+const secureStore = resourceCache;
+
+export { secureStore };
