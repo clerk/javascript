3b269a93cbfa0614098998e61fbe507e4e8df1a9
Dylan Staley <88163+dstaley@users.noreply.github.com>
2025-03-21 10:55:39 -0700

    fix(clerk-js): Use logical OR instead of nullish coalescing

--- packages/clerk-js/src/ui/contexts/components/SignIn.ts
+++ packages/clerk-js/src/ui/contexts/components/SignIn.ts
@@ -129,7 +129,7 @@ export const useSignInContext = (): SignInContextType => {
   return {
     ...(ctx as SignInCtx),
     transferable: ctx.transferable ?? true,
-    oauthFlow: ctx.oauthFlow ?? 'auto',
+    oauthFlow: ctx.oauthFlow || 'auto',
     componentName,
     signUpUrl,
     signInUrl,
