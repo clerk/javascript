7f51666eea94e16963184a883b352c7cd4f00c18
Tom Milewski <me@tm.codes>
2025-03-27 14:56:47 -0400

    chore: Latest backend-sdk generation & linting

--- packages/backend-sdk/src/lib/config.ts
+++ packages/backend-sdk/src/lib/config.ts
@@ -52,7 +52,7 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = {
   language: 'typescript',
   openapiDocVersion: '2025-12-03',
-  sdkVersion: '0.3.0',
+  sdkVersion: '0.3.1',
   genVersion: '2.562.2',
-  userAgent: 'speakeasy-sdk/typescript 0.3.0 2.562.2 2025-12-03 @clerk/backend-sdk',
+  userAgent: 'speakeasy-sdk/typescript 0.3.1 2.562.2 2025-12-03 @clerk/backend-sdk',
 } as const;
