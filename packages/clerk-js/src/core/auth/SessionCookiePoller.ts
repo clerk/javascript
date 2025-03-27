5b382a30e54d5afefe3bf4a9a62c75f35763a3b5
panteliselef <panteliselef@outlook.com>
2025-03-27 15:56:50 +0200

    protect from multiple invocations until timerId is populated.

--- packages/clerk-js/src/core/auth/SessionCookiePoller.ts
+++ packages/clerk-js/src/core/auth/SessionCookiePoller.ts
@@ -9,13 +9,16 @@ export class SessionCookiePoller {
   private lock = SafeLock(REFRESH_SESSION_TOKEN_LOCK_KEY);
   private workerTimers = createWorkerTimers();
   private timerId: ReturnType<typeof this.workerTimers.setInterval> | null = null;
+  // Disallows for multiple `startPollingForSessionToken()` calls before `callback` is executed.
+  private initiated = false;
 
   public startPollingForSessionToken(cb: () => Promise<unknown>): void {
-    if (this.timerId) {
+    if (this.timerId || this.initiated) {
       return;
     }
 
     const run = async () => {
+      this.initiated = true;
       await this.lock.acquireLockAndRun(cb);
       this.timerId = this.workerTimers.setTimeout(run, INTERVAL_IN_MS);
     };
@@ -29,5 +32,6 @@ export class SessionCookiePoller {
       this.workerTimers.clearTimeout(this.timerId);
       this.timerId = null;
     }
+    this.initiated = false;
   }
 }
