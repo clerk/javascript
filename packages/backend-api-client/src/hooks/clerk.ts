0cdaffead7470168f8f7deaabbc578995bfedfea
Tom Milewski <me@tm.codes>
2025-04-03 17:16:00 -0400

    chore: Apply latest hook updates

--- packages/backend-api-client/src/hooks/clerk.ts
+++ packages/backend-api-client/src/hooks/clerk.ts
@@ -1,30 +1,26 @@
-// const myHook = new ExampleHook();
-// hooks.registerBeforeRequestHook(myHook);
-
-// import { HTTPClient } from "../lib/http";
 import {
   AfterErrorContext,
   AfterErrorHook,
-  // Awaitable,
-  // AfterSuccessContext,
-  // AfterSuccessHook,
   BeforeRequestContext,
   BeforeRequestHook,
-  // SDKInitHook,
-  // SDKInitOptions,
 } from './types.js';
 
-export const API_URL = 'https://api.clerk.com';
-export const API_VERSION = 'v1';
 export const SUPPORTED_BAPI_VERSION = '2024-10-01';
-export const USER_AGENT = '@clerk/backend@0.0.0-test'; // TODO: Replace with the actual user agent: `${PACKAGE_NAME}@${PACKAGE_VERSION}`
+export const USER_AGENT = '@clerk/backend-api-client@0.0.0-test'; // TODO: Replace with the actual user agent: `${PACKAGE_NAME}@${PACKAGE_VERSION}`
+
+const SKIP_SECRET_KEY_OPERATION_IDS = new Set([
+  'CreateAccountlessApplication',
+  'CompleteAccountlessApplication',
+]);
 
 export class BeforeRequest implements BeforeRequestHook {
   beforeRequest(ctx: BeforeRequestContext, req: Request): Request {
-    if (this.assertSecretKeyIsRequired(ctx, req)) {
-      // TODO: Do we need to remove Authorization header if /accountless_applications?
-      // this.assertSecretKeyIsPresent(ctx);
-      // console.log('assertSecretKeyIsPresent');
+    const secretKeyRequired = this.assertSecretKeyIsRequired(ctx);
+
+    if (secretKeyRequired) {
+      this.assertSecretKeyIsPresent(req);
+    } else {
+      req.headers.delete('Authorization');
     }
 
     req.headers.set('Clerk-API-Version', SUPPORTED_BAPI_VERSION);
@@ -33,25 +29,17 @@ export class BeforeRequest implements BeforeRequestHook {
     return req;
   }
 
-  private assertSecretKeyIsRequired(_ctx: BeforeRequestContext, req: Request) {
-    // ALT: if (ctx.operationID === "createAccountlessApplication")
-    return !new URL(req.url).pathname.startsWith('/v1/accountless_applications');
+  private assertSecretKeyIsPresent(req: Request) {
+    const secretKey = req.headers?.get('Authorization')?.split(' ')[1];
+    
+    if (!secretKey || typeof secretKey !== 'string') {
+      throw Error('Missing Clerk Secret Key. Go to https://dashboard.clerk.com and get your key for your instance.');
+    }
   }
 
-  // private assertSecretKeyIsPresent(ctx: BeforeRequestContext) {
-  //   // ctx.operationID = "assertSecretKeyIsPresent";
-
-  //   const secretKey =
-  //     ctx.resolvedSecurity?.oauth2.type === "secret_key"
-  //       ? ctx.resolvedSecurity?.oauth2.secretKey
-  //       : undefined;
-
-  //   if (!secretKey || typeof secretKey !== "string") {
-  //     throw Error(
-  //       "Missing Clerk Secret Key. Go to https://dashboard.clerk.com and get your key for your instance."
-  //     );
-  //   }
-  // }
+  private assertSecretKeyIsRequired(ctx: BeforeRequestContext) {
+    return !SKIP_SECRET_KEY_OPERATION_IDS.has(ctx.operationID);
+  }
 }
 
 export class AfterError implements AfterErrorHook {
