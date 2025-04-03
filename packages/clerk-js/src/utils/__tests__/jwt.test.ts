1eba471de22f9be979e306410026823455facea0
Vaggelis Yfantis <me@octoper.me>
2025-04-03 14:39:47 +0300

    wip

--- packages/clerk-js/src/utils/__tests__/jwt.test.ts
+++ packages/clerk-js/src/utils/__tests__/jwt.test.ts
@@ -1,18 +1,10 @@
-import type { JWT } from '@clerk/types';
-
-import { encodeB64 } from '../encoders';
 import { decode } from '../jwt';
-
-const createJWTHelper = (header: JWT['header'], payload: Record<string, unknown>) => {
-  const headerB64 = encodeB64(JSON.stringify(header));
-  const payloadB64 = encodeB64(JSON.stringify(payload));
-  const signatureB64 = encodeB64('a-string-secret-at-least-256-bits-long');
-
-  return `${headerB64}.${payloadB64}.${signatureB64}`;
-};
 
 const jwt =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzU4NzY3OTAsImRhdGEiOiJmb29iYXIiLCJpYXQiOjE2NzU4NzY3MzB9.Z1BC47lImYvaAtluJlY-kBo0qOoAk42Xb-gNrB2SxJg';
+
+const versionedJwt =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzU4NzY3OTAsImRhdGEiOiJmb29iYXIiLCJpYXQiOjE2NzU4NzY3MzAsInZlciI6Mn0.yV1pt0f3Riy6jOwPqFOmVKa93G0lqbMLjjP5KYr94zU';
 
 describe('decode(token)', () => {
   it('decodes a JWT token', () => {
@@ -43,19 +35,6 @@ describe('decode(token)', () => {
   });
 
   it('decodes versioned token', () => {
-    const versionedJwt = createJWTHelper(
-      {
-        alg: 'HS256',
-        typ: 'JWT',
-        kid: '',
-      },
-      {
-        exp: 1675876790,
-        data: 'foobar',
-        iat: 1675876730,
-        ver: 2,
-      },
-    );
     const parts = versionedJwt.split('.');
     const [header, payload, signature] = parts;
 
