4a3d4c95095c9e9de4da1bc32987a1134ed179ee
Dylan Staley <88163+dstaley@users.noreply.github.com>
2025-04-14 13:29:58 -0700

    fix(clerk-js): Actually jkjk

--- packages/clerk-js/rspack.config.js
+++ packages/clerk-js/rspack.config.js
@@ -68,7 +68,7 @@ const common = ({ mode, variant, disableRHC = false }) => {
         }),
     ].filter(Boolean),
     output: {
-      chunkFilename: `${variant}_[name]_[fullhash:6]_${packageJSON.version}.js`,
+      chunkFilename: `[name]_${variant}_[fullhash:6]_${packageJSON.version}.js`,
     },
     /**
      * Remove the Stripe dependencies from the bundle, if RHC is disabled.
