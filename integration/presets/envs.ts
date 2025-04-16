6976aae99ea51449c6fd7dd690533ac6f14fe3e5
panteliselef <panteliselef@outlook.com>
2025-04-16 11:25:02 +0300

    chore(e2e): Run existing protect tests with a JWT v2 instance (#5615)

--- integration/presets/envs.ts
+++ integration/presets/envs.ts
@@ -144,6 +144,13 @@ const withSessionTasks = base
   .setEnvVariable('public', 'CLERK_PUBLISHABLE_KEY', instanceKeys.get('with-session-tasks').pk)
   .setEnvVariable('private', 'CLERK_ENCRYPTION_KEY', constants.E2E_CLERK_ENCRYPTION_KEY || 'a-key');
 
+const withBillingStaging = base
+  .clone()
+  .setId('withBillingStaging')
+  .setEnvVariable('private', 'CLERK_API_URL', 'https://api.clerkstage.dev')
+  .setEnvVariable('private', 'CLERK_SECRET_KEY', instanceKeys.get('with-billing-staging').sk)
+  .setEnvVariable('public', 'CLERK_PUBLISHABLE_KEY', instanceKeys.get('with-billing-staging').pk);
+
 export const envs = {
   base,
   withKeyless,
@@ -165,4 +172,5 @@ export const envs = {
   withSignInOrUpEmailLinksFlow,
   withSignInOrUpwithRestrictedModeFlow,
   withSessionTasks,
+  withBillingStaging,
 } as const;
