62a1c5418af1235c15a3ac1956547039f8571af4
Keiran Flanigan <keiran@clerk.dev>
2025-04-18 13:11:28 -0700

    fix(clerk-js): Rollback drawer suspense change (#5670)

--- packages/clerk-js/src/ui/lazyModules/providers.tsx
+++ packages/clerk-js/src/ui/lazyModules/providers.tsx
@@ -147,29 +147,31 @@ type LazyDrawerRendererProps = React.PropsWithChildren<
 
 export const LazyDrawerRenderer = (props: LazyDrawerRendererProps) => {
   return (
-    <VirtualRouter startPath=''>
-      <AppearanceProvider
-        globalAppearance={props.globalAppearance}
-        appearanceKey={props.appearanceKey}
-        appearance={props.componentAppearance}
-      >
-        <FlowMetadataProvider flow={props.flowName || ('' as any)}>
-          <InternalThemeProvider>
-            <DrawerRoot
-              open={props.open}
-              onOpenChange={props.onOpenChange}
-              strategy={props.portalId ? 'absolute' : 'fixed'}
-              portalProps={{
-                id: props.portalId ? props.portalId : undefined,
-              }}
-            >
-              <DrawerOverlay />
-              <Suspense fallback={''}>{props.children}</Suspense>
-            </DrawerRoot>
-          </InternalThemeProvider>
-        </FlowMetadataProvider>
-      </AppearanceProvider>
-    </VirtualRouter>
+    <Suspense fallback={''}>
+      <VirtualRouter startPath=''>
+        <AppearanceProvider
+          globalAppearance={props.globalAppearance}
+          appearanceKey={props.appearanceKey}
+          appearance={props.componentAppearance}
+        >
+          <FlowMetadataProvider flow={props.flowName || ('' as any)}>
+            <InternalThemeProvider>
+              <DrawerRoot
+                open={props.open}
+                onOpenChange={props.onOpenChange}
+                strategy={props.portalId ? 'absolute' : 'fixed'}
+                portalProps={{
+                  id: props.portalId ? props.portalId : undefined,
+                }}
+              >
+                <DrawerOverlay />
+                {props.children}
+              </DrawerRoot>
+            </InternalThemeProvider>
+          </FlowMetadataProvider>
+        </AppearanceProvider>
+      </VirtualRouter>
+    </Suspense>
   );
 };
 
