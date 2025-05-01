7b8fe53bbad530d26d4591d71f1f6d3be6251fef
Alex Carpenter <alex.carpenter@clerk.dev>
2025-05-01 11:23:45 -0400

    fix

--- packages/clerk-js/src/ui/utils/getClosestProfileScrollBox.ts
+++ packages/clerk-js/src/ui/utils/getClosestProfileScrollBox.ts
@@ -4,12 +4,17 @@
  * @param event - The event.
  * @returns The closest profile scroll box to the event target.
  */
-export function getClosestProfileScrollBox(mode: 'mounted' | 'modal', event?: React.MouseEvent<HTMLElement>) {
+export function getClosestProfileScrollBox(
+  mode: 'mounted' | 'modal',
+  event?: React.MouseEvent<HTMLElement>,
+): HTMLElement | undefined {
   if (!event || mode === 'mounted') {
     return undefined;
   }
 
-  const portalRoot = (event?.target as HTMLElement)?.closest(`[data-clerk-profile-scroll-box-root]`) ?? undefined;
+  const portalRoot = (event?.target as HTMLElement)?.closest(`[data-clerk-profile-scroll-box-root]`) as
+    | HTMLElement
+    | undefined;
 
   return portalRoot;
 }
