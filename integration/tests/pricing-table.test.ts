49d3e2b57d2615b2d35a1d95e5c7c61bddc6268d
Vaggelis Yfantis <me@octoper.me>
2025-05-05 16:10:23 +0300

    fix(e2e): Fix checkout and pricing table helpers

--- integration/tests/pricing-table.test.ts
+++ integration/tests/pricing-table.test.ts
@@ -33,8 +33,7 @@ testAgainstRunningApps({ withEnv: [appConfigs.envs.withBilling] })('pricing tabl
     await u.po.page.goToRelative('/pricing-table');
 
     await u.po.pricingTable.waitForMounted();
-    // await u.po.pricingTable.startCheckout({ planSlug: 'plus' }); - This is not working consistently for some reason
-    await u.po.page.locator(`.cl-pricingTableCard__plus .cl-pricingTableCardAction`).getByRole('button').click();
+    await u.po.pricingTable.startCheckout({ planSlug: 'plus' });
     await u.po.signIn.waitForMounted();
     await expect(u.po.page.getByText('Checkout')).toBeHidden();
   });
@@ -46,8 +45,7 @@ testAgainstRunningApps({ withEnv: [appConfigs.envs.withBilling] })('pricing tabl
     await u.po.page.goToRelative('/pricing-table');
 
     await u.po.pricingTable.waitForMounted();
-    // await u.po.pricingTable.startCheckout({ planSlug: 'plus' }); - This is not working consistently for some reason
-    await u.po.page.locator(`.cl-pricingTableCard__plus .cl-pricingTableCardAction`).getByRole('button').click();
+    await u.po.pricingTable.startCheckout({ planSlug: 'plus' });
     await u.po.checkout.waitForMounted();
     await expect(u.po.page.getByText('Checkout')).toBeVisible();
   });
@@ -59,8 +57,7 @@ testAgainstRunningApps({ withEnv: [appConfigs.envs.withBilling] })('pricing tabl
     await u.po.page.goToRelative('/pricing-table');
 
     await u.po.pricingTable.waitForMounted();
-    // await u.po.pricingTable.startCheckout({ planSlug: 'plus' }); - This is not working consistently for some reason
-    await u.po.page.locator(`.cl-pricingTableCard__plus .cl-pricingTableCardAction`).getByRole('button').click();
+    await u.po.pricingTable.startCheckout({ planSlug: 'plus' });
     await u.po.checkout.waitForMounted();
     await u.po.checkout.fillTestCard();
     await u.po.checkout.clickPayOrSubscribe();
