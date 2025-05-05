ef20a7d5f28cef67dbd2822f3b00710690f3e4a9
Vaggelis Yfantis <me@octoper.me>
2025-05-05 14:25:49 +0300

    e2e fixes

--- packages/testing/src/playwright/unstable/page-objects/pricingTable.ts
+++ packages/testing/src/playwright/unstable/page-objects/pricingTable.ts
@@ -16,7 +16,7 @@ export const createPricingTablePageObject = (testArgs: { page: EnhancedPage }) =
     },
     startCheckout: async ({ planSlug, shouldSwitch }: { planSlug: string; shouldSwitch?: boolean }) => {
       const targetButtonName =
-        shouldSwitch === true ? 'Switch to this plan' : shouldSwitch === false ? 'Subscribe' : /get|switch|subscribe/i;
+        shouldSwitch === true ? 'Switch to this plan' : shouldSwitch === false ? /subscribe/i : /get|switch|subscribe/i;
 
       await page
         .locator(`.cl-pricingTableCard__${planSlug} .cl-pricingTableCardAction`)
