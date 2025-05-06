9b4ddebc8cd291915bcbce893aa560d294a5a053
Alex Carpenter <alex.carpenter@clerk.dev>
2025-05-06 13:06:03 -0400

    fix test

--- packages/testing/src/playwright/unstable/page-objects/pricingTable.ts
+++ packages/testing/src/playwright/unstable/page-objects/pricingTable.ts
@@ -19,7 +19,7 @@ export const createPricingTablePageObject = (testArgs: { page: EnhancedPage }) =
         shouldSwitch === true ? 'Switch to this plan' : shouldSwitch === false ? /subscribe/i : /get|switch|subscribe/i;
 
       await page
-        .locator(`.cl-pricingTableCard__${planSlug} .cl-pricingTableCardAction`)
+        .locator(`.cl-pricingTableCard__${planSlug} .cl-pricingTableCardFooter`)
         .getByRole('button', {
           name: targetButtonName,
         })
