5dc7bc69e054801ae19d9f75a38d513d39f725d5
wobsoriano <sorianorobertc@gmail.com>
2025-05-15 14:52:42 -0700

    chore: clean up fetchers and mutations

--- packages/clerk-js/src/ui/components/ApiKeys/useApiKeys.ts
+++ packages/clerk-js/src/ui/components/ApiKeys/useApiKeys.ts
@@ -2,10 +2,9 @@ import { useClerk } from '@clerk/shared/react';
 import { useState } from 'react';
 import useSWR from 'swr';
 
-import { unixEpochToDate } from '../../../utils/date';
 import type { Expiration } from './CreateApiKeyForm';
 
-function getTimeLeftInSeconds(expirationOption: Expiration) {
+export function getTimeLeftInSeconds(expirationOption: Expiration) {
   if (expirationOption === 'never') {
     return;
   }
@@ -25,47 +24,29 @@ function getTimeLeftInSeconds(expirationOption: Expiration) {
 
 export const useApiKeys = ({ subject, perPage = 5 }: { subject: string; perPage?: number }) => {
   const clerk = useClerk();
-  const {
-    data: apiKeys,
-    isLoading,
-    mutate,
-  } = useSWR(['api-keys', subject], ([_, userIdOrOrgId]) => clerk.getApiKeys({ subject: userIdOrOrgId }));
-  const [page, setPage] = useState(1);
+  const cacheKey = {
+    key: 'api-keys',
+    subject,
+  };
+  const { data: apiKeys, isLoading, mutate } = useSWR(cacheKey, () => clerk.getApiKeys({ subject }));
   const [search, setSearch] = useState('');
-  const itemsPerPage = perPage;
-
   const filteredApiKeys = (apiKeys ?? []).filter(key => key.name.toLowerCase().includes(search.toLowerCase()));
-  const itemCount = filteredApiKeys.length;
-  const pageCount = Math.max(1, Math.ceil(itemCount / itemsPerPage));
-  const startingRow = itemCount > 0 ? (page - 1) * itemsPerPage + 1 : 0;
-  const endingRow = Math.min(page * itemsPerPage, itemCount);
-  const paginatedApiKeys = filteredApiKeys.slice(startingRow - 1, endingRow);
 
-  const handleCreate = async (params: {
-    name: string;
-    description?: string;
-    expiration: Expiration;
-    closeFn: () => void;
-  }) => {
-    await clerk.createApiKey({
-      name: params.name,
-      creationReason: params.description,
-      secondsUntilExpiration: getTimeLeftInSeconds(params.expiration),
-    });
-    params.closeFn();
-    void mutate();
-  };
-
-  const revokeApiKey = async (apiKeyID: string) => {
-    await clerk.revokeApiKey({ apiKeyID, revocationReason: 'Revoked by user' });
-    setPage(1);
-    void mutate();
-  };
+  const {
+    page,
+    setPage,
+    pageCount,
+    itemCount,
+    startingRow,
+    endingRow,
+    paginatedItems: paginatedApiKeys,
+  } = usePagination(filteredApiKeys, perPage);
 
   return {
     apiKeys: paginatedApiKeys,
-    isLoading: isLoading ?? false,
-    revokeApiKey,
+    cacheKey,
+    mutate,
+    isLoading,
     search,
     setSearch,
     page,
@@ -74,77 +55,25 @@ export const useApiKeys = ({ subject, perPage = 5 }: { subject: string; perPage?
     itemCount,
     startingRow,
     endingRow,
-    handleCreate,
   };
 };
 
-export const testFakeData = [
-  {
-    object: 'api_key',
-    id: 'ak_391b78b3c0356d365886003bc0bf82ec',
-    type: 'api_key',
-    subject: 'user_2w6CPzG5HYSgQ0RDTMPcxSt9VTf',
-    name: 'cv-api-key',
-    scopes: [],
-    claims: null,
-    revoked: false,
-    revocationReason: null,
-    expired: false,
-    expiration: null,
-    createdBy: 'user_2w6CPzG5HYSgQ0RDTMPcxSt9VTf',
-    creationReason: null,
-    createdAt: unixEpochToDate(1746558122876),
-    updatedAt: unixEpochToDate(1746558122876),
-  },
-  {
-    object: 'api_key',
-    id: 'ak_207c161bb75c3307cd29875ec749003d',
-    type: 'api_key',
-    subject: 'user_2w6CPzG5HYSgQ0RDTMPcxSt9VTf',
-    name: 'moi-api-key',
-    scopes: [],
-    claims: null,
-    revoked: false,
-    revocationReason: null,
-    expired: false,
-    expiration: null,
-    createdBy: 'user_2w6CPzG5HYSgQ0RDTMPcxSt9VTf',
-    creationReason: null,
-    createdAt: unixEpochToDate(1746736006701),
-    updatedAt: unixEpochToDate(1746736006701),
-  },
-  {
-    object: 'api_key',
-    id: 'ak_aee92e09468b93050f3e427aefa4a8b4',
-    type: 'api_key',
-    subject: 'user_2w6CPzG5HYSgQ0RDTMPcxSt9VTf',
-    name: 'samson-api-key',
-    scopes: [],
-    claims: null,
-    revoked: false,
-    revocationReason: null,
-    expired: false,
-    expiration: null,
-    createdBy: 'user_2w6CPzG5HYSgQ0RDTMPcxSt9VTf',
-    creationReason: null,
-    createdAt: unixEpochToDate(1746758834447),
-    updatedAt: unixEpochToDate(1746758834447),
-  },
-  {
-    object: 'api_key',
-    id: 'ak_8ff8da01d421308f1bd84619874e96a8',
-    type: 'api_key',
-    subject: 'user_2w6CPzG5HYSgQ0RDTMPcxSt9VTf',
-    name: 'joshua-tree-api-key',
-    scopes: [],
-    claims: null,
-    revoked: false,
-    revocationReason: null,
-    expired: false,
-    expiration: unixEpochToDate(1749350855253),
-    createdBy: 'user_2w6CPzG5HYSgQ0RDTMPcxSt9VTf',
-    creationReason: '30 days api key expiration',
-    createdAt: unixEpochToDate(1746758855253),
-    updatedAt: unixEpochToDate(1746758855253),
-  },
-];
+const usePagination = <T>(items: T[], itemsPerPage: number) => {
+  const [page, setPage] = useState(1);
+
+  const itemCount = items.length;
+  const pageCount = Math.max(1, Math.ceil(itemCount / itemsPerPage));
+  const startingRow = itemCount > 0 ? (page - 1) * itemsPerPage + 1 : 0;
+  const endingRow = Math.min(page * itemsPerPage, itemCount);
+  const paginatedItems = items.slice(startingRow - 1, endingRow);
+
+  return {
+    page,
+    setPage,
+    pageCount,
+    itemCount,
+    startingRow,
+    endingRow,
+    paginatedItems,
+  };
+};
