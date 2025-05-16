74d2b212a2d0ab0bb33991bd242ac35e120b5e62
Alex Carpenter <alex.carpenter@clerk.dev>
2025-05-16 16:10:14 -0400

    fix(clerk-js): Translate username error in username form (#5948)

---
+++ .changeset/slick-windows-listen.md
@@ -0,0 +1,5 @@
+---
+'@clerk/clerk-js': patch
+---
+
+Fixes issue where min/max username lengths errors were not properly interpolated within profile component.
--- packages/clerk-js/src/ui/components/UserProfile/UsernameForm.tsx
+++ packages/clerk-js/src/ui/components/UserProfile/UsernameForm.tsx
@@ -1,10 +1,10 @@
 import { useReverification, useUser } from '@clerk/shared/react';
 
 import { useEnvironment } from '../../contexts';
-import { localizationKeys } from '../../customizables';
+import { localizationKeys, useLocalizations } from '../../customizables';
 import type { FormProps } from '../../elements';
 import { Form, FormButtons, FormContainer, useCardState, withCardStateProvider } from '../../elements';
-import { handleError, useFormControl } from '../../utils';
+import { createUsernameError, handleError, useFormControl } from '../../utils';
 
 type UsernameFormProps = FormProps;
 
@@ -16,10 +16,13 @@ export const UsernameForm = withCardStateProvider((props: UsernameFormProps) => 
 
   const { userSettings } = useEnvironment();
   const card = useCardState();
+  const { t, locale } = useLocalizations();
+  const { usernameSettings } = userSettings;
   const usernameField = useFormControl('username', user?.username || '', {
     type: 'text',
     label: localizationKeys('formFieldLabel__username'),
     placeholder: localizationKeys('formFieldInputPlaceholder__username'),
+    buildErrorMessage: errors => createUsernameError(errors, { t, locale, usernameSettings }),
   });
 
   if (!user) {
