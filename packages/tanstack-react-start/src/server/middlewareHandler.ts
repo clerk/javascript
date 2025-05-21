0c1ed86af61e5acad0224594b89c9d13548912b5
wobsoriano <sorianorobertc@gmail.com>
2025-05-20 21:45:41 -0700

    chore: Update handler types

--- packages/tanstack-react-start/src/server/middlewareHandler.ts
+++ packages/tanstack-react-start/src/server/middlewareHandler.ts
@@ -1,42 +1,27 @@
 import type { AnyRouter } from '@tanstack/react-router';
-import { type EventHandler } from '@tanstack/react-start/server';
+import type { CustomizeStartHandler, HandlerCallback, RequestHandler } from '@tanstack/react-start/server';
 
 import { authenticateRequest } from './authenticateRequest';
 import { loadOptions } from './loadOptions';
 import type { LoaderOptions } from './types';
 import { getResponseClerkState } from './utils';
 
-export type HandlerCallback<TRouter extends AnyRouter> = (ctx: {
-  request: Request;
-  router: TRouter;
-  responseHeaders: Headers;
-}) => Response | Promise<Response>;
-export type CustomizeStartHandler<TRouter extends AnyRouter> = (cb: HandlerCallback<TRouter>) => EventHandler;
-
 export function createClerkHandler<TRouter extends AnyRouter>(
   eventHandler: CustomizeStartHandler<TRouter>,
   clerkOptions: LoaderOptions = {},
 ) {
-  return (cb: HandlerCallback<TRouter>): EventHandler => {
+  return (cb: HandlerCallback<TRouter>): RequestHandler => {
     return eventHandler(async ({ request, router, responseHeaders }) => {
       try {
         const loadedOptions = loadOptions(request, clerkOptions);
 
-        const requestState = await authenticateRequest(request, {
-          ...loadedOptions,
-          acceptsToken: 'any',
-        });
+        const requestState = await authenticateRequest(request, loadedOptions);
 
-        const { clerkInitialState, headers } = getResponseClerkState(requestState, loadedOptions);
+        const clerkInitialState = getResponseClerkState(requestState, loadedOptions);
 
         // Merging the TanStack router context with the Clerk context and loading the router
         router.update({
-          context: { ...router.options.context, clerkInitialState },
-        });
-
-        // Adding the Clerk response headers to the response
-        headers.forEach((value, key) => {
-          responseHeaders.set(key, value);
+          context: { ...router.options.context, ...clerkInitialState },
         });
 
         await router.load();
