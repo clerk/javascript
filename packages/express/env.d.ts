c3ff7fc55a36216fd0080ef0cec8b794086b5138
wobsoriano <sorianorobertc@gmail.com>
2025-05-23 16:30:51 -0700

    fix types

--- packages/express/env.d.ts
+++ packages/express/env.d.ts
@@ -1,9 +1,10 @@
 import type { AuthObject } from '@clerk/backend';
+import type { PendingSessionOptions } from '@clerk/types';
 
 declare global {
   namespace Express {
     interface Request {
-      auth: AuthObject;
+      auth: (options?: PendingSessionOptions) => AuthObject;
     }
   }
 }
