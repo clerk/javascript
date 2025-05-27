b1f9d1c1a7ecab6dc65bacd0aa9e304549b9b270
LekoArts <lekoarts@gmail.com>
2025-05-27 12:54:49 +0200

    dummy commit

--- packages/react-router/src/__tests__/exports.test.ts
+++ packages/react-router/src/__tests__/exports.test.ts
@@ -1,6 +1,8 @@
 import * as publicExports from '../index';
 import * as ssrExports from '../ssr/index';
 
+// Foobar
+
 describe('root public exports', () => {
   it('should not change unexpectedly', () => {
     expect(Object.keys(publicExports).sort()).toMatchSnapshot();
