9b446e9551e8d7356ce3e801ee0766970090b338
LekoArts <lekoarts@gmail.com>
2025-05-27 15:27:06 +0200

    add missing existing content

--- packages/backend/src/api/resources/RedirectUrl.ts
+++ packages/backend/src/api/resources/RedirectUrl.ts
@@ -1,10 +1,28 @@
 import type { RedirectUrlJSON } from './JSON';
 
+/**
+ * Redirect URLs are whitelisted URLs that facilitate secure authentication flows in native applications (e.g. React Native, Expo). In these contexts, Clerk ensures that security-critical nonces are passed only to the whitelisted URLs.
+
+The Backend `RedirectUrl` object represents a redirect URL in your application. This object is used in the Backend API.
+ */
 export class RedirectUrl {
   constructor(
+    /**
+     * The unique identifier for the redirect URL.
+     */
     readonly id: string,
+    /**
+     * The full URL value prefixed with `https://` or a custom scheme.
+     * @example `https://my-app.com/oauth-callback` or `my-app://oauth-callback`
+     */
     readonly url: string,
+    /**
+     * The date when the redirect URL was first created.
+     */
     readonly createdAt: number,
+    /**
+     * The date when the redirect URL was last updated.
+     */
     readonly updatedAt: number,
   ) {}
 
