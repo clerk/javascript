ca83aff679c005d055b2aa25e1970a0aef5f6800
wobsoriano <sorianorobertc@gmail.com>
2025-05-30 07:23:20 -0700

    chore: sort imports

--- packages/express/src/types.ts
+++ packages/express/src/types.ts
@@ -1,7 +1,7 @@
 import type { createClerkClient } from '@clerk/backend';
 import type { AuthenticateRequestOptions, SignedInAuthObject, SignedOutAuthObject } from '@clerk/backend/internal';
-import type { Request as ExpressRequest } from 'express';
 import type { PendingSessionOptions } from '@clerk/types';
+import type { Request as ExpressRequest } from 'express';
 
 export type ExpressRequestWithAuth = ExpressRequest & {
   auth: (options?: PendingSessionOptions) => SignedInAuthObject | SignedOutAuthObject;
