a560b342180965d89f9f1eb9e92bac72e12d98a0
wobsoriano <sorianorobertc@gmail.com>
2025-06-04 19:18:15 -0700

    fix fastify test

--- packages/fastify/src/__tests__/getAuth.test.ts
+++ packages/fastify/src/__tests__/getAuth.test.ts
@@ -4,9 +4,9 @@ import { getAuth } from '../getAuth';
 
 describe('getAuth(req)', () => {
   test('returns req.auth', () => {
-    const req = { key1: 'asa', auth: 'authObj' } as any as FastifyRequest;
+    const req = { key1: 'asa', auth: { tokenType: 'session_token' } } as any as FastifyRequest;
 
-    expect(getAuth(req)).toEqual('authObj');
+    expect(getAuth(req)).toEqual({ tokenType: 'session_token' });
   });
 
   test('throws error if clerkPlugin is on registered', () => {
