796ba7a17b190dc0336492972a525d36078a658d
wobsoriano <sorianorobertc@gmail.com>
2025-06-10 08:59:22 -0700

    chore: pass error to be handled by client

--- packages/clerk-js/src/core/modules/apiKeys/index.ts
+++ packages/clerk-js/src/core/modules/apiKeys/index.ts
@@ -43,8 +43,7 @@ export class APIKeys implements APIKeysNamespace {
       .then(res => {
         const apiKeysJSON = res.payload as unknown as { api_keys: ApiKeyJSON[] };
         return apiKeysJSON.api_keys.map(json => new APIKey(json));
-      })
-      .catch(() => []);
+      });
   }
 
   async getSecret(id: string): Promise<string> {
@@ -58,8 +57,7 @@ export class APIKeys implements APIKeysNamespace {
       .then(res => {
         const { secret } = res.payload as unknown as { secret: string };
         return secret;
-      })
-      .catch(() => '');
+      });
   }
 
   async create(params: CreateAPIKeyParams): Promise<APIKeyResource> {
