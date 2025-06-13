a0edca2d8e41afd5af882d8758ff9fbd22541a46
Robert Soriano <sorianorobertc@gmail.com>
2025-06-13 12:45:22 -0700

    Apply suggestions from code review
    Co-authored-by: panteliselef <panteliselef@outlook.com>

--- packages/clerk-js/src/ui/utils/timeAgo.ts
+++ packages/clerk-js/src/ui/utils/timeAgo.ts
@@ -10,17 +10,17 @@ export function timeAgo(date: Date | string | number) {
   if (seconds < 60) return localizationKeys('apiKeys.dates.lastUsed__seconds', { seconds });
 
   const minutes = Math.floor(seconds / 60);
-  if (minutes < 60) return localizationKeys('apiKeys.dates.lastUsed__minutes', { date: minutes });
+  if (minutes < 60) return localizationKeys('apiKeys.dates.lastUsed__minutes', { minutes });
 
   const hours = Math.floor(minutes / 60);
   if (hours < 24) return localizationKeys('apiKeys.dates.lastUsed__hours', { hours });
 
   const days = Math.floor(hours / 24);
-  if (days < 30) return localizationKeys('apiKeys.dates.lastUsed__days', { date: days });
+  if (days < 30) return localizationKeys('apiKeys.dates.lastUsed__days', { days });
 
   const months = Math.floor(days / 30);
-  if (months < 12) return localizationKeys('apiKeys.dates.lastUsed__months', { date: months });
+  if (months < 12) return localizationKeys('apiKeys.dates.lastUsed__months', { months });
 
   const years = Math.floor(months / 12);
-  return localizationKeys('apiKeys.dates.lastUsed__years', { date: years });
+  return localizationKeys('apiKeys.dates.lastUsed__years', { years });
 }
