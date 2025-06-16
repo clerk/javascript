0b8c12c214d8a98ba169d132cbc81f73ae9f9e77
Keiran Flanigan <keiran@aeliox.com>
2025-06-16 15:37:07 -0700

    chore(clerk-js): Rollback changes to subscription endpoints

---
+++ .changeset/lucky-dodos-exist.md
@@ -0,0 +1,5 @@
+---
+'@clerk/clerk-js': patch
+---
+
+Rollback subscriptions endpoints change
--- packages/clerk-js/src/core/resources/CommerceSubscription.ts
+++ packages/clerk-js/src/core/resources/CommerceSubscription.ts
@@ -52,8 +52,8 @@ export class CommerceSubscription extends BaseResource implements CommerceSubscr
     const json = (
       await BaseResource._fetch({
         path: orgId
-          ? `/organizations/${orgId}/commerce/subscription_items/${this.id}`
-          : `/me/commerce/subscription_items/${this.id}`,
+          ? `/organizations/${orgId}/commerce/subscriptions/${this.id}`
+          : `/me/commerce/subscriptions/${this.id}`,
         method: 'DELETE',
       })
     )?.response as unknown as DeletedObjectJSON;
