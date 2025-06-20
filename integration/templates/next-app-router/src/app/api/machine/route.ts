5a1b77a1e99b4852e55b18171bce44f88db1440e
wobsoriano <sorianorobertc@gmail.com>
2025-06-20 15:57:15 -0700

    debug

--- integration/templates/next-app-router/src/app/api/machine/route.ts
+++ integration/templates/next-app-router/src/app/api/machine/route.ts
@@ -2,7 +2,10 @@ import { NextResponse } from 'next/server';
 import { auth } from '@clerk/nextjs/server';
 
 export async function GET() {
-  const { userId } = await auth({ acceptsToken: 'api_key' });
+  const { userId, ...rest } = await auth({ acceptsToken: 'api_key' });
+
+  console.log('userId', userId);
+  console.log('rest', rest);
 
   if (!userId) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
--- integration/testUtils/usersService.ts
+++ integration/testUtils/usersService.ts
@@ -196,13 +196,7 @@ export const createUserService = (clerkClient: ClerkClient) => {
       return {
         apiKey,
         secret,
-        revoke: async () => {
-          try {
-            return await clerkClient.apiKeys.revoke({ apiKeyId: apiKey.id, revocationReason: 'For testing purposes' });
-          } catch (err) {
-            console.log(`Error revoking API key "${apiKey.id}": ${err.message}`);
-          }
-        },
+        revoke: () => clerkClient.apiKeys.revoke({ apiKeyId: apiKey.id, revocationReason: 'For testing purposes' }),
       } satisfies FakeAPIKey;
     },
   };
