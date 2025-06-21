6ad36ee022cafbeb5502a187be5436066a4a2a85
wobsoriano <sorianorobertc@gmail.com>
2025-06-20 21:19:59 -0700

    chore: rerun test

--- packages/nextjs/src/app-router/server/auth.ts
+++ packages/nextjs/src/app-router/server/auth.ts
@@ -187,7 +187,7 @@ auth.protect = async (...args: any[]) => {
   require('server-only');
 
   const request = await buildRequestLike();
-  const requestedToken = args[0]?.token || args[1]?.token || TokenType.SessionToken;
+  const requestedToken = args?.[0]?.token || args?.[1]?.token || TokenType.SessionToken;
   const authObject = await auth({ acceptsToken: requestedToken });
 
   const protect = createProtect({
