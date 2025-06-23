1ef53b716f466977c5ce9c4b63c5915d885648f9
Tom Milewski <me@tm.codes>
2025-06-23 09:42:26 -0400

    chore: Remove PricingTable from unsupported components

--- .changeset/twelve-ducks-refuse.md
+++ .changeset/twelve-ducks-refuse.md
@@ -2,4 +2,4 @@
 '@clerk/chrome-extension': minor
 ---
 
-Explicitly mark `GoogleOneTap`, `PricingTable`, and `SignInWithMetamaskButton` UI components as deprecated and unsupported (due to the requirement of Remotely Hosted Code) to help avoid confusion.
+Explicitly mark `GoogleOneTap` and `SignInWithMetamaskButton` UI components as deprecated and unsupported (due to the requirement of Remotely Hosted Code) to help avoid confusion.
--- packages/chrome-extension/src/react/NotSupported.tsx
+++ packages/chrome-extension/src/react/NotSupported.tsx
@@ -2,12 +2,6 @@
  * @deprecated This component is not supported in Browser Extensions due to Chrome's security restrictions around remotely hosted code.
  * @see https://clerk.com/docs/references/browser-extensions/browser-security-restrictions
  */
-export const PricingTable = () => null;
-
-/**
- * @deprecated This component is not supported in Browser Extensions due to Chrome's security restrictions around remotely hosted code.
- * @see https://clerk.com/docs/references/browser-extensions/browser-security-restrictions
- */
 export const SignInWithMetamaskButton = () => null;
 
 /**
