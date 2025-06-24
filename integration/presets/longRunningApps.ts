cbfeb9c2dfb3997e9537d8b0d463b406e689f506
wobsoriano <sorianorobertc@gmail.com>
2025-06-24 14:27:28 -0700

    chore: remove previous commit

--- integration/presets/longRunningApps.ts
+++ integration/presets/longRunningApps.ts
@@ -47,7 +47,6 @@ export const createLongRunningApps = () => {
     { id: 'withBilling.next.appRouter', config: next.appRouter, env: envs.withBilling },
     { id: 'withBillingStaging.vue.vite', config: vue.vite, env: envs.withBillingStaging },
     { id: 'withBilling.vue.vite', config: vue.vite, env: envs.withBilling },
-    { id: 'withBillingStaging.astro.node', config: astro.node, env: envs.withBillingStaging },
     { id: 'withBilling.astro.node', config: astro.node, env: envs.withBilling },
     // { id: 'withBillingStaging.nuxt.node', config: nuxt.node, env: envs.withBillingStaging },
     // { id: 'withBilling.nuxt.node', config: nuxt.node, env: envs.withBilling },
