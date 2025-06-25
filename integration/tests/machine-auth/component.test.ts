b07a3646849ddab50b89084a5666ae2df759eacc
wobsoriano <sorianorobertc@gmail.com>
2025-06-25 12:44:28 -0700

    chore: introduce more tests

--- integration/tests/machine-auth/component.test.ts
+++ integration/tests/machine-auth/component.test.ts
@@ -1,10 +1,10 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 import { appConfigs } from '../../presets';
 import type { FakeUser } from '../../testUtils';
 import { createTestUtils, testAgainstRunningApps } from '../../testUtils';
 
-testAgainstRunningApps({ withEnv: [appConfigs.envs.withAPIKeys] })('api keys @xgeneric', ({ app }) => {
+testAgainstRunningApps({ withEnv: [appConfigs.envs.withAPIKeys] })('api keys @generic', ({ app }) => {
   test.describe.configure({ mode: 'serial' });
 
   let fakeUser: FakeUser;
@@ -20,7 +20,7 @@ testAgainstRunningApps({ withEnv: [appConfigs.envs.withAPIKeys] })('api keys @xg
     await app.teardown();
   });
 
-  test('renders api keys', async ({ page, context }) => {
+  test('can create api keys', async ({ page, context }) => {
     const u = createTestUtils({ app, page, context });
     await u.po.signIn.goTo();
     await u.po.signIn.waitForMounted();
@@ -29,5 +29,101 @@ testAgainstRunningApps({ withEnv: [appConfigs.envs.withAPIKeys] })('api keys @xg
 
     await u.po.page.goToRelative('/api-keys');
     await u.po.apiKeys.waitForMounted();
+
+    // Create API key 1
+    await u.po.apiKeys.clickAddButton();
+    await u.po.apiKeys.waitForFormOpened();
+    await u.po.apiKeys.typeName(`${fakeUser.firstName}-api-key-1`);
+    await u.po.apiKeys.selectExpiration('1d');
+    await u.po.apiKeys.clickSaveButton();
+
+    await u.po.apiKeys.waitForFormClosed();
+
+    // Create API key 2
+    await u.po.apiKeys.clickAddButton();
+    await u.po.apiKeys.waitForFormOpened();
+    await u.po.apiKeys.typeName(`${fakeUser.firstName}-api-key-2`);
+    await u.po.apiKeys.selectExpiration('7d');
+    await u.po.apiKeys.clickSaveButton();
+
+    // Check if both API keys are created
+    await expect(u.page.locator('.cl-apiKeysTable .cl-tableRow')).toHaveCount(2);
+  });
+
+  test('can revoke api keys', async ({ page, context }) => {
+    const u = createTestUtils({ app, page, context });
+    await u.po.signIn.goTo();
+    await u.po.signIn.waitForMounted();
+    await u.po.signIn.signInWithEmailAndInstantPassword({ email: fakeUser.email, password: fakeUser.password });
+    await u.po.expect.toBeSignedIn();
+
+    await u.po.page.goToRelative('/api-keys');
+    await u.po.apiKeys.waitForMounted();
+
+    const apiKeyName = `${fakeUser.firstName}-${Date.now()}`;
+
+    // Create API key
+    await u.po.apiKeys.clickAddButton();
+    await u.po.apiKeys.waitForFormOpened();
+    await u.po.apiKeys.typeName(apiKeyName);
+    await u.po.apiKeys.selectExpiration('1d');
+    await u.po.apiKeys.clickSaveButton();
+    await u.po.apiKeys.waitForFormClosed();
+
+    // Retrieve API key
+    const table = u.page.locator('.cl-apiKeysTable');
+    const row = table.locator('.cl-tableRow', { hasText: apiKeyName });
+    await row.waitFor({ state: 'attached' });
+
+    // Revoke API key
+    await row.locator('.cl-tableBodyCell').nth(3).locator('.cl-menuButton').click();
+    const revokeButton = u.page.getByRole('menuitem', { name: 'Revoke key' });
+    await revokeButton.waitFor({ state: 'attached' });
+    await revokeButton.click();
+
+    // Wait for revoke modal and confirm revocation
+    await u.po.apiKeys.waitForRevokeModalOpened();
+    await u.po.apiKeys.typeRevokeConfirmation('Revoke');
+    await u.po.apiKeys.clickConfirmRevokeButton();
+    await u.po.apiKeys.waitForRevokeModalClosed();
+
+    // Check if record is removed from the table
+    await expect(table.locator('.cl-apiKeysTable .cl-tableRow', { hasText: apiKeyName })).toHaveCount(0);
+  });
+
+  test('can copy api keys', async ({ page, context }) => {
+    const u = createTestUtils({ app, page, context });
+    await u.po.signIn.goTo();
+    await u.po.signIn.waitForMounted();
+    await u.po.signIn.signInWithEmailAndInstantPassword({ email: fakeUser.email, password: fakeUser.password });
+    await u.po.expect.toBeSignedIn();
+
+    await u.po.page.goToRelative('/api-keys');
+    await u.po.apiKeys.waitForMounted();
+
+    const apiKeyName = `${fakeUser.firstName}-${Date.now()}`;
+
+    // Create API key
+    await u.po.apiKeys.clickAddButton();
+    await u.po.apiKeys.waitForFormOpened();
+    await u.po.apiKeys.typeName(apiKeyName);
+    await u.po.apiKeys.selectExpiration('1d');
+    await u.po.apiKeys.clickSaveButton();
+    await u.po.apiKeys.waitForFormClosed();
+
+    const responsePromise = page.waitForResponse(
+      response => response.url().includes('/secret') && response.request().method() === 'GET',
+    );
+
+    // Copy API key
+    const table = u.page.locator('.cl-apiKeysTable');
+    const row = table.locator('.cl-tableRow', { hasText: apiKeyName });
+    await row.waitFor({ state: 'attached' });
+    await row.locator('.cl-tableBodyCell').nth(2).locator('.cl-apiKeysCopyButton').click();
+
+    // Read clipboard contents
+    const data = await (await responsePromise).json();
+    const clipboardText = await page.evaluate('navigator.clipboard.readText()');
+    expect(clipboardText).toBe(data.secret);
   });
 });
--- packages/testing/src/playwright/unstable/page-objects/apiKeys.ts
+++ packages/testing/src/playwright/unstable/page-objects/apiKeys.ts
@@ -3,6 +3,18 @@ import { common } from './common';
 
 export const createAPIKeysComponentPageObject = (testArgs: { page: EnhancedPage }) => {
   const { page } = testArgs;
+
+  const expirationOptions = {
+    never: 'Never',
+    '1d': '1 Day',
+    '7d': '7 Days',
+    '30d': '30 Days',
+    '60d': '60 Days',
+    '90d': '90 Days',
+    '180d': '180 Days',
+    '1y': '1 Year',
+  } as const;
+
   const self = {
     ...common(testArgs),
     waitForMounted: () => {
@@ -14,17 +26,33 @@ export const createAPIKeysComponentPageObject = (testArgs: { page: EnhancedPage 
     waitForFormOpened: () => {
       return page.waitForSelector('.cl-apiKeysCreateForm', { state: 'attached' });
     },
+    waitForFormClosed: () => {
+      return page.waitForSelector('.cl-apiKeysCreateForm', { state: 'detached' });
+    },
+    waitForRevokeModalOpened: () => {
+      return page.waitForSelector('.cl-apiKeysRevokeModal', { state: 'attached' });
+    },
+    waitForRevokeModalClosed: () => {
+      return page.waitForSelector('.cl-apiKeysRevokeModal', { state: 'detached' });
+    },
     typeName: (value: string) => {
       return page.getByLabel(/Secret key name/i).fill(value);
     },
     typeDescription: (value: string) => {
       return page.getByLabel(/Description/i).fill(value);
     },
-    selectExpiration: (value: string) => {
-      return page.getByLabel(/Expiration/i).selectOption(value);
+    selectExpiration: async (value?: keyof typeof expirationOptions) => {
+      await page.getByRole('button', { name: /Select date/i }).click();
+      return page.getByText(expirationOptions[value ?? 'never'], { exact: true }).click();
     },
     clickSaveButton: () => {
       return page.getByText(/Create key/i).click();
+    },
+    typeRevokeConfirmation: (value: string) => {
+      return page.getByLabel(/Type "Revoke" to confirm/i).fill(value);
+    },
+    clickConfirmRevokeButton: () => {
+      return page.getByText(/Revoke key/i).click();
     },
   };
   return self;
