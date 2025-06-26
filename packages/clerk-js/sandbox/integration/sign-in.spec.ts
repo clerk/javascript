aaf1ecd8cad1bf105eb715e633d7ed84a261866c
Alex Carpenter <alex.carpenter@clerk.dev>
2025-06-26 10:16:57 -0400

    capture interactive states

--- packages/clerk-js/sandbox/integration/sign-in.spec.ts
+++ packages/clerk-js/sandbox/integration/sign-in.spec.ts
@@ -2,13 +2,19 @@ import { clerk } from '@clerk/testing/playwright';
 import { expect, test } from '@playwright/test';
 
 const rootElement = '.cl-signIn-root';
-const buttonElement = '.cl-formButtonPrimary';
+const primaryButtonElement = '.cl-formButtonPrimary';
+const secondaryButtonElement = '.cl-socialButtonsBlockButton__google';
+const actionLinkElement = '.cl-footerActionLink';
 
 test('sign in', async ({ page }) => {
   await page.goto('/sign-in');
   await clerk.loaded({ page });
   await page.waitForSelector(rootElement, { state: 'attached' });
   await expect(page.locator(rootElement)).toHaveScreenshot('sign-in.png');
-  await page.locator(buttonElement).hover();
-  await expect(page.locator(rootElement)).toHaveScreenshot('sign-in-button-hover.png');
+  await page.locator(primaryButtonElement).hover();
+  await expect(page.locator(rootElement)).toHaveScreenshot('sign-in-primary-button-hover.png');
+  await page.locator(secondaryButtonElement).hover();
+  await expect(page.locator(rootElement)).toHaveScreenshot('sign-in-secondary-button-hover.png');
+  await page.locator(actionLinkElement).hover();
+  await expect(page.locator(rootElement)).toHaveScreenshot('sign-in-action-link-hover.png');
 });
