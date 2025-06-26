065445c95086e28040d1e958e7492b15a67f2a94
Alex Carpenter <alex.carpenter@clerk.dev>
2025-06-26 19:02:03 -0400

    feedback

--- packages/clerk-js/src/ui/utils/colors/index.ts
+++ packages/clerk-js/src/ui/utils/colors/index.ts
@@ -6,60 +6,37 @@ import { colors as modernColors } from './modern';
 
 const hasModernColorSupport = cssSupports.hasModernColorSupport;
 
-export const colors = {
-  toHslaColor: (color: string | undefined): string | HslaColor | undefined => {
-    if (!color) return undefined;
-    if (hasModernColorSupport()) {
-      return color;
+export const colors = hasModernColorSupport()
+  ? {
+      toHslaColor: (color: string | undefined): string | HslaColor | undefined => {
+        if (!color) return undefined;
+        return color;
+      },
+      toHslaString: (color: HslaColor | string | undefined): string | undefined => {
+        if (!color) return undefined;
+        if (typeof color === 'string') {
+          return color;
+        }
+        return legacyColors.toHslaString(color);
+      },
+      changeHslaLightness: legacyColors.changeHslaLightness,
+      setHslaAlpha: legacyColors.setHslaAlpha,
+      lighten: modernColors.lighten,
+      makeTransparent: modernColors.makeTransparent,
+      makeSolid: modernColors.makeSolid,
+      setAlpha: modernColors.setAlpha,
+      adjustForLightness: modernColors.adjustForLightness,
     }
-    return legacyColors.toHslaColor(color);
-  },
-  toHslaString: (color: HslaColor | string | undefined): string | undefined => {
-    if (!color) return undefined;
-    if (hasModernColorSupport() && typeof color === 'string') {
-      return color;
-    }
-    return legacyColors.toHslaString(color);
-  },
-
-  changeHslaLightness: legacyColors.changeHslaLightness,
-  setHslaAlpha: legacyColors.setHslaAlpha,
-
-  // High-level utilities with automatic modern/legacy selection
-  lighten: (color: string | undefined, percentage = 0): string | undefined => {
-    if (hasModernColorSupport()) {
-      return modernColors.lighten(color, percentage);
-    }
-    return legacyColors.lighten(color, percentage);
-  },
-
-  makeTransparent: (color: string | undefined, percentage = 0): string | undefined => {
-    if (hasModernColorSupport()) {
-      return modernColors.makeTransparent(color, percentage);
-    }
-    return legacyColors.makeTransparent(color, percentage);
-  },
-
-  makeSolid: (color: string | undefined): string | undefined => {
-    if (hasModernColorSupport()) {
-      return modernColors.makeSolid(color);
-    }
-    return legacyColors.makeSolid(color);
-  },
-
-  setAlpha: (color: string, alpha: number): string => {
-    if (hasModernColorSupport()) {
-      return modernColors.setAlpha(color, alpha);
-    }
-    return legacyColors.setAlpha(color, alpha);
-  },
-
-  adjustForLightness: (color: string | undefined, lightness = 5): string | undefined => {
-    if (hasModernColorSupport()) {
-      return modernColors.adjustForLightness(color, lightness);
-    }
-    return legacyColors.adjustForLightness(color, lightness);
-  },
-};
+  : {
+      toHslaColor: legacyColors.toHslaColor,
+      toHslaString: legacyColors.toHslaString,
+      changeHslaLightness: legacyColors.changeHslaLightness,
+      setHslaAlpha: legacyColors.setHslaAlpha,
+      lighten: legacyColors.lighten,
+      makeTransparent: legacyColors.makeTransparent,
+      makeSolid: legacyColors.makeSolid,
+      setAlpha: legacyColors.setAlpha,
+      adjustForLightness: legacyColors.adjustForLightness,
+    };
 
 export { modernColors, legacyColors, hasModernColorSupport };
