c4efa2e81f5100b98326c45a2fd7deab6f461385
wobsoriano <sorianorobertc@gmail.com>
2025-06-26 22:14:10 -0700

    chore: remove unused Protect usage

--- integration/templates/nuxt-node/pages/pricing-table.vue
+++ integration/templates/nuxt-node/pages/pricing-table.vue
@@ -1,17 +1,10 @@
 <script setup>
+import { useRoute, PricingTable } from '#imports';
+
 const route = useRoute();
 const { newSubscriptionRedirectUrl } = route.query;
 </script>
 
 <template>
-  <Protect plan="free_user">
-    <p>user in free</p>
-  </Protect>
-  <Protect plan="pro">
-    <p>user in pro</p>
-  </Protect>
-  <Protect plan="plus">
-    <p>user in plus</p>
-  </Protect>
   <PricingTable :newSubscriptionRedirectUrl="newSubscriptionRedirectUrl" />
 </template>
