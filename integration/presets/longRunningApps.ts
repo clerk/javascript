12aa88c047985610e009387b135589fd6b99faf7
wobsoriano <sorianorobertc@gmail.com>
2025-06-26 22:00:08 -0700

    chore: clean up

--- integration/presets/longRunningApps.ts
+++ integration/presets/longRunningApps.ts
@@ -49,7 +49,6 @@ export const createLongRunningApps = () => {
     { id: 'withBillingStaging.vue.vite', config: vue.vite, env: envs.withBillingStaging },
     { id: 'withBilling.vue.vite', config: vue.vite, env: envs.withBilling },
     { id: 'withBilling.astro.node', config: astro.node, env: envs.withBilling },
-    // { id: 'withBillingStaging.nuxt.node', config: nuxt.node, env: envs.withBillingStaging },
     { id: 'withBilling.nuxt.node', config: nuxt.node, env: envs.withBilling },
     {
       id: 'next.appRouter.withLegalConsent',
