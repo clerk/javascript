ffdbaf62ad87f450be52c54003d718b68a746cbe
Alex Carpenter <alex.carpenter@clerk.dev>
2025-07-16 10:13:13 -0400

    add variables

--- packages/themes/src/themes/shadcn.ts
+++ packages/themes/src/themes/shadcn.ts
@@ -1,5 +1,18 @@
 import { experimental_createTheme } from '../createTheme';
 
 export const shadcn = experimental_createTheme({
-  variables: {},
+  variables: {
+    borderRadius: 'calc(var(--radius) - 2px)',
+    colorBackground: 'var(--card)',
+    colorForeground: 'var(--card-foreground)',
+    colorPrimary: 'var(--primary)',
+    colorPrimaryForeground: 'var(--primary-foreground)',
+    colorMuted: 'var(--muted)',
+    colorMutedForeground: 'var(--muted-foreground)',
+    colorRing: 'var(--ring)',
+    colorInput: 'transparent',
+    colorInputForeground: 'var(--card-foreground)',
+    colorDanger: 'var(--destructive)',
+    colorNeutral: 'var(--foreground)',
+  },
 });
