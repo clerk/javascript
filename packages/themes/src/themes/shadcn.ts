60804724c47cd301b8bbd3e33e53c56177d4a9d8
Alex Carpenter <alex.carpenter@clerk.dev>
2025-07-16 14:24:32 -0400

    update vars

--- packages/themes/src/themes/shadcn.ts
+++ packages/themes/src/themes/shadcn.ts
@@ -3,15 +3,19 @@ import { experimental_createTheme } from '../createTheme';
 export const shadcn = experimental_createTheme({
   variables: {
     colorBackground: 'var(--card)',
+    colorDanger: 'var(--destructive)',
     colorForeground: 'var(--card-foreground)',
-    colorPrimary: 'var(--primary)',
-    colorPrimaryForeground: 'var(--primary-foreground)',
+    colorInput: 'var(--input)',
+    colorInputForeground: 'var(--card-foreground)',
+    colorModalBackdrop: 'var(--color-black)',
     colorMuted: 'var(--muted)',
     colorMutedForeground: 'var(--muted-foreground)',
-    colorRing: 'var(--ring)',
-    colorInput: 'transparent',
-    colorInputForeground: 'var(--card-foreground)',
-    colorDanger: 'var(--destructive)',
     colorNeutral: 'var(--foreground)',
+    colorPrimary: 'var(--primary)',
+    colorPrimaryForeground: 'var(--primary-foreground)',
+    colorRing: 'var(--ring)',
+  },
+  elements: {
+    input: 'bg-transparent dark:bg-input/30',
   },
 });
