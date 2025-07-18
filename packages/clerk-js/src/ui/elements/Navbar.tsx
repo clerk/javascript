72eea014aec6bce45f75f347e837b2030adbcb31
Alex Carpenter <alex.carpenter@clerk.dev>
2025-07-18 15:46:27 -0400

    revert NavButton changes

--- packages/clerk-js/src/ui/elements/Navbar.tsx
+++ packages/clerk-js/src/ui/elements/Navbar.tsx
@@ -279,13 +279,14 @@ const NavButton = (props: NavButtonProps) => {
           gap: t.space.$3,
           justifyContent: 'flex-start',
           backgroundColor: isActive ? t.colors.$neutralAlpha100 : undefined,
-          color: isActive ? t.colors.$primary500 : t.colors.$colorMutedForeground,
+          color: isActive ? t.colors.$primary500 : t.colors.$neutralAlpha600,
           '&:hover': {
             backgroundColor: isActive ? undefined : t.colors.$neutralAlpha25,
           },
           '&:focus': {
             backgroundColor: isActive ? undefined : t.colors.$neutralAlpha50,
           },
+          opacity: isActive ? 1 : 0.6,
         }),
         sx,
       ]}
