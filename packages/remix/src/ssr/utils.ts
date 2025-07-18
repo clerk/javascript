9c0a3eec9c772d5e35538bc0ce490aecf59c19e0
Tom Milewski <me@tm.codes>
2025-07-18 04:29:37 -0400

    chore(repo): Update various dependencies (#6348)
    Co-authored-by: Nikos Douvlis <nikosdouvlis@gmail.com>

--- packages/remix/src/ssr/utils.ts
+++ packages/remix/src/ssr/utils.ts
@@ -2,7 +2,7 @@ import { constants, debugRequestState } from '@clerk/backend/internal';
 import { isTruthy } from '@clerk/shared/underscore';
 import type { AppLoadContext, defer } from '@remix-run/server-runtime';
 import { json } from '@remix-run/server-runtime';
-import cookie from 'cookie';
+import { parse as parseCookie } from 'cookie';
 
 import { getEnvVariable } from '../utils/utils';
 import type { RequestStateWithRedirectUrls } from './types';
@@ -22,7 +22,7 @@ export function isRedirect(res: Response): boolean {
 }
 
 export const parseCookies = (req: Request) => {
-  return cookie.parse(req.headers.get('cookie') || '');
+  return parseCookie(req.headers.get('cookie') || '');
 };
 
 export function assertValidHandlerResult(val: any, error?: string): asserts val is Record<string, unknown> | null {
