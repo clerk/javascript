c0bbd775eafba20464f2870355040e7af50ca4b8
panteliselef <panteliselef@outlook.com>
2025-07-22 12:07:56 +0300

    add description

--- packages/shared/src/react/hooks/useSubscription.tsx
+++ packages/shared/src/react/hooks/useSubscription.tsx
@@ -23,6 +23,10 @@ type UseSubscriptionParams = {
 
 /**
  * @internal
+ *
+ * @experimental This is an experimental API for the Billing feature that is available under a public beta, and the API is subject to change.
+ *
+ * Fetches subscription data for the current user or organization.
  */
 export const useSubscription = (params?: UseSubscriptionParams) => {
   useAssertWrappedByClerkProvider(hookName);
