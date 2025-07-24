9edfad5b12e3e4a54e0da7fd245b2a73d08aa407
panteliselef <panteliselef@outlook.com>
2025-07-24 13:31:39 +0300

    fix(clerk-js): Display alert on plan details error

--- packages/clerk-js/src/ui/components/Plans/PlanDetails.tsx
+++ packages/clerk-js/src/ui/components/Plans/PlanDetails.tsx
@@ -86,6 +86,8 @@ const PlanDetailsInternal = ({
     () => clerk.billing.getPlan({ id: planId || initialPlan?.id }),
     {
       fallbackData: initialPlan,
+      revalidateOnFocus: false,
+      shouldRetryOnError: false,
       keepPreviousData: true,
     },
   );
