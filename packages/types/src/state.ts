63c4a45f57640a328428f5d07de4cfb0821c22af
Dylan Staley <88163+dstaley@users.noreply.github.com>
2025-08-06 12:26:22 -0500

    chore(clerk-js): Add JSDoc comments

--- packages/types/src/state.ts
+++ packages/types/src/state.ts
@@ -1,6 +1,9 @@
 import type { SignInFutureResource } from './signIn';
 
 export interface State {
+  /**
+   * A Signal that updates when the underlying `SignIn` resource changes, including errors.
+   */
   signInSignal: {
     (): {
       errors: unknown;
@@ -8,6 +11,23 @@ export interface State {
     };
     (value: { errors: unknown; signIn: SignInFutureResource | null }): void;
   };
+
+  /**
+   * @experimental This experimental API is subject to change.
+   *
+   * An alias for `effect()` from `alien-signals`, which can be used to subscribe to changes from Signals.
+   *
+   * @see https://github.com/stackblitz/alien-signals#usage
+   */
   __internal_effect: (callback: () => void) => () => void;
+
+  /**
+   * @experimental This experimental API is subject to change.
+   *
+   * An alias for `computed()` from `alien-signals`, which can be used to create a computed Signal that updates when
+   * its dependencies change.
+   *
+   * @see https://github.com/stackblitz/alien-signals#usage
+   */
   __internal_computed: <T>(getter: (previousValue?: T) => T) => () => T;
 }
