a5d18585e8bc16e58b48fc6f9fffb00e0e47d479
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2025-08-06 22:57:16 -0300

    Fix E2E test assertion for eject flow

--- integration/tests/session-tasks-eject-flow.test.ts
+++ integration/tests/session-tasks-eject-flow.test.ts
@@ -96,7 +96,7 @@ return (
     await u.po.expect.toBeSignedIn();
 
     // Complete the organization selection task
-    await u.page.waitForAppUrl('/onboarding/select-organization');
+    await u.page.waitForAppUrl('/onboarding/choose-organization');
     const fakeOrganization = Object.assign(u.services.organizations.createFakeOrganization(), {
       slug: u.services.organizations.createFakeOrganization().slug + '-eject-flow',
     });
