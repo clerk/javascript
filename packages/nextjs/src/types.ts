66ac9bc75505277d79cc6cb54666439537008b72
Mike Wickett <mikewickett@clerk.dev>
2025-08-07 14:31:53 -0400

    drop unecessary type change

--- packages/nextjs/src/types.ts
+++ packages/nextjs/src/types.ts
@@ -1,24 +1,12 @@
 import type { ClerkProviderProps } from '@clerk/clerk-react';
 import type { Without } from '@clerk/types';
 
-export type NextClerkProviderProps = Without<ClerkProviderProps, 'publishableKey' | 'telemetry'> & {
+export type NextClerkProviderProps = Without<ClerkProviderProps, 'publishableKey'> & {
   /**
    * Used to override the default NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY env variable if needed.
    * This is optional for NextJS as the ClerkProvider will automatically use the NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY env variable if it exists.
    */
   publishableKey?: string;
-  /**
-   * Controls whether or not Clerk will collect [telemetry data](https://clerk.com/docs/telemetry). If set to `debug`, telemetry events are only logged to the console and not sent to Clerk.
-   */
-  telemetry?:
-    | false
-    | {
-        disabled?: boolean;
-        /**
-         * Telemetry events are only logged to the console and not sent to Clerk
-         */
-        debug?: boolean;
-      };
   /**
    * If set to true, the NextJS middleware will be invoked
    * every time the client-side auth state changes (sign-out, sign-in, organization switch etc.).
