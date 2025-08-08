1b16e6d086b8ffe3e1665fc87a0f893b2a2fa833
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2025-08-08 14:50:07 -0300

    Fix navigation between sign-in/sign-up steps

--- packages/clerk-js/src/core/clerk.ts
+++ packages/clerk-js/src/core/clerk.ts
@@ -1820,7 +1820,7 @@ export class Clerk implements ClerkInterface {
 
     const setActiveNavigate: SetActiveNavigate = async ({ session }) => {
       await navigateIfTaskExists(session, {
-        baseUrl: displayConfig.signInUrl,
+        baseUrl: params.signInUrl ?? displayConfig.signInUrl,
         navigate: this.navigate,
       });
     };
