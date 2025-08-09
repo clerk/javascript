a9787ef265b9871047a1da83a4147003a0aa7967
wobsoriano <sorianorobertc@gmail.com>
2025-08-08 23:28:02 -0700

    chore(repo): Improve express e2e tests

---
+++ integration/templates/express-vite/src/client/main.ts
@@ -0,0 +1,26 @@
+import { Clerk } from '@clerk/clerk-js';
+
+const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+document.addEventListener('DOMContentLoaded', async function () {
+  const clerk = new Clerk(publishableKey);
+  await clerk.load();
+
+  if (clerk.isSignedIn) {
+    document.getElementById('app')!.innerHTML = `
+          <div id="user-button"></div>
+        `;
+
+    const userButtonDiv = document.getElementById('user-button');
+
+    clerk.mountUserButton(userButtonDiv);
+  } else {
+    document.getElementById('app')!.innerHTML = `
+          <div id="sign-in"></div>
+        `;
+
+    const signInDiv = document.getElementById('sign-in');
+
+    clerk.mountSignIn(signInDiv);
+  }
+});
