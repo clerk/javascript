070e81698a1aa694bd447ea84840a808af5e53e8
panteliselef <panteliselef@outlook.com>
2025-08-20 16:53:42 +0300

    add experimental tags

--- packages/vue/src/experimental.ts
+++ packages/vue/src/experimental.ts
@@ -1,9 +1,41 @@
+/**
+ * @experimental
+ * These components and their prop types are unstable and may change in future releases.
+ * They are part of Clerk's Billing feature which is available under public beta.
+ */
 export { default as SubscriptionDetailsButton } from './components/SubscriptionDetailsButton.vue';
+
+/**
+ * @experimental
+ * These components and their prop types are unstable and may change in future releases.
+ * They are part of Clerk's Billing feature which is available under public beta.
+ */
 export { default as CheckoutButton } from './components/CheckoutButton.vue';
+
+/**
+ * @experimental
+ * These components and their prop types are unstable and may change in future releases.
+ * They are part of Clerk's Billing feature which is available under public beta.
+ */
 export { default as PlanDetailsButton } from './components/PlanDetailsButton.vue';
 
 export type {
+  /**
+   * @experimental
+   * These components and their prop types are unstable and may change in future releases.
+   * They are part of Clerk's Billing feature which is available under public beta.
+   */
   __experimental_SubscriptionDetailsButtonProps as SubscriptionDetailsButtonProps,
+  /**
+   * @experimental
+   * These components and their prop types are unstable and may change in future releases.
+   * They are part of Clerk's Billing feature which is available under public beta.
+   */
   __experimental_CheckoutButtonProps as CheckoutButtonProps,
+  /**
+   * @experimental
+   * These components and their prop types are unstable and may change in future releases.
+   * They are part of Clerk's Billing feature which is available under public beta.
+   */
   __experimental_PlanDetailsButtonProps as PlanDetailsButtonProps,
 } from '@clerk/types';
