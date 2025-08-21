580e2a5a41d87528b895f126de49cb488043986d
wobsoriano <sorianorobertc@gmail.com>
2025-08-21 00:25:32 -0700

    fix tests

--- integration/templates/tanstack-react-start/src/server.tsx
+++ integration/templates/tanstack-react-start/src/server.tsx
@@ -1,9 +1,13 @@
-import { createStartHandler, defaultStreamHandler } from '@tanstack/react-start/server';
+import { createStartHandler, defineHandlerCallback, defaultStreamHandler } from '@tanstack/react-start/server';
 import { createRouter } from './router';
 import { createClerkHandler } from '@clerk/tanstack-react-start/server';
 
-export default createClerkHandler(
-  createStartHandler({
-    createRouter,
-  }),
-)(defaultStreamHandler);
+export default defineHandlerCallback(async event => {
+  const startHandler = await createClerkHandler(
+    createStartHandler({
+      createRouter,
+    }),
+  )(defaultStreamHandler);
+
+  return startHandler(event);
+});
