65b12eeeb57ee80cdd8c36c5949d51f1227a413e
Alexis Aguilar <98043211+alexisintech@users.noreply.github.com>
2025-08-29 14:41:59 -0400

    chore(backend,types): Update JSDoc comments & export Feature type (#6649)

--- packages/backend/src/api/resources/Feature.ts
+++ packages/backend/src/api/resources/Feature.ts
@@ -1,11 +1,31 @@
 import type { FeatureJSON } from './JSON';
 
+/**
+ * The `Feature` object represents a feature of a subscription plan.
+ *
+ * @experimental This is an experimental API for the Billing feature that is available under a public beta, and the API is subject to change. It is advised to pin the SDK version to avoid breaking changes.
+ */
 export class Feature {
   constructor(
+    /**
+     * The unique identifier for the feature.
+     */
     readonly id: string,
+    /**
+     * The name of the feature.
+     */
     readonly name: string,
+    /**
+     * The description of the feature.
+     */
     readonly description: string,
+    /**
+     * The URL-friendly identifier of the feature.
+     */
     readonly slug: string,
+    /**
+     * The URL of the feature's avatar image.
+     */
     readonly avatarUrl: string,
   ) {}
 
