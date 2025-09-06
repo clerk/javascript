116be6684f161a2e4de1998e1cfd2ace70c20d72
Tom Milewski <me@tm.codes>
2025-09-05 23:04:02 -0400

    chore: Clean up Badge

--- packages/clerk-js/src/ui/elements/Badge.tsx
+++ packages/clerk-js/src/ui/elements/Badge.tsx
@@ -1,43 +1,43 @@
 import { Badge, descriptors, localizationKeys } from '../customizables';
 import type { InternalTheme, PropsOfComponent, ThemableCssProp } from '../styledSystem';
 
-export type LastAuthenticationStrategyBadgeProps = PropsOfComponent<typeof Badge> & { overlay?: boolean };
-export const LastAuthenticationStrategyBadge = ({ sx, overlay, ...props }: LastAuthenticationStrategyBadgeProps) => {
-  const overlayStyles = (t: InternalTheme): ThemableCssProp => [
-    {
-      background: `linear-gradient(${t.colors.$borderAlpha25}),${t.colors.$colorBackground}`,
-      borderColor: t.colors.$colorForeground,
-      boxShadow: 'none',
-      outline: `${t.space.$px} solid ${t.colors.$colorBackground}`,
+const overlayStyles = (t: InternalTheme): ThemableCssProp => [
+  {
+    background: `linear-gradient(${t.colors.$borderAlpha25}, ${t.colors.$borderAlpha25}),${t.colors.$colorBackground}`,
+    borderColor: t.colors.$colorForeground,
+    boxShadow: 'none',
+    outline: `${t.space.$px} solid ${t.colors.$colorBackground}`,
+    pointerEvents: 'none',
+    position: 'absolute',
+    right: `calc(${t.space.$2x5} * -1)`,
+    top: '-35%',
+    ':after': {
+      border: `${t.space.$px} solid ${t.colors.$borderAlpha150}`,
+      borderRadius: t.radii.$lg,
+      content: '""',
+      display: 'block',
+      height: '100%',
+      inset: 0,
+      padding: t.space.$px,
       position: 'absolute',
-      top: '-35%',
-      right: `calc(${t.space.$2x5} * -1)`,
-      ':after': {
-        border: `${t.space.$px} solid ${t.colors.$borderAlpha150}`,
-        borderRadius: t.radii.$lg,
-        content: '""',
-        display: 'block',
-        height: '100%',
-        padding: t.space.$px,
-        position: 'absolute',
-        width: '100%',
-      },
+      width: '100%',
     },
-  ];
+  },
+];
 
-  return (
-    <Badge
-      elementDescriptor={descriptors.lastAuthenticationStrategyBadge}
-      localizationKey={localizationKeys('lastAuthenticationStrategy')}
-      {...props}
-      sx={[
-        t => ({
-          background: t.colors.$borderAlpha25,
-          borderRadius: t.radii.$lg,
-        }),
-        overlay && overlayStyles,
-        sx,
-      ]}
-    />
-  );
-};
+export type LastAuthenticationStrategyBadgeProps = PropsOfComponent<typeof Badge> & { overlay?: boolean };
+export const LastAuthenticationStrategyBadge = ({ sx, overlay, ...props }: LastAuthenticationStrategyBadgeProps) => (
+  <Badge
+    elementDescriptor={descriptors.lastAuthenticationStrategyBadge}
+    localizationKey={localizationKeys('lastAuthenticationStrategy')}
+    {...props}
+    sx={[
+      t => ({
+        background: t.colors.$borderAlpha25,
+        borderRadius: t.radii.$lg,
+      }),
+      overlay && overlayStyles,
+      sx,
+    ]}
+  />
+);
