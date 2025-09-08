024806000342c808c6fe15b74e8107ec2ba4e5bb
Alex Carpenter <alex.carpenter@clerk.dev>
2025-09-08 11:26:25 -0400

    refactor(clerk-js): Adjust LastAuthenticationStrategyBadge styling

--- packages/clerk-js/src/ui/elements/Badge.tsx
+++ packages/clerk-js/src/ui/elements/Badge.tsx
@@ -1,47 +1,39 @@
-import { Badge, descriptors, localizationKeys } from '../customizables';
-import type { InternalTheme, PropsOfComponent, ThemableCssProp } from '../styledSystem';
-
-const overlayStyles = (t: InternalTheme): ThemableCssProp => [
-  {
-    background: `linear-gradient(${t.colors.$borderAlpha25}, ${t.colors.$borderAlpha25}),${t.colors.$colorBackground}`,
-    borderColor: t.colors.$colorForeground,
-    boxShadow: 'none',
-    outline: `${t.space.$px} solid ${t.colors.$colorBackground}`,
-    pointerEvents: 'none',
-    position: 'absolute',
-    right: `calc(${t.space.$2x5} * -1)`,
-    top: '-35%',
-    ':after': {
-      border: `${t.space.$px} solid ${t.colors.$borderAlpha150}`,
-      borderRadius: t.radii.$lg,
-      content: '""',
-      display: 'block',
-      height: '100%',
-      inset: 0,
-      padding: t.space.$px,
-      position: 'absolute',
-      width: '100%',
-    },
-  },
-];
+import type { Badge } from '../customizables';
+import { descriptors, localizationKeys, Span } from '../customizables';
+import { common, type PropsOfComponent } from '../styledSystem';
 
 export type LastAuthenticationStrategyBadgeProps = PropsOfComponent<typeof Badge> & { overlay?: boolean };
 export const LastAuthenticationStrategyBadge = ({
   sx,
   overlay,
   ...props
-}: LastAuthenticationStrategyBadgeProps): JSX.Element => (
-  <Badge
-    {...props}
-    elementDescriptor={descriptors.lastAuthenticationStrategyBadge}
-    localizationKey={localizationKeys('lastAuthenticationStrategy')}
-    sx={[
-      t => ({
-        background: t.colors.$borderAlpha25,
-        borderRadius: t.radii.$lg,
-      }),
-      overlay && overlayStyles,
-      ...(Array.isArray(sx) ? sx : sx ? [sx] : []),
-    ]}
-  />
-);
+}: LastAuthenticationStrategyBadgeProps): JSX.Element => {
+  return (
+    <Span
+      {...props}
+      elementDescriptor={descriptors.lastAuthenticationStrategyBadge}
+      localizationKey={localizationKeys('lastAuthenticationStrategy')}
+      sx={[
+        t => ({
+          ...common.textVariants(t).caption,
+          background: `linear-gradient(${t.colors.$borderAlpha25}, transparent), ${t.colors.$colorBackground}`,
+          border: `${t.space.$px} solid ${t.colors.$borderAlpha150}`,
+          borderRadius: t.radii.$lg,
+          color: t.colors.$colorMutedForeground,
+          height: t.space.$4x5,
+          paddingInline: t.space.$1x5,
+          pointerEvents: 'none',
+          whiteSpace: 'nowrap',
+          boxShadow: `0 0 0 1px ${t.colors.$colorBackground}`,
+          ...(overlay && {
+            position: 'absolute',
+            right: -1,
+            top: -1,
+            transform: `translate(${t.space.$2x5}, -50%)`,
+          }),
+        }),
+        sx,
+      ]}
+    />
+  );
+};
