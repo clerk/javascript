97b18708afe415a4f85117cde5bc8c880a6ad330
wobsoriano <sorianorobertc@gmail.com>
2025-09-09 15:39:15 -0700

    chore(remix): Add deprecation warning for maintenance mode

--- packages/remix/src/index.ts
+++ packages/remix/src/index.ts
@@ -1,5 +1,16 @@
 import './globalPolyfill';
 
+console.warn(`
+Clerk - DEPRECATION WARNING: @clerk/remix is now in maintenance mode.
+
+@clerk/remix will only receive security updates. No new features will be added.
+
+Please migrate to @clerk/react-router for continued development and new features:
+
+Migration guide: https://reactrouter.com/upgrading/remix
+React Router SDK: https://clerk.com/docs/quickstarts/react-router
+`);
+
 export * from './client';
 
 // Override Clerk React error thrower to show that errors come from @clerk/remix
