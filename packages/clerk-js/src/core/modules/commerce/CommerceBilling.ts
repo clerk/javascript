62446718b6adcf8210e96320db84e96685e979b4
panteliselef <panteliselef@outlook.com>
2025-09-09 18:56:39 +0300

    fix missing type

--- packages/clerk-js/src/core/modules/commerce/CommerceBilling.ts
+++ packages/clerk-js/src/core/modules/commerce/CommerceBilling.ts
@@ -14,6 +14,7 @@ import type {
   GetPaymentAttemptsParams,
   GetPlansParams,
   GetStatementsParams,
+  GetSubscriptionParams,
 } from '@clerk/types';
 
 import { convertPageToOffsetSearchParams } from '../../../utils/convertPageToOffsetSearchParams';
