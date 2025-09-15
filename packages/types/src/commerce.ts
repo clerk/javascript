dc69c34e2da1c83f9a474a21e683e83c6c869f62
Alexis Aguilar <98043211+alexisintech@users.noreply.github.com>
2025-09-15 17:01:47 -0400

    forgot to save

--- packages/types/src/commerce.ts
+++ packages/types/src/commerce.ts
@@ -521,11 +521,7 @@ export interface CommerceSubscriptionItemResource extends ClerkResource {
    */
   credit?: {
     /**
-     * @experimental This is an experimental API for the Billing feature that is available under a public beta, and the API is subject to change.
-     * It is advised to pin the SDK version and the clerk-js version to a specific version to avoid breaking changes.
-     * @example
-     * ```tsx
-     * <ClerkProvider clerkJsVersion="x.x.x" />
+     * @experimental This is an experimental API for the Billing feature that is available under a public beta, and the API is subject to change. It is advised to [pin](https://docs.renovatebot.com/dependency-pinning/#what-is-dependency-pinning) the SDK version and the clerk-js version to avoid breaking changes.
      * ```
      */
     amount: CommerceMoneyAmount;
