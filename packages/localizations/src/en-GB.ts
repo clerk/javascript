7a31231cc40905aea39949eda39aaf9e1ee5e28d
Tom Milewski <me@tm.codes>
2025-09-15 15:58:32 -0400

    chore: Clarify "Delete Account" messaging

--- packages/localizations/src/en-GB.ts
+++ packages/localizations/src/en-GB.ts
@@ -1036,7 +1036,8 @@ export const enGB: LocalizationResource = {
     deletePage: {
       actionDescription: 'Type "Delete account" below to continue.',
       confirm: 'Delete account',
-      messageLine1: 'Are you sure you want to delete your account?',
+      messageLine1:
+        'Are you sure you want to delete your account from Clerk? Please contact the application owner to delete your information from their site and other services.',
       messageLine2: 'This action is permanent and irreversible.',
       title: 'Delete account',
     },
