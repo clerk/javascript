a87f384f340758deb02c5cb109a6f28a4b2cc18e
Alexis Aguilar <98043211+alexisintech@users.noreply.github.com>
2025-09-15 17:43:30 -0400

    cleanup code ticks in comment

--- packages/types/src/commerce.ts
+++ packages/types/src/commerce.ts
@@ -522,7 +522,6 @@ export interface CommerceSubscriptionItemResource extends ClerkResource {
   credit?: {
     /**
      * @experimental This is an experimental API for the Billing feature that is available under a public beta, and the API is subject to change. It is advised to [pin](https://docs.renovatebot.com/dependency-pinning/#what-is-dependency-pinning) the SDK version and the clerk-js version to avoid breaking changes.
-     * ```
      */
     amount: CommerceMoneyAmount;
   };
