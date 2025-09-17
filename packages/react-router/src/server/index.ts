14450a396743b7d1446386fb0cbb9761b8b9ab02
wobsoriano <sorianorobertc@gmail.com>
2025-09-17 11:48:22 -0700

    pin versions

--- packages/react-router/src/server/index.ts
+++ packages/react-router/src/server/index.ts
@@ -2,3 +2,4 @@ export * from '@clerk/backend';
 export { clerkMiddleware } from './clerkMiddleware';
 export { rootAuthLoader } from './rootAuthLoader';
 export { getAuth } from './getAuth';
+export { clerkClient } from './clerkClient';
