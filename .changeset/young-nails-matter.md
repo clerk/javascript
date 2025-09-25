3aa3a073715b8a470126b4110d60d86f756c2605
panteliselef <panteliselef@outlook.com>
2025-09-25 12:45:16 +0300

    chore(clerk-js,backend): Replace `/commerce` endpoints with `/billing` endpoints

---
+++ .changeset/young-nails-matter.md
@@ -0,0 +1,6 @@
+---
+'@clerk/clerk-js': patch
+'@clerk/backend': patch
+---
+
+Replace `/commerce` endpoints with `/billing` endpoints.
--- packages/clerk-js/src/core/resources/BillingPaymentSource.ts
+++ packages/clerk-js/src/core/resources/BillingPaymentSource.ts
@@ -9,6 +9,7 @@ import type {
   RemovePaymentMethodParams,
 } from '@clerk/types';
 
+import { Billing } from '../modules/billing/namespace';
 import { BaseResource, DeletedObject } from './internal';
 
 export class BillingPaymentMethod extends BaseResource implements BillingPaymentMethodResource {
@@ -47,9 +48,7 @@ export class BillingPaymentMethod extends BaseResource implements BillingPayment
     const { orgId } = params ?? {};
     const json = (
       await BaseResource._fetch({
-        path: orgId
-          ? `/organizations/${orgId}/commerce/payment_sources/${this.id}`
-          : `/me/commerce/payment_sources/${this.id}`,
+        path: Billing.path(`/payment_sources/${this.id}`, { orgId }),
         method: 'DELETE',
       })
     )?.response as unknown as DeletedObjectJSON;
@@ -60,9 +59,7 @@ export class BillingPaymentMethod extends BaseResource implements BillingPayment
   public async makeDefault(params?: MakeDefaultPaymentMethodParams) {
     const { orgId } = params ?? {};
     await BaseResource._fetch({
-      path: orgId
-        ? `/organizations/${orgId}/commerce/payers/default_payment_source`
-        : `/me/commerce/payers/default_payment_source`,
+      path: Billing.path(`/payers/default_payment_source`, { orgId }),
       method: 'PUT',
       body: { payment_source_id: this.id } as any,
     });
