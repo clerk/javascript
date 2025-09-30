8853dc8684765f1bb5b7a335efa88d37efba6c49
Nikos Douvlis <nikosdouvlis@gmail.com>
2025-09-30 19:46:05 +0300

    attempt to fix tests

--- packages/clerk-js/src/ui/hooks/useDebounce.ts
+++ packages/clerk-js/src/ui/hooks/useDebounce.ts
@@ -2,29 +2,14 @@ import { useEffect, useState } from 'react';
 
 export function useDebounce<T>(value: T, delayInMs?: number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
-  const [timeoutState, setTimeoutState] = useState<ReturnType<typeof setTimeout> | undefined>(undefined);
 
   useEffect(() => {
-    const handleDebounce = () => {
-      if (timeoutState) {
-        clearTimeout(timeoutState);
-        setTimeoutState(undefined);
-      }
+    const timeoutId = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delayInMs || 500);
 
-      setTimeoutState(
-        setTimeout(() => {
-          setDebouncedValue(value);
-          setTimeoutState(undefined);
-        }, delayInMs || 500),
-      );
-    };
-
-    handleDebounce();
     return () => {
-      if (timeoutState) {
-        clearTimeout(timeoutState);
-        setTimeoutState(undefined);
-      }
+      clearTimeout(timeoutId);
     };
   }, [JSON.stringify(value), delayInMs]);
 
