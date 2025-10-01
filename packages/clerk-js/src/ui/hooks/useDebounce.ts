79e5c792538bb3b9cbe809443d81cc651615a0aa
Nikos Douvlis <nikosdouvlis@gmail.com>
2025-10-01 14:15:50 +0300

    revert usedebounce changes

--- packages/clerk-js/src/ui/hooks/useDebounce.ts
+++ packages/clerk-js/src/ui/hooks/useDebounce.ts
@@ -2,14 +2,29 @@ import { useEffect, useState } from 'react';
 
 export function useDebounce<T>(value: T, delayInMs?: number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
+  const [timeoutState, setTimeoutState] = useState<ReturnType<typeof setTimeout> | undefined>(undefined);
 
   useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setDebouncedValue(value);
-    }, delayInMs || 500);
+    const handleDebounce = () => {
+      if (timeoutState) {
+        clearTimeout(timeoutState);
+        setTimeoutState(undefined);
+      }
 
+      setTimeoutState(
+        setTimeout(() => {
+          setDebouncedValue(value);
+          setTimeoutState(undefined);
+        }, delayInMs || 500),
+      );
+    };
+
+    handleDebounce();
     return () => {
-      clearTimeout(timeoutId);
+      if (timeoutState) {
+        clearTimeout(timeoutState);
+        setTimeoutState(undefined);
+      }
     };
   }, [JSON.stringify(value), delayInMs]);
 
