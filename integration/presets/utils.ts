5848fa53cdffbc47ababcc2c7c1353c31fbcea2e
wobsoriano <sorianorobertc@gmail.com>
2025-10-05 12:46:58 -0700

    chore: bump integration tanstack versions

--- integration/presets/utils.ts
+++ integration/presets/utils.ts
@@ -1,10 +1,8 @@
-import path from 'node:path';
-
 export function linkPackage(pkg: string) {
-  // eslint-disable-next-line turbo/no-undeclared-env-vars
-  if (process.env.CI === 'true') {
-    return '*';
-  }
+  // if (process.env.CI === 'true') {
+  //   return '*';
+  // }
 
-  return `link:${path.resolve(process.cwd(), `packages/${pkg}`)}`;
+  // return `link:${path.resolve(process.cwd(), `packages/${pkg}`)}`;
+  return '*';
 }
