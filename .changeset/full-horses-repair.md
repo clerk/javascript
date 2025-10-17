d7c98af561f669fe670666e8c71afd7c677f21cd
guillaume <guillaume@p9f.fr>
2025-10-17 16:06:45 +0100

    fix(tanstack-react-start): do not consume the body of the request in middleware

---
+++ .changeset/full-horses-repair.md
@@ -0,0 +1,5 @@
+---
+'@clerk/tanstack-react-start': patch
+---
+
+fix bug where the clerkMiddleware would consume the body of the request
--- packages/tanstack-react-start/src/server/loadOptions.ts
+++ packages/tanstack-react-start/src/server/loadOptions.ts
@@ -1,4 +1,4 @@
-import { createClerkRequest } from '@clerk/backend/internal';
+import { ClerkRequest } from '@clerk/backend/internal';
 import { apiUrlFromPublishableKey } from '@clerk/shared/apiUrlFromPublishableKey';
 import { getEnvVariable } from '@clerk/shared/getEnvVariable';
 import { isDevelopmentFromSecretKey } from '@clerk/shared/keys';
@@ -9,18 +9,16 @@ import { errorThrower } from '../utils';
 import { getPublicEnvVariables } from '../utils/env';
 import { commonEnvs } from './constants';
 import type { LoaderOptions } from './types';
-import { patchRequest } from './utils';
 
-export const loadOptions = (request: Request, overrides: LoaderOptions = {}) => {
-  const clerkRequest = createClerkRequest(patchRequest(request));
+export const loadOptions = (clerkRequest: ClerkRequest, overrides: LoaderOptions = {}) => {
   const commonEnv = commonEnvs();
   const secretKey = overrides.secretKey || commonEnv.SECRET_KEY;
   const machineSecretKey = overrides.machineSecretKey || commonEnv.MACHINE_SECRET_KEY;
   const publishableKey = overrides.publishableKey || commonEnv.PUBLISHABLE_KEY;
   const jwtKey = overrides.jwtKey || commonEnv.CLERK_JWT_KEY;
   const apiUrl = getEnvVariable('CLERK_API_URL') || apiUrlFromPublishableKey(publishableKey);
-  const domain = handleValueOrFn(overrides.domain, new URL(request.url)) || commonEnv.DOMAIN;
-  const isSatellite = handleValueOrFn(overrides.isSatellite, new URL(request.url)) || commonEnv.IS_SATELLITE;
+  const domain = handleValueOrFn(overrides.domain, new URL(clerkRequest.url)) || commonEnv.DOMAIN;
+  const isSatellite = handleValueOrFn(overrides.isSatellite, new URL(clerkRequest.url)) || commonEnv.IS_SATELLITE;
   const relativeOrAbsoluteProxyUrl = handleValueOrFn(overrides?.proxyUrl, clerkRequest.clerkUrl, commonEnv.PROXY_URL);
   const signInUrl = overrides.signInUrl || commonEnv.SIGN_IN_URL;
   const signUpUrl = overrides.signUpUrl || commonEnv.SIGN_UP_URL;
