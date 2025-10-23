65c1be6baf336a3f9501a50edd0192d86fe11833
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2025-10-23 12:48:36 -0300

    Use shared `Action` across AIOs

--- packages/clerk-js/src/ui/components/OrganizationSwitcher/OtherOrganizationActions.tsx
+++ packages/clerk-js/src/ui/components/OrganizationSwitcher/OtherOrganizationActions.tsx
@@ -1,10 +1,8 @@
-import { useUser } from '@clerk/shared/react';
 import React from 'react';
 
-import { Action } from '@/ui/elements/Actions';
+import { CreateOrganizationAction } from '@/ui/common/CreateOrganizationAction';
 
 import { descriptors, localizationKeys } from '../../customizables';
-import { Add } from '../../icons';
 import { UserInvitationSuggestionList } from './UserInvitationSuggestionList';
 import type { UserMembershipListProps } from './UserMembershipList';
 import { UserMembershipList } from './UserMembershipList';
@@ -16,21 +14,14 @@ export interface OrganizationActionListProps extends UserMembershipListProps {
 const CreateOrganizationButton = ({
   onCreateOrganizationClick,
 }: Pick<OrganizationActionListProps, 'onCreateOrganizationClick'>) => {
-  const { user } = useUser();
-
-  if (!user?.createOrganizationEnabled) {
-    return null;
-  }
-
   return (
-    <Action
+    <CreateOrganizationAction
       elementDescriptor={descriptors.organizationSwitcherPopoverActionButton}
       elementId={descriptors.organizationSwitcherPopoverActionButton.setId('createOrganization')}
       iconBoxElementDescriptor={descriptors.organizationSwitcherPopoverActionButtonIconBox}
       iconBoxElementId={descriptors.organizationSwitcherPopoverActionButtonIconBox.setId('createOrganization')}
       iconElementDescriptor={descriptors.organizationSwitcherPopoverActionButtonIcon}
       iconElementId={descriptors.organizationSwitcherPopoverActionButtonIcon.setId('createOrganization')}
-      icon={Add}
       label={localizationKeys('organizationSwitcher.action__createOrganization')}
       onClick={onCreateOrganizationClick}
       sx={t => ({
