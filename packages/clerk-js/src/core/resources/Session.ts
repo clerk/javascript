5d22353711e895afa0b3ac8af3dccad74e4ffc26
Jacek <jacek@clerk.dev>
2025-10-30 13:41:47 -0500

    feat: remove hydrateCache

--- packages/clerk-js/src/core/resources/Session.ts
+++ packages/clerk-js/src/core/resources/Session.ts
@@ -188,7 +188,7 @@ export class Session extends BaseResource implements SessionResource {
         body: {
           ...config,
           strategy: factor.strategy,
-        } as any,
+        },
       })
     )?.response as unknown as SessionVerificationJSON;
 
@@ -214,7 +214,7 @@ export class Session extends BaseResource implements SessionResource {
       await BaseResource._fetch({
         method: 'POST',
         path: `/client/sessions/${this.id}/verify/attempt_first_factor`,
-        body: { ...config, strategy: attemptFactor.strategy } as any,
+        body: { ...config, strategy: attemptFactor.strategy },
       })
     )?.response as unknown as SessionVerificationJSON;
 
