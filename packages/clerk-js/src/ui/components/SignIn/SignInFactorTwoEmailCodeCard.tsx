b70b5dbfe4b2f01b2f7af1b24d262a303871899c
Tom Milewski <me@tm.codes>
2025-11-04 16:29:22 -0500

    chore: Ensure types pull from shared pkg

--- packages/clerk-js/src/ui/components/SignIn/SignInFactorTwoEmailCodeCard.tsx
+++ packages/clerk-js/src/ui/components/SignIn/SignInFactorTwoEmailCodeCard.tsx
@@ -1,4 +1,4 @@
-import type { EmailCodeFactor } from '@clerk/types';
+import type { EmailCodeFactor } from '@clerk/shared/types';
 
 import { useCoreSignIn } from '../../contexts';
 import { Flow, localizationKeys } from '../../customizables';
