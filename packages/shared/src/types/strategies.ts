85b5acc5ba192a8247f072fa93d5bc7d42986293
Christopher Canin <chris@clerk.dev>
2025-11-04 10:03:19 -0800

    feat(clerk-expo): Add native Apple Sign-In support for iOS (#7053)

--- packages/shared/src/types/strategies.ts
+++ packages/shared/src/types/strategies.ts
@@ -2,6 +2,7 @@ import type { OAuthProvider } from './oauth';
 import type { Web3Provider } from './web3';
 
 export type GoogleOneTapStrategy = 'google_one_tap';
+export type AppleIdTokenStrategy = 'oauth_token_apple';
 export type PasskeyStrategy = 'passkey';
 export type PasswordStrategy = 'password';
 export type PhoneCodeStrategy = 'phone_code';
