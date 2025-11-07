6bf564cb045154900041c8f30b2bcc62069b682e
Jacek <jacek@clerk.dev>
2025-11-07 15:08:05 -0600

    wip

--- packages/react/src/contexts/AuthContext.ts
+++ packages/react/src/contexts/AuthContext.ts
@@ -8,16 +8,16 @@ import type {
 } from '@clerk/shared/types';
 
 export type AuthContextValue = {
-  userId: string | null | undefined;
-  sessionId: string | null | undefined;
-  sessionStatus: SessionStatusClaim | null | undefined;
-  sessionClaims: JwtPayload | null | undefined;
   actor: ActClaim | null | undefined;
+  factorVerificationAge: [number, number] | null;
   orgId: string | null | undefined;
+  orgPermissions: OrganizationCustomPermissionKey[] | null | undefined;
   orgRole: OrganizationCustomRoleKey | null | undefined;
   orgSlug: string | null | undefined;
-  orgPermissions: OrganizationCustomPermissionKey[] | null | undefined;
-  factorVerificationAge: [number, number] | null;
+  sessionClaims: JwtPayload | null | undefined;
+  sessionId: string | null | undefined;
+  sessionStatus: SessionStatusClaim | null | undefined;
+  userId: string | null | undefined;
 };
 
 export const [AuthContext, useAuthContext] = createContextAndHook<AuthContextValue>('AuthContext');
