a9bc9c8ff522433b57e9c645e0da06ba752165a5
Alex Carpenter <alex.carpenter@clerk.dev>
2025-11-18 14:04:10 -0500

    wip

--- packages/clerk-js/src/ui/components/devPrompts/shared.tsx
+++ packages/clerk-js/src/ui/components/devPrompts/shared.tsx
@@ -17,7 +17,6 @@ export function PromptContainer({ children, sx, ...props }: React.ComponentProps
           background: 'linear-gradient(180deg, rgba(255, 255, 255, 0.16) 0%, rgba(255, 255, 255, 0) 100%), #1f1f1f',
           boxShadow:
             '0px 0px 0px 0.5px #2F3037 inset, 0px 1px 0px 0px rgba(255, 255, 255, 0.08) inset, 0px 0px 0.8px 0.8px rgba(255, 255, 255, 0.20) inset, 0px 0px 0px 0px rgba(255, 255, 255, 0.72), 0px 16px 36px -6px rgba(0, 0, 0, 0.36), 0px 6px 16px -2px rgba(0, 0, 0, 0.20);',
-          transition: 'all 195ms cubic-bezier(0.2, 0.61, 0.1, 1)',
         },
         sx,
       ]}
