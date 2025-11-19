015be7ff7324922a4fa42e2f6df9653bb532a832
Vaggelis Yfantis <vaggelisyfantis@gmail.com>
2025-11-19 13:11:46 +0200

    feat(clerk-js): Initial work for reset password task

--- packages/shared/src/error.ts
+++ packages/shared/src/error.ts
@@ -24,6 +24,7 @@ export {
   isMetamaskError,
   isNetworkError,
   isPasswordPwnedError,
+  isPasswordUntrustedError,
   isReverificationCancelledError,
   isUnauthorizedError,
   isUserLockedError,
