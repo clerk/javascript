4776e50b86dd323fb64d6ee0a5fcbe96143d328f
wobsoriano <sorianorobertc@gmail.com>
2025-11-20 11:48:25 -0800

    inherit attrs

--- packages/nuxt/src/runtime/components/uiComponents.ts
+++ packages/nuxt/src/runtime/components/uiComponents.ts
@@ -2,7 +2,6 @@ import type {
   CreateOrganizationProps,
   OrganizationListProps,
   OrganizationProfileProps,
-  RoutingOptions,
   SignInProps,
   SignUpProps,
   UserProfileProps,
@@ -52,10 +51,14 @@ export const usePathnameWithoutSplatRouteParams = () => {
 // The assignment of UserProfile with BaseUserProfile props is used
 // to support the CustomPage functionality (eg UserProfile.Page)
 export const UserProfile = Object.assign(
-  defineComponent((props: UserProfileProps) => {
+  defineComponent((_props: UserProfileProps, { attrs, slots }) => {
     const path = usePathnameWithoutSplatRouteParams();
-    const routingProps = useRoutingProps('UserProfile', props, () => ({ path: path.value }));
-    return () => h(BaseUserProfile, routingProps.value);
+    const routingProps = useRoutingProps(
+      'UserProfile',
+      () => attrs,
+      () => ({ path: path.value }),
+    );
+    return () => h(BaseUserProfile, routingProps.value, slots);
   }),
   { Page: BaseUserProfile.Page, Link: BaseUserProfile.Link },
 ) as unknown as typeof BaseUserProfile;
@@ -63,34 +66,54 @@ export const UserProfile = Object.assign(
 // The assignment of OrganizationProfile with BaseOrganizationProfile props is used
 // to support the CustomPage functionality (eg OrganizationProfile.Page)
 export const OrganizationProfile = Object.assign(
-  defineComponent((props: OrganizationProfileProps) => {
+  defineComponent((_props: OrganizationProfileProps, { attrs, slots }) => {
     const path = usePathnameWithoutSplatRouteParams();
-    const routingProps = useRoutingProps('OrganizationProfile', props, () => ({ path: path.value }));
-    return () => h(BaseOrganizationProfile, routingProps.value);
+    const routingProps = useRoutingProps(
+      'OrganizationProfile',
+      () => attrs,
+      () => ({ path: path.value }),
+    );
+    return () => h(BaseOrganizationProfile, routingProps.value, slots);
   }),
   { Page: BaseOrganizationProfile.Page, Link: BaseOrganizationProfile.Link },
 ) as unknown as typeof BaseOrganizationProfile;
 
-export const CreateOrganization = defineComponent((props: CreateOrganizationProps) => {
+export const CreateOrganization = defineComponent((_props: CreateOrganizationProps, { attrs }) => {
   const path = usePathnameWithoutSplatRouteParams();
-  const routingProps = useRoutingProps('CreateOrganization', props, () => ({ path: path.value }));
+  const routingProps = useRoutingProps(
+    'CreateOrganization',
+    () => attrs,
+    () => ({ path: path.value }),
+  );
   return () => h(BaseCreateOrganization, routingProps.value);
 });
 
-export const OrganizationList = defineComponent((props: OrganizationListProps) => {
+export const OrganizationList = defineComponent((_props: OrganizationListProps, { attrs }) => {
   const path = usePathnameWithoutSplatRouteParams();
-  const routingProps = useRoutingProps('OrganizationList', props as RoutingOptions, () => ({ path: path.value }));
+  const routingProps = useRoutingProps(
+    'OrganizationList',
+    () => attrs,
+    () => ({ path: path.value }),
+  );
   return () => h(BaseOrganizationList, routingProps.value);
 });
 
-export const SignIn = defineComponent((props: SignInProps) => {
+export const SignIn = defineComponent((_props: SignInProps, { attrs }) => {
   const path = usePathnameWithoutSplatRouteParams();
-  const routingProps = useRoutingProps('SignIn', props, () => ({ path: path.value }));
+  const routingProps = useRoutingProps(
+    'SignIn',
+    () => attrs,
+    () => ({ path: path.value }),
+  );
   return () => h(BaseSignIn, routingProps.value);
 });
 
-export const SignUp = defineComponent((props: SignUpProps) => {
+export const SignUp = defineComponent((_props: SignUpProps, { attrs }) => {
   const path = usePathnameWithoutSplatRouteParams();
-  const routingProps = useRoutingProps('SignUp', props, () => ({ path: path.value }));
+  const routingProps = useRoutingProps(
+    'SignUp',
+    () => attrs,
+    () => ({ path: path.value }),
+  );
   return () => h(BaseSignUp, routingProps.value);
 });
