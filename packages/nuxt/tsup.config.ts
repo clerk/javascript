00e830381d81f2c1e917c91d6c0dd889f0105ca1
wobsoriano <sorianorobertc@gmail.com>
2025-11-21 08:21:28 -0800

    chore: remove manual external vue

--- packages/nuxt/tsup.config.ts
+++ packages/nuxt/tsup.config.ts
@@ -26,6 +26,6 @@ export default defineConfig(() => {
       PACKAGE_NAME: `"${name}"`,
       PACKAGE_VERSION: `"${version}"`,
     },
-    external: ['#imports', 'vue'],
+    external: ['#imports'],
   };
 });
