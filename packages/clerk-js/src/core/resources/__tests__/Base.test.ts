63ee32b6a31455ec9944113b26e43a1d06112257
Henry Snopek <henry@clerk.dev>
2025-11-24 22:28:53 -0600

    feat: retry on 503, add Retry-After date (RFC1123) support

--- packages/clerk-js/src/core/resources/__tests__/Base.test.ts
+++ packages/clerk-js/src/core/resources/__tests__/Base.test.ts
@@ -38,6 +38,27 @@ describe('BaseResource', () => {
     expect(errResponse.retryAfter).toBe(60);
   });
 
+  it('populates retryAfter on 503 error responses', async () => {
+    BaseResource.clerk = {
+      // @ts-expect-error - We're not about to mock the entire FapiClient
+      getFapiClient: () => {
+        return {
+          request: vi.fn().mockResolvedValue({
+            payload: {},
+            status: 503,
+            statusText: 'Service Unavailable',
+            headers: new Headers({ 'Retry-After': new Date(new Date().getTime() + 60000).toUTCString() }),
+          }),
+        };
+      },
+      __internal_setCountry: vi.fn(),
+    };
+    const resource = new TestResource();
+    const errResponse = await resource.fetch().catch(err => err);
+    console.dir(errResponse);
+    expect(errResponse.retryAfter).toBe(60);
+  });
+
   it('does not populate retryAfter on invalid header', async () => {
     BaseResource.clerk = {
       // @ts-expect-error - We're not about to mock the entire FapiClient
