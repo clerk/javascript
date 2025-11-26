1c13a4ac7ef8ce707dc149e66f9460ca07b49d32
Kenton Duprey <kenton@clerk.dev>
2025-11-25 23:44:17 -0500

    refactor(clerk-js): simplify constructor and improve wallet retrieval logic
    Signed-off-by: Kenton Duprey <kenton@clerk.dev>

--- packages/clerk-js/src/utils/injectedWeb3SolanaProviders.ts
+++ packages/clerk-js/src/utils/injectedWeb3SolanaProviders.ts
@@ -7,11 +7,7 @@ class InjectedWeb3SolanaProviders {
   #wallets: readonly Wallet[] | undefined = undefined;
   static #instance: InjectedWeb3SolanaProviders | null = null;
 
-  private constructor() {
-    if (typeof window === 'undefined') {
-      return;
-    }
-  }
+  private constructor() {}
 
   async #initialize() {
     const wallets = await import('@wallet-standard/core').then(mod => mod.getWallets());
@@ -42,15 +38,16 @@ class InjectedWeb3SolanaProviders {
 
   get = async (walletName: string): Promise<SolanaWalletAdapterWallet | undefined> => {
     await this.#initialize();
-    if (this.#wallets) {
-      const wallet = this.#wallets.find(
-        w => w.name === walletName && this.#isSolanaWallet(w) && this.#hasSignMessage(w),
-      );
-      if (wallet && this.#isSolanaWallet(wallet)) {
-        return wallet;
-      }
+    const wallet = (this.#wallets || []).find(
+      w => w.name === walletName && this.#isSolanaWallet(w) && this.#hasSignMessage(w),
+    );
+    if (wallet && this.#isSolanaWallet(wallet)) {
+      return wallet;
     }
 
+    if (typeof window === 'undefined') {
+      return undefined;
+    }
     // In case we weren't able to find the requested provider, fallback to the
     // global injected provider instead, if any, to allow the user to continue
     // the flow rather than blocking it
