166f3aed55ec577d4fd7537e9e5ef3f9adcb1dba
Alex Bratsos <alexisbratsos@gmail.com>
2025-11-27 20:04:15 +0100

    feat(repo): Add a library-agnostic `getToken` helper

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -8,6 +8,7 @@ export * from './components';
 export * from './contexts';
 
 export * from './hooks';
+export { getToken } from '@clerk/shared/getToken';
 export type {
   BrowserClerk,
   BrowserClerkConstructor,
