0f8419f687e152924da5d2577cc88e56672588da
Alex Bratsos <alexisbratsos@gmail.com>
2025-11-27 20:04:15 +0100

    feat(repo): Add a library-agnostic `getToken` helper

--- packages/astro/src/client/index.ts
+++ packages/astro/src/client/index.ts
@@ -1,2 +1,3 @@
 export { updateClerkOptions } from '../internal/create-clerk-instance';
 export * from '../stores/external';
+export { getToken } from '@clerk/shared/getToken';
--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -7,6 +7,7 @@ export * from './composables';
 
 export { clerkPlugin, type PluginOptions } from './plugin';
 export { updateClerkOptions } from './utils';
+export { getToken } from '@clerk/shared/getToken';
 
 setErrorThrowerOptions({ packageName: PACKAGE_NAME });
 setClerkJsLoadingErrorPackageName(PACKAGE_NAME);
