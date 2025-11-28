f871d5a2a80d352f034b493dd43fdfbf5e6ae966
Vaggelis Yfantis <vaggelisyfantis@gmail.com>
2025-11-28 18:47:01 +0200

    fix(clerk-js): Remove uneeded changes

--- packages/clerk-js/src/ui/components/SessionTasks/index.tsx
+++ packages/clerk-js/src/ui/components/SessionTasks/index.tsx
@@ -97,8 +97,7 @@ export const SessionTasks = withCardStateProvider(({ redirectUrlComplete }: Sess
     // Tasks can only exist on pending sessions, but we check both conditions
     // here to be defensive and ensure proper redirection
     const task = clerk.session?.currentTask;
-    if (!task || clerk.session?.status === 'active' || clerk.__internal_setActiveInProgress) {
-      console.log('navigating to redirectUrlComplete', redirectUrlComplete);
+    if (!task || clerk.session?.status === 'active') {
       void navigate(redirectUrlComplete);
       return;
     }
