577871af40f067bfeff668eed02cdd19e563cf8b
Vaggelis Yfantis <vaggelisyfantis@gmail.com>
2025-12-03 17:35:44 +0200

    chore: Update localization

--- packages/localizations/src/en-US.ts
+++ packages/localizations/src/en-US.ts
@@ -903,6 +903,8 @@ export const enUS: LocalizationResource = {
     form_password_pwned__sign_in:
       'This password has been found as part of a breach and can not be used, please reset your password.',
     form_password_size_in_bytes_exceeded: undefined,
+    form_password_compromised__sign_in:
+      "Your password appears to have been compromised or it's no longer trusted and cannot be used. Please use another method to continue.",
     form_password_untrusted__sign_in:
       "Your password appears to have been compromised or it's no longer trusted and cannot be used. Please use another method to continue.",
     form_password_validation_failed: undefined,
