5e4ed9e17c82df4534603a92a46fecab39cb9448
Bryce Kalow <bryce@clerk.dev>
2025-12-08 15:05:22 -0600

    fix lint

--- packages/clerk-js/src/global.d.ts
+++ packages/clerk-js/src/global.d.ts
@@ -1,21 +1,23 @@
+import type { ClerkUiConstructor } from '@clerk/shared/types';
+
 declare module '*.svg' {
   const value: React.FC<React.SVGAttributes<SVGElement>>;
   export default value;
 }
 
-const __PKG_NAME__: string;
-const __PKG_VERSION__: string;
-const __DEV__: boolean;
+declare const __PKG_NAME__: string;
+declare const __PKG_VERSION__: string;
+declare const __DEV__: boolean;
 
 /**
  * Build time feature flags.
  */
-const __BUILD_DISABLE_RHC__: string;
-const __BUILD_VARIANT_CHANNEL__: boolean;
-const __BUILD_VARIANT_CHIPS__: boolean;
+declare const __BUILD_DISABLE_RHC__: string;
+declare const __BUILD_VARIANT_CHANNEL__: boolean;
+declare const __BUILD_VARIANT_CHIPS__: boolean;
 
 interface Window {
   __internal_onBeforeSetActive: (intent?: 'sign-out') => Promise<void> | void;
   __internal_onAfterSetActive: () => Promise<void> | void;
-  __internal_ClerkUiCtor?: import('@clerk/shared').ClerkUiConstructor;
+  __internal_ClerkUiCtor?: ClerkUiConstructor;
 }
