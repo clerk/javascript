00b626ab2516c64d8c48ec067b98282775b26e13
Jacek <jacek@clerk.dev>
2025-12-09 09:01:23 -0600

    wip

--- packages/chrome-extension/src/internal/clerk.ts
+++ packages/chrome-extension/src/internal/clerk.ts
@@ -37,6 +37,7 @@ export function createClerkClient({
 }: CreateClerkClientOptions) {
   if (scope === SCOPE.BACKGROUND) {
     // TODO @nikos
+    // @ts-expect-error will be replaced by clerk ui
     Clerk.mountComponentRenderer = undefined;
   }
 
