72f167e7791f1c4b293c7d406c96e9663697f22e
wobsoriano <sorianorobertc@gmail.com>
2025-12-11 15:57:06 -0800

    retry

--- packages/clerk-js/src/utils/assertNoLegacyProp.ts
+++ packages/clerk-js/src/utils/assertNoLegacyProp.ts
@@ -8,14 +8,40 @@ export function assertNoLegacyProp(props: Record<string, any>) {
   if (legacyProp && props[legacyProp]) {
     // Don't warn about redirectUrl if it's auto-set by Clerk for preservation
     // (i.e., it equals the current window.location.href, which is what buildSignInUrl sets automatically)
-    if (
-      legacyProp === 'redirectUrl' &&
-      inBrowser() &&
-      typeof props[legacyProp] === 'string' &&
-      props[legacyProp] === window.location.href
-    ) {
+    const isRedirectUrl = legacyProp === 'redirectUrl';
+    const browserCheck = inBrowser();
+    const isString = typeof props[legacyProp] === 'string';
+    const windowHref = typeof window !== 'undefined' ? window.location.href : 'N/A';
+    const propValue = props[legacyProp];
+    const valuesMatch = isString && propValue === windowHref;
+
+    // #region agent log
+    console.log('[DEBUG assertNoLegacyProp]', {
+      legacyProp,
+      isRedirectUrl,
+      browserCheck,
+      isString,
+      propValue: typeof propValue === 'string' ? propValue.substring(0, 200) : propValue,
+      windowHref: typeof windowHref === 'string' ? windowHref.substring(0, 200) : windowHref,
+      valuesMatch,
+      propValueLength: typeof propValue === 'string' ? propValue.length : 0,
+      windowHrefLength: typeof windowHref === 'string' ? windowHref.length : 0,
+      allPropsKeys: Object.keys(props),
+    });
+    // #endregion
+
+    if (isRedirectUrl && browserCheck && isString && valuesMatch) {
+      // #region agent log
+      console.log('[DEBUG assertNoLegacyProp] Returning early - no warning');
+      // #endregion
       return;
     }
+
+    // #region agent log
+    console.log('[DEBUG assertNoLegacyProp] Warning will be logged', {
+      reasonNotSkipped: { isRedirectUrl, browserCheck, isString, valuesMatch },
+    });
+    // #endregion
 
     logger.warnOnce(
       `Clerk: The prop "${legacyProp}" is deprecated and should be replaced with the new "fallbackRedirectUrl" or "forceRedirectUrl" props instead. Learn more: https://clerk.com/docs/guides/custom-redirects#redirect-url-props`,
