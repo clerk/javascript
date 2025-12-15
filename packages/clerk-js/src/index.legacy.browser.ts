177cfb885447db9f4ef047b824bc735c36145c49
Jacek <jacek@clerk.dev>
2025-12-15 15:24:12 -0600

    fix lint

--- packages/clerk-js/src/index.legacy.browser.ts
+++ packages/clerk-js/src/index.legacy.browser.ts
@@ -1,6 +1,6 @@
 // It's crucial this is the first import,
 // otherwise chunk loading will not work
-// eslint-disable-next-line
+
 import './utils/setWebpackChunkPublicPath';
 
 import { Clerk } from './core/clerk';
