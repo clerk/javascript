a43dbd93ff9ba0ef01d34bfb81efc70990e9e5f9
Kenton Duprey <kenton@clerk.dev>
2025-12-17 15:55:53 -0500

    fix(ui): Correct webpack chunk name for Solana wallet buttons
    Signed-off-by: Kenton Duprey <kenton@clerk.dev>

--- packages/clerk-js/src/ui/components/SignUp/SignUpStartSolanaWalletsCard.tsx
+++ packages/clerk-js/src/ui/components/SignUp/SignUpStartSolanaWalletsCard.tsx
@@ -10,7 +10,7 @@ import { Header } from '@/ui/elements/Header';
 import { web3CallbackErrorHandler } from '@/ui/utils/web3CallbackErrorHandler';
 
 const Web3SolanaWalletButtons = lazy(() =>
-  import(/* webpackChunkName: "web3-wallet-buttons" */ '@/ui/elements/Web3SolanaWalletButtons').then(m => ({
+  import(/* webpackChunkName: "web3-solana-wallet-buttons" */ '@/ui/elements/Web3SolanaWalletButtons').then(m => ({
     default: m.Web3SolanaWalletButtons,
   })),
 );
