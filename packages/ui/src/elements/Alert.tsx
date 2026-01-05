593d282ba1949cabf88890058a0173fc3b4c928b
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2026-01-05 17:03:52 -0300

    Update `Alert` to use same color scheme between subtitle and title

--- packages/ui/src/elements/Alert.tsx
+++ packages/ui/src/elements/Alert.tsx
@@ -1,17 +1,18 @@
 import type { LocalizationKey } from '../customizables';
 import { Alert as AlertCust, AlertIcon, Col, descriptors, Text } from '../customizables';
-import type { PropsOfComponent } from '../styledSystem';
+import type { PropsOfComponent, ThemableCssProp } from '../styledSystem';
 
 type _AlertProps = {
   variant?: 'danger' | 'warning' | 'info';
   title?: LocalizationKey | string;
   subtitle?: LocalizationKey | string;
+  titleSx?: ThemableCssProp;
 };
 
 type AlertProps = Omit<PropsOfComponent<typeof AlertCust>, keyof _AlertProps> & _AlertProps;
 
 export const Alert = (props: AlertProps): JSX.Element | null => {
-  const { children, title, subtitle, variant = 'warning', ...rest } = props;
+  const { children, title, subtitle, variant = 'warning', titleSx, ...rest } = props;
 
   if (!children && !title && !subtitle) {
     return null;
@@ -44,7 +45,7 @@ export const Alert = (props: AlertProps): JSX.Element | null => {
           colorScheme={variant === 'danger' ? 'danger' : variant === 'warning' ? 'warning' : 'secondary'}
           variant='body'
           localizationKey={title}
-          sx={{ textAlign: 'left' }}
+          sx={[{ textAlign: 'left' }, titleSx]}
         >
           {children}
         </Text>
@@ -52,7 +53,7 @@ export const Alert = (props: AlertProps): JSX.Element | null => {
           <Text
             elementDescriptor={descriptors.alertText}
             elementId={descriptors.alert.setId(variant)}
-            colorScheme={variant === 'danger' ? 'danger' : 'secondary'}
+            colorScheme={variant === 'danger' ? 'danger' : variant === 'warning' ? 'warning' : 'secondary'}
             variant='body'
             localizationKey={subtitle}
           />
