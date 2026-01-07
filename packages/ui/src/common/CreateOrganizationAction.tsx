b6796ea2e36cb7e8e4ad16a01184638c17deeec2
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2026-01-07 19:28:34 -0300

    Fix boolean check

---
+++ packages/ui/src/common/CreateOrganizationAction.tsx
@@ -0,0 +1,27 @@
+import { useUser } from '@clerk/shared/react/index';
+
+import { useEnvironment } from '../contexts';
+import { Action } from '../elements/Actions';
+import { Add } from '../icons';
+
+type CreateOrganizationActionProps = Omit<React.ComponentProps<typeof Action>, 'icon'>;
+
+export const CreateOrganizationAction = (props: CreateOrganizationActionProps) => {
+  const { user } = useUser();
+  const { organizationSettings } = useEnvironment();
+
+  const currentMembershipCount = (user?.organizationMemberships ?? []).length;
+  const canCreateAdditionalMembership =
+    !organizationSettings.maxAllowedMemberships || currentMembershipCount < organizationSettings.maxAllowedMemberships;
+
+  if (!user?.createOrganizationEnabled || !canCreateAdditionalMembership) {
+    return null;
+  }
+
+  return (
+    <Action
+      {...props}
+      icon={Add}
+    />
+  );
+};
