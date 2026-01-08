125f1ab4ccbccc6f0fa5f5e3803bb6f93981a1e4
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2026-01-08 12:38:25 -0300

    Conditionally render localization key

--- packages/localizations/src/pt-PT.ts
+++ packages/localizations/src/pt-PT.ts
@@ -862,6 +862,7 @@ export const ptPT: LocalizationResource = {
       action__invitationAccept: 'Participar',
       action__suggestionsAccept: 'Solicitar participação',
       subtitle: 'Junte-se a uma organização existente ou crie uma nova',
+      subtitle__createOrganizationDisabled: undefined,
       suggestionsAcceptedLabel: 'Aprovação pendente',
       title: 'Escolha uma organização',
     },
