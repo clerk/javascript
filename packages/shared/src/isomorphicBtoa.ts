36f827b79a041cf6bf53981e34d7baffe35c529b
Jacek Radko <jacek@clerk.dev>
2026-01-22 13:26:23 -0600

    feat(repo): Add ESLint rule to ban direct `global.` usage in shared package (#7649)

--- packages/shared/src/isomorphicBtoa.ts
+++ packages/shared/src/isomorphicBtoa.ts
@@ -1,8 +1,8 @@
 export const isomorphicBtoa = (data: string) => {
   if (typeof btoa !== 'undefined' && typeof btoa === 'function') {
     return btoa(data);
-  } else if (typeof global !== 'undefined' && global.Buffer) {
-    return new global.Buffer(data).toString('base64');
+  } else if (typeof globalThis.Buffer !== 'undefined') {
+    return globalThis.Buffer.from(data).toString('base64');
   }
   return data;
 };
