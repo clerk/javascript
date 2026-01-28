05e551e5d0ad7629953c0793c40adcde31194311
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2026-01-28 12:57:43 -0300

    Improve JSDocs example

--- packages/shared/src/react/hooks/useOrganizationCreationDefaults.rq.tsx
+++ packages/shared/src/react/hooks/useOrganizationCreationDefaults.rq.tsx
@@ -17,18 +17,17 @@ import type {
  * ```tsx
  * import { useOrganizationCreationDefaults } from '@clerk/clerk-react'
  *
- * export default function OrganizationCreationForm() {
+ * export default function CreateOrganizationForm() {
  *   const { data, isLoading } = useOrganizationCreationDefaults()
  *
- *   if (isLoading) {
- *     return <div>Loading...</div>
- *   }
+ *   if (isLoading) return <div>Loading...</div>
  *
  *   return (
- *     <div>
- *       <p>Default organization name: {data?.name}</p>
- *       <p>Default organization slug: {data?.slug}</p>
- *     </div>
+ *     <form>
+ *       <input defaultValue={data?.form.name} placeholder="Organization name" />
+ *       <input defaultValue={data?.form.slug} placeholder="Slug" />
+ *       <button type="submit">Create</button>
+ *     </form>
  *   )
  * }
  * ```
