5eab51d34f06127c2b17836a2e1ba8a061eb6906
wobsoriano <sorianorobertc@gmail.com>
2026-01-30 15:06:08 -0800

    chore: update e2e test

--- integration/templates/tanstack-react-start/src/routeTree.gen.ts
+++ integration/templates/tanstack-react-start/src/routeTree.gen.ts
@@ -9,7 +9,7 @@
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
 import { Route as rootRouteImport } from './routes/__root';
-import { Route as SignInRouteImport } from './routes/sign-in';
+import { Route as SignInRouteImport } from './routes/sign-in.$';
 import { Route as IndexRouteImport } from './routes/index';
 
 const SignInRoute = SignInRouteImport.update({
