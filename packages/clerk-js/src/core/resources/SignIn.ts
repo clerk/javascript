4998b6a11bf2b8798e89bc73b5e0f5ceb119a095
Daniel Moerner <daniel@clerk.dev>
2026-02-03 16:12:22 -0500

    chore: Remove unused import

--- packages/clerk-js/src/core/resources/SignIn.ts
+++ packages/clerk-js/src/core/resources/SignIn.ts
@@ -16,7 +16,6 @@ import type {
   AuthenticateWithPopupParams,
   AuthenticateWithRedirectParams,
   AuthenticateWithWeb3Params,
-  CaptchaWidgetType,
   ClientTrustState,
   CreateEmailLinkFlowReturn,
   EmailCodeConfig,
