a186868b782779bbb64336dd43ea2593f6aad34a
Alex Carpenter <alex.carpenter@clerk.dev>
2026-02-03 16:02:42 -0500

    barrel exports bad

--- packages/ui/src/mosaic/index.ts
+++
@@ -1,4 +0,0 @@
-export { variants } from './variants';
-export { buttonStyles } from './button';
-export { MosaicThemeProvider, useMosaicTheme } from './theme-provider';
-export { mosaicTheme, type MosaicTheme } from './theme';
