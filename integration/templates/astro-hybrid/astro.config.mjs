00f15ca2d4cd31cd98ee857f64300fd23dcfbc9e
Jacek <jacek@clerk.dev>
2026-02-06 16:07:35 -0600

    fix(e2e): add node adapter to astro-hybrid template for build mode

--- integration/templates/astro-hybrid/astro.config.mjs
+++ integration/templates/astro-hybrid/astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 import clerk from '@clerk/astro';
 import react from '@astrojs/react';
 
 export default defineConfig({
   output: 'hybrid',
+  adapter: node({
+    mode: 'standalone',
+  }),
   integrations: [
     clerk({
       appearance: {
