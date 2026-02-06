4a58c10b94de5bee04d8b7ee24e14910ee8d675d
Sarah Soutoul <sarah@clerk.dev>
2026-02-06 08:21:49 -0600

    docs(repo): Add types for WaitlistEntry (#7784)

--- packages/backend/src/api/resources/WaitlistEntry.ts
+++ packages/backend/src/api/resources/WaitlistEntry.ts
@@ -2,14 +2,38 @@ import type { WaitlistEntryStatus } from './Enums';
 import { Invitation } from './Invitation';
 import type { WaitlistEntryJSON } from './JSON';
 
+/**
+ * The Backend `WaitlistEntry` object holds information about a waitlist entry for a given email address.
+ */
 export class WaitlistEntry {
   constructor(
+    /**
+     * The unique identifier for this waitlist entry.
+     */
     readonly id: string,
+    /**
+     * The email address to add to the waitlist.
+     */
     readonly emailAddress: string,
+    /**
+     * The status of the waitlist entry.
+     */
     readonly status: WaitlistEntryStatus,
+    /**
+     * The invitation associated with this waitlist entry.
+     */
     readonly invitation: Invitation | null,
+    /**
+     * The date when the waitlist entry was first created.
+     */
     readonly createdAt: number,
+    /**
+     * The date when the waitlist entry was last updated.
+     */
     readonly updatedAt: number,
+    /**
+     * Whether the waitlist entry is locked or not.
+     */
     readonly isLocked?: boolean,
   ) {}
 
