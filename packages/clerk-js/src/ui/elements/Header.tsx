95b710f5f1e3fd133e325832894fe2a472e46568
Vaggeilis Yfantis <vaggelisyfantis@gmail.com>
2026-02-16 16:51:51 +0200

    refactor(Header): Remove duplicate import of LocalizationKey

--- packages/clerk-js/src/ui/elements/Header.tsx
+++ packages/clerk-js/src/ui/elements/Header.tsx
@@ -8,9 +8,9 @@ import {
   Heading,
   Icon,
   Link,
+  type LocalizationKey,
   Text,
   useAppearance,
-  type LocalizationKey,
 } from '../customizables';
 import { ArrowLeftIcon } from '../icons';
 import type { PropsOfComponent, ThemableCssProp } from '../styledSystem';
