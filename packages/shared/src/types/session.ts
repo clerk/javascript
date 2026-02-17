f994e9da5d231ad0a6f83fb293a471647e20f975
Fredrik Höglund <fredrik@clerk.dev>
2026-02-17 10:23:11 +0100

    fix(clerk-js): Fix broken transitive state for org switch and sign out (#7823)

--- packages/shared/src/types/session.ts
+++ packages/shared/src/types/session.ts
@@ -1,3 +1,4 @@
+import type { ClientResource } from './client';
 import type {
   BackupCodeAttempt,
   EmailCodeAttempt,
@@ -260,6 +261,7 @@ export interface SessionResource extends ClerkResource {
   ) => Promise<SessionVerificationResource>;
   verifyWithPasskey: () => Promise<SessionVerificationResource>;
   __internal_toSnapshot: () => SessionJSONSnapshot;
+  __internal_touch: () => Promise<ClientResource | undefined>;
 }
 
 /**
