d1059c16f504ffa074cd08690c753aa87a49e228
Dylan Staley <88163+dstaley@users.noreply.github.com>
2026-02-18 14:28:36 -0600

    feat(clerk-js,localizations,shared,ui): Render credits in payment attempts and statements

--- packages/shared/src/types/billing.ts
+++ packages/shared/src/types/billing.ts
@@ -593,6 +593,7 @@ export interface BillingSubscriptionItemResource extends ClerkResource {
      */
     amount: BillingMoneyAmount;
   };
+  credits?: BillingCredits;
   /**
    * A function to cancel the subscription item. Accepts the following parameters:
    * <ul>
