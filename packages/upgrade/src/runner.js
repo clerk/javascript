ce11b960064b4263e4ee6a5e5864d0450f87c7f0
Jacek <jacek@clerk.dev>
2026-02-20 11:04:08 -0600

    fix(upgrade): exclude .md and .tsbuildinfo from scan ignores

--- packages/upgrade/src/runner.js
+++ packages/upgrade/src/runner.js
@@ -24,6 +24,8 @@ const GLOBBY_IGNORE = [
   '**/yarn.lock',
   'pnpm-lock.yaml',
   '**/pnpm-lock.yaml',
+  '**/*.md',
+  '**/*.tsbuildinfo',
   '**/*.{png,webp,svg,gif,jpg,jpeg}',
   '**/*.{mp4,mkv,wmv,m4v,mov,avi,flv,webm,flac,mka,m4a,aac,ogg}',
 ];
