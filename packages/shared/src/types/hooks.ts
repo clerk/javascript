33706c1104ed7741ee2e9789e526bb89cc19d3e9
Sarah Soutoul <sarah@clerk.dev>
2026-03-03 20:01:51 -0600

    docs(repo): Test new typedoc workfow (#7971)

--- packages/shared/src/types/hooks.ts
+++ packages/shared/src/types/hooks.ts
@@ -26,7 +26,7 @@ type CheckAuthorizationSignedOut = CheckAuthorizationWithoutOrgOrUser;
 export type UseAuthReturn =
   | {
       /**
-       * A boolean that indicates whether Clerk has completed initialization. Initially `false`, becomes `true` once Clerk loads.
+       * A boolean indicating whether Clerk has completed initialization. Initially `false`, becomes `true` once Clerk loads.
        */
       isLoaded: false;
       /**
