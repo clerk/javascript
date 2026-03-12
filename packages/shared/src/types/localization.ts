c0052f1a00d65fbdaac746f1b4f74acffb2c372e
Dylan Staley <88163+dstaley@users.noreply.github.com>
2026-03-12 12:43:49 -0500

    feat(clerk-js,localizations,msw,shared,ui): Add display for member limits (#7920)

--- packages/shared/src/types/localization.ts
+++ packages/shared/src/types/localization.ts
@@ -1022,6 +1022,7 @@ export type __internal_LocalizationResource = {
     badge__manualInvitation: LocalizationValue;
     start: {
       headerTitle__members: LocalizationValue;
+      membershipSeatUsageLabel: LocalizationValue<'count' | 'limit'>;
       headerTitle__general: LocalizationValue;
       profileSection: {
         title: LocalizationValue;
