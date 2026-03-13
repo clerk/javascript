42302db76549bae8a706a19ab6f5a31912cb0a2a
Dylan Staley <88163+dstaley@users.noreply.github.com>
2026-03-13 13:46:40 -0500

    fix(ui): assert non-null fee when planPeriod is monthly

--- packages/ui/src/components/SubscriptionDetails/index.tsx
+++ packages/ui/src/components/SubscriptionDetails/index.tsx
@@ -415,8 +415,10 @@ const SubscriptionCardActions = ({ subscription }: { subscription: BillingSubscr
                     currency: subscription.plan.annualFee!.currencySymbol,
                   })
                 : localizationKeys('billing.switchToMonthlyWithPrice', {
-                    price: normalizeFormatted(subscription.plan.fee.amountFormatted),
-                    currency: subscription.plan.fee.currencySymbol,
+                    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                    price: normalizeFormatted(subscription.plan.fee!.amountFormatted),
+                    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                    currency: subscription.plan.fee!.currencySymbol,
                   }),
             onClick: () => {
               openCheckout({
@@ -508,7 +510,8 @@ const SubscriptionCard = ({ subscription }: { subscription: BillingSubscriptionI
 
   const fee =
     subscription.planPeriod === 'month'
-      ? subscription.plan.fee
+      ? // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        subscription.plan.fee!
       : // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         subscription.plan.annualFee!;
 
