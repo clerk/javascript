2ee7fdb26d03bef8bd2756c91c6c4d1bf817914e
brkalow <bryce@clerk.dev>
2026-03-17 10:02:01 -0500

    improve type narrowing using assert()

--- packages/expo/src/utils/errors.ts
+++ packages/expo/src/utils/errors.ts
@@ -6,15 +6,21 @@ import { isNative } from './runtime';
 
 export const errorThrower = buildErrorThrower({ packageName: PACKAGE_NAME });
 
+function assert(condition: boolean, thrower: () => never): asserts condition {
+  if (!condition) {
+    thrower();
+  }
+}
+
 export const assertValidProxyUrl = (proxyUrl: DomainOrProxyUrl['proxyUrl']) => {
   if (!proxyUrl) {
     return;
   }
 
   if (isNative()) {
-    if (typeof proxyUrl !== 'string') {
-      return errorThrower.throw('`proxyUrl` must be a string in non-browser environments.');
-    }
+    assert(typeof proxyUrl === 'string', () =>
+      errorThrower.throw('`proxyUrl` must be a string in non-browser environments.'),
+    );
 
     if (!isHttpOrHttps(proxyUrl)) {
       errorThrower.throw('`proxyUrl` must be an absolute URL in non-browser environments.');
