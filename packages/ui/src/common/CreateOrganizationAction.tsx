812c4514d1b290e29ffb04d330d1141679663641
Alex Carpenter <alex.carpenter@clerk.dev>
2026-04-17 10:58:12 -0400

    Update CreateOrganizationAction.tsx

--- packages/ui/src/common/CreateOrganizationAction.tsx
+++ packages/ui/src/common/CreateOrganizationAction.tsx
@@ -1,14 +1,14 @@
 import { useUser } from '@clerk/shared/react/index';
 
 import { Action } from '../elements/Actions';
-import { Plus } from '../icons';
+import { Add } from '../icons';
 
 type CreateOrganizationActionProps = Omit<React.ComponentProps<typeof Action>, 'icon'> & {
   icon?: React.ComponentProps<typeof Action>['icon'];
 };
 
 export const CreateOrganizationAction = (props: CreateOrganizationActionProps) => {
-  const { icon = Plus, ...rest } = props;
+  const { icon = Add, ...rest } = props;
   const { user } = useUser();
 
   if (!user?.createOrganizationEnabled) {
