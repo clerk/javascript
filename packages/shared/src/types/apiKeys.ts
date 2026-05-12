d7c48214ebe7a566ae11f3ba3cb4ff02ec967175
Alexis Aguilar <98043211+alexisintech@users.noreply.github.com>
2026-05-12 14:27:41 -0700

    apikeys: remove duplication for create method

--- packages/shared/src/types/apiKeys.ts
+++ packages/shared/src/types/apiKeys.ts
@@ -82,7 +82,7 @@ export interface APIKeysNamespace {
    */
   getAll(params?: GetAPIKeysParams): Promise<ClerkPaginatedResponse<APIKeyResource>>;
   /**
-   * Creates a new API key. **The secret is only available in the response from `create()` and cannot be retrieved later.**
+   * Creates a new API key.
    * @returns An [`APIKeyResource`](https://clerk.com/docs/reference/types/api-key-resource) object that includes the `secret` property.
    * > [!WARNING]
    * > Make sure to store the API key secret immediately after creation, as it will not be available again.
