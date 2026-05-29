76150c11d5af511d6054da7553104eadce37a38e
Mike Pitre <12040919+mikepitre@users.noreply.github.com>
2026-05-29 11:07:51 -0400

    fix(expo): allow codegen-only user button props

--- packages/expo/src/specs/NativeClerkUserButtonView.ts
+++ packages/expo/src/specs/NativeClerkUserButtonView.ts
@@ -4,6 +4,8 @@ import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNati
 import type { HostComponent, ViewProps } from 'react-native';
 /* eslint-enable import/namespace, import/default, import/no-named-as-default, import/no-named-as-default-member, simple-import-sort/imports */
 
+// Codegen requires an interface declaration here; a type alias fails Android codegen.
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface NativeProps extends ViewProps {}
 
 export default codegenNativeComponent<NativeProps>('ClerkUserButtonView') as HostComponent<NativeProps>;
