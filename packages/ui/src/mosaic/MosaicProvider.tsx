ef34b70aa1136d52ac2022497cde84e47936948d
Alex Carpenter <alex.carpenter@clerk.dev>
2026-06-05 13:17:29 -0400

    Update MosaicProvider.tsx

--- packages/ui/src/mosaic/MosaicProvider.tsx
+++ packages/ui/src/mosaic/MosaicProvider.tsx
@@ -4,25 +4,28 @@ import createCache from '@emotion/cache';
 import { CacheProvider, type SerializedStyles } from '@emotion/react';
 import React from 'react';
 
-import { useOptions } from '../contexts/OptionsContext';
 import { defaultMosaicVariables, resolveVariables } from './variables';
 import type { MosaicTheme, MosaicVariables } from './variables';
+
+const el = document.querySelector('style#cl-mosaic-style-insertion-point');
 
 const MosaicThemeContext = React.createContext<MosaicTheme | null>(null);
 
 export interface MosaicProviderProps {
   children: React.ReactNode;
   variables?: MosaicVariables;
+  nonce?: string;
+  cssLayerName?: string;
 }
 
-export function MosaicProvider({ children, variables }: MosaicProviderProps) {
-  const { nonce, appearance } = useOptions();
-  const cssLayerName = appearance?.cssLayerName;
+export function MosaicProvider({ children, variables, nonce, cssLayerName }: MosaicProviderProps) {
   const theme = React.useMemo(() => resolveVariables(defaultMosaicVariables, variables), [variables]);
   const cache = React.useMemo(() => {
     const emotionCache = createCache({
       key: 'cl-mosaic',
       stylisPlugins: [],
+      prepend: cssLayerName ? false : !el,
+      insertionPoint: el ? (el as HTMLElement) : undefined,
       nonce,
     });
 
