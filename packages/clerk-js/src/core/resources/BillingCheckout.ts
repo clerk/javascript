45690a854386d65dc79f3373441dcd8122798cad
Dylan Staley <88163+dstaley@users.noreply.github.com>
2026-06-08 10:48:26 -0500

    fix(clerk-js,shared): Update type for next_payment (#8739)

--- packages/clerk-js/src/core/resources/BillingCheckout.ts
+++ packages/clerk-js/src/core/resources/BillingCheckout.ts
@@ -4,7 +4,7 @@ import { retry } from '@clerk/shared/retry';
 import type {
   BillingCheckoutJSON,
   BillingCheckoutResource,
-  BillingCheckoutTotals,
+  BillingTotals,
   BillingPayerResource,
   BillingPaymentMethodResource,
   BillingSubscriptionPlanPeriod,
@@ -34,7 +34,7 @@ export class BillingCheckout extends BaseResource implements BillingCheckoutReso
   planPeriod!: BillingSubscriptionPlanPeriod;
   planPeriodStart!: number | undefined;
   status!: 'needs_confirmation' | 'completed';
-  totals!: BillingCheckoutTotals;
+  totals!: BillingTotals;
   isImmediatePlanChange!: boolean;
   freeTrialEndsAt?: Date;
   payer!: BillingPayerResource;
