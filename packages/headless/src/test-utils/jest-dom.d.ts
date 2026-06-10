d4e892cc39850c02c66133629b72869b95f33b3a
Alex Carpenter <alex.carpenter@clerk.dev>
2026-06-10 09:26:57 -0400

    fix(headless): fix lint errors in test-utils type declaration files

--- packages/headless/src/test-utils/jest-dom.d.ts
+++ packages/headless/src/test-utils/jest-dom.d.ts
@@ -1,2 +1,2 @@
-/// <reference path="../../node_modules/@testing-library/jest-dom/types/vitest.d.ts" />
-/// <reference path="./vitest-axe.d.ts" />
+import '@testing-library/jest-dom/vitest';
+import './vitest-axe';
--- packages/headless/src/test-utils/vitest-axe.d.ts
+++ packages/headless/src/test-utils/vitest-axe.d.ts
@@ -1,7 +1,10 @@
-import type { AxeMatchers } from 'vitest-axe/matchers';
 import 'vitest';
 
+import type { AxeMatchers } from 'vitest-axe/matchers';
+
 declare module 'vitest' {
-  interface Assertion<T = unknown> extends AxeMatchers {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  interface Assertion<_T = unknown> extends AxeMatchers {}
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   interface AsymmetricMatchersContaining extends AxeMatchers {}
 }
