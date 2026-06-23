c574b126189b60cfef5d0e620cc800ce17250ba1
Steve Hayes <13389614+stvhayes@users.noreply.github.com>
2026-06-23 13:02:26 -0400

    Update global.d.ts

--- packages/swingset/src/types/global.d.ts
+++ packages/swingset/src/types/global.d.ts
@@ -2,6 +2,16 @@
 
 declare module '*.css';
 
+declare module '*.jpg' {
+  const image: { src: string; height: number; width: number; blurDataURL?: string };
+  export default image;
+}
+
+declare module '*.png' {
+  const image: { src: string; height: number; width: number; blurDataURL?: string };
+  export default image;
+}
+
 declare module '*.mdx' {
   import type React from 'react';
   const MDXComponent: React.ComponentType;
