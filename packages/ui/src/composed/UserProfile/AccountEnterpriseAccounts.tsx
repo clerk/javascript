48041fb45613d283d6407b5130e5aa8f0d15bd44
Alex Carpenter <alex.carpenter@clerk.dev>
2026-07-16 08:03:48 -0400

    feat(ui): add composed UserProfile panels and section components (#9134)

---
+++ packages/ui/src/composed/UserProfile/AccountEnterpriseAccounts.tsx
@@ -0,0 +1,6 @@
+'use client';
+
+import { AccountEnterpriseAccounts as Section } from '../../components/UserProfile/AccountSections';
+import { createSection } from '../createSection';
+
+export const UserProfileEnterpriseAccountsSection = createSection('UserProfileEnterpriseAccountsSection', Section);
