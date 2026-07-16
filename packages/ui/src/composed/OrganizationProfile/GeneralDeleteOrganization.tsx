f3753316b6ab9b0b237164e6916e00e34852403b
Alex Carpenter <alex.carpenter@clerk.dev>
2026-07-16 08:04:25 -0400

    feat(ui): add composed OrganizationProfile panels and section components (#9135)

---
+++ packages/ui/src/composed/OrganizationProfile/GeneralDeleteOrganization.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { OrganizationDeleteSection } from '../../components/OrganizationProfile/OrganizationGeneralPage';
+import { createSection } from '../createSection';
+
+export const OrganizationProfileDeleteSection = createSection(
+  'OrganizationProfileDeleteSection',
+  OrganizationDeleteSection,
+);
---
+++ packages/ui/src/composed/OrganizationProfile/GeneralOrganizationProfile.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { OrganizationProfileSection } from '../../components/OrganizationProfile/OrganizationGeneralPage';
+import { createSection } from '../createSection';
+
+export const OrganizationProfileProfileSection = createSection(
+  'OrganizationProfileProfileSection',
+  OrganizationProfileSection,
+);
