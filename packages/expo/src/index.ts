836d42aec2c35a216ab36975d52ef492e68fbc80
wobsoriano <sorianorobertc@gmail.com>
2026-07-24 18:04:40 -0700

    chore(expo): Add deprecation notice to clerk-expo

--- packages/expo/src/index.ts
+++ packages/expo/src/index.ts
@@ -1,4 +1,13 @@
 import { setErrorThrowerOptions } from '@clerk/clerk-react/internal';
+import { logger } from '@clerk/shared/logger';
+
+logger.warnOnce(`
+Clerk - DEPRECATION WARNING: @clerk/clerk-expo is deprecated.
+
+Please migrate to @clerk/expo.
+
+Migration guide: https://clerk.com/docs/guides/development/upgrading/upgrade-guides/core-3
+`);
 
 export {
   isClerkAPIResponseError,
