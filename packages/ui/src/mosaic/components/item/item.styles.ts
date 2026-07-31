9be113480468cdb270c1a4c481e215ace3cd20b7
Alex Carpenter <alex.carpenter@clerk.dev>
2026-07-31 11:13:33 -0400

    default item color to neutral faded

--- packages/ui/src/mosaic/components/item/item.styles.ts
+++ packages/ui/src/mosaic/components/item/item.styles.ts
@@ -13,6 +13,7 @@ export const item = stylex.create({
     paddingInline: space['2'],
     alignItems: 'center',
     color: colorVars['--cl-color-card-foreground'],
+    color: colorVars['--cl-color-neutral-faded'],
     display: 'flex',
     fontFamily: fontFamilyVars['--cl-font-family-sans'],
     fontSize: typeScaleVars['--cl-text-sm-size'],
