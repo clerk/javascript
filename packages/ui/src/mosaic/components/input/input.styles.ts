4c2cf4b1d198d735effbc309fdda16ed486b429c
austincalvelage <austin.calvelage@icloud.com>
2026-08-03 14:59:02 -0600

    fix(ui): prevent iOS zoom on Mosaic inputs

--- packages/ui/src/mosaic/components/input/input.styles.ts
+++ packages/ui/src/mosaic/components/input/input.styles.ts
@@ -71,21 +71,30 @@ export const sizes = stylex.create({
   sm: {
     borderRadius: radiusVars['--cl-radius-control'],
     paddingInline: space['3'],
-    fontSize: typeScaleVars['--cl-text-xs-size'],
+    fontSize: {
+      default: typeScaleVars['--cl-text-xs-size'],
+      '@media (pointer: coarse)': `max(1rem, ${typeScaleVars['--cl-text-xs-size']})`,
+    },
     lineHeight: typeScaleVars['--cl-text-xs-leading'],
     height: space['7'],
   },
   md: {
     borderRadius: radiusVars['--cl-radius-control'],
     paddingInline: space['3'],
-    fontSize: typeScaleVars['--cl-text-sm-size'],
+    fontSize: {
+      default: typeScaleVars['--cl-text-sm-size'],
+      '@media (pointer: coarse)': `max(1rem, ${typeScaleVars['--cl-text-sm-size']})`,
+    },
     lineHeight: typeScaleVars['--cl-text-sm-leading'],
     height: space['8'],
   },
   lg: {
     borderRadius: radiusVars['--cl-radius-element'],
     paddingInline: space['3'],
-    fontSize: typeScaleVars['--cl-text-base-size'],
+    fontSize: {
+      default: typeScaleVars['--cl-text-base-size'],
+      '@media (pointer: coarse)': `max(1rem, ${typeScaleVars['--cl-text-base-size']})`,
+    },
     lineHeight: 1.375,
     height: space['9'],
   },
