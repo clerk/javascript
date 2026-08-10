76faade617166a242774b856e2c1af907b0e27d1
austincalvelage <austin.calvelage@icloud.com>
2026-08-10 11:23:09 -0600

    feat(ui): add user profile account section

---
+++ packages/swingset/src/stories/user-profile-account-section.stories.tsx
@@ -0,0 +1,33 @@
+import { UserProfileAccountSectionView } from '@clerk/ui/mosaic/user-profile/user-profile-account-section.view';
+
+import type { StoryMeta } from '@/lib/types';
+
+export { default as __source } from './user-profile-account-section.stories?raw';
+
+export const meta: StoryMeta = {
+  group: 'User',
+  title: 'UserProfileAccountSection',
+  source: 'packages/ui/src/mosaic/user-profile/user-profile-account-section.view.tsx',
+};
+
+export function Default() {
+  return (
+    <UserProfileAccountSectionView
+      emails={[
+        { id: 'email_1', value: 'item1@clerk.dev', isDefault: true, isVerified: true },
+        { id: 'email_2', value: 'item2@clerk.dev', isVerified: true },
+      ]}
+      imageUrl='https://avatars.githubusercontent.com/u/51144033?v=4'
+      name='Preston Booth'
+      phones={[{ id: 'phone_1', value: '+1 801-888-8181', isDefault: true, isVerified: true }]}
+      username='prestonxyz'
+      onAddEmail={() => undefined}
+      onAddPhone={() => undefined}
+      onEditProfilePicture={() => undefined}
+      onManageEmail={() => undefined}
+      onManagePhone={() => undefined}
+      onNameChange={() => undefined}
+      onUsernameChange={() => undefined}
+    />
+  );
+}
