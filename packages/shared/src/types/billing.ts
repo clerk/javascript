8427edae3b134a9cce5328052aa60f3bb7a49e61
Sarah Soutoul <sarah@clerk.dev>
2026-08-11 17:17:41 +0100

    Fix updateCheckoutParams

--- packages/shared/src/types/billing.ts
+++ packages/shared/src/types/billing.ts
@@ -1418,8 +1418,6 @@ export type CreateCheckoutParams = WithOptionalOrgType<{
 /**
  * The `updateCheckout()` method accepts the following parameters.
  *
- * @inline
- *
  * @experimental This is an experimental API for the Billing feature that is available under a public beta, and the API is subject to change. It is advised to [pin](https://clerk.com/docs/pinning) the SDK version and the clerk-js version to avoid breaking changes.
  */
 export type UpdateCheckoutParams = WithOptionalOrgType<{
