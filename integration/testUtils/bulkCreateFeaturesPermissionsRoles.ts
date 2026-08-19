92fd13b69d7f06c8e9d268bbbdfef9fe4abbebab
Dylan Staley <88163+dstaley@users.noreply.github.com>
2026-08-19 14:00:31 -0500

    feat: add with-custom-roles to dynamic config

---
+++ integration/testUtils/bulkCreateFeaturesPermissionsRoles.ts
@@ -0,0 +1,50 @@
+import type { ClerkClient } from '@clerk/backend';
+
+interface Permission {
+  name: string;
+  description: string;
+}
+
+interface Feature {
+  name: string;
+  permissions: Record<string, Permission>;
+}
+
+interface Role {
+  name: string;
+  description: string;
+  permissions: Record<string, boolean>;
+}
+
+interface FeaturesPermissionsRoles {
+  features: Record<string, Feature>;
+  roles: Record<string, Role>;
+}
+
+export async function bulkCreateFeaturesPermissionsRoles(
+  client: ClerkClient,
+  config: FeaturesPermissionsRoles,
+): Promise<void> {
+  // since BAPI doesn't currently support directly creating features, we rely on the implicit creation of features when
+  // creating permissions
+  for (const featureKey in config.features) {
+    for (const permissionKey in config.features[featureKey].permissions) {
+      const permission = config.features[featureKey].permissions[permissionKey];
+      await client.organizationPermissions.createOrganizationPermission({
+        key: `org:${featureKey}:${permissionKey}`,
+        ...permission,
+      });
+    }
+  }
+
+  for (const roleKey in config.roles) {
+    const role = config.roles[roleKey];
+    await client.organizationRoles.createOrganizationRole({
+      key: roleKey,
+      ...role,
+      permissions: Object.entries(role.permissions)
+        .filter(([_, enabled]) => enabled)
+        .map(([permission]) => permission),
+    });
+  }
+}
