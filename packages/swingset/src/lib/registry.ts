5ed2d8a264ab457c80f2add038068cdd2be97509
austincalvelage <austin.calvelage@icloud.com>
2026-08-20 13:20:54 -0600

    feat(ui): add nested password verification example

--- packages/swingset/src/lib/registry.ts
+++ packages/swingset/src/lib/registry.ts
@@ -153,6 +153,7 @@ import {
   Change as UserProfilePasswordDialogChange,
   meta as userProfilePasswordDialogMeta,
   Set as UserProfilePasswordDialogSet,
+  Verification as UserProfilePasswordDialogVerification,
 } from '../stories/user-profile-password-dialog.stories';
 import {
   Default as UserProfilePasswordSectionDefault,
@@ -374,6 +375,7 @@ const userProfilePasswordDialogModule: StoryModule = {
   meta: userProfilePasswordDialogMeta,
   Change: UserProfilePasswordDialogChange,
   Set: UserProfilePasswordDialogSet,
+  Verification: UserProfilePasswordDialogVerification,
 };
 
 export const registry: StoryModule[] = [
