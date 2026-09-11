64ccc207614d5f3f72a5e477d4ebe272ec3cf3bc
Austin Calvelage <austin.calvelage@icloud.com>
2026-09-11 10:10:28 -0600

    fix(headless): measure flow steps without parent transforms (#9726)
    Co-authored-by: Alex Carpenter <alex.carpenter@clerk.dev>

--- packages/headless/src/primitives/flow/flow-root.tsx
+++ packages/headless/src/primitives/flow/flow-root.tsx
@@ -31,7 +31,7 @@ export const FlowRoot = React.forwardRef<HTMLDivElement, FlowRootProps>(function
     }
 
     const measure = () => {
-      setActiveStepHeight(activeStep.getBoundingClientRect().height);
+      setActiveStepHeight(activeStep.offsetHeight);
     };
 
     measure();
