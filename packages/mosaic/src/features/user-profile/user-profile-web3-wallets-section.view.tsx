51e8590913ded5a9b6681dff4cff6c1bf65828d8
Fredrik Höglund <fredrik@clerk.dev>
2026-09-17 15:44:43 +0200

    Duplicate truncateTextWithEndVisible into Mosaic

--- packages/mosaic/src/features/user-profile/user-profile-web3-wallets-section.view.tsx
+++ packages/mosaic/src/features/user-profile/user-profile-web3-wallets-section.view.tsx
@@ -1,9 +1,9 @@
 import { useMemo } from 'react';
 
-import { truncateWithEndVisible } from '../../../utils/truncateTextWithEndVisible';
 import { Confirmation } from '../../blocks/confirmation';
 import { Section } from '../../components/section';
 import { fill } from '../../utils/messages';
+import { truncateWithEndVisible } from '../../utils/truncateTextWithEndVisible';
 import { UserProfileWeb3WalletRowView } from './user-profile-web3-wallet-row.view';
 import { userProfileWeb3WalletsMessages as m } from './user-profile-web3-wallets.messages';
 
@@ -59,12 +59,12 @@ export function UserProfileWeb3WalletsSectionView({
             ))}
             {onConnect
               ? availableProviders.map(provider => (
-                  <UserProfileWeb3WalletRowView
-                    key={provider.id}
-                    wallet={provider}
-                    onConnect={onConnect}
-                  />
-                ))
+                <UserProfileWeb3WalletRowView
+                  key={provider.id}
+                  wallet={provider}
+                  onConnect={onConnect}
+                />
+              ))
               : null}
           </Section.Group>
         </Section.Root>
