81a641348101f720acef64915b2a701bc858387d
Alex Carpenter <alex.carpenter@clerk.dev>
2026-09-21 19:31:23 -0400

    feat(mosaic): support anchored toasts in the Toast primitive (#9850)

--- packages/mosaic/src/primitives/toast/parts.ts
+++ packages/mosaic/src/primitives/toast/parts.ts
@@ -1,7 +1,9 @@
 export { type ToastProviderProps, ToastProvider as Provider } from './toast-provider';
 export { type ToastPortalProps, ToastPortal as Portal } from './toast-portal';
 export { type ToastViewportProps, ToastViewport as Viewport } from './toast-viewport';
+export { type ToastPositionerProps, ToastPositioner as Positioner } from './toast-positioner';
 export { type ToastRootProps, ToastRoot as Root } from './toast-root';
+export { type ToastArrowProps, ToastArrow as Arrow } from './toast-arrow';
 export { type ToastContentProps, ToastContent as Content } from './toast-content';
 export { type ToastTitleProps, ToastTitle as Title } from './toast-title';
 export { type ToastDescriptionProps, ToastDescription as Description } from './toast-description';
@@ -12,10 +14,13 @@ export {
   createToastManager,
   type ExternalToastManager,
   type ToastAddOptions,
+  type ToastAlign,
   type ToastManager,
   type ToastObject,
+  type ToastPositionerOptions,
   type ToastPriority,
   type ToastPromiseOptions,
+  type ToastSide,
   type ToastUpdateOptions,
   type ToastUpdater,
 } from './toast-manager';
