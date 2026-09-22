b5f1a08bde3c120273903baa9802602add0a6545
Alex Carpenter <alex.carpenter@clerk.dev>
2026-09-22 13:25:54 -0400

    feat(mosaic): add TagInput primitive (#9838)

--- packages/swingset/src/components/DocsViewer.tsx
+++ packages/swingset/src/components/DocsViewer.tsx
@@ -95,6 +95,7 @@ const docModules: Record<string, Record<string, React.ComponentType>> = {
     popover: dynamic(() => import('../stories/popover.mdx')),
     select: dynamic(() => import('../stories/select.mdx')),
     tabs: dynamic(() => import('../stories/tabs.mdx')),
+    'tag-input': dynamic(() => import('../stories/tag-input.mdx')),
     toast: dynamic(() => import('../stories/toast.primitive.mdx')),
     tooltip: dynamic(() => import('../stories/tooltip.mdx')),
   },
