74c33487b6f07ced8dab34a9cf72a82580db03a6
Fredrik Höglund <fredrik@clerk.dev>
2026-09-24 14:07:57 +0200

    Wire up delete account

--- packages/mosaic/src/features/user-profile/user-profile-profile-panel.view.tsx
+++ packages/mosaic/src/features/user-profile/user-profile-profile-panel.view.tsx
@@ -132,7 +132,7 @@ export function UserProfileProfilePanelView({
           onSetPrimary={onSetPrimaryWeb3Wallet}
         />
         {/* TODO: This is temporary, a view should not render a connected component */}
-        {onDeleteAccount ? <UserProfileDeleteSection onDelete={onDeleteAccount} /> : null}
+        {onDeleteAccount ? <UserProfileDeleteSection /> : null}
       </div>
     </div>
   );
